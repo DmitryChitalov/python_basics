4d76b24febc41864c9b2fe500daba80166d7577d
Dmitry Chitalov <35995629+DmitryChitalov@users.noreply.github.com>
2022-03-23 17:16:42 +0500

    all file

---
+++ Урок 1. Практическое задание/main.py
@@ -0,0 +1,50 @@
+name1 = "Tom"
+print(name1)
+a = 10
+b = 20
+print(a, b)
+string = input("hello")
+print(input)
+
+time = int(input("Введите время в секундах "))
+hours = time // 3600
+minutes = (time - hours * 3600) // 60
+seconds = time - (hours * 3600 + minutes * 60)
+print(f"Время в формате чч:мм:сс   {hours} : {minutes} : {seconds}")
+
+n = int(input("Введите число - "))
+total = (n + int(str(n) + str(n)) + int(str(n) + str(n)+ str(n)))
+print("Сумма чисел n + nn + nnn - %d" % total)
+
+n = abs(int(input("Введите целое положительное число ")))
+max = n % 10
+while n >= 1:
+    n = n // 10
+    if n % 10 > max:
+        max = n % 10
+    if n > 9:
+        continue
+    else:
+        print("Максимальное цифра в числе ", max)
+        break
+
+profit = float(input("Введите выручку фирмы "))
+costs = float(input("Введите издержки фирмы "))
+if profit > costs:
+    print(f"Фирма работает с прибылью. Рентабельность выручки составила {profit / costs:.2f}")
+    workers = int(input("Введите количество сотрудников фирмы "))
+    print(f"прибыль в расчете на одного сторудника сотавила {profit / workers:.2f}")
+elif profit == costs:
+    print("Фирма работает в ноль")
+else:
+    print("Фирма работает в убыток")
+
+a = int(input("Введите результаты пробежки первого дня в км "))
+b = int(input("Введите общий желаемый результат в км "))
+result_days = 1
+result_km = a
+while result_km < b:
+        a = a + 0.1 * a
+        result_days += 1
+        result_km = result_km + a
+print(f"Вы достигнете требуемых показателей на %.d день" % result_days)
