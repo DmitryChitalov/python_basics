359a595928e71543b17cee92721cc0c090a89ceb
Anton Shkamanov <anton.shkamanov@yandex.ru>
2022-03-27 14:55:48 +0300

    Домашняя работа. Урок 1.

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,10 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+
+num = int(input("Теперь введите любое число, а я предоставлю сумму его ввиде n+nn+nnn:"))
+numstr = str(num)
+nn = numstr + numstr
+nnn = numstr + numstr + numstr
+summ = num + int(nn) + int(nnn)
+print(f"{num}+{nn}+{nnn}={summ}, как-то так. Давайте дальше?")
