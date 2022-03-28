71ae4a603bd233ab91567a33c71e2b8708e04244
Mikael1977 <mik_h@mail.ru>
2022-03-28 18:24:41 +0300

    Done lesson 1_3

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,16 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+inputed_value = int(input("Введите число n: "))
+
+two_times_value = str(inputed_value) + str(inputed_value)
+three_times_value = str(inputed_value) + str(inputed_value) + str(inputed_value)
+c_sum = int(inputed_value) + int(two_times_value) + int(three_times_value)
+
+"""
+print(f'inputed_value = {inputed_value},\r\n '
+      f'two_times_value = {two_times_value},\r\n '
+      f'three_times_value = {three_times_value},\r\n '
+      f'sum = {c_sum}')
+"""
+print(f'n + nn + nnn = {c_sum}')
