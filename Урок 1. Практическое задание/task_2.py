7a1fe3989c4699300cb4b2bbad9145ba89f0b366
Mikael1977 <mik_h@mail.ru>
2022-03-28 17:58:20 +0300

    Done lesson 1_2

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -9,3 +9,12 @@
 Введите время в секундах: 3600
 Время в формате ч:м:с - 1.0 : 60.0 : 3600
 """
+time_in_sec = int(input("Введите время в секундах: "))
+
+time_in_hours = time_in_sec // 3600
+time_in_minutes = (time_in_sec - time_in_hours * 3600) // 60
+time_in_minutes_2 = (time_in_sec) // 60
+time_in_seconds = time_in_sec % 60
+
+print('Время в общепринятом формате: %02d:%02d:%02d' % (time_in_hours, time_in_minutes, time_in_seconds))
+print('Время в формате ДЗ: %.1f : %.1f : %d' % (time_in_hours, time_in_minutes_2, time_in_sec))
