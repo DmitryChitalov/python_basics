4ed8132df94aaeacb18a156cce62bf71aa1322c2
Mikael1977 <mik_h@mail.ru>
2022-03-31 16:33:54 +0300

    Done lesson 2_5

--- Урок 2. Практическое задание/task_5.py
+++ Урок 2. Практическое задание/task_5.py
@@ -15,3 +15,23 @@
 Набор натуральных чисел можно задать непосредственно в коде,
 например, my_list = [7, 5, 3, 3, 2].
 """
+
+my_list = [7, 5, 3, 3, 2]
+
+print(f"Набор натуральных чисел: {', '.join(map(str,my_list))}")
+
+
+digit = int(input("Введите число (777 - выход): "))
+while digit != 777:
+    for el in range(len(my_list)):
+        if my_list[el] == digit:
+            my_list.insert(el + 1, digit)
+            break
+        elif my_list[0] < digit:
+            my_list.insert(0, digit)
+        elif my_list[-1] > digit:
+            my_list.append(digit)
+        elif my_list[el] > digit and my_list[el + 1] < digit:
+            my_list.insert(el + 1, digit)
+    print(f"Пользователь ввел число {str(digit)}. Результат: {', '.join(map(str,my_list))}")
+    digit = int(input("Введите число: "))
