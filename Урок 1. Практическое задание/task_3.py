3781f83d17c7a96d02ace3f39a23f061de808704
adit <aphural@gmail.com>
2022-03-31 16:07:57 +0500

    Задание 3 иссправленно , нехватало * в третьем выражении

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -10,18 +10,4 @@ n + nn + nnn = 369
 """
 
 n = int(input("Введите целое число : "))
-print(str(n)+''+str(n**2)+''+str(n*3))
-"""
-Пытался сделать проверку, оно работает, но я не понял как отрабатывает цикл, он 4 раза запрашивает одно и тоже число, при этом считает по первому
-def getnumber():
-    while True:
-        n = input("Введите целое число : ")
-        #  if n.isdigit(): return n
-        if n.isnumeric():  return n
-
-
-print(getnumber())
-nn = int(getnumber()) * int(getnumber())
-nnn = int(getnumber()) ** 3
-print(int(getnumber()), int(nn), int(nnn))
-"""
+print(str(n) + '' + str(n ** 2) + '' + str(n ** 3))
