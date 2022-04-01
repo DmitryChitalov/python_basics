9bf3327b7e05ae714dc25852bde85f9c3f54fbfc
sefimov <sefimov@navicons.com>
2022-04-01 12:53:34 +0300

    Выполнение домашнего задания

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -10,3 +10,17 @@
 Ведите целое положительное число: 123456789
 Самая большая цифра в числе: 9
 """
+
+input_value = input("Please, input a integer value n=")
+if str.isdigit(input_value):
+    int_value = int(input_value)
+    max_number = 0
+    division_int = int_value
+    while division_int != 0:
+        division_remainder = division_int % 10
+        division_int = division_int // 10
+        if max_number < division_remainder:
+            max_number = division_remainder
+    print(f"Max char is {max_number}")
+else:
+    print("Error: your value is not a integer number")
