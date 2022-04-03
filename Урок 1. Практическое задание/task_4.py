bd26816958a5d60077298f61be42f19b3b347613
Orkhan <57174188+oiskenderov@users.noreply.github.com>
2022-04-03 12:43:12 +0400

    Update task_4.py
    Updated

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -10,3 +10,11 @@
 Ведите целое положительное число: 123456789
 Самая большая цифра в числе: 9
 """
+numb = int(input('input your digital:'))
+max_dig = 0
+while max_dig != 0:
+    current_n = numb % 10
+    if max_dig < current_n:
+        max_dig = current_n
+numb = numb // 10
+print(f'Max huge digital:{max_dig}')
