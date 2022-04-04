cf138b93059fe0a5859afcc4766bb805bbf5412e
awefw7 <72689987+awefw7@users.noreply.github.com>
2022-04-04 02:23:05 -0700

    Там было 2 варианта. Первый вариант убрал.
    Думаю, второй подходит.

--- Home2-3.py
+++ Home2-3.py
@@ -7,25 +7,7 @@ Created on Fri Apr  1 02:53:40 2022
 
 # Task3
 
-seasons = ["winter", "spring", "summer", "autumn_fall"]
-winter = [12, 1, 2]
-spring = [3, 4, 5]
-summer = [6, 7, 8]
-autumn_fall = [9, 10, 11]
 month_number = int(input("Enter the number of a month:\n"))
-for i in winter:
-    if month_number == winter[0] or month_number == winter[1] or month_number == winter[2]:
-        print(f"You chose the {seasons[0]} month!")
-        break
-    if month_number == spring[0] or month_number == spring[1] or month_number == spring[2]:
-        print(f"You chose the {seasons[1]} month!")
-        break
-    if month_number == summer[0] or month_number == summer[1] or month_number == summer[2]:
-        print(f"You chose the {seasons[2]} month!")
-        break
-    if month_number == autumn_fall[0] or month_number == autumn_fall[1] or month_number == autumn_fall[2]:
-        print(f"You chose the {seasons[3]} month!")
-        break
 
 m_season = {
     "зима": (12, 1, 2),
