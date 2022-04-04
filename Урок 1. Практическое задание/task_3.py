592d2ff55ec624449ce9e333bdeac82f4db0a264
653268746 <aetelmor@gmail.com>
2022-04-04 12:57:28 +0300

    Исправление ДЗ

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -1,5 +1,3 @@
-a = int(input("Число a: "))
-b = int(input("Число b: "))
-c = int(input("Число c: "))
-n = a + b + c
-print(n)
+n = input("Введите число n: ")
+result = int(n) + int(n * 2) + int(n * 3)
+print(f'{n} + {n * 2} + {n * 3} = {result}')
