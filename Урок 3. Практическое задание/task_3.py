f3b2e9a5c989b244deba2414e28c249f13550ae4
Anton Shkamanov <anton.shkamanov@yandex.ru>
2022-04-05 19:56:58 +0300

    Домашняя работа. Урок 3

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -5,3 +5,14 @@
 1) используя функцию sort()
 2) без функции sort()
 """
+
+
+def my_func(arg_1, arg_2, arg_3):
+    print(f'Сумма двух наибольших аргументов равна: {arg_1 + arg_2 + arg_3 - min([arg_1, arg_2, arg_3])}')
+
+
+my_func(
+    int(input('Аргумент 1:')),
+    int(input('Аргумент 2:')),
+    int(input('Аргумент 3:')),
+)
