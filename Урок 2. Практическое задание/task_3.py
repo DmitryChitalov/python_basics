d44c0b0ccd530d4fc15f3b75fce4c340a6759776
ErastovAlexandr <erastov_sasha@mail.ru>
2022-04-05 21:04:15 +0800

    lesson2

--- Урок 2. Практическое задание/task_3.py
+++ Урок 2. Практическое задание/task_3.py
@@ -1,10 +1,17 @@
-"""
-3. Пользователь вводит месяц в виде целого числа от 1 до 12.
-Сообщить к какому времени года относится месяц (зима, весна, лето, осень).
-Напишите два варианта решения: через list и через dict.
-
-Пример:
-Введите номер месяца: 10
-Результат через список: Осень
-Результат через словарь: Осень
-"""
+mes = int(input("Введите номер месяца"))
+season = ["зима", "весна", "лето", "осень"]
+uslovie ={1: "зима", 2: "весна", 3: "лето", 4: "осень"}
+if mes == 12 or mes == 1 or mes == 2:
+    print(uslovie.get(1))
+    print(season[0])
+elif mes == 3 or mes == 4 or mes == 5:
+    print(uslovie.get(2))
+    print(season[1])
+elif mes == 6 or mes == 7 or mes == 8:
+    print(uslovie.get(3))
+    print(season[2])
+elif mes == 9 or mes == 10 or mes == 11:
+    print(uslovie.get(4))
+    print(season[3])
+else:
+    print("нет такого месяца")
