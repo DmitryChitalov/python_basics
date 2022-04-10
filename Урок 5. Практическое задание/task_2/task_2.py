edfa5c95bdeece47d8555c6618e571cb7c19688c
Mikael1977 <mik_h@mail.ru>
2022-04-10 20:34:46 +0300

    Done lesson 5_2

--- Урок 5. Практическое задание/task_2/task_2.py
+++ Урок 5. Практическое задание/task_2/task_2.py
@@ -2,3 +2,15 @@
 2)	Создать текстовый файл (не программно), сохранить в нем несколько строк,
 выполнить подсчет количества строк, количества слов в каждой строке.
 """
+row_count = 0
+word_count_dict = {}
+
+
+with open("my_file05_02.txt", "r") as my_file:
+    for row in my_file:
+        row_count += 1
+        words = row.split(" ")
+        word_count_dict[row_count] = len(words)
+
+for info in word_count_dict.items():
+    print(f"В строке {info[0]} содержится {info[1]} слов.")
