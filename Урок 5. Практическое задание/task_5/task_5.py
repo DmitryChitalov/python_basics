8bbdf736b06ef8f2670ab911c803d246e1d4cbdb
Mikael1977 <mik_h@mail.ru>
2022-04-10 20:46:00 +0300

    Done lesson 5_5

--- Урок 5. Практическое задание/task_5/task_5.py
+++ Урок 5. Практическое задание/task_5/task_5.py
@@ -2,3 +2,24 @@
 5)	Создать (программно) текстовый файл, записать в него программно набор чисел,
  разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 """
+
+from random import randint
+
+i = 0
+my_string = ""
+
+while i <= 100:
+    if i < 100:
+        my_string += f"{randint(0, 1000)} "
+    else:
+        my_string += f"{randint(0, 1000)}"
+
+    i += 1
+
+my_file = open("my_file05_05.txt", "w")
+my_file.write(my_string)
+my_file.close()
+
+with open("my_file05_05.txt", "r") as my_file_for_read:
+    new_list = [int(item) for item in my_file_for_read.read().split(' ')]
+    print(f"Сумма числе в файле равна: {sum(new_list)}")
