ba326fe9f168c4e44163907be7958bf509ccf0fc
Anton Shkamanov <anton.shkamanov@yandex.ru>
2022-04-11 19:39:26 +0300

    Домашняя работа. Урок 5

--- Урок 5. Практическое задание/task_2/task_2.py
+++ Урок 5. Практическое задание/task_2/task_2.py
@@ -2,3 +2,17 @@
 2)	Создать текстовый файл (не программно), сохранить в нем несколько строк,
 выполнить подсчет количества строк, количества слов в каждой строке.
 """
+
+
+def count_info():
+    try:
+        with open('text2.txt', 'r', encoding="utf-8") as file:
+            my_list = file.readlines()
+            for i in range(len(my_list)):
+                new_l = my_list[i].split()
+                print(f'Количество строк в файле {len(my_list)}. В {i + 1}-ой строке слов {len(new_l)}')
+    except FileNotFoundError:
+        return 'Файл не найден.'
+
+
+count_info()
--- Урок 5. Практическое задание/task_3/task_3.py
+++ Урок 5. Практическое задание/task_3/task_3.py
@@ -8,3 +8,9 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
+
+rus = {"One": "Один", "Two": "Два", "Three": "Три", "Four": "Четыре"}
+
+with open("text3.txt", "r+", encoding="utf-8") as text:
+    with open("text3 (new).txt", "r+", encoding="utf-8") as newtext:
+        newtext.writelines([el.replace(el.split()[0], rus.get(el.split()[0])) for el in text])
