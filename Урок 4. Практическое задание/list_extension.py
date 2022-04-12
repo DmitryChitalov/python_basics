9e7e89cb0b33a94d0ec8ed01e21fc103c2f8b59d
sefimov <sefimov@navicons.com>
2022-04-12 21:46:45 +0300

    Исправление опечатки

--- Урок 4. Практическое задание/list_extension.py
+++ Урок 4. Практическое задание/list_extension.py
@@ -1,6 +1,6 @@
 import string_extension
 
-def get__int_numbers_from_list(input_list):
+def get_int_numbers_from_list(input_list):
     number_list =[]
     for el in input_list:
         if string_extension.is_int(el):
