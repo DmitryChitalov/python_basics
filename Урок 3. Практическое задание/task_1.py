dd8031651c55406530bfd3811a8e41b817d7a392
ErastovAlexandr <erastov_sasha@mail.ru>
2022-04-14 17:27:40 +0800

    lesson_3.1

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -1,9 +1,9 @@
-def delenie (a, b):
-        try:
-                c = a / b
-                return c
-        except ZeroDivisionError:
-                return "деление на 0 невозможно"
+def delenie(a, b):
+    try:
+        c = a / b
+        return c
+    except ZeroDivisionError:
+        return "деление на 0 невозможно"
 
 
 print(delenie(float(input("введите делимое ")), float(input("введите делитель "))))
--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -1,13 +1,21 @@
-def my_func(a, b, c):
+def not_my_func(*args):
+    rez = sum(sorted(list(args), reverse=True)[:2])
+    return rez
+
+
+def my_func(*args):
     rez = 0
-    if a > b:
-        rez = rez + a
+    if args[0] > args[1]:
+        rez = rez + args[0]
     else:
-        rez = rez + b
-    if c > b:
-        rez = rez + c
+        rez = rez + args[1]
+    if args[2] > args[1]:
+        rez = rez + args[2]
     else:
-        rez = rez + a
+        if args[2] > args[0]:
+            rez = rez + args[2]
+        else:
+            rez = rez + args[0]
     return rez
 
 
@@ -15,3 +23,4 @@ a = int(input("введите первый аргумент "))
 b = int(input("введите второй аргумент "))
 c = int(input("введите третий аргумент "))
 print(my_func(a, b, c))
+print(not_my_func(a, b, c))
