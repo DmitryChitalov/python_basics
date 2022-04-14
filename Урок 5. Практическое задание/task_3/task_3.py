e7db3933bf8ceb9ad8c7c3e08e3fca458f9a2f7d
MikhailS777 <quakastic@mail.ru>
2022-04-14 09:12:49 +0300

    дз5

--- Урок 5. Практическое задание/task_3/task_3.py
+++ Урок 5. Практическое задание/task_3/task_3.py
@@ -8,3 +8,15 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
+
+book = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре'}
+try:
+    with open('t3.txt', 'r', encoding="utf-8") as t_obj:
+        with open('t3.1.txt', 'w', encoding="utf-8") as t1_obj:
+            for line in t_obj:
+                st = line.split(' ', 1)
+                st.insert(0, book[st.pop(0)])
+                my = ' '.join(st)
+                t1_obj.write(my)
+except FileNotFoundError:
+    print("Файл не найден!")
--- Урок 5. Практическое задание/task_7/task_7.py
+++ Урок 5. Практическое задание/task_7/task_7.py
@@ -9,3 +9,33 @@
 [{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit": 2000}]
 Подсказка: использовать менеджер контекста.
 """
+
+import json
+
+fin = []
+profit = {}
+pr_avr = {}
+prof = 0
+i = 0
+try:
+    with open('t7.txt', 'r', encoding='utf-8') as file:
+        for line in file:
+            name, firm, rev, cost = line.split()
+            diff = round(float(rev) - float(cost), 2)
+            profit[name] = diff
+            if diff > 0:
+                prof = prof + diff
+                i += 1
+        fin.append(profit)
+        if i > 0:
+            pr_avr["Средняя прибыль"] = round(prof / i, 2)
+            fin.append(pr_avr)
+        else:
+            print('Все компании работают в убыток')
+except FileNotFoundError:
+    print("Файл не найден!")
+
+with open('t7.json', 'w+', encoding='utf-8') as json_file:
+    json.dump(fin, json_file, ensure_ascii=False)
+    json_file.seek(0)
+    print(f"json файл: ", json_file.read())
