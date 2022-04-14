ca70de36584e2319651d956bb05a43fb340297f0
naem <naem@reg87.ru>
2022-04-14 21:06:56 +0300

    adding completed homework to the sixth lesson

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -15,3 +15,30 @@
 П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку __str__
 __str__(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
 """
+
+
+class Worker:
+
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+
+class Position(Worker):
+
+    def __init__(self, name, surname, position, wage, bonus):
+        super().__init__(name, surname, position, wage, bonus)
+
+    def get_full_name(self):
+        return self.name + ' ' + self.surname
+
+    def get_total_income(self):
+        return self._income.get('wage') + self._income.get('bonus')
+
+
+a = Position('Vladimir', 'Ivanov', 'Policeman', 120000, 15000)
+print(a.get_full_name())
+print(a.position)
+print(a.get_total_income())
