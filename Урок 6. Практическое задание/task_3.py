f3c6361aee19542cc76d990750bd6a1c67a72c30
sefimov <sefimov@navicons.com>
2022-04-16 16:22:09 +0300

    Выполнение домашнего задания по 6 уроку

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -15,3 +15,47 @@
 П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку __str__
 __str__(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
 """
+
+class Worker:
+    _income = {
+        "wage": 0,
+        "bonus": 0
+    }
+
+    name = None
+    surname = None
+    position = None
+
+    def __init__(self, name, surname, position) -> None:
+        self.name = name
+        self.surname = surname
+        self.position = position
+
+    def set_income(self, wage, bonus) -> None:
+        if type(wage) is int:
+            self._income["wage"] = wage
+
+        if type(bonus) is int:
+            self._income["bonus"] = bonus
+
+class Position(Worker):
+
+    def __init__(self, name, surname, position) -> None:
+        super().__init__(name, surname, position)
+
+    def get_full_name(self) -> str:
+        return f"{self.surname} {self.name}"
+
+    def get_total_income(self) -> int:
+        return self._income["wage"] + self._income["bonus"]
+    
+    def __str__(self) -> str:
+        return str({
+            "fullname": self.get_full_name(),
+            "income": self.get_total_income()
+        })
+
+
+position_1 = Position("Ivan", "Ivanov", "Developer")
+position_1.set_income(50000, 30000)
+print(position_1)
--- Урок 6. Практическое задание/task_5.py
+++ Урок 6. Практическое задание/task_5.py
@@ -13,3 +13,55 @@
 Для каждого из классов метод должен выводить уникальное сообщение.
 Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
 """
+
+from typing import List
+
+class Stationery:
+    title = None
+
+    def __init__(self, title) -> None:
+        if type(title) is str:
+            self.title = title
+        else:
+            raise ValueError("The title of stationery must be as string.")
+
+    def draw(self) -> None:
+        print("Start drawing")
+
+
+
+class Pen(Stationery):
+    def __init__(self) -> None:
+        super().__init__("Pen")
+
+    def draw(self) -> None:
+        print("Start pen drawing")
+
+
+
+class Pencil(Stationery):
+    def __init__(self) -> None:
+        super().__init__("Pencil")
+
+    def draw(self) -> None:
+        print("Start pencil drawing")
+
+
+
+class Handle(Stationery):
+    def __init__(self) -> None:
+        super().__init__("Handle")
+
+    def draw(self) -> None:
+        print("Start handle drawing")
+
+
+stationeries:List[Stationery] = [
+    Stationery("Empty"),
+    Pen(),
+    Pencil(),
+    Handle()
+]
+
+for stationery in stationeries:
+    stationery.draw()
