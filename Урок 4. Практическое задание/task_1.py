916cc213062fd44f260352bb4070fe183053017d
kulikovka99 <killymally@mail.ru>
2022-04-17 23:11:29 +0300

    Task 1
    Task 1 done

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -6,3 +6,15 @@
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
 """
+
+import sys
+
+script_name, numb_hours, sal_per_hour, prem = sys.argv
+
+
+def sal_staff(numb_hours, sal_per_hour, prem):
+    sal = (int(numb_hours) * int(sal_per_hour)) + int(prem)
+    return sal
+
+
+print(sal_staff(numb_hours, sal_per_hour, prem))
