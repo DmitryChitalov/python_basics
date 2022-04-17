bff1ab2810bdb396cbefb32bd438f799110ccc93
kulikovka99 <killymally@mail.ru>
2022-04-17 23:11:45 +0300

    Task 2
    Task 2 done

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -7,3 +7,18 @@
 
 Реализуйте вариант без и с генераторным выражением
 """
+
+lst = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+
+# вариант без включения
+new_lst = []
+for el in range(len(lst)):
+    if lst[el] > lst[el - 1]:
+        new_lst.append(lst[el])
+
+print(new_lst)
+
+# вариант с включением
+my_lst = [lst[el] for el in range(len(lst)) if lst[el] > lst[el - 1]]
+
+print(my_lst)
