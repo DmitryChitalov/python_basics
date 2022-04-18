b255d9980514a509404d8c9693c546620a5106e4
Dozer <do0zer@mail.ru>
2022-04-18 19:07:43 +0300

    homework 6

--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -15,3 +15,36 @@
 
 Проверить работу примера, создав экземпляр и вызвав описанный метод.
 """
+
+from time import sleep
+
+
+def out_red(text):
+    print("\033[31m {}".format(text))
+
+
+def out_yellow(text):
+    print("\033[33m {}".format(text))
+
+
+def out_green(text):
+    print("\033[32m {}".format(text))
+
+
+class TrafficLight:
+    __color = ['red', 'green', 'yellow']
+
+    def running (self):
+        while True:
+            out_red('RED')
+            sleep(7)
+            out_yellow('YELLOW')
+            sleep(2)
+            out_green('GREEN')
+            sleep(7)
+            out_yellow('YELLOW')
+            sleep(2)
+
+
+TrafficLight = TrafficLight()
+TrafficLight.running()
