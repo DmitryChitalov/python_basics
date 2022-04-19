dccb189201b8c30fee47575720d7f247d6d1afed
priborist92 <priborist92@mail.ru>
2022-04-19 21:29:43 +0300

    Lesson 6
    Lesson 6 (1-5)

--- Урок 6. Практическое задание/task_4.py
+++ Урок 6. Практическое задание/task_4.py
@@ -20,3 +20,55 @@ speed, color, name, is_police (булево).
 Выполните доступ к атрибутам, выведите результат.
 Выполните вызов методов и также покажите результат.
 """
+class Car:
+
+    def __init__(self, name, speed, color, is_police=True):
+        self.name = name
+        self.speed = speed
+        self.color = color
+        self.is_police = is_police
+
+    def go(self):
+        return f'The {self.name} went.'
+
+    def stop(self):
+        return f'\nThe {self.name} has stopped.'
+
+    def turn(self, direction):
+        return f'\nThe {self.name} turned {direction}'
+
+    def show_speed(self):
+        return f'\nYour speed is {self.speed}'
+
+
+class TownCar(Car):
+    def show_speed(self):
+        if self.speed > 60:
+            return f'\nYour speed is higher than allow! Your speed is {self.speed}'
+        else:
+            return f'Speed of {self.name} is normal'
+
+
+class SportCar(Car):
+    pass
+
+
+class WorkCar(Car):
+    def show_speed(self):
+        if self.speed > 40:
+            return f'\nYour speed is higher than allow! Your speed is {self.speed}'
+        else:
+            return f'Speed of {self.name} is normal'
+
+
+class PoliceCar(Car):
+    pass
+
+
+town = TownCar('Smart', 85, 'blue', False)
+print('1:\n' + town.go(), town.show_speed(), town.turn('left'), town.turn('right'), town.stop())
+sport = SportCar('MercedesAMG', 200, 'red', False)
+print('2:\n' + sport.go(), sport.show_speed(), sport.turn('left'), sport.stop())
+
+work = WorkCar('Volkswagen', 45, 'black', False)
+print('3:\n' + work.go(), work.show_speed(), work.turn('right'), work.stop())
