963ea25e50c9e656c1e623b05bd355c101fd6e3f
kulikovka99 <killymally@mail.ru>
2022-04-20 21:00:28 +0300

    Task 2
    Task 2 done

--- Урок 5. Практическое задание/task_2/task_2.py
+++ Урок 5. Практическое задание/task_2/task_2.py
@@ -2,3 +2,18 @@
 2)	Создать текстовый файл (не программно), сохранить в нем несколько строк,
 выполнить подсчет количества строк, количества слов в каждой строке.
 """
+
+count_w = 0
+
+with open("HW5_task2.txt", "r", encoding='utf-8') as f_obj:
+    my_str = f_obj.readlines()
+    count_l = len(my_str)
+
+with open("HW5_task2.txt", "r", encoding='utf-8') as f_obj:
+    my_str2 = f_obj.read()
+    a = (my_str2.replace('\n', ' '))
+    for el in a:
+        if el == " ":
+            count_w += 1
+
+print(f"Количество строк - {count_l}, Количество слов - {count_w}")
