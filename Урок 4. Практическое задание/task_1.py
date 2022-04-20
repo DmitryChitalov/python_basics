6cdb62692ee34a3e8700818c18b731439e5883c3
kulikovka99 <killymally@mail.ru>
2022-04-20 23:05:06 +0300

    Working on mistakes
    For task 1 & task 7

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -13,7 +13,7 @@ script_name, numb_hours, sal_per_hour, prem = sys.argv
 
 
 def sal_staff(numb_hours, sal_per_hour, prem):
-    sal = (int(numb_hours) * int(sal_per_hour)) + int(prem)
+    sal = int(numb_hours) * int(sal_per_hour) + int(prem)
     return sal
 
 
--- Урок 4. Практическое задание/task_7.py
+++ Урок 4. Практическое задание/task_7.py
@@ -11,15 +11,16 @@
 Например, факториал четырёх 4! = 1 * 2 * 3 * 4 = 24.
 """
 
-from math import factorial
-
 
 def fact(n):
-    result = factorial(n)
-    yield result
+    f = 1
+    while n > 1:
+        f = f * n
+        n = n - 1
+    yield f
 
 
 n = int(input("Введите целое положительное число: "))
-for i in range(1, n+1):
+for i in range(1, n + 1):
     for el in fact(i):
         print(f"{i}! = {el}")
