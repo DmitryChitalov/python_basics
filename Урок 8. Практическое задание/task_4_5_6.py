b85a8e47df1b618af9b5b029bff79de74df28fa1
Anton Shkamanov <anton.shkamanov@yandex.ru>
2022-04-21 22:21:16 +0300

    Домашняя работа. Урок 8

--- Урок 8. Практическое задание/task_4_5_6.py
+++ Урок 8. Практическое задание/task_4_5_6.py
@@ -19,3 +19,139 @@
 Подсказка: постарайтесь по возможности реализовать в проекте
 «Склад оргтехники» максимум возможностей, изученных на уроках по ООП.
 """
+
+
+class AppError(Exception):
+    def __init__(self, text):
+        self.text = text
+
+    def __str__(self):
+        return self.text
+
+
+class AcceptStorageError(AppError):
+    def __init__(self, text):
+        self.text = f"Невозможно добавить товар на склад:\n {text}"
+
+
+class TransferStorageError(AppError):
+    def __init__(self, text):
+        self.text = f"Ошибка прередачи оборудования:\n {text}"
+
+
+AddStorageError = AcceptStorageError
+
+
+class ValidateEquipmentError(AppError):
+    pass
+
+
+class Storage:
+    def __init__(self):
+        self.__storage = []
+
+    def add(self, equipments):
+        if not all([isinstance(equipment, OfficeEquipment) for equipment in equipments]):
+            raise AddStorageError("Вы пытаетесь добавить на склад не оргтехнику")
+
+        self.__storage.extend(equipments)
+
+    def transfer(self, idx: int, department: str, item=None):
+        if not isinstance(idx, int):
+            raise TransferStorageError(f"Недопустимый тип '{type(item)}'")
+
+        item: OfficeEquipment = self.__storage[idx]
+
+        if item.department:
+            raise TransferStorageError(f"Оборудование {item} уже закреплено за отделом {item.department}")
+
+        item.department = department
+
+    def filter_by(self, **kwargs):
+        for id_, item in enumerate(self):
+            if all([item.__getattribute__(key) == kwargs[key] for key in kwargs]):
+                yield id_, item
+
+    def __getitem__(self, key):
+        if not isinstance(key, int):
+            raise TypeError
+
+        return self.__storage[key]
+
+    def __delitem__(self, key):
+        if not isinstance(key, int):
+            raise TypeError
+
+        del self.__storage[key]
+
+    def __iter__(self):
+        return self.__storage.__iter__()
+
+    def __str__(self):
+        return f"На складе сейчас {len(self.__storage)} устройств"
+
+
+class OfficeEquipment:
+    __required_props = ("eq_type", "vendor", "model")
+
+    def __init__(self, eq_type: str = None, vendor: str = "", model: str = ""):
+        self.type = eq_type
+        self.vendor = vendor
+        self.model = model
+
+        self.department = None
+
+    def __setattr__(self, key, value):
+        if key in self.__required_props and not value:
+            raise AttributeError(f"'{key}' должен иметь значение отличное от false")
+
+        object.__setattr__(self, key, value)
+
+    def __str__(self):
+        return f"{self.type} {self.vendor} {self.model}"
+
+    @staticmethod
+    def validate(cnt):
+        if not isinstance(cnt, int):
+            ValidateEquipmentError(f"'{type(cnt)}'; количество инстансов должен быть типа 'int'")
+
+    @classmethod
+    def create(cls, cnt, **properties):
+        cls.validate(cnt)
+        return [cls(**properties) for _ in range(cnt)]
+
+
+class Printer(OfficeEquipment):
+    def __init__(self, **kwargs):
+        super().__init__(eq_type='Printer', **kwargs)
+
+
+class Scanner(OfficeEquipment):
+    def __init__(self, **kwargs):
+        super().__init__(eq_type='Scanner', **kwargs)
+
+
+class Xerox(OfficeEquipment):
+    def __init__(self, **kwargs):
+        super().__init__(eq_type='Xerox', **kwargs)
+
+
+if __name__ == '__main__':
+    storage = Storage()
+    storage.add(Printer.create(4, vendor="Epson", model="XP-400"))
+    storage.add(Scanner.create(3, vendor="OKI", model="5367-AD"))
+    storage.add(Scanner.create(2, vendor="OKI", model="5368-AD"))
+    storage.add(Xerox.create(6, vendor="Xerox", model="F123"))
+    print(storage)
+
+    for idx, itm in storage.filter_by(department=None, vendor="OKI", model="5367-AD"):
+        print(f"Резервируем {itm} в 'Отдел ЯФ'")
+        storage.transfer(idx, 'Отдел ЯФ')
+
+    for idx, itm in storage.filter_by(department=None):
+        print(idx, f"{itm} не распределены по отделам")
+
+    print(storage)
+    print("Списываем 1 устройство")
+    del storage[0]
+    print(storage)
