d026f452b95453603a6291ad6a2d97273fef9e59
Mikael1977 <mik_h@mail.ru>
2022-04-21 15:36:00 +0300

    Done lesson 8_3

--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -9,3 +9,30 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+
+
+class NoDigitError(ValueError):
+    def __init__(self, txt):
+        self.txt = txt
+
+
+my_list = []
+next_element = ""
+
+while next_element != "q":
+    next_element = input("Введите следующий элемент списка (или \'q\'для выхода): ")
+    try:
+        if next_element.isdigit():
+            next_element = int(next_element)
+        else:
+            if next_element != "q":
+                raise NoDigitError("Элемент не является числом")
+
+    except NoDigitError as err:
+        print(err)
+    else:
+        if next_element != "q":
+            my_list.append(next_element)
+
+
+print(my_list)
