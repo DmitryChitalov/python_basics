19921fc7b9b804f73c3a41d0ee535e45d4dfb29d
priborist92 <priborist92@mail.ru>
2022-04-23 17:41:20 +0300

    Lesson 8
    Lesson 8 (1-3, 4-6,7)

--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -9,3 +9,25 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+
+class Error:
+    def __init__(self, *args):
+        self.list = []
+
+    def new_input(self):
+        while True:
+
+            try:
+                val = int(input('Введите число '))
+                self.list.append(val)
+                print(f'Текущий список - {self.list} \n ')
+            except:
+                print("Это не число!")
+                decision = input("если хотите закончить, напишите stop, иначе продолжим ")
+                if decision == "stop":
+                    return "Exit"
+                else:
+                    print(try_except.new_input())
+
+try_except = Error(1)
+print(try_except.new_input())
