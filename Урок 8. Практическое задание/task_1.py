09a62cc1895f520c838956498456544439d5ad04
MikhailS777 <quakastic@mail.ru>
2022-04-25 19:04:10 +0300

    дз8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,34 @@
 Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца
 и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
 """
+
+from datetime import datetime
+
+
+class Data:
+    day_month_year = '11-05-2022'
+
+    @classmethod
+    def extract(cls):
+        if cls.valid(cls.day_month_year):
+            day, month, year = map(int, cls.day_month_year.split('-'))
+            setattr(cls, 'c_day', day)
+            setattr(cls, 'c_month', month)
+            setattr(cls, 'c_year', year)
+            return f'{Data.c_day:02}-{Data.c_month:02}-{Data.c_year:04}'
+        return 'Ошибка ввода'
+
+    @staticmethod
+    def valid(data):
+        try:
+            if data == datetime.strptime(data, "%d-%m-%Y").strftime('%d-%m-%Y'):
+                return True
+        except ValueError:
+            print("Некорректные данные или ввод, дата должна быть в формате: dd-mm-yyyy")
+            return False
+
+
+d = Data
+print(d.extract())
+d.day_month_year = '29-02-2022'
+print(d.extract())
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,27 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+
+
+class MyExcept(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+    @staticmethod
+    def div_err():
+        try:
+            a = float(input('Введите делимое: '))
+            b = float(input('Введите делитель: '))
+            if b == 0:
+                raise MyExcept('Делить на ноль нельзя!')
+
+            res = a / b
+            return res
+        except ValueError:
+            return 'Ошибка ввода!'
+        except Exception as err:
+            return err
+
+
+d = MyExcept
+print('Результат: ', d.div_err())
