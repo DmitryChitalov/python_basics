f9bfbe44f59ed8e13c0e4a745b5d7765f91d8c8b
kulikovka99 <killymally@mail.ru>
2022-04-25 11:18:06 +0300

    Task 5
    Task 5 done

--- Урок 6. Практическое задание/task_5.py
+++ Урок 6. Практическое задание/task_5.py
@@ -13,3 +13,53 @@
 Для каждого из классов метод должен выводить уникальное сообщение.
 Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
 """
+
+
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print("Запуск отрисовки!")
+
+
+class Pen(Stationery):
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print("Запуск отрисовки ручкой!")
+
+
+class Pencil(Stationery):
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print("Запуск отрисовки карандашом!")
+
+
+class Handle(Stationery):
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print("Запуск отрисовки маркером!")
+
+
+s = Stationery('Канцелярия')
+p = Pen('Ручка')
+pe = Pencil('Карандаш')
+h = Handle('Маркер')
+
+print(s.title)
+s.draw()
+
+print(p.title)
+p.draw()
+
+print(pe.title)
+pe.draw()
+
+print(h.title)
+h.draw()
