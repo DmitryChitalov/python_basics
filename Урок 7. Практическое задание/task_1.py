58207bf4e621f1cd8b4b1865d4e2541996bf2909
kulikovka99 <killymally@mail.ru>
2022-04-25 21:33:15 +0300

    Task 1
    Task 1 done

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -28,3 +28,31 @@
 8 10 12
 14 16 18
 """
+
+
+class Matrix:
+    def __init__(self, *args):
+        self.new_lst = list(args)
+
+    def __str__(self):
+        b = str(self.new_lst)
+        return b.replace("]", "\n").replace("[", " ").replace(",", "")
+
+    def __add__(self, other):
+        new_sum = []
+        line_sum = []
+        for x in range(len(self.new_lst)):
+            for y in range(len(self.new_lst[x])):
+                line_sum.append(self.new_lst[x][y] + other.new_lst[x][y])
+            new_sum.append(line_sum)
+            line_sum = []
+        c = str(new_sum)
+        return c.replace("]", "\n").replace("[", " ").replace(",", "")
+
+
+
+my_m = Matrix([6, 7, 8], [7, 8, 9], [9, 8, 9])
+my_m2 = Matrix([6, 7, 8], [7, 8, 9], [9, 8, 9])
+print(my_m.__str__())
+print(my_m2.__str__())
+print(my_m.__add__(my_m2))
