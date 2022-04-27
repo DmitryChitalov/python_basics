a948aa9ce263e54d1b5c2b397f05ff8bc4584a67
MaximFateev <studentmaximfateev@gmail.com>
2022-04-27 11:50:44 +0300

    ДЗ по 5 уроку

--- Урок 5. Практическое задание/task_2/task_2.py
+++ Урок 5. Практическое задание/task_2/task_2.py
@@ -2,3 +2,22 @@
 2)	Создать текстовый файл (не программно), сохранить в нем несколько строк,
 выполнить подсчет количества строк, количества слов в каждой строке.
 """
+
+
+def count_words(some_str):
+    words_count = 0
+    for el in some_str:
+        if el == " ":
+            words_count += 1
+    if some_str[len(some_str) - 1] != " ":
+        words_count += 1
+    return words_count
+
+
+my_file = open('textfile.txt', 'r')
+
+counter = 1
+for line in my_file:
+    print(f"в {counter}-ой строке {count_words(line)} слов")
+
+my_file.close()
--- Урок 5. Практическое задание/task_3/task_3.py
+++ Урок 5. Практическое задание/task_3/task_3.py
@@ -8,3 +8,30 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
+
+
+def replace_word(some_str):
+    words_dict = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'ЧЕТЫРЕ', 'Five': 'Пять', 'Six': 'Шесть',
+                  'Seven': 'Семь', 'Eight': 'Восемь', 'Nine': 'Девять', 'Ten': 'Ноль', }
+    result_str = ""
+    word = ""
+    for el in some_str:
+        if el != " ":
+            word = word + el
+        else:
+            if word in words_dict:
+                result_str = words_dict[word] + some_str[len(word):]
+                word = ""
+    return result_str
+
+
+input_file = open('inputtextfile.txt', 'r')
+output_file = open('outputtextfile.txt', 'w+')
+
+for line in input_file:
+    tmp = replace_word(line)
+    print(tmp)
+    output_file.write(replace_word(line))
+
+input_file.close()
+output_file.close()
