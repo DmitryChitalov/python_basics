fb02475f2c5d12175b43dffec0088e9e61240c4e
653268746 <aetelmor@gmail.com>
2022-04-28 03:02:08 +0300

    Урок 5 Работа с файлами

--- Урок 5. Практическое задание/task_1/task_1.py
+++ Урок 5. Практическое задание/task_1/task_1.py
@@ -1,5 +1,13 @@
-"""
-1)	Создать программно файл в текстовом формате,
-записать в него построчно данные, вводимые пользователем.
-Об окончании ввода данных свидетельствует пустая строка.
-"""
+file_open = open('1.txt', 'w')
+while True:
+    text_line = str(input("Input text line: \n"))
+    if text_line == "print":
+        file_open = open('1.txt', 'r')
+        print("\nPrint file:\n----------\n" + file_open.read() + "---------- \nEnd file.")
+        break
+    elif not text_line:
+        print("Exit.\n")
+        break
+    elif text_line:
+        file_open.write(text_line + "\n")
+file_open.close()
