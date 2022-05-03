7d27a75cf3dddca1c9ef73b7da4169f07be248c5
ErastovAlexandr <erastov_sasha@mail.ru>
2022-05-03 22:28:08 +0800

    Lesson_6

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -1,17 +1,18 @@
-"""
-Задание 3.
+class Worker:
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
 
-Реализовать базовый класс Worker (работник),
-в котором определить публичные атрибуты name, surname, position (должность),
-и защищенный атрибут income (доход). Последний атрибут должен ссылаться
-на словарь, содержащий элементы: оклад и премия, например, {"wage": wage, "bonus": bonus}.
 
-Создать класс Position (должность) на базе класса Worker. В классе Position реализовать публичные методы
-получения полного имени сотрудника (get_full_name) и дохода с учетом премии (get_total_income).
+class Position(Worker):
+    def get_full_name(self):
+        return f'{self.name} {self.surname}'
 
-Проверить работу примера на реальных данных (создать экземпляры класса Position, передать данные,
-проверить значения атрибутов, вызвать методы экземпляров).
+    def get_total_income(self):
+        return int(self._income['wage']) + int(self._income['bonus'])
 
-П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку __str__
-__str__(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
-"""
+
+a = Position("igor", "mkrtchan", "lord", "1000", "500000")
+print(f"Сотрудник {a.get_full_name()} имеет доход {a.get_total_income()} тенге")
