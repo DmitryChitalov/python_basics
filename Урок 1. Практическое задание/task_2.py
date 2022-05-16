9985a64d2b9acc23fce5f2d8f1c7bbae553cc37b
Zhanna Nizhegorodova <nizhegorodova.zhanna@yandex.ru>
2022-05-16 16:59:44 +0300

    Практическая работа к 1 уроку

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -9,3 +9,10 @@
 Введите время в секундах: 3600
 Время в формате ч:м:с - 1.0 : 60.0 : 3600
 """
+
+seconds = int(input('Введите время в секундах: '))
+hours = float(seconds // 3600)
+minutes = float(seconds % 3600 // 60)
+sec = float(seconds % 60)
+
+print(f'Время в формате ч:м:с - {hours} : {minutes} : {sec}')
--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,11 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+
+n = int(input('Введите число n: '))
+str_n = str(n)
+nn = str_n + str_n
+nnn = str_n + str_n + str_n
+amount = n + int(nn) + int(nnn)
+print(f'{n} + {nn} + {nnn} = {amount}')
+
