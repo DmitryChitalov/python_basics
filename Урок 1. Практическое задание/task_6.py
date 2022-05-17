7cc3bb3bff842543995ab6e4478ee478d62f0694
Sergey281172 <105632812+Sergey281172@users.noreply.github.com>
2022-05-17 13:39:02 +0300

    Update task_6.py

--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -19,3 +19,12 @@
 6-й день: 3,22
 Ответ: на 6-й день спортсмен достиг результата — не менее 3 км.
 """
+a = float(input("Enter start: "))
+b = float(input("Enter finish: "))
+day = 1
+if a > b:
+    print(day)
+while a < b:
+    a = a + a/10
+    day += 1
+print(day)
