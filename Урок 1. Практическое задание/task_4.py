10f61024ea7fc46ad6c03d5f7d48da60a02e36c9
Nick Bodrov <nickbbtc@yahoo.com>
2022-05-18 03:30:41 +0300

    Домашнее задание к 1-му уроку Никита Черкасов

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -10,3 +10,29 @@
 Ведите целое положительное число: 123456789
 Самая большая цифра в числе: 9
 """
+def check_for_number(num):
+    try:
+        if int(num) > 0:
+            return True
+        else:
+            return False
+    except:
+        return False
+
+while True:
+    number = input('Введите число n ==>')
+    if check_for_number(number):
+        number = int(number)
+        max_number = number % 10
+        while number >= 1:
+            number = number // 10
+            if number % 10 > max_number:
+                max_number = number % 10
+            if number > 9:
+                continue
+            else:
+                print(f'Максимальное число - {max_number}')
+                break
+        break
+    else:
+        print('Введите целое положительное число...')
