4fff9831bf6b0d577e9213130b3cd08efc533eae
Zhanna Nizhegorodova <nizhegorodova.zhanna@yandex.ru>
2022-05-23 18:53:19 +0300

    Сделаны задания к уроку 2

--- Урок 2. Практическое задание/task_2.py
+++ Урок 2. Практическое задание/task_2.py
@@ -11,3 +11,9 @@
 Введите целые числа через пробел: 1 2 3
 Результат: 2 1 3
 """
+
+my_list = input("Введите элементы списка: ").split()
+my_list[:-1:2], my_list[1::2] = my_list[1::2], my_list[:-1:2]
+print(*my_list)
+new_list = " ".join(my_list)
+print(new_list)
