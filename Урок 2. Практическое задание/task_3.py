a1a7b95d4c1e33733cc7310bafa2a1d3cd584818
Sergey281172 <105632812+Sergey281172@users.noreply.github.com>
2022-05-23 16:47:20 +0300

    Update task_3.py

--- Урок 2. Практическое задание/task_3.py
+++ Урок 2. Практическое задание/task_3.py
@@ -8,3 +8,23 @@
 Результат через список: Осень
 Результат через словарь: Осень
 """
+
+month = int(input("Введите месяц по номеру "))
+seasons_dict = {1 : 'зима', 2 : 'весна', 3 : 'лето', 4 : 'осень'}
+seasons_list = ['зима', 'весна', 'лето', 'осень']
+if 12 >= month >= 1:
+    if 3 <= month <= 5:
+        print(seasons_dict.get(2))
+        print(seasons_list[1])
+    elif 6 <= month <= 8:
+        print(seasons_dict.get(3))
+        print(seasons_list[2])
+    elif 9 <= month <= 11:
+        print(seasons_dict.get(4))
+        print(seasons_list[3])
+    else:
+        print(seasons_dict.get(1))
+        print(seasons_list[0])
+
+else:
+    print("Такого месяца не существует")
