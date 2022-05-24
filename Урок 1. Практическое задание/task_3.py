c3942ebe9ac64fcfb38c80088928724fff02be3f
Evil <motorist11951@yandex.ru>
2022-05-24 21:54:08 +1200

    Выполнил задачу № 3

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,6 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+user_numbers = int(input('Введите целое положительное число: '))
+Calculation = user_numbers + user_numbers * 11 + user_numbers * 111
+print(f' n+nn+nnn= {Calculation}')
