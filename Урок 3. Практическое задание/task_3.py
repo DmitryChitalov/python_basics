03accb2b9a728fa42313f536a2134eaad1e1e889
Vladyslav Nikolaichuk <erengard97@gmail.com>
2022-05-24 19:12:43 +0300

    Задача 3 исправлена

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -7,20 +7,20 @@
 """
 
 
-def sum_two_biggest_numbers_without_sort(first_arg, second_arg, third_arg):
+def sum_two_biggest_numbers_v1(first_arg, second_arg, third_arg):
     lst = [first_arg, second_arg, third_arg]
     return sum(lst) - min(lst)
 
 
-def sum_two_biggest_numbers_with_sort(first_arg, second_arg, third_arg):
+def sum_two_biggest_numbers_v2(first_arg, second_arg, third_arg):
     lst = [first_arg, second_arg, third_arg]
     lst.sort()
     return lst[1] + lst[2]
 
 
-firstNumb = int(input("Введите первое число: "))
-secondNumb = int(input("Введите второе число: "))
-thirdNumb = int(input("Введите третье число: "))
+first_numb = int(input("Введите первое число: "))
+second_numb = int(input("Введите второе число: "))
+third_numb = int(input("Введите третье число: "))
 
-print(f'Без сортировки: {sum_two_biggest_numbers_without_sort(firstNumb, secondNumb, thirdNumb)}')
-print(f'С сортировкой: {sum_two_biggest_numbers_with_sort(firstNumb, secondNumb, thirdNumb)}')
+print(f'Без сортировки: {sum_two_biggest_numbers_v1(first_numb, second_numb, third_numb)}')
+print(f'С сортировкой: {sum_two_biggest_numbers_v2(first_numb, second_numb, third_numb)}')
