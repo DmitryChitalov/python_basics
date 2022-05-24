5e8db209023e585b679424d6425b72df7c5cea97
Sergey281172 <105632812+Sergey281172@users.noreply.github.com>
2022-05-24 16:01:45 +0300

    Update task_1.py

--- Урок 2. Практическое задание/task_1.py
+++ Урок 2. Практическое задание/task_1.py
@@ -15,3 +15,13 @@
 <class 'bool'>
 <class 'NoneType'>
 """
+my_int = 5
+my_float = 1.2
+my_str = "Hello world"
+my_list = ['a', '2']
+my_tuple = ('b', '3')
+my_dict = {'city': 'Moscow', 'country': 'Russia'}
+
+super_list = [my_int, my_float, my_str, my_list, my_tuple, my_dict]
+for i in super_list:
+    print(f'{i} is {type(i)}')
