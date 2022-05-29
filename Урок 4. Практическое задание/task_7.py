f18def717ea0e21ebb8097599357f2c4d175fdf8
Georgii <mga@nn.ru>
2022-05-29 20:07:04 +0300

    task4 done

--- Урок 4. Практическое задание/task_7.py
+++ Урок 4. Практическое задание/task_7.py
@@ -10,3 +10,13 @@
 Подсказка: факториал числа n — произведение чисел от 1 до n.
 Например, факториал четырёх 4! = 1 * 2 * 3 * 4 = 24.
 """
+
+def fact(n):
+    for i in range(1, n + 1):
+        yield i
+res = 1
+
+for el in fact(4):
+    #print(res, el)
+    res = res * el
+print(res)
