7acc87884e4a5bcb551cd4dafa4e11da1d56ebdc
RESHbnm <reshbnm19@gmail.com>
2022-05-29 19:17:54 +0300

    Выполнено задание 1

--- Урок 5. Практическое задание/task_1/task_1.py
+++ Урок 5. Практическое задание/task_1/task_1.py
@@ -3,3 +3,10 @@
 записать в него построчно данные, вводимые пользователем.
 Об окончании ввода данных свидетельствует пустая строка.
 """
+s = input('Введите текст. Для окончания введите пустую строку: ')
+with open("out_file.txt", "w", encoding='utf-8') as file:
+    file.write(s + '\n')
+while s:
+    s = input()
+    with open("out_file.txt", "a", encoding='utf-8') as file:
+        file.write(s + '\n')
