15b7e795d5241bd91feac44db5ce165d6b218584
Nana <vayfiel@gmail.com>
2022-05-29 05:12:05 +0300

    ls_3

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -17,3 +17,24 @@ Process finished with exit code 0
 
 Process finished with exit code 0
 """
+
+
+def s_calc():
+    try:
+        r_val = float(input("Введите число: "))
+        h_val = float(input("Введите число: "))
+        if h_val == 0:
+            print(f"Вы что? Пытаетесь делить на 0!")
+            return
+        elif r_val == 0:
+            print(f"Вы что? Пытаетесь делить на 0!")
+            return
+
+    except ValueError:
+        return
+    s_side = r_val / h_val
+    return s_side
+
+
+s_side_val = s_calc()
+print(f"Ответ - {s_side_val}")
