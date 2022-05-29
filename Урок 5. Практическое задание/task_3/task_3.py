045b956af6b58dc15e46784a9e6f84cccecb6bc2
RESHbnm <reshbnm19@gmail.com>
2022-05-29 22:01:22 +0300

    Выполнено задание 3

--- Урок 5. Практическое задание/task_3/task_3.py
+++ Урок 5. Практическое задание/task_3/task_3.py
@@ -8,3 +8,12 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
+number = {"1": "Один", "2": "Два", "3": "Три", "4": "Четыре"}
+result = []
+with open("input.txt", "r", encoding='utf-8') as file:
+    for line in file:
+        num = line.split()[-1]
+        result.append(" - ".join([number.get(num), num]) + "\n")
+
+with open("output.txt", "w", encoding="utf-8") as file:
+    file.writelines(result)
