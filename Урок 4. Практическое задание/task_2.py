31d04496e0a1089b758ebd7257dffa99cf7020e0
RESHbnm <reshbnm19@gmail.com>
2022-05-29 13:20:46 +0300

    Выполнено задание 2

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -7,3 +7,12 @@
 
 Реализуйте вариант без и с генераторным выражением
 """
+my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+print(my_list)
+# new_list = [my_list[i] for i in range(1, len(my_list)) if my_list[i-1] < my_list[i]]
+# print(new_list)
+new_list =[]
+for i in range(1, len(my_list)):
+    if my_list[i - 1] < my_list[i]:
+        new_list.append(my_list[i])
+print(new_list)
