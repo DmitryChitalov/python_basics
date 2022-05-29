f3463d5be1f0217019e35b395e708933ecc0c989
ImNSv <100615545+ImNSv@users.noreply.github.com>
2022-05-29 19:00:23 +0400

    Update task_2.py

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -12,5 +12,4 @@ email: jackie@gmail.com, телефон: 01005321456
 def user_param(name, second_name, birthday, city, email, phone):
     return print (f'{name} {second_name} {birthday} года рождения, проживает в городе {city}, email: {email}, телефон {phone}')
 
-user_param(name = 'Ilon', second_name = 'Mask', birthday = '2100', 
-city = 'Keyptown', email = 'supermail', phone = '‪121211212‬')
+user_param(name = 'Ilon', second_name = 'Mask', birthday = '2100', city = 'Keyptown', email = 'supermail', phone = '‪121211212‬')
