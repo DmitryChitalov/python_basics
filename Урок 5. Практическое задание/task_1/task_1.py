001d7f6293e9aeb5b80ba737460ce13bff874cf2
Ksenia Sevostianova <info.sevostianova@gmail.com>
2022-05-30 21:15:35 +0300

    Выполнение практического задания к 5 уроку

--- Урок 5. Практическое задание/task_1/task_1.py
+++ Урок 5. Практическое задание/task_1/task_1.py
@@ -3,3 +3,14 @@
 записать в него построчно данные, вводимые пользователем.
 Об окончании ввода данных свидетельствует пустая строка.
 """
+new_list = []
+while True:
+    new_line = input('Enter any text to add txt: ')
+    if new_line == '':
+        print(new_list)
+        exit()
+    else:
+        other_line = new_line + '\n'
+        new_list.append(other_line)
+    with open("text1.txt", "w") as file_obj:
+        file_obj.writelines(new_list)
--- Урок 5. Практическое задание/task_5/task_5.py
+++ Урок 5. Практическое задание/task_5/task_5.py
@@ -2,3 +2,17 @@
 5)	Создать (программно) текстовый файл, записать в него программно набор чисел,
  разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 """
+from random import randint
+
+with open("text5.txt", "w") as f1:
+    for i in range(10):
+        f1.write(str(randint(0, 10000)))
+        f1.write(" ")
+
+total = 0
+with open("text5.txt", "r") as f2:
+    line = f2.readline()
+    for integer in line.split():
+        total += int(integer)
+
+print(f"Total: {total}")
