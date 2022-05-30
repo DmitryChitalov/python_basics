73bae8979322cbe7bfcbb34f0dcd18c27a4b82fa
MIKHAIL KUZNETSOV <9162203636@mail.ru>
2022-05-30 22:05:52 +0300

    готово

--- Урок 5. Практическое задание/task_1/task_1.py
+++ Урок 5. Практическое задание/task_1/task_1.py
@@ -3,3 +3,12 @@
 записать в него построчно данные, вводимые пользователем.
 Об окончании ввода данных свидетельствует пустая строка.
 """
+with open('xyz.txt', 'w') as f:
+    while True:
+        user_input = input('Enter the value or ENTER to exit: ')
+        if user_input:
+            f.write(user_input + '\n')
+        else:
+            break
+
+
