524f0e0020369a1f5d2e792226c546f773dfa761
Georgii <mga@nn.ru>
2022-06-01 22:56:26 +0300

    first_part_of_les

--- Урок 5. Практическое задание/task_4/task_4.py
+++ Урок 5. Практическое задание/task_4/task_4.py
@@ -7,3 +7,12 @@
 Иванов 23543.12
 Петров 13749.32
 """
+from functools import reduce
+
+with open('tex.txt', 'r', encoding='utf-8') as f:
+    di = {}
+    di = {x.split()[0] : float(x.split()[1]) for x in f.readlines()}
+    val = [i for i in di.values() if i > 20000]
+    fam = [k for k, v in di.items() if v in val]
+    print('\n'.join(fam), f'средняя величена дохода - {(reduce(lambda a,b : a + b, di.values()) / len(di.values()))}',
+          sep='\n\n')
