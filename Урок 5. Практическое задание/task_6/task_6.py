f46a7c562392a0871fe5ae5318de4b3ac25b7744
MIKHAIL KUZNETSOV <9162203636@mail.ru>
2022-06-02 18:23:41 +0300

    готово

--- Урок 5. Практическое задание/task_6/task_6.py
+++ Урок 5. Практическое задание/task_6/task_6.py
@@ -5,3 +5,23 @@
                                         Физкультура:   —   30(пр)   —
 Пример словаря: {“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}
 """
+file = open("file_task.txt")
+onstring = file.read().split("\n")[:-1]
+print(onstring)
+
+dict = {}
+
+for item in onstring:
+    key = item.split(" ")[0]
+    value = item.split(" ")[1:]
+    dict[key] = value
+print(dict)
+
+print("\n<< Общее количество занятий по предметам >>")
+for i in dict:
+    list = dict[i]
+    summ = 0
+    for j in range(0, len(list)):
+        summ += int(list[j])
+    print(i, ":", summ)
+file.close()
