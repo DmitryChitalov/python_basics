7a2163b31c2936ab926d64cbbc55c528ac6c4cb2
MIKHAIL KUZNETSOV <9162203636@mail.ru>
2022-06-03 19:20:10 +0300

    готово

--- Урок 5. Практическое задание/task_7/task_7.py
+++ Урок 5. Практическое задание/task_7/task_7.py
@@ -9,3 +9,31 @@
 [{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit": 2000}]
 Подсказка: использовать менеджер контекста.
 """
+import json
+profit = {}
+pr = {}
+prof = 0
+prof_aver = 0
+i = 0
+with open('file_7.txt', 'r') as file:
+    for line in file:
+        name, firm, earning, damage = line.split()
+        profit[name] = int(earning) - int(damage)
+        if profit.setdefault(name) >= 0:
+            prof = prof + profit.setdefault(name)
+            i += 1
+    if i != 0:
+        prof_aver = prof / i
+        print(f'Прибыль средняя - {prof_aver:.2f}')
+    else:
+        print(f'Прибыль средняя - отсутсвует. Все работают в убыток')
+    pr = {'средняя прибыль': round(prof_aver)}
+    profit.update(pr)
+    print(f'Прибыль каждой компании - {profit}')
+
+with open('file_7.json', 'w') as write_js:
+    json.dump(profit, write_js)
+
+    js_str = json.dumps(profit)
+    print(f'Создан файл с расширением json со следующим содержимым: \n '
+          f' {js_str}')
--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -15,3 +15,59 @@
 
 Проверить работу примера, создав экземпляр и вызвав описанный метод.
 """
+import time
+
+class TrafficLight():
+
+    # Определяем время ожидания сигнала светофора
+    red_color_wait = 7
+    yellow_color_wait = 2
+    green_color_wait = 4
+
+    # Определяем названия цветов сигналов светофора
+    red_color_name = 'красный'
+    yellow_color_name = 'желтый'
+    green_color_name = 'зеленый'
+
+    def __init__(self, color):
+        self.__color = color
+        print(f'\nСоздан новый объект TrafficLight со стартовым цветом {self.__color}')
+
+    def switch_light(self, color, wait_period):
+        self.wait_period = wait_period
+        print(f'Включен {color} свет, время ожидания {self.wait_period} сек')
+        time.sleep(self.wait_period)
+
+    def running(self, color = ''):
+
+        # Если при вызове метода цвет не указан, берем из родительского класса
+        # В противном случае стартуем с цвета, объявленного при вызове метода
+        if not color:
+            loc_color = self.__color
+        else:
+            loc_color = color
+
+        if loc_color == self.red_color_name:
+            self.switch_light('красный', self.red_color_wait)
+            self.switch_light('желтый', self.yellow_color_wait)
+            self.switch_light('зеленый', self.green_color_wait)
+        elif loc_color == self.yellow_color_name:
+            self.switch_light('желтый', self.yellow_color_wait)
+            self.switch_light('зеленый', self.green_color_wait)
+        else:
+            self.switch_light('зеленый', self.green_color_wait)
+
+        print('Цикл переключения цветов завершен')
+
+tl1 = TrafficLight('красный')
+tl1.running()
+
+tl2 = TrafficLight('желтый')
+tl2.running()
+
+tl3 = TrafficLight('зеленый')
+tl3.running()
+
+# Инициализируем класс красным цветом, а метод запускаем с желтого
+tl1 = TrafficLight('красный')
+tl1.running('желтый')
