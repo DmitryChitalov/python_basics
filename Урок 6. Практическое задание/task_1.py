673c8496bf1d64e001093251e38d86f967bb4376
RESHbnm <reshbnm19@gmail.com>
2022-06-05 15:23:12 +0300

    Выполнено задание 1

--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -15,3 +15,21 @@
 
 Проверить работу примера, создав экземпляр и вызвав описанный метод.
 """
+import time
+
+
+class TrafficLight:
+    __color = ''
+
+    def __init__(self):
+        self.color_and_duration = [('red', 7), ('yellow', 2), ('green', 5)]
+
+    def running(self):
+        for color, duration in self.color_and_duration:
+            self.__color = color
+            print(f'Цвет светофора: {self.__color}, продолжительность: {duration}')
+            time.sleep(duration)
+
+
+trafficlight = TrafficLight()
+trafficlight.running()
