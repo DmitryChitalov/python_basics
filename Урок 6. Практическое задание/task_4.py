ed9309c23cbadd37629a3803c714ab6ab5842768
RESHbnm <reshbnm19@gmail.com>
2022-06-05 15:24:31 +0300

    Выполнено задание 4

--- Урок 6. Практическое задание/task_4.py
+++ Урок 6. Практическое задание/task_4.py
@@ -20,3 +20,66 @@ speed, color, name, is_police (булево).
 Выполните доступ к атрибутам, выведите результат.
 Выполните вызов методов и также покажите результат.
 """
+
+
+class Car:
+    def __init__(self, speed: int, color: str, name: str, is_police: bool):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def go(self):
+        print(f'Car "{self.name}" went.')
+
+    def stop(self):
+        print(f'Car "{self.name}" stopped.')
+
+    def turn(self, direction: str):
+        print(f'Car "{self.name}" turned {direction}.')
+
+    def show_speed(self):
+        print(f'Speed of car "{self.name}" is {self.speed}.')
+
+
+class TownCar(Car):
+    def show_speed(self):
+        message = f'Speed of car "{self.name}" is {self.speed}.'
+        if self.speed > 60:
+            message += f' Attention! The speed of the car "{self.name}" is above 60 km.'
+        print(message)
+
+
+class WorkCar(Car):
+    def show_speed(self):
+        message = f'Speed of car "{self.name}" is {self.speed}.'
+        if self.speed > 40:
+            message += f' Attention! The speed of the car "{self.name}" is above 40 km.'
+        print(message)
+
+
+class PoliceCar(Car):
+    def __init__(self, speed: int, color: str, name: str, is_police: bool = True):
+        super().__init__(speed, color, name, is_police)
+
+
+class SportCar(Car):
+    pass
+
+
+car_1 = TownCar(speed=10, color='red', name='Toyota', is_police=False)
+car_1.show_speed()
+car_1.speed = 70
+car_1.show_speed()
+
+car_2 = PoliceCar(speed=100, color='blue', name='Lexus')
+print(car_2.is_police)
+car_2.show_speed()
+car_2.go()
+car_2.turn('right')
+car_2.stop()
+
+car_1 = WorkCar(speed=25, color='yellow', name='JBC', is_police=False)
+car_1.show_speed()
+car_1.speed = 45
+car_1.show_speed()
