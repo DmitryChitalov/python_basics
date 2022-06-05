cd7f54330d501b1e16b1ec82e6e832145124dd81
RESHbnm <reshbnm19@gmail.com>
2022-06-05 15:23:56 +0300

    Выполнено задание 2

--- Урок 6. Практическое задание/task_2.py
+++ Урок 6. Практическое задание/task_2.py
@@ -16,3 +16,23 @@ length (длина в метрах), width (ширина в метрах).
 
 Например: 20м*5000м*25кг*0.05м = 125000 кг = 125 т
 """
+
+
+class Road:
+    unit_mass = 25
+    thickness = 0.05
+
+    def __init__(self, length, width):
+        self._lenght = length
+        self._width = width
+
+    def calculation_mass_asphalt(self):
+        return self._lenght * self._width * self.unit_mass * self.thickness
+
+
+road_1 = Road(5000, 20)
+road_2 = Road(3500, 10)
+
+Road.unit_mass = 75
+print(road_1.calculation_mass_asphalt())
+print(road_2.calculation_mass_asphalt())
