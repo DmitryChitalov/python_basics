2aa037422e378f5359e3165d82c46b2ab48cb0d3
SergeyAtman <nikonov@ellara.ru>
2022-06-06 07:25:25 +0300

    16
    16

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -15,3 +15,30 @@
 П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку __str__
 __str__(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
 """
+
+
+class Worker:
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {'wages': wage, 'bonus': bonus}
+
+
+class Position(Worker):
+    def __init__(self, name, surname, position, wage, bonus):
+        super().__init__(name, surname, position, wage, bonus)
+
+    def get_full_name(self):
+        return f'{self.name} {self.surname}'
+
+    def get_total_income(self):
+        return self._income['wages'] + self._income['bonus']
+
+    def __str__(self):
+        return f'Сотрудник: {self.name} {self.surname}, доход: {self.get_total_income()}'
+
+
+p_obj = Position('Ivan', 'Ivanov', 'manager', 500, 100)
+print(p_obj.get_full_name(), p_obj.get_total_income())
+print(p_obj)
--- Урок 6. Практическое задание/task_4.py
+++ Урок 6. Практическое задание/task_4.py
@@ -20,3 +20,91 @@ speed, color, name, is_police (булево).
 Выполните доступ к атрибутам, выведите результат.
 Выполните вызов методов и также покажите результат.
 """
+
+
+class Car:
+
+    def __init__(self, speed, color, name, is_police):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    # methods
+    def go(self):
+        return f'{self.name} поехала'
+
+    def stop(self):
+        return f'{self.name} остановилась'
+
+    def turn_right(self):
+        return f'{self.name} повернула на право'
+
+    def turn_left(self):
+        return f'{self.name} повернула на лево'
+
+    def show_speed(self):
+        return f'Текущая скорость автомобиля {self.name} равна {self.speed}'
+
+
+class TownCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def show_speed(self):
+        print(f'Текущая скорость городского автомобиля {self.name} равна {self.speed}')
+
+        if self.speed > 40:
+            return f'Скорость автомобиля {self.name} выше нормы для городского автомобиля'
+        else:
+            return f'Скорость автомобиля {self.name} нормальна для городского автомобиля'
+
+
+class SportCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def sport(self):
+        if self.is_police:
+            return f'Автомобиль {self.name} является спортивным'
+        else:
+            return f'Автомобиль {self.name} не является спортивным'
+
+
+class WorkCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def show_speed(self):
+        print(f'Текущая скорость рабочего автомобиля {self.name} равна {self.speed}')
+
+        if self.speed > 60:
+            return f'Скорость автомобиля {self.name} выше нормы для рабочего автомобиля'
+        else:
+            return f'Скорость автомобиля {self.name} нормальна для рабочего автомобиля'
+
+
+class PoliceCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def police(self):
+        if self.is_police:
+            return f'Автомобиль {self.name} является полицейским'
+        else:
+            return f'Автомобиль {self.name} не является полицейским'
+
+
+audi = SportCar(100, 'Красная', 'Audi', False)
+oka = TownCar(30, 'Белая', 'Ока', False)
+lada = WorkCar(70, 'Розовая', 'Лада', True)
+ford = PoliceCar(110, 'Синяя', 'Ford', True)
+
+print(f'{lada.color} {lada.go()}')
+print(f'{lada.turn_right()}')
+print(f'{lada.show_speed()}')
+print(audi.show_speed())
+print(oka.show_speed())
+print(oka.turn_left())
+print(ford.police())
+print(ford.show_speed())
