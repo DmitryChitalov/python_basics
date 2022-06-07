d53d52d70b13680d985dfcb897c77c85158ec654
Ksenia Sevostianova <info.sevostianova@gmail.com>
2022-06-07 20:06:49 +0300

    Выполнение 6 практического задания

--- Урок 6. Практическое задание/task_4.py
+++ Урок 6. Практическое задание/task_4.py
@@ -20,3 +20,68 @@ speed, color, name, is_police (булево).
 Выполните доступ к атрибутам, выведите результат.
 Выполните вызов методов и также покажите результат.
 """
+class Car:
+    def __init__(self, speed, color, name, is_police=False):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def go(self):
+        return print(f"{self.color} машина {self.name} поехала!")
+
+    def stop(self):
+        return print(f"Машина {self.name} остановилась!")
+
+    def turn(self):
+        turn = input("Куда вы хотите повернуть? ('left' или 'right'):")
+        if turn == "left":
+            print(f"Машина {self.name} поворачивает налево")
+        elif turn == "right":
+            print(f"Машина {self.name} поворачивает направо")
+        else:
+            print(f"Машина {self.name} упала в яму!")
+
+    def police(self):
+        if self.is_police == True:
+            print("Это полицейская машина!")
+
+    def show_speed(self):
+        return print(f"Вы едите со скоростью {self.speed} км/ч.")
+
+
+class TownCar(Car):
+    def show_speed(self):
+        if self.speed > 60:
+            return print(f"Вы едите слишком быстро {self.speed} км/ч. , притормозите. Ограничение 60 км/ч!")
+        else:
+            print("Вы едите с допустимой скоростью.")
+
+
+class WorkCar(Car):
+    def show_speed(self):
+        if self.speed > 40:
+            return print(f"Вы едите слишком быстро {self.speed} км/ч. , притормозите. Ограничение 40 км/ч!")
+        else:
+            print("Вы едите с допустимой скоростью.")
+
+
+class SportCar(Car):
+    pass
+
+
+class PoliceCar(Car):
+    pass
+
+
+tc = TownCar(70, "Голубая", "BMW", False)
+tc.go(), tc.police(), tc.show_speed(), tc.turn(), tc.stop()
+
+wk = WorkCar(50, "Зеленая", "Opel", False)
+wk.go(), wk.police(), wk.show_speed(), wk.turn(), wk.stop()
+
+sc = SportCar(200, "Красная", "Lamborghini", False)
+sc.go(), sc.police(), sc.show_speed(), sc.turn(), sc.stop()
+
+pc = PoliceCar(120, "Черная", "Mercedes-Benz", True)
+pc.go(), pc.police(), pc.show_speed(), pc.turn(), pc.stop()
