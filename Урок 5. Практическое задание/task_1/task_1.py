a0b65b746025647d560a19677011984ba49b47d0
Kamakar <privratnikovanton@gmail.com>
2022-06-07 22:27:24 +0300

    ls5

--- Урок 5. Практическое задание/task_1/task_1.py
+++ Урок 5. Практическое задание/task_1/task_1.py
@@ -3,3 +3,18 @@
 записать в него построчно данные, вводимые пользователем.
 Об окончании ввода данных свидетельствует пустая строка.
 """
+f_file = open('my_text.txt','w')
+line = input('Enter' + '\n')
+while True:
+    f_file.writelines(line + '\n' )
+    line = input('Enter' + '\n')
+    if not line:
+        break
+
+
+f_file.close()
+with open('my_text.txt', 'r') as file:
+    content = file.read()
+    print(content)
+
+
