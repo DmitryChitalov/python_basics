009cb4d201a02ad152c1dfc8180d0156b526f268
Nick <neekeech@gmail.com>
2022-06-09 00:25:13 +0300

    Домашнее задание к 6 уроку

--- Урок 6. Практическое задание/task_4.py
+++ Урок 6. Практическое задание/task_4.py
@@ -20,3 +20,54 @@ speed, color, name, is_police (булево).
 Выполните доступ к атрибутам, выведите результат.
 Выполните вызов методов и также покажите результат.
 """
+class Car():
+    def __init__(self, speed, color, name):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = False
+    def show_speed(self):
+        print(f'Скорость: {self.speed}')
+    def go(self):
+        print('Go...')
+    def stop(self):
+        print('Stop...')
+    def turn(self, side):
+        print(f'Turn {side}')
+
+class SportCar(Car):
+    def show_speed(self):
+        return print('RB POOOWEEER!')
+
+class TownCar(Car):
+    def show_speed(self):
+        if float(self.speed) > 60:
+            return print('Превышение скорости')
+        else:
+            return print(f'Скорость - {self.speed}')
+class WorkCar(Car):
+    def show_speed(self):
+        if float(self.speed) > 40:
+            return print('Превышение скорости')
+        else:
+            return print(f'Скорость - {self.speed}')
+class PoliceCar(Car):
+    def __init__(self, speed, color, name):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = True
+
+sport_car = SportCar(100, 'White', 'Skyline R34')
+sport_car.show_speed()
+
+town_car = TownCar(70, 'Gray', 'Toyota BB')
+town_car.show_speed()
+
+work_car = WorkCar(50, 'Black', 'Lada')
+work_car.turn('right')
+
+police_car = PoliceCar(100, 'White/Blue', 'Skoda')
+police_car.stop()
+
+print(police_car.is_police)
--- Урок 6. Практическое задание/task_5.py
+++ Урок 6. Практическое задание/task_5.py
@@ -13,3 +13,34 @@
 Для каждого из классов метод должен выводить уникальное сообщение.
 Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
 """
+
+class Stationery():
+    def __init__(self):
+        self.title = self.__str__().split()[0][10:]
+    
+    def draw(self):
+        return print('Draw smth')
+
+class Pen(Stationery):
+    def draw(self):
+        return print('Blue ink line')
+
+class Pencil(Stationery):
+    def draw(self):
+        return print('Gray line')
+
+class Handle(Stationery):
+    def draw(self):
+        return print('Thick line')
+
+pen = Pen()
+print('\n', pen.title)
+pen.draw()
+
+pencil = Pencil()
+print('\n', pencil.title)
+pencil.draw()
+
+handle = Handle()
+print('\n', handle.title)
+handle.draw()
