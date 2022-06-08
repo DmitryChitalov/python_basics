c98c50a289187bcac8f4c13d21518413e15e4371
MIKHAIL KUZNETSOV <9162203636@mail.ru>
2022-06-08 13:43:11 +0300

    готово

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -28,3 +28,13 @@
 8 10 12
 14 16 18
 """
+class Matrix:
+    def __init__(self, my_list):
+        self.my_list = my_list
+
+    def __str__(self):
+        for row in self.my_list:
+            for i in row:
+                print(f"{i:4}", end="")
+            print()
+        return ''
