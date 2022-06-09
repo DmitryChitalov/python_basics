8a91d80ccb0e284c699fffd57c7a08abb874836d
Nana <vayfiel@gmail.com>
2022-06-09 16:57:12 +0300

    lesson_7

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -25,3 +25,61 @@
 
 Два класса: абстрактный и Clothes
 """
+from abc import abstractmethod
+
+
+class Clothes:
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @abstractmethod
+    def cloth_out(self):
+        pass
+
+
+class Coat(Clothes):
+
+    def __init__(self, size):
+        super(Coat, self).__init__(size)
+
+    def cloth_out(self):
+        return self.size / 6.5 + 0.5
+
+
+class Suit(Clothes):
+
+    def __init__(self, size):
+        super(Suit, self).__init__(size)
+
+    def cloth_out(self):
+        return self.size * 2 + 0.3
+
+    @staticmethod
+    def __define_size_by_value(val):
+        return (val - 0.3) // 2
+
+    @property
+    def size_out(self):
+        return self.size
+
+    @size_out.setter
+    def size_out(self, val):
+        tmp_size = int(self.__define_size_by_value(val))
+        print(f'This is enough for the {tmp_size}-th size.')
+        if tmp_size < 1:
+            print('The impossibility of using such an amount of tissue.')
+        else:
+            print('Use.')
+            self.size = tmp_size
+
+
+if __name__ == '__main__':
+    c = Coat(30)
+    print(c.cloth_out())
+
+    s = Suit(14)
+    print(s.cloth_out())
+    print(s.size_out)
+    s.size_out = 42
+    print(s.cloth_out())
