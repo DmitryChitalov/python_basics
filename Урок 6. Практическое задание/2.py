33c033031780487139e0fd5821f299d56d99bf4b
skvorcovv <49515959+skvorcovv@users.noreply.github.com>
2022-06-09 23:06:18 +0700

    Update and rename task_2.py to 2.py

--- Урок 6. Практическое задание/2.py
+++ Урок 6. Практическое задание/2.py
@@ -16,3 +16,18 @@ length (длина в метрах), width (ширина в метрах).
 
 Например: 20м*5000м*25кг*0.05м = 125000 кг = 125 т
 """
+class Road:
+    weight = 25
+    thickness = 0.05
+
+    def __init__(self, length, width):
+        self._length = length
+        self._width = width
+
+    def calc(self):
+        res = (self._length * self._width * Road.weight * Road.thickness) / 1000
+        print(f'Потребуется {res} тонн.')
+
+
+r_obj = Road(20, 5000)
+r_obj.calc()
