920506205b64218356190077dd827a8058d942df
Litva63 <ghostkilla98@mail.ru>
2022-06-12 18:50:07 +0300

    ДЗ_Урок_7

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -25,3 +25,40 @@
 
 Два класса: абстрактный и Clothes
 """
+
+from abc import ABC, abstractmethod
+
+
+class AbsClassClothes(ABC):
+    @abstractmethod
+    def get_material_rate(self):
+        pass
+
+
+class Clothes(AbsClassClothes):
+    name: str
+    size: float
+
+    def __init__(self, my_name, my_size):
+        self.name = my_name
+        self.size = my_size
+
+    @property
+    def get_material_rate(self):
+        material_rate = 0.0
+        if self.name == "Пальто":
+            material_rate = self.size / 6.5 + 0.5
+        elif self.name == "Костюм":
+            material_rate = self.size * 2 + 0.3
+        else:
+            material_rate = 0
+
+        return material_rate
+
+
+costume = Clothes("Костюм", 10)
+palto = Clothes("Пальто", 10)
+
+print(f"Расход ткани на пальто = {round(palto.get_material_rate, 2)}" )
+print(f"Расход ткани на костюм = {round(costume.get_material_rate, 2)}")
+print(f"Общий расход ткани = {round(palto.get_material_rate, 2) + round(costume.get_material_rate, 2)}")
