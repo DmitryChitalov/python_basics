c81f1204e9ef03ef511c72184c67e8fa992e1e78
Zhanna Nizhegorodova <nizhegorodova.zhanna@yandex.ru>
2022-06-13 14:00:01 +0300

    Выполнены задания к уроку 8

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,21 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+
+
+class DivideByZero(Exception):
+    pass
+
+
+try:
+    number_1 = int(input('Введите делимое число: '))
+    number_2 = int(input('Введите число делитель: '))
+    if number_2 == 0:
+        raise DivideByZero('Вы пытаетесь разделить на ноль!')
+    result = number_1 / number_2
+except ValueError:
+    print('Вы ввели не число')
+except DivideByZero as err:
+    print(err)
+else:
+    print(f'{number_1} / {number_2} = {round(result, 2)}')
