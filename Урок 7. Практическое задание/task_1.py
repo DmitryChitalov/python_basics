2098693d13e045f9a0f6898d554bb2fadcb18b2d
Kamakar <privratnikovanton@gmail.com>
2022-06-14 21:50:04 +0300

    ls7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -31,30 +31,25 @@
 
 
 class Matrix:
-    def __init__(self, list_1, list_2):
-        self.list_1 = list_1
-        self.list_2 = list_2
-
-    def __add__(self):
-        matr = [[0, 0, 0],
-                  [0, 0, 0],
-                  [0, 0, 0]]
-        for i in range(len(self.list_1)):
-
-            for j in range(len(self.list_2[i])):
-                matr[i][j] = self.list_1[i][j] + self.list_2[i][j]
-
-        return str('\n'.join(['\t'.join([str(j) for j in i]) for i in matr]))
-
+    def __init__(self, *args):
+        self.nw_list = list(args)
     def __str__(self):
-        return str('\n'.join(['\t'.join([str(j) for j in i]) for i in matr]))
-my_matrix = Matrix([[5, 18, 11],
-                     [6, 17, 23],
-                     [41, 50, 9]],
-                    [[45, 8, 2],
-                     [6, 7, 93],
-                     [24, 5, 97]])
+        result = '\n'.join(map(str, self.nw_list))
+        result = result.replace(',', '').replace('[', '').replace(']', '')
+        return result
+    def __add__(self, other):
+        new_sum = []
+        other_sum = []
+        for i in range(len(self.nw_list)):
+            for j in range(len(self.nw_list[i])):
+                other_sum.append((self.nw_list[i][j] + other.nw_list[i][j]))
+            new_sum.append(other_sum)
+            other_sum = []
+        return Matrix('\n'.join((map(str, new_sum))))
 
 
+a = Matrix([1,2,4], [1,5,6], [5, 6, 7])
+b = Matrix([1,3,4], [5,6,6], [7, 7, 7])
 
-print(my_matrix.__add__())
+print(a + b)
+
