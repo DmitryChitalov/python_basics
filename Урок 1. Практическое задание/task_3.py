bfcfcbc7e74aa35bbbb6f5289d0115b5c461d626
Aleksandr Sh <aleksandr_shel@inbox.ru>
2022-06-14 20:41:50 +0300

    task_3/4/5

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,10 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+n = int(input("Введите число n: "))
+d1 = str(n)
+d2 = d1 + d1
+d3 = d1 + d1 + d1
+comp = n + int(d2) + int(d3)
+print(f"n + nn + nnn = {comp}")
+print(f"{d1} + {d2} + {d3} = {comp}")  # test
