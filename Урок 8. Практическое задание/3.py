00741f2e72034999255f46294b0fadb4ede630f0
skvorcovv <49515959+skvorcovv@users.noreply.github.com>
2022-06-15 20:16:34 +0700

    Update and rename task_3.py to 3.py

--- Урок 8. Практическое задание/3.py
+++ Урок 8. Практическое задание/3.py
@@ -9,3 +9,22 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+class MyOwnExc(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
+num_lst = []
+while True:
+    try:
+        elem = input("Введите число или stop для завершения: ")
+        if elem == 'stop':
+            break
+        elif elem.isdigit():
+            num_lst.append(int(elem))
+        else:
+            raise MyOwnExc("Вы ввели не число")
+    except MyOwnExc as e:
+        print(e.txt)
+
+print(num_lst)
