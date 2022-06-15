dde438f7554519e7440b0b7875fe119ae38c917b
skvorcovv <49515959+skvorcovv@users.noreply.github.com>
2022-06-15 20:16:05 +0700

    Update and rename task_2.py to 2.py

--- Урок 8. Практическое задание/2.py
+++ Урок 8. Практическое задание/2.py
@@ -5,3 +5,18 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+class DivisionByNull(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
+def divide_by_null(divider, denominator):
+    try:
+        if denominator == 0:
+            raise DivisionByNull("Деление на ноль недопустимо")
+        print(divider / denominator)
+    except DivisionByNull as err:
+        print(err)
+
+
+divide_by_null(100, 0)
