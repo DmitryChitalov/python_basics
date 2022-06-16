9edad61c87289eff6deb3f824e2be8d7ed67abcd
vetalvet13 <vetalvet@mail.ru>
2022-06-16 17:33:06 +0300

    готово

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -10,3 +10,12 @@
 Ведите целое положительное число: 123456789
 Самая большая цифра в числе: 9
 """
+number = int(input("Введите число - "))
+value = 0
+
+while (number):
+    if (number % 10 > value):
+        value = number % 10
+    number //= 10
+
+print(f"Наибольшее число {value}")
