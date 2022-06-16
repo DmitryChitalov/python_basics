060eff18223bc2dc0894ff38cc9571721da96338
Georgii <mga@nn.ru>
2022-06-16 22:28:17 +0300

    lesson7_done

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -100,3 +100,54 @@ print(cell2.make_order(10))
 *****\n *****\n *****\n *****\n *****\n *****\n
 **********\n **********\n *****
 """
+
+class Cell:
+    def __init__(self, quan):
+        self.quantity = quan
+
+
+    def __add__(self, other):
+        return f'sum of cells is - {self.quantity + other.quantity}'
+
+    def __sub__(self, other):
+        if self.quantity > other.quantity:
+            return f'subtraction of cells is - {self.quantity - other.quantity}'
+        else:
+            return f'error, try with other numbers'
+
+    def __mul__(self, other):
+        return f'multiplication of cells is - {self.quantity * other.quantity}'
+
+    def __truediv__(self, other):
+        return f'division of cells is - {self.quantity // other.quantity}'
+
+
+cell1 = Cell(30)
+cell2 = Cell(25)
+
+cell3 = Cell(10)
+cell4 = Cell(15)
+
+print()
+
+print("Складываем")
+print(cell1 + cell2)
+
+print()
+
+print("Вычитаем")
+print(cell2 - cell1)
+print(cell4 - cell3)
+
+print()
+
+print("Умножаем")
+print(cell2 * cell1)
+
+print()
+
+print("Делим")
+print(cell1 / cell2)
+
+print()
+
