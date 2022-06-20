817e4b5486cd1233143aac1a9324ef8ccb033d0d
Yura.A.Mamontov <zzbike@gmail.com>
2022-06-20 15:47:00 +0800

    добавил решения к 1уроку

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,7 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+n = int(input('Введите число n: '))
+nn = int(str(n) + str(n))
+nnn = int(str(n) + str(n) + str(n))
+print(f'n + nn + nnn = {n} + {nn} + {nnn} = {n + nn + nnn}')
