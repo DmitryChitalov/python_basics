b0bc9ec1bfab1ec11f2bea867701078c3a7c0c14
a.arshinnikov <a.arshinnikov@fintech.ru>
2022-06-23 16:13:53 +0300

    add homework for lesson 2

--- Урок 2. Практическое задание/task_3.py
+++ Урок 2. Практическое задание/task_3.py
@@ -8,3 +8,30 @@
 Результат через список: Осень
 Результат через словарь: Осень
 """
+
+count = input('Enter month number')
+season_dict = {"1": "winter",
+               "2": "winter",
+               "3": "spring",
+               "4": "spring",
+               "5": 'spring',
+               "6": "summer",
+               "7": "summer",
+               "8": "summer",
+               "9": "autumn",
+               "10": "autumn",
+               "11": "autumn",
+               "12": "winter"}
+
+if (int(count) > 0) and (int(count) < 13):
+    print(f'With dict method: {count} month is {season_dict[count]}')
+    if count in ["12", "1", "2"]:
+        print(f'With list method:{count} month is winter')
+    elif count in ["3", "4", "5"]:
+        print(f'With list method:{count} month is spring')
+    elif count in ["6", "7", "8"]:
+        print(f'In list method:{count} month is summer')
+    else:
+        print(f'With list method:{count} month is autumn')
+else:
+    print('mistake')
