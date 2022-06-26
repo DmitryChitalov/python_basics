a3b76911b68f5ec8853381dcfa6f3ace90a8a032
Andrii Maslovskyi <maslovskyi@duck.com>
2022-06-26 12:04:11 +0300

    Добавил правки по заданию 4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -10,6 +10,6 @@ import sys
 
 _, work_hours, hour_cost, bonus = sys.argv
 
-salary = (float(hour_cost) * float(work_hours)) + float(bonus)
+salary = float(hour_cost) * float(work_hours) + float(bonus)
 
 print(f"Зар. плата = {salary}")
