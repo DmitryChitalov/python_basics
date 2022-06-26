b3b3e6ff637782863c0b9cd564eaf5cb0521b34b
Andrii Maslovskyi <maslovskyi@duck.com>
2022-06-26 10:33:06 +0300

    Lesson 4

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -7,3 +7,10 @@
 
 Реализуйте вариант без и с генераторным выражением
 """
+numbers = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+
+result_list = [
+    val for idx, val in enumerate(numbers)
+]
+
+print(result_list)
