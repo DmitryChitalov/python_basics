d0daba84230c01763daef9336bc3564a71bc1713
Golubiatnikov Aleksei <aleks.golubyatnikov@gmail.com>
2022-07-08 10:45:49 +0300

    lesson_6

--- Урок 6. Практическое задание/task_2.py
+++ Урок 6. Практическое задание/task_2.py
@@ -16,3 +16,26 @@ length (длина в метрах), width (ширина в метрах).
 
 Например: 20м*5000м*25кг*0.05м = 125000 кг = 125 т
 """
+
+
+class Road:
+    weight_cm = 1
+    height = 1
+
+    def __init__(self, length, width):
+        self.__length = length
+        self.__width = width
+
+    def weight_calculation(self):
+        try:
+            weight = int(self.__length * self.__width * self.height * self.weight_cm)
+            print(f"Масса: {weight}кг ({weight / 1000})т")
+        except BaseException as e:
+            print(f"General error: {e}")
+
+
+my_road = Road(20, 5000)
+my_road.weight_cm = 25
+my_road.height = 0.05
+
+my_road.weight_calculation()
--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -15,3 +15,40 @@
 П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку __str__
 __str__(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
 """
+
+
+class Worker:
+    def __init__(self, name, surname, position, income):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = income
+
+
+class Position(Worker):
+    def __init__(self, name, surname, position, income):
+        super().__init__(name, surname, position, income)
+
+    def get_full_name(self):
+        print(f"{self.surname} {self.name}")
+
+    def get_total_income(self):
+        print(f"{float(self._income['wage'] + self._income['bonus'])}")
+
+    def __str__(self):
+        print(f"{type(self).__name__}(name={self.name},"
+              f" surname={self.surname},"
+              f" position={self.position},"
+              f" income={self._income})")
+
+
+my_position = Position("Алексей", "Голубятников", "Инженер", {"wage": 300.00, "bonus": 120.53})
+print(my_position.name)
+print(my_position.surname)
+print(my_position.position)
+
+my_position.get_full_name()
+my_position.get_total_income()
+
+my_position.__str__()
+
