0f28677b8e78a39af0742769606aa206e35d93fd
neokortex8 <neokortex8@gmail.com>
2022-07-09 12:46:26 +0300

    Выполнил третье задание шестого урока.

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -15,3 +15,26 @@
 П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку __str__
 __str__(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
 """
+
+class Worker:
+
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self.wage = wage
+        self.bonus = bonus
+
+class Position(Worker):
+    def __init__(self, name, surname, position, wage, bonus):
+        super().__init__(name, surname, position, wage, bonus)
+
+    def get_full_name(self):
+        return self.name + ' ' + self.surname
+
+    def get_total_income(self):
+        self.__income = {'оклад': self.wage, 'премия': self.bonus}
+        return self.__income
+
+manager = Position('Иван', 'Петров', 'кладовщик', 35000, 25000)
+print(manager.get_full_name(), manager.get_total_income())
