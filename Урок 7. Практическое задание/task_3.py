6b35545c34a624d7ff24f8a7a1c9dc62df7dba45
diez <info@diez87.ru>
2022-07-11 22:49:11 +1200

    Домашнее задание к уроку № 7

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -100,3 +100,39 @@ print(cell2.make_order(10))
 *****\n *****\n *****\n *****\n *****\n *****\n
 **********\n **********\n *****
 """
+
+
+class Cell():
+
+    def __init__(self, cell_count):
+        self.cell_count = cell_count
+
+    def __add__(self, other):
+        return self.cell_count + other.cell_count
+
+    def __sub__(self, other):
+        ret_val = self.cell_count - other.cell_count
+        return ret_val if ret_val > 0 else 'error'
+
+    def __mul__(self, other):
+        return self.cell_count * other.cell_count
+
+    def __truediv__(self, other):
+        return self.cell_count // other.cell_count
+
+    def make_order(self, row_cells):
+        self.row_cells = row_cells
+
+        return ('*' * self.row_cells + '\n') * (self.cell_count // self.row_cells) + '*' * (
+                    self.cell_count % self.row_cells)
+
+
+c1 = Cell(100)
+c2 = Cell(6)
+
+print(c1 + c2)
+print(c1 - c2)
+print(c1 * c2)
+print(c1 / c2)
+
+print(c1.make_order(15))
