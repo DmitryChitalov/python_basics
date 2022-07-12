a2c0ab0f4237f7d033cd16d3679defce8daff55d
vetalvet13 <vetalvet@mail.ru>
2022-07-12 18:51:56 +0300

    урок 8

--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -4,8 +4,35 @@
 Создайте собственный класс-исключение,
 который должен проверять содержимое списка на наличие только чисел.
 
-Проверить работу исключения на реальном примере. Необходимо запрашивать у пользователя данные и заполнять
+Проверить работу исключения на реальном примере. Необходимо запрашивать
+у пользователя данные и заполнять
 список только числами.
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+
+
+class NonNumber(Exception):
+    """info"""
+
+
+def append_number(number_list: list):
+    """info"""
+    value = input("Введите число >>> ")
+
+    try:
+        number_list.append(float(value))
+    except ValueError as error:
+        raise NonNumber(f"Вы ввели '{value}' вместо числа") from error
+
+
+numbers = []
+
+while True:
+    try:
+        append_number(numbers)
+    except NonNumber as exception:
+        print(exception)
+    except KeyboardInterrupt:
+        print(f"\nСписок чисел = {numbers}")
+        break
