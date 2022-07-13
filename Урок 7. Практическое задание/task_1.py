f9cab439580a322c1aa4389a86a3034ccd37fd61
Golubiatnikov Aleksei <aleks.golubyatnikov@gmail.com>
2022-07-13 18:02:36 +0300

    lesson_7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -28,3 +28,41 @@
 8 10 12
 14 16 18
 """
+
+
+class Matrix:
+    def __init__(self, arg):
+        self.arg = arg
+
+    def __str__(self):
+        try:
+            __matrix = ""
+
+            for el in self.arg:
+                __matrix += ' '.join([str(e) for e in el]) + '\n'
+            return f"{__matrix}"
+        except BaseException as e:
+            print(f"General error: {e}")
+
+    def __add__(self, other):
+        try:
+            out_list = []
+            for i, el in enumerate(self.arg, 0):
+                int_list = []
+                for j, e in enumerate(el, 0):
+                    int_list.append(int(e + other.arg[i][j]))
+                out_list.append(int_list)
+
+            return f"Сумма матриц: \n{out_list}"
+
+        except BaseException as e:
+            print(f"General error: {e}")
+
+
+matrix_1 = Matrix([[1, 2], [3, 4], [5, 6]])
+matrix_2 = Matrix([[7, 8], [9, 10], [11, 12]])
+
+print(matrix_1)
+print(matrix_2)
+
+print(matrix_1 + matrix_2)
