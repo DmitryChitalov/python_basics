17649effd5d7a344f9b970f40102cf3c24785c93
alexey.denisov <alexey.denisov@corp.mail.ru>
2022-07-19 08:29:45 +0300

    lesson-8-homework

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,29 @@
 Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца
 и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
 """
+
+from datetime import datetime
+
+class Date:
+
+    def __init__(self, date):
+        self.date = str(date)
+
+    @classmethod
+    def extract(cls, date):
+        day, month, year = [int(el) for el in date.split(".")]
+        return f'Число - {day}, месяц - {month}, год - {year}'
+
+    @staticmethod
+    def validate(day_month_year):
+        try:
+            datetime.strptime(day_month_year, '%d.%m.%Y')
+            return "Дата верна"
+        except ValueError:
+            return "Дата не корректна!"
+
+
+print(Date.validate('21.07.2022'))
+print(Date.validate('32.07.2021'))
+print(Date.validate('01.08.21'))
+print(Date.extract('21.07.2022'))
--- Урок 8. Практическое задание/task_4_5_6.py
+++ Урок 8. Практическое задание/task_4_5_6.py
@@ -19,3 +19,113 @@
 Подсказка: постарайтесь по возможности реализовать в проекте
 «Склад оргтехники» максимум возможностей, изученных на уроках по ООП.
 """
+
+
+from abc import ABC, abstractmethod
+
+
+class Warehouse:
+    __max_count: int
+    _items: list
+    _transferred: dict
+    _departments = ['ИТ', 'АХО', 'Продажи', 'Бухгалтерия']
+
+    def __init__(self, max_count):
+        self.items = []
+        self.max_count = max_count
+        self._transferred = {}
+
+    def __validate(self, item):
+        if not isinstance(item, OfficeEquipment):
+            raise ValueError(f"Это не офисная техника: {type(item)}")
+        elif len(self.items) >= self.max_count:
+            raise ValueError('Склад переполнен!')
+
+    def add_items(self, items):
+        for item in items:
+            self.__validate(item)
+            self.items.append(item)
+            print(f'Оборудование {item.name} принято на склад.'
+                  f'Количество: {len(self.items)}')
+
+    def add_item(self, item):
+        self.__validate(item)
+        self.items.append(item)
+        print(f'Оборудование {item.name} принято на склад.'
+              f'Количество: {len(self.items)}')
+
+    def transfer_item_to_department(self, item, department):
+        if item not in self.items:
+            print('Такого оборудования нет на складе!')
+            return
+        if department not in self._transferred:
+            self._transferred[department] = []
+        self._transferred[department].append(item)
+        print(f'Оборудование {item.name} перемещено в отдел '
+              f'{self._departments[department]}.')
+        self.items.remove(item)
+
+
+class OfficeEquipment(ABC):
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+    @abstractmethod
+    def pass_to_output(self, number_of_lists):
+        pass
+
+    @staticmethod
+    def create_equipment():
+        try:
+            unit = input('Введите наименование: ')
+            unit_p = int(input('Введите стоимость: '))
+            type = int(input('Выберите тип'
+                             '(0 - принтер, 1 - сканер, 2 - МФУ): '))
+        except ValueError as err:
+            print(f'Некорректный ввод! f"Ошибка {err=}, {type(err)=}')
+            return
+
+        if type == 0:
+            return Printer(unit, unit_p)
+        if type == 1:
+            return Scanner(unit, unit_p)
+        if type == 2:
+            return Copier(unit, unit_p)
+
+    def __str__(self):
+        return f'Наименование: {self.name}; стоимость: {self.price}'
+
+
+class Printer(OfficeEquipment):
+    def pass_to_output(self, number_of_lists):
+        return f'для печати {number_of_lists} листов'
+
+
+class Scanner(OfficeEquipment):
+    def pass_to_output(self, number_of_lists):
+        return f'для сканирования {number_of_lists} листов'
+
+
+class Copier(OfficeEquipment):
+    def pass_to_output(self, number_of_lists):
+        return f'для копирования {number_of_lists} листов'
+
+
+unit1 = Printer('hp', 2000)
+unit2 = Scanner('Canon', 1200)
+unit3 = Copier('Xerox', 1500)
+warehouse = Warehouse(2)
+try:
+    warehouse.add_item(unit1)
+    warehouse.add_item(unit2)
+    warehouse.transfer_item_to_department(unit2, 0)
+    warehouse.transfer_item_to_department(unit3, 1)
+    warehouse.add_item(unit3)
+except BaseException as error:
+    print(f"Unexpected {error=}, {type(error)=}")
+
+unit4 = OfficeEquipment.create_equipment()
+warehouse.transfer_item_to_department(unit3, 2)
+warehouse.add_item(unit4)
+warehouse.transfer_item_to_department(unit4, 3)
