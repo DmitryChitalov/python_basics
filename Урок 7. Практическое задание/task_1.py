ecd49c8ac19fe5fbb6c1cd2f3670b8dcdcb4e3fa
Andrey <pryadko.av@yandex.ru>
2022-07-28 23:38:08 +0300

    lesson_7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -28,3 +28,31 @@
 8 10 12
 14 16 18
 """
+
+class Matrix:
+    def __init__(self, m_list):
+        self.m_list = m_list
+
+    def __str__(self):
+        for row in self.m_list:
+            for i in row:
+                print(f"{i:4}", end="")
+            print()
+        return ''
+
+    def __str__(self):
+        return '\n'.join(map(str, self.m_list))
+
+    def __str__(self):
+        return str('\n'.join(['\t'.join([str(el) for el in i]) for i in self.m_list]))
+
+    def __add__(self, other):
+        for i in range(len(self.m_list)):
+            for i_2 in range(len(other.m_list[i])):
+                self.m_list[i][i_2] = self.m_list[i][i_2] + other.m_list[i][i_2]
+        return Matrix.__str__(self)
+
+
+m = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+m_new = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(f"Сумма матрицы\n{m.__add__(m_new)}")
