ec8c101993fb3d7916bb87936a17d96e62cb6552
Ivan Saykin <saikin_89@mail.ru>
2022-08-02 15:27:25 +0300

    lesson_1 done

--- Урок 1. Практическое задание/task_1.py
+++ Урок 1. Практическое задание/task_1.py
@@ -11,3 +11,19 @@
 Введите ваш возраст: 45
 Ваши данные для входа в аккаунт: имя - Василий, пароль - vas, возраст - 45
 """
+
+var_int = 1
+var_float = 2.8
+var_str = "hello world"
+var_bool = True
+var_tuple = (1, 2, 3)
+var_list = [1, "string", False]
+var_dict = {"one": 1, "two": 2}
+
+print(var_int, var_float, var_str, var_bool, var_tuple, var_list, var_dict, sep='\n')
+
+firstname = input("Как вас зовут? ")
+age = input("Сколько вам лет? ")
+city = input("Из какого вы города? ")
+
+print(f"Ваше имя: {firstname}, Ваш возраст: {age}, Ваш город: {city}")
--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -9,3 +9,16 @@
 Введите время в секундах: 3600
 Время в формате ч:м:с - 1.0 : 60.0 : 3600
 """
+
+time_in_second = int(input("Введите время в секундах: "))
+
+hour = int(time_in_second / 3600)
+minute = int(time_in_second % 3600 / 60)
+second = int(time_in_second % 3600 % 60)
+if hour < 10:
+    hour = "0"+str(hour)
+if minute < 10:
+    minute = "0"+str(minute)
+if second < 10:
+    second = "0"+str(second)
+print(f"Время в формате чч:мм:сс: {hour}:{minute}:{second}")
