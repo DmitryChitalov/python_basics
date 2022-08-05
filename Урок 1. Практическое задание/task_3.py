7cac7df988ca8b532248a208083c8aa604b0bd96
error-feature <37775171+error-feature@users.noreply.github.com>
2022-08-05 23:54:27 +0300

    third task

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,9 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+num = None
+while not (isinstance(num, str) and num.isdigit()):
+    num = input('Введите любимое число: ')
+
+res = sum(map(int, [num, num*2, num*3]))
+print(res)
