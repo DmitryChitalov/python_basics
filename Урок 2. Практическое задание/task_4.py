9cc5b6cb08946820aed75cec8c17de27217becb9
Konstantin Gromov <konstantin.gromov@itiviti.com>
2022-08-05 14:31:49 +0200

    Homework done

--- Урок 2. Практическое задание/task_4.py
+++ Урок 2. Практическое задание/task_4.py
@@ -14,3 +14,13 @@
 1. раз
 2. перерефриж
 """
+text = input("Please enter text with some spaces:")     # Ask for user input
+text = text.split(" ")                                  # Split string into list by space
+
+start_line = 1                                          # Initial line number
+
+for word in text:
+    if len(word) >= 10:                                 # If word length is more than 10 characters, shorten it
+        word = word[:9]
+    print(f"{start_line}. {word}")                      # Print line number and word
+    start_line += 1                                     # Increment line number
