d137048e1881f12ebe0e67046b79b76088de47df
Janassov <bekbolat.janassov@gmail.com>
2022-08-06 10:56:08 +0600

    Homework 2 completed

--- Урок 2. Практическое задание/task_2.py
+++ Урок 2. Практическое задание/task_2.py
@@ -11,3 +11,17 @@
 Введите целые числа через пробел: 1 2 3
 Результат: 2 1 3
 """
+user_vals = input('Введите целые числа через пробел: ').split(' ')
+lenght = len(user_vals)
+odd = (lenght % 2) != 0
+reodered_array = []
+for i in range(lenght):
+    if i == (lenght - 1) and odd:
+        current_val = user_vals[i]
+        reodered_array.append(current_val)
+    elif ((i + 1) % 2) != 0:
+        next_val = user_vals[i + 1]
+        current_val = user_vals[i]
+        reodered_array.append(next_val)
+        reodered_array.append(current_val)
+print(f'Результат: {" ".join(str(x) for x in reodered_array)}')
