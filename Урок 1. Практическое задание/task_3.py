f97a97f959edbce6f0e9b974381166d35da254f9
a-kuh <98696175+a-kuh@users.noreply.github.com>
2022-08-07 18:56:34 +0300

    Update task_3.py

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,10 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+
+
+number = int(input("Введите целое число n: "))
+a = str(number)
+b = a + a
+c = a + b
+print("n + nn + nnn = ", number + int(b) + int(c))
