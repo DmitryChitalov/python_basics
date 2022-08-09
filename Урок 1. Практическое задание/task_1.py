90a44f8764d81290d1dd07b8d037fae9bed6ab87
unk1d <100078915+unk1d@users.noreply.github.com>
2022-08-09 09:05:44 +0300

    Update task_1.py

--- Урок 1. Практическое задание/task_1.py
+++ Урок 1. Практическое задание/task_1.py
@@ -1,4 +1,5 @@
-number = int(input('Введите число:'))
-two_number = int(f'{number}{number}')
-three_number = int(f'{two_number}{number}')
-print(number + two_number + three_number)
+insert = int(input("Введите время в секундах?"))
+hours = insert // 3600
+minutes = (insert - (hours * 3600)) // 60
+seconds = insert - (hours * 3600 + minutes * 60)
+print(f'{hours}:{minutes}:{seconds}')
