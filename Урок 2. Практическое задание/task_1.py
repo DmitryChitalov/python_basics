544145f892d3e1583bbb6a68c90a6f1fc225aa13
AlexLis27 <krasnekov@gmail.com>
2022-08-09 18:04:33 +0300

    Lesson_2

--- Урок 2. Практическое задание/task_1.py
+++ Урок 2. Практическое задание/task_1.py
@@ -15,9 +15,6 @@
 <class 'bool'>
 <class 'NoneType'>
 """
-my_list = [12, None, -77, 'True', True, 9.5]
-def my_type(el):
-    for el in range(len(my_list)):
-        print(type(my_list[el]))
-    return
-my_type(my_list)
+my_list = [6, None, -87, 'True', False, 9.5]
+for el in my_list:
+    print(type(el))
--- Урок 2. Практическое задание/task_2.py
+++ Урок 2. Практическое задание/task_2.py
@@ -11,3 +11,15 @@
 Введите целые числа через пробел: 1 2 3
 Результат: 2 1 3
 """
+el_count = int(input("Введите количество элементов списка "))
+my_list = []
+i = 0
+el = 0
+while i < el_count:
+    my_list.append(input("Введите следующее значение списка "))
+    i += 1
+
+for elem in range(int(len(my_list)/2)):
+    my_list[el], my_list[el + 1] = my_list [el + 1], my_list[el]
+    el += 2
+print(my_list)
