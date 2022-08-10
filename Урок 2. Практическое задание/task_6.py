b53d8877c9350cfde2f727d963e0b33c701ec3a4
EvgeniyPyak <s_k_a-t_93@mail.ru>
2022-08-10 11:55:52 +0500

    Задание 6

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -25,3 +25,17 @@
 “ед”: [“шт.”]
 }
 """
+quantity = int(input("Введите количество товара "))
+n = 1
+my_dict = []
+my_list = []
+my_analys = {}
+while n <= quantity:
+    my_dict = dict({'название': input("введите название "), 'цена': input("Введите цену "),
+                    'количество': input('Введите количество '), 'eд': input("Введите единицу измерения ")})
+    my_list.append((n, my_dict))
+    n += 1
+    my_analys = dict({'название': my_dict.get('название'), 'цена': my_dict.get('цена'),
+                      'количество': my_dict.get('количество'), 'ед': my_dict.get('ед')})
+print(my_list)
+print(my_analys)
