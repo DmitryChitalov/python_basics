cb64912be1aebe0a5f0d83441c7afe9db4c045fd
spa <sanych.spa@gmail.com>
2022-08-10 19:54:26 +0300

    1 задание, первого урока

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,6 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+n = int(input("Введите число - "))
+total = (n + int(str(n) + str(n)) + int(str(n) + str(n) + str(n)))
+print("Сумма чисел n + nn + nnn = %d" % total)
--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -10,3 +10,14 @@
 Ведите целое положительное число: 123456789
 Самая большая цифра в числе: 9
 """
+number = abs(int(input("Введите целое положительное число ")))
+max = number % 10
+while number >= 1:
+    number = number // 10
+    if number % 10 > max:
+        max = number % 10
+    if number > 9:
+        continue
+    else:
+        print("Самая большая цифра в числе ", max)
+        break
