25d23a4ce5a4f65f392ae7d3e24dc23c80b0ab2a
Chingis Kuzhagaliyev <ckuzhagaliyev@gmail.com>
2022-08-11 16:32:16 +0600

    дополнили файл

--- Урок 1. Практическое задание/task_1.py
+++ Урок 1. Практическое задание/task_1.py
@@ -13,3 +13,5 @@ number1 = int(input("Введите первое число "))
 string2 = input("Введите вторую строку ")
 number2 = int(input("Введите второе число "))
 print("Введеные значения - %10s %5d %10s %5d" % (string1, number1, string2, number2))
+
+что то дописали тут
