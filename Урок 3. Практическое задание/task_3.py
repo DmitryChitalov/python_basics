7541b96fadd58b86fe8df0ef5e39737536e3e9c9
Artem <gghicat@gmail.com>
2022-08-11 15:29:37 +0600

    lesson-3 kotov

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -5,3 +5,15 @@
 1) используя функцию sort()
 2) без функции sort()
 """
+
+
+def sum_sort(*args):
+    return sum(sorted(args)[-2:])
+
+
+def sum_no_sort(*args):
+    return sum(args) - min(args)
+
+
+print(f"Результат с сортировкой: {sum_sort(5, 2, 3)}")
+print(f"Результат без сортировки: {sum_no_sort(1, 2, 3)}")
