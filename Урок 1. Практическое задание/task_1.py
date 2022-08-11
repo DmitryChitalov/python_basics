d69793847ad46c50d488275ff066081ea67166ba
Chingis Kuzhagaliyev <ckuzhagaliyev@gmail.com>
2022-08-11 17:03:17 +0600

    Решение задания 1

--- Урок 1. Практическое задание/task_1.py
+++ Урок 1. Практическое задание/task_1.py
@@ -14,4 +14,3 @@ string2 = input("Введите вторую строку ")
 number2 = int(input("Введите второе число "))
 print("Введеные значения - %10s %5d %10s %5d" % (string1, number1, string2, number2))
 
-что то дописали тут
--- Урок 2. Практическое задание/task_1.py
+++ Урок 2. Практическое задание/task_1.py
@@ -14,4 +14,13 @@
 <class 'float'>
 <class 'bool'>
 <class 'NoneType'>
+
+Задание 1
 """
+
+my_list = [12, None, -77, 'True', True, 9.5]
+def my_type(el):
+    for el in range(len(my_list)):
+        print(type(my_list[el]))
+    return
+my_type(my_list)
