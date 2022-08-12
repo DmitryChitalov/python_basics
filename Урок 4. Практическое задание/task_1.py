dc6d5263c62fdfff8b6a95b0ee50494602501e71
ddulebenets <dd@redtrack.io>
2022-08-12 23:02:12 +0400

    homework_4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -6,3 +6,29 @@
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
 """
+from sys import argv
+
+
+def income_calc(arg_work_hours=0, arg_hour_payment=0, arg_bonus=0):
+    return print(f"Заработано: {(arg_work_hours * arg_hour_payment) + arg_bonus}")
+
+
+#    return kwargs.get('bonus')
+
+
+script_name, work_hours, hour_payment, bonus = argv
+
+print("Имя скрипта: ", script_name)
+print("Часов отработано: ", work_hours)
+print("Ставка (час): ", hour_payment)
+print("Премия: ", bonus)
+
+income_calc(arg_bonus=float(bonus), arg_work_hours=float(work_hours), arg_hour_payment=float(hour_payment))
+"""
+python.exe "D:/GeekBrains/python/python_basics/Урок 4. Практическое задание/task_1.py" 8.5 10 100 
+Имя скрипта:  D:/GeekBrains/python/python_basics/Урок 4. Практическое задание/task_1.py
+Часов отработано:  8.5
+Ставка (час):  10
+Премия:  100
+Заработано: 185.0
+"""
--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -7,3 +7,38 @@
 
 Реализуйте вариант без и с генераторным выражением
 """
+my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+
+
+def non_generator(arg1):
+    new_list = []
+    for el in arg1:
+        if arg1.index(el) != 0:
+            curr_el = el
+            if curr_el > prev_el:
+                new_list.append(curr_el)
+        prev_el = el
+    return new_list
+
+
+def generator(arg1):
+    """
+    arg1.index(el) != 0 - убираем из вывода элемент с индексом 0, поскольку его не с кем сравнивать
+    arg1.__getitem__(arg1.index(el)) > arg1.__getitem__(arg1.index(el) - 1) - берем значение элемента из списка
+    по индексу и сравниваем с предыдущим arg1.index(el) - 1
+    """
+    new_list = [el for el in arg1 if arg1.__getitem__(arg1.index(el)) > arg1.__getitem__(arg1.index(el) - 1) and
+                arg1.index(el) != 0]
+    return new_list
+
+
+print(f"Исходный список: {my_list}")
+print(f"Новый список без генератора: {non_generator(my_list)}")
+print(f"Новый список  с генератором: {generator(my_list)}")
+"""
+Исходный список: [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+Новый список без генератора: [12, 44, 4, 10, 78, 123]
+Новый список  с генератором: [12, 44, 4, 10, 78, 123]
+
+Process finished with exit code 0
+"""
