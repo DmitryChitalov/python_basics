da63e468700d03a4dddca0e9478915657afb88a1
mouz <amin-magerramov@mail.ru>
2022-08-12 15:38:27 +0400

    lesson_2

--- Урок 2. Практическое задание/task_3.py
+++ Урок 2. Практическое задание/task_3.py
@@ -8,3 +8,20 @@
 Результат через список: Осень
 Результат через словарь: Осень
 """
+seasons_lst = ["Зима", "Весна", "Лето", "Осень"]
+seasons_dict = {1: "Зима", 2: "Весна", 3: "Лето", 4: "Осень"}
+months = int(input("Введите номер месяца от 1 до 12: "))
+if months == 1 or months == 2 or months == 12:
+    print(seasons_lst[0])
+    print(seasons_dict.get(1))
+elif months == 3 or months == 4 or months == 5:
+    print(seasons_lst[1])
+    print(seasons_dict.get(2))
+elif months == 6 or months == 7 or months == 8:
+    print(seasons_lst[2])
+    print(seasons_dict.get(3))
+elif months == 9 or months == 10 or months == 11:
+    print(seasons_lst[3])
+    print(seasons_dict.get(4))
+else:
+    print("Error")
