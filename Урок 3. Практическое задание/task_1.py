e2b69c9ba6ee5cf1a27992c4ba6219e6914e2199
AlexLis27 <krasnekov@gmail.com>
2022-08-13 12:25:25 +0300

    Lesson_3

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -17,3 +17,10 @@ Process finished with exit code 0
 
 Process finished with exit code 0
 """
+def division (first_num, second_num)
+    try:
+        return first_num / second_num
+    except ZeroDivisionError:
+        print("Попытка делить на ноль!")
+try:
+    first_number = int(input())
