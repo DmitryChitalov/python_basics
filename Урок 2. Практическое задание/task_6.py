0532baa11f9f267e3e182ab4035372a6ebc65f72
Ivan1P <vip-77@list.ru>
2022-08-14 20:02:19 +0300

    Задание 6 выполнено

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -32,42 +32,38 @@ while True:
                  {"Название": input("Название: "), \
                   "Цена": input("Цена: "),
                   "Количество": input("Количество: "),
-                  "Ед.": input("Единица учета ")}))
+                  "Ед.": input("Единица учета: ")}))
     q = input("Выйти? да/нет ")
     if q == "да":
         break
 print(my_l)
-print(len(my_l))
+# print(len(my_l))
 i = len(my_l)
 print("#####")
 ######
 print(my_l[0][0])
 print(my_l[0][1])
-print(my_l[0][1]["Название"])
-print("222#####")
+print(my_l[0][1]["Название"])  # печать элемента - это работает
+
+# print("222#####")
 my_d = dict()  # создание пустого словаря
-my_d['Название'] = ""
-my_d['Цена'] = ""
-my_d["Количество"] = ""
-my_d["Ед."] = ""
+# my_d['Название'] = ""
+# my_d['Цена'] = ""
+# my_d["Количество"] = ""
+# my_d["Ед."] = ""
+
+l_names = []  # создание пустых списков
+l_prices = []
+l_counts = []
+l_units = []
+
 for y in range(i):
     print(y)
-    print(my_l[y][1]["Название"])  # непростая конструкция обращения к элементу
-    str = my_l[y][1]["Название"]
-    try:
-        my_d['Название'].append(my_l[y][1]["Название"])
-    except KeyError:
-        my_d['Название'] = [my_l[y][1]["Название"]]
-    my_d['Название'] = my_d.get('Название', []) + ['str']
-    str1 = my_d['Цена']
-    str2 = my_l[y][1]["Цена"]
-    my_d['Цена'] = str1 + str2
-    str1 = my_d['Количество']
-    str2 = my_l[y][1]["Количество"]
-    my_d["Количество"] = str1 + str2
-    str1 = my_d['Ед.']
-    str2 = my_l[y][1]["Ед."]
-    my_d["Ед."] = str1 + str2
+    l_names.append(my_l[y][1]["Название"])
+    l_prices.append(my_l[y][1]["Цена"])
+    l_counts.append(my_l[y][1]["Количество"])
+    l_units.append(my_l[y][1]["Ед."])
 
-print("333####")
+my_d.update({"Наименования": l_names, "Цены": l_prices, "Количества": l_counts, "Единицы учета": l_units})
+
 print(my_d)
