a0b81c2360743f4d79286306e524094acb9bb4a1
i.p <2626@unistar.ru>
2022-08-15 17:45:06 +0300

    Задание 1 в процессе

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -18,5 +18,24 @@ Process finished with exit code 0
 Process finished with exit code 0
 """
 
-######
 
+######
+# from sys import argv
+# print(argv)
+# def my_func(arg1, arg2):
+#    return arg1 + arg2
+# a = my_func(80, 30)
+# print(a)
+
+def my_func1(a1, a2):
+    if a2 != 0:
+        return round(a1 / a2, 1)
+    else:
+        print(f"Деление на 0, так нельзя")
+        return None
+
+a1 = int(input("Введите первое число: "))
+a2 = int(input("Введите второе число: "))
+print(type(my_func1(a1,a2)))
+if type(my_func1(a1, a2)) != "None":
+    print(my_func1(a1, a2))
