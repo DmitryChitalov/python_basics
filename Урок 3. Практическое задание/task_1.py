ec174e7186d8a38e7a95d7092c44ebd179cc9d69
Chingis Kuzhagaliyev <ckuzhagaliyev@gmail.com>
2022-08-15 15:18:56 +0600

    Решение задания 2

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -1,19 +1,32 @@
 """
-1. Реализовать функцию, принимающую два числа (позиционные аргументы)
-и выполняющую их деление. Числа запрашивать у пользователя,
-предусмотреть обработку ситуации деления на ноль (try except).
+'''
+1.Реализовать функцию, принимающую два числа
+(позиционные аргументы) и выполняющую их деление.
+Числа запрашивать у пользователя,
+предусмотреть обработку ситуации деления на ноль.
 
-Пример:
-Введите первое число: 10
-Введите второе число: 0
-Вы что? Пытаетесь делить на 0!
+'''
 
-Process finished with exit code 0
+    def div(*args):
 
-Пример:
-Введите первое число: 10
-Введите второе число: 10
-1.0
+    try:
+        arg1 = int(input("Input dividend "))
+        arg2 = int(input("Input divider "))
+        res = arg1 / arg2
+    except ValueError:
+        return 'Value error'
+    except ZeroDivisionError:
+        return "Wrong devider! You can't use zero as a devider"
 
-Process finished with exit code 0
+    return res
+
+    '''
+    if arg2 != 0:
+        return arg1 / arg2
+    else:
+        print("Wrong number! Devider can't be null")
+    '''
+
+
+print(f'result  {div()}')
 """
--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -8,3 +8,16 @@
 Иван Иванов 1846 года рождения, проживает в городе Москва,
 email: jackie@gmail.com, телефон: 01005321456
 """
+
+
+ name = input('enter name')
+    surname = input('enter surname')
+    year = int(input('enter year'))
+    city = input('enter city')
+    email = input('enter email')
+    telephone = input('input telephone')
+'''
+
+    def my_func (name, surname, year, city, email, telephone):
+     return ' '.join([name, surname, year, city, email, telephone])
+print(my_func(surname = 'Frolov', name = 'Sergey', year = '1990', city = 'Syzran', email = 'error@mail.ru', telephone = '8-903-300-99-87'))
