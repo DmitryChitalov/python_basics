59b4ec9a435d279234503118aa06bae00fa16861
Artem <gghicat@gmail.com>
2022-08-15 13:26:33 +0600

    lesson-4 kotov

--- Урок 4. Практическое задание/task_6.py
+++ Урок 4. Практическое задание/task_6.py
@@ -12,3 +12,38 @@
 Во втором также необходимо предусмотреть условие,
 при котором повторение элементов списка будет прекращено.
 """
+
+from number_generator import get_number, get_number_repeating
+
+while True:
+    start_gen = input("Введите начальный диапазон генерации списка: ")
+    try:
+        start_gen = int(start_gen)
+        break
+    except ValueError:
+        print("Введите целое положительное число")
+        continue
+
+while True:
+    end_gen = input("Введите конечный диапазон генерации списка: ")
+    try:
+        end_gen = int(end_gen)
+        break
+    except ValueError:
+        print("Введите целое положительное число")
+        continue
+
+print(get_number(start_gen, end_gen))
+
+lst_gen = [int(el) for el in input("Введите числа через пробел для генерации повторяющегося списка: ").split(" ")]
+
+while True:
+    end_gen = input("Введите конечный диапазон генерации повторяющегося списка: ")
+    try:
+        end_gen = int(end_gen)
+        break
+    except ValueError:
+        print("Введите целое положительное число")
+        continue
+
+print(get_number_repeating(lst_gen, end_gen))
