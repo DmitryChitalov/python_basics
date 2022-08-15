afe3c0706bcd9e23798c50986b0c32bee011556d
Chingis Kuzhagaliyev <ckuzhagaliyev@gmail.com>
2022-08-15 15:20:27 +0600

    Решение задания 3

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -4,7 +4,7 @@
 Попробуйте решить задачу двумя способами:
 1) используя функцию sort()
 2) без функции sort()
-"""
+""" 
 def my_func(arg1 , arg2, arg3):
     if arg1 >= arg3 and arg2 >= arg3:
         return arg1 + arg2
