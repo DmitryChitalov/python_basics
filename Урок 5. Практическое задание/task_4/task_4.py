7443b71e3f7c859aea3677151fd5535a2e05ec21
Janassov <bekbolat.janassov@gmail.com>
2022-08-16 07:39:24 +0600

    tasks 4-5

--- Урок 5. Практическое задание/task_4/task_4.py
+++ Урок 5. Практическое задание/task_4/task_4.py
@@ -7,3 +7,19 @@
 Иванов 23543.12
 Петров 13749.32
 """
+average_salary = 0
+processed_data = []
+less_than_20 = []
+with open('task_4_data.txt', 'r', encoding='utf-8') as f:
+    data = f.readlines()
+    for el in data:
+        surname, salary = el.split(' ')
+        salary = float(salary)
+        average_salary += salary
+        processed_data.append([surname, salary])
+        if salary < 20000.0:
+            less_than_20.append([surname, salary])
+    average_salary = average_salary / len(processed_data)
+    print('Фамилии сотрудников с окладом меньше 20 000: ')
+    print(less_than_20)
+    print(f'Средняя величина дохода сотрудников: {average_salary}')
--- Урок 5. Практическое задание/task_5/task_5.py
+++ Урок 5. Практическое задание/task_5/task_5.py
@@ -2,3 +2,15 @@
 5)	Создать (программно) текстовый файл, записать в него программно набор чисел,
  разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 """
+values_list = []
+while True:
+    user_val = input('Введите число или пустую строку для завершения: ')
+    if user_val == '':
+        break
+    else:
+        values_list.append(int(user_val))
+values_sum = sum(values_list)
+data_to_write = ' '.join(map(str, values_list))
+print(f'Сумма введенных значений: {values_sum}')
+with open('task_5_result.txt', 'a', encoding='utf-8') as f:
+    f.write(data_to_write)
