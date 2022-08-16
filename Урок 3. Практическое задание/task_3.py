2eb363f7e010d2007f163aaf54b13fc86d67bfe7
i.p <2626@unistar.ru>
2022-08-16 13:47:02 +0300

    Задание 3 (способ 2)

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -6,26 +6,26 @@
 2) без функции sort()
 """
 
-### Первый способ (sort)
-print("Первый способ...")
+### Второй способ
+print("Второй способ...")
 
-
-def my_sum():
+def my_sum2():
     try:
         a1 = float(input("Введите первое число: "))
         a2 = float(input("Введите второе число: "))
         a3 = float(input("Введите третье число: "))
     except ValueError:
-        print("Нужно вводить числа")
+        print
+        "Нужно вводить числа"
         return
-    my_list = [a1, a2, a3]
-    my_list.sort()
-    s = my_list[1] + my_list[2]
+    if a1 > a2:
+        if a2 > a3:
+            s = a1 + a2
+        else: s = a1 + a3
+    elif a3 > a1:
+        s = a2 + a3
+    else: s = a2 + a1
     return s
-
-
-print(my_sum())
-
-
+print(my_sum2())
 
 
