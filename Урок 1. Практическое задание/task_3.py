7f7dba61dee56d4e916e13a80023619e08af2939
125Ruslan <53310255+125Ruslan@users.noreply.github.com>
2022-08-16 10:10:17 -0400

    урок1

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -3,7 +3,12 @@
 
 Узнайте у пользователя целое положительное число n.
 Найдите сумму чисел n + nn + nnn.
-
+digit = int(input("Input number n: "))
+x = str(digit)
+x1 = x + x
+x2 = x + x + x
+result = digit + int(x1) + int(x2)
+print("Print: ", result)
 Пример:
 Введите число n: 3
 n + nn + nnn = 369
