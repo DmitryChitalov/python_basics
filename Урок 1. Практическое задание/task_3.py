7fbe2131d76fca2769156e7790bd6925112442c5
Ariman999 <85644065+Ariman999@users.noreply.github.com>
2022-08-16 21:23:29 +0300

    first

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,6 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+n = int(input('Введите число n: '))
+total = (n + int(str(n) + str(n)) + int(str(n) + str(n)+ str(n)))
+print(f"Сумма чисел n + nn + nnn - %d" % total)
