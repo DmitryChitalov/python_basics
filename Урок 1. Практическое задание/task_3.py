7cdde535638f79e32c939430ad2e5b115614c4c7
Policedog3409 <yodamoreland@yahoo.com>
2022-08-17 01:54:01 +1000

    выполнили третью задачу

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,7 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+time = int(input("Введите время в секундах: "))
+hours = time / 3600
+minutes = time / 60
+print(f"Время в формате ч:м:с - {hours}: {minutes}: {time}")
