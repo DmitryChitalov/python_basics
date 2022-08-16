5e23a67430b47fa2ff392f39bd549c553fd6b80d
i.p <2626@unistar.ru>
2022-08-16 12:05:07 +0300

    Задание 2 выполнено.

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -8,3 +8,28 @@
 Иван Иванов 1846 года рождения, проживает в городе Москва,
 email: jackie@gmail.com, телефон: 01005321456
 """
+
+name = input("Введите имя пользователя: ")
+fam = input("Введите фамилию пользователя: ")
+try:
+    year = int(input("Введите год рождения: "))
+except ValueError:
+    print("Некорректный ввод")
+    exit(1)
+city = input("Введите город: ")
+mail = input("Введите e-mail: ")
+tel = input("Введите телефон: ")
+
+
+def user_data(name, fam, year, city, mail, tel):
+    #   try:
+
+    #  except ValueError:
+    #      print("Некорректный ввод")
+    #      return
+    year = str(year)
+    string1 = name + " " + fam + ", " + year + " года рождения, проживает  в городе: " + city + ", " + mail + ", " + tel
+    return string1
+
+
+print(user_data(name, fam, year, city, mail, tel))
