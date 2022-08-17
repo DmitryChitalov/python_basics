1ce89b9914239b74fbbd81c5f46e212cbbdee609
sergalbov <sergalbov@gmail.com>
2022-08-17 11:01:13 +0300

    task_1 OK

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -6,8 +6,13 @@
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
 """
-def salary (param_1, param_2, param_3):
-    return param_1 * param_2 + param_3
+from sys import argv
 
+script_name, first_param, second_param, third_param = argv
 
-print(salary(int(input("Укажите кол-во часов: ")), int(input("Ставка в час: ")), int(input("Премия: "))))
+print("Имя скрипта: ", script_name)
+print("Кол-во часов: ", first_param)
+print("Ставка в час: ", second_param)
+print("Премия: ", third_param)
+salary = int(first_param) * int(second_param) + int(third_param)
+print(f"Зарплата составляет: {salary}")
