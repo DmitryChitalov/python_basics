9da11bcabcd4facdaa60cb42f5a2df7455a2c427
Andrew Emelyanenko <and.maced-one@yandex.ru>
2022-08-17 13:49:47 +0300

    Update task_1.py

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -21,3 +21,20 @@ def simple():
 
 
 print(f'Размер заработной платы составил: {simple()}')
+
+------
+
+"""
+Выполенине! Емельяненко А.А.
+"""
+
+from sys import argv
+
+script_name, hours_production, rate_per_hour, bonus = argv
+
+print("Имя скрипта: ", script_name)
+print("\n<< Программа рассчета заработной платы сотрудника >>")
+print("Выработка в часах: ", hours_production)
+print("Ставка в час: ", rate_per_hour)
+print("Премия: ", bonus)
+print("Зарплата сотрудника: ", (float(hours_production) * float(rate_per_hour)) + float(bonus))
