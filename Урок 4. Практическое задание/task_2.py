e6e16df911e5b0c6d9d101c52f3b021da1b36fbb
EvgeniyPyak <s_k_a-t_93@mail.ru>
2022-08-17 10:10:59 +0500

    Доработка заданий 1 и 2

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -15,3 +15,7 @@ for i in range(len(lst)):
     if lst[i - 1] < lst[i]:
         new_lst.append(lst[i])
 print(new_lst)
+
+in_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+res_list = [number for i, number in enumerate(in_list) if i > 0 and in_list[i] > in_list[i - 1]]
+print(res_list)
