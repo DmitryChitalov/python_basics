4f064b9221ff0607406b3dc046bd1c7f508e30e9
ACherepeckoi <111152206+ACherepeckoi@users.noreply.github.com>
2022-08-18 00:53:02 +0300

    home_work_cherepetckoi
    Домашняя работа Черепецкой А В

--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -19,3 +19,13 @@
 6-й день: 3,22
 Ответ: на 6-й день спортсмен достиг результата — не менее 3 км.
 """
+"""
+"""
+quest1 = int(input('Результат первого дня пробежки: '))
+quest2 = int(input('Цель которую нужно достичь: '))
+compite_day = 1
+while quest1 < quest2:
+    compite_day += 1
+    quest1 *= 1.1
+    print(quest1)
+print(f"День достижения цели {compite_day}")
