df10463315857445e684649514c08484c8150a66
MaratEvseev <maratevseev@gmail.com>
2022-08-17 08:50:03 +0300

    L3 Task 2 done

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -8,3 +8,16 @@
 Иван Иванов 1846 года рождения, проживает в городе Москва,
 email: jackie@gmail.com, телефон: 01005321456
 """
+
+def personal_data(name, lastname, year_of_birth, city, email, phone):
+    return print(f'Имя: {name} Фамилия: {lastname} Год рождения: {year_of_birth}'
+                 f'Город проживания: {city} Email: {email} Телефон: {phone}')
+
+personal_data(
+    name=input('Имя: '),
+    lastname=input('Фамилия: '),
+    year_of_birth=input('Год Рождения: '),
+    city=input('Город проживания: '),
+    email=input('email: '),
+    phone=input('phone: '),
+)
