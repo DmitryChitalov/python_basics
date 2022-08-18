2a88104be9adbea04caf36d4f2f9a21fe03147d3
Artem <gghicat@gmail.com>
2022-08-18 17:53:28 +0600

    lesson-5 kotov

--- Урок 5. Практическое задание/task_2/task_2.py
+++ Урок 5. Практическое задание/task_2/task_2.py
@@ -2,3 +2,11 @@
 2)	Создать текстовый файл (не программно), сохранить в нем несколько строк,
 выполнить подсчет количества строк, количества слов в каждой строке.
 """
+
+with open("file_1.txt", "r", encoding="utf-8") as f_obj:
+    content = f_obj.read()
+    content_lst = content.split("\n")
+
+    print(f"Всего строк: {len(content_lst)}")
+    for i in content_lst:
+        print(f"Строка {content_lst.index(i) + 1} -> Количество слов:{len(i.split())} --> {i}")
