cb90151b064e0657189097093bbaef516f581a8b
Andrew Emelyanenko <and.maced-one@yandex.ru>
2022-08-20 16:23:18 +0300

    Update task_3.py

--- Урок 5. Практическое задание/task_3/task_3.py
+++ Урок 5. Практическое задание/task_3/task_3.py
@@ -13,25 +13,19 @@ Four — 4
 Выполенине! Емельяненко А.А.
 """
 
-
-def workers_statistics():
-    workers = [['Иванов ', '15000 \n'], ['Сидоров ', '18000 \n'], ['Емельяненко ', '140000 \n'], ['Смирнов ', '90000']]
+def rewrite_file():
+    num = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре'}
+    new_text = []
     try:
-        with open('new_file.txt', 'r+', encoding="utf-8") as file:
-            for i in range(len(workers)):
-                file.writelines(workers[i])
-            r = file.readlines()
-            poor = []
-            sum = 0
-            for i in range(len(r)):
-                salary = int((r[i].split())[1])
-                if salary < 20000:
-                    poor.append((r[i].split())[0])
-                sum += salary
-            print(f'Средняя величина дохода сотрудников равна {sum / len(workers) / 12:.2f}')
-            print(f'Меньше 20 тыс. рублей получает сотрудник: {", ".join(poor)}')
+        with open('file.txt', 'r+', encoding="utf-8") as file:
+            with open('new_file.txt', 'r+', encoding="utf-8") as new_file:
+                l = file.readlines()
+                for i in l:
+                    i = i.split(' ', 1)
+                    new_text.append(num[i[0]] + ' ' + i[1])
+                new_file.writelines(new_text)
     except FileNotFoundError:
         return 'Файл не найден.'
 
 
-workers_statistics()
+rewrite_file()
