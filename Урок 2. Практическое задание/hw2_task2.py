62b7724822d8553fd7d4ff4ebe7f19a0049b6e3d
a-kuh <98696175+a-kuh@users.noreply.github.com>
2022-08-20 18:38:46 +0300

    Delete hw2_task2.py

--- Урок 2. Практическое задание/hw2_task2.py
+++
@@ -1,8 +0,0 @@
-my_list = input("Введите целые числа через запятую: ").split(',')
-a = 0
-b = 1
-while len(my_list) > b:
-    my_list[a], my_list[b] = my_list[b], my_list[a]
-    a += 2
-    b += 2
-print('Результат: ', my_list)
