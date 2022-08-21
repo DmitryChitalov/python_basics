a9c6134b22bab8004bd52156ccd10d39b227c252
DmSenatorov <gitl@mail.ru>
2022-08-22 02:01:31 +0300

    Homework_5

--- Урок 5. Практическое задание/task_4/task_4.py
+++ Урок 5. Практическое задание/task_4/task_4.py
@@ -7,3 +7,18 @@
 Иванов 23543.12
 Петров 13749.32
 """
+result = 0
+count = 0
+list_1 = list()
+with open('task_4.txt', 'r', encoding='utf-8') as my_file:
+    for line in my_file.readlines():
+        list_1.extend(line.rstrip().split(' '))
+print("Оклад меньше 20 тыс. у сотрудников: ")
+for i in range(1, len(list_1), 2):
+    salary = float(list_1[i])
+    result += salary
+    count = len(list_1) / 2
+    if salary <= 20000:
+        print(list_1[i - 1])
+revenue = result / count
+print(f"Средняя величина дохода сотрудников: {revenue}")
