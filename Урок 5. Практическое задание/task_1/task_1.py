1ef9a2a70ba4acf70c0c49b7b261d4fa3ea60cf9
AlexLis27 <krasnekov@gmail.com>
2022-08-21 13:19:13 +0300

    Lesson_5

--- Урок 5. Практическое задание/task_1/task_1.py
+++ Урок 5. Практическое задание/task_1/task_1.py
@@ -3,16 +3,12 @@
 записать в него построчно данные, вводимые пользователем.
 Об окончании ввода данных свидетельствует пустая строка.
 """
-my_f = open('task_1.txt', 'w', encoding = "utf-8")
-line = input('Введите текст \n ')
-while line:
-    my_f.writelines(line)
+with open('task_1.txt', 'w', encoding="utf-8") as my_f:
     line = input('Введите текст \n ')
-    if not line:
-        break
+    while line:
+        my_f.writelines(line)
+        line = input('Введите текст \n ')
+        if not line:
+            break
 
-my_f.close()
-my_f = open('task_1.txt', 'r')
-content = my_f.readlines()
-print(content)
-my_f.close()
+
