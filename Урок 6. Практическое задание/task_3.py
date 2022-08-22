c56ce7583bca94f00aa1115e3c6557a6ad18e73e
delpara <seo@reezz.ru>
2022-08-22 21:03:44 +0300

    lesson_6_Dh

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -15,3 +15,25 @@
 П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку __str__
 __str__(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
 """
+class Worker():
+
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {'wage': wage, 'bonus': bonus}
+
+class Position(Worker):
+
+    def get_full_name(self):
+        return f'{self.name} {self.surname}'
+
+    def get_total_income(self):
+        a = self._income['wage']
+        b = self._income['bonus']
+        return a + b
+
+a = Position('Den', 'Don', 'manager', 30000, 5000)
+print(a.get_full_name())
+print(a.position)
+print(a.get_total_income())
--- Урок 6. Практическое задание/task_5.py
+++ Урок 6. Практическое задание/task_5.py
@@ -13,3 +13,38 @@
 Для каждого из классов метод должен выводить уникальное сообщение.
 Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
 """
+class Statoinery():
+
+    def __init__(self, title = 'ABC'):
+        self.title = title
+
+    def draw(self):
+        print('Запуск отрисовки')
+
+class Pen(Statoinery):
+
+    def draw(self):
+        self.title = 'pen'
+        return print(f'{self.title}. Запуск отрисоввки')
+
+
+class Pencil(Statoinery):
+
+    def draw(self):
+        self.title = 'Pencil'
+        return print(f'{self.title}. Запуск отрисоввки')
+
+class Handle(Statoinery):
+
+    def draw(self):
+        self.title = 'Handle'
+        return print(f'{self.title}. Запуск отрисоввки')
+
+a = Pen()
+a.draw()
+a = Pencil()
+a.draw()
+a = Handle()
+a.draw()
+a = Statoinery()
+a.draw()
