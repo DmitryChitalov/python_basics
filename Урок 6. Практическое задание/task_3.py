30c36e372bfa292dce81cb2c80241578e25539d5
Ivan Saykin <saikin_89@mail.ru>
2022-08-22 16:40:17 +0300

    lesson6 done

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -6,12 +6,50 @@
 и защищенный атрибут income (доход). Последний атрибут должен ссылаться
 на словарь, содержащий элементы: оклад и премия, например, {"wage": wage, "bonus": bonus}.
 
-Создать класс Position (должность) на базе класса Worker. В классе Position реализовать публичные методы
-получения полного имени сотрудника (get_full_name) и дохода с учетом премии (get_total_income).
+Создать класс Position (должность) на базе класса Worker. В классе Position реализовать публичные
+методы получения полного имени сотрудника (get_full_name) и дохода с учетом премии
+(get_total_income).
 
 Проверить работу примера на реальных данных (создать экземпляры класса Position, передать данные,
 проверить значения атрибутов, вызвать методы экземпляров).
 
 П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку __str__
-__str__(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
+__str__(self) - вызывается функциями str, print и format. Возвращает строковое представление
+объекта.
 """
+
+
+class Worker:
+    """Worker"""
+
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self.wage = wage
+        self.bonus = bonus
+        self._income = {"wage": self.wage, "bonus": self.bonus}
+
+    def __str__(self):
+        return f"Имя: {self.name}, Фамилия: {self.surname}, Должность: {self.position}"
+
+
+class Position(Worker):
+    """Position"""
+
+    # def __init__(self, name, surname, position, wage, bonus):
+    #     super().__init__(name, surname, position, wage, bonus)
+
+    def get_full_name(self):
+        """Get full name"""
+        print(f"Полное имя: {self.name} {self.surname}")
+
+    def get_total_income(self):
+        """Get total income"""
+        print(f"Доход: {self._income['wage'] + self._income['bonus']}")
+
+
+a = Position("Иван", "Сайкин", "Администратор", 100000, 50000)
+print(a)
+a.get_full_name()
+a.get_total_income()
--- Урок 6. Практическое задание/task_4.py
+++ Урок 6. Практическое задание/task_4.py
@@ -20,3 +20,80 @@ speed, color, name, is_police (булево).
 Выполните доступ к атрибутам, выведите результат.
 Выполните вызов методов и также покажите результат.
 """
+
+
+class Car:
+    """Car"""
+
+    def __init__(self, speed, color, name, is_police=False):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def go(self):
+        """go"""
+        print("Машина поехала")
+
+    def stop(self):
+        """stop"""
+        print("Машина остановилась")
+
+    def turn(self, direction):
+        """turn"""
+        print(f"Машина повернула {direction}")
+
+    def show_speed(self):
+        """show speed"""
+        print(f"Текущая скорость {self.speed}")
+
+
+class TownCar(Car):
+    """Town Car"""
+
+    def show_speed(self):
+        if self.speed > 60:
+            print(f"Скорость превышена. Текущая скорость {self.speed}.")
+        else:
+            print(f"Текущая скорость {self.speed}")
+
+
+class SportCar(Car):
+    """Sport Car"""
+    max_speed = 350
+
+
+class WorkCar(Car):
+    """Work car"""
+
+    def show_speed(self):
+        if self.speed > 40:
+            print(f"Скорость превышена. Текущая скорость {self.speed}.")
+        else:
+            print(f"Текущая скорость {self.speed}")
+
+
+class PoliceCar(Car):
+    """Police car"""
+
+    def __init__(self, speed, color, name, is_police=True):
+        super().__init__(self, speed, color, name)
+        self.is_police = is_police
+
+
+town_car = TownCar(50, "Красный", "Audi")
+print(town_car.speed, town_car.color, town_car.name, town_car.is_police)
+town_car.show_speed()
+
+work_car = WorkCar(60, "Желтый", "Cat")
+print(work_car.is_police)
+work_car.show_speed()
+
+police_car = PoliceCar(120, "Синий", "Ford")
+print(police_car.is_police)
+police_car.go()
+police_car.stop()
+police_car.turn("налево")
+
+sport_car = SportCar(200, "Красный", "BMW", False)
+print(sport_car.max_speed)
