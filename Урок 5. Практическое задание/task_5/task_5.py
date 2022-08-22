723bd8bcad514ad855c32828bdf082f6df582e6d
Nike6op3oB <st.dimon.it@gmail.com>
2022-08-22 21:37:10 +0300

    Lesson_5_done

--- Урок 5. Практическое задание/task_5/task_5.py
+++ Урок 5. Практическое задание/task_5/task_5.py
@@ -2,3 +2,8 @@
 5)	Создать (программно) текстовый файл, записать в него программно набор чисел,
  разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 """
+with open('task_5.txt', 'w', encoding='utf-8') as file:
+    line = input('Enter numbers through space ')
+    file.writelines(line)
+    numbers = line.split()
+    print(sum(map(int, numbers)))
