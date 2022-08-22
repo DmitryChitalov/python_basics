509a3d6ed495d2ec0c010b74ef06f80fab860638
Nike6op3oB <st.dimon.it@gmail.com>
2022-08-22 21:37:24 +0300

    Lesson_4_done

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -7,3 +7,12 @@
 
 Реализуйте вариант без и с генераторным выражением
 """
+my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+new_list = []
+for el in range(1, 12):
+    if my_list[el] > my_list[el - 1]:
+        new_list.append(my_list[el])
+print(new_list)
+
+new_list_sec = [my_list[el] for el in range(1, 12) if my_list[el] > my_list[el - 1]]
+print(new_list_sec)
