485aa93688935b1acf1cd05399233095d13431bf
Ariman999 <85644065+Ariman999@users.noreply.github.com>
2022-08-22 11:24:26 +0300

    1
    1

--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -19,3 +19,18 @@
 6-й день: 3,22
 Ответ: на 6-й день спортсмен достиг результата — не менее 3 км.
 """
+start = float(input('Введите число км : '))
+finish = float(input('Введите конечную цель: '))
+day = 1
+
+if start > finish:
+    print(day)
+while True:
+    if start <= 0 or finish <= 0:
+        print('Результат должен быть больше нуля! Стартовое значение != 0')
+    else:
+        while start < finish:
+            start = start + start/10
+            day += 1
+        print(f'Ответ: на {day}-й день спортсмен достиг результата — не менее {finish} км.')
+        break
