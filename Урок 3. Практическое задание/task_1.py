4bff65171266e26c8f56bb74a25a5ba7d80f6666
RomanCvet <iceloop@mail.ru>
2022-08-23 14:40:31 +0300

    Tasks from lesson 3

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -17,3 +17,13 @@ Process finished with exit code 0
 
 Process finished with exit code 0
 """
+
+
+def calc(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return 'Вы что? Пытаетесь делить на 0!'
+
+
+print(calc(int(input('Первое число: ')), int(input('Второе число: '))))
