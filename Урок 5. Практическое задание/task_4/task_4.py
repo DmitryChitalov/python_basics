796e629200f4214906cafdbabc45c531edd5989e
MaratEvseev <maratevseev@gmail.com>
2022-08-24 15:55:54 +0300

    L5 Task 4 done

--- Урок 5. Практическое задание/task_4/task_4.py
+++ Урок 5. Практическое задание/task_4/task_4.py
@@ -7,3 +7,15 @@
 Иванов 23543.12
 Петров 13749.32
 """
+zp = {}
+
+with open("task_4.txt", "r", encoding="utf-8") as my_file:
+    for el in my_file:
+        zp[el.split()[0]] = float(el.split()[1])
+
+print("Меньше 20000 руб. получают сотрудники: ")
+for name, salary in zp.items():
+    if salary < 20000:
+        print(name)
+
+print(f"В среднем сотрудники получают {sum(zp.values()) / len(zp)}")
