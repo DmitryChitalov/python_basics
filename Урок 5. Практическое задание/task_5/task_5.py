879eab191b569a46eb099bdc5688be85ab21b125
sergalbov <sergalbov@gmail.com>
2022-08-24 11:14:43 +0300

    task_5 corrected

--- Урок 5. Практическое задание/task_5/task_5.py
+++ Урок 5. Практическое задание/task_5/task_5.py
@@ -2,9 +2,9 @@
 5)	Создать (программно) текстовый файл, записать в него программно набор чисел,
  разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 """
-with open(r"C:\Users\Serg\Desktop\ddd.txt", 'w', encoding="utf-8") as my_file:
+with open("ddd.txt", 'w', encoding="utf-8") as my_file:
     my_file.write(input('Введите числа через пробел: '))
-with open(r"C:\Users\Serg\Desktop\ddd.txt", 'r', encoding="utf-8") as my_file_2:
+with open("ddd.txt", 'r', encoding="utf-8") as my_file_2:
     text = list(my_file_2.read().split(" "))
     sum_num = 0
     for el in range(len(text)):
