eaa7a68e63769509eae3b19b0753cc1541ff4039
Orehov Roman <rorekhov@rating.kz>
2022-08-25 22:05:06 +0600

    Орехов Роман ДЗ №3

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -8,3 +8,18 @@
 Иван Иванов 1846 года рождения, проживает в городе Москва,
 email: jackie@gmail.com, телефон: 01005321456
 """
+
+
+def set_user_info(**user):
+    print(f"{user['name']} {user['sur_name']} {user['year']} года рождения, проживает в городе {user['city']}, "
+          f"email: {user['email']}, телефон: {user['phone']}")
+
+
+properties = {"name": "имя", "sur_name": "фамилия", "year": "год рождения", "city": "город проживания",
+              "email": "email", "phone": "телефон"}
+
+user_info = {}
+for key, value in properties.items():
+    user_info[key] = input(f"Введите {value}: ")
+
+set_user_info(**user_info)
