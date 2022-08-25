ae3654b9e3d74c32a4e73208f4363b9a966b85fb
Lordofwinterfield <tim979767@yandex.ru>
2022-08-25 20:45:22 +0300

    task4

--- Урок 5. Практическое задание/task_4/task_4.py
+++ Урок 5. Практическое задание/task_4/task_4.py
@@ -7,3 +7,15 @@
 Иванов 23543.12
 Петров 13749.32
 """
+with open("test.txt", 'r', encoding="utf-8") as my_file:
+    lines = my_file.read().splitlines()
+    dic = {}
+    money = []
+    for line in lines:
+        key, value = line.split(' ')
+        dic.update({key: float(value)})
+        money.append(float(value))
+    for keys, values in dic.items():
+        if float(values) < 20000:
+            print(f"Сотрудник получающий менее 20000: {keys}")
+    print(f"Средняя ЗП: {sum(money) / len(money)} в год")
