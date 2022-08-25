b02d8eaa89cf92823b29888d3799145cf7fc2639
Artem <gghicat@gmail.com>
2022-08-25 16:53:25 +0600

    lesson-7 kotov

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -28,3 +28,32 @@
 8 10 12
 14 16 18
 """
+
+
+class Matrix:
+    def __init__(self, *param):
+        self.matrix = []
+        self.matrix.append(param)
+
+    def __str__(self):
+        result = ''
+        for el in self.matrix[0]:
+            result += f"{el} \n"
+        return result
+
+    def __add__(self, other):
+        result = []
+        for i in range(0, len(self.matrix[0])):
+            el_new = []
+            for j in range(0, len(self.matrix[0][i])):
+                el_new.append(self.matrix[0][i][j] + other.matrix[0][i][j])
+            result.append(el_new)
+        return result
+
+
+matrix1 = Matrix([1, 2, 3], [4, 5, 6], [7, 8, 9])
+matrix2 = Matrix([1, 2, 3], [4, 5, 6], [7, 8, 9])
+matrix_sum = matrix1 + matrix2
+
+for el in matrix_sum:
+    print(f"{el}")
