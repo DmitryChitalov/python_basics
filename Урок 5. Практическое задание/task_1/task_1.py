3e564075cea720b19bed0789131ac6f3532c1049
Lordofwinterfield <tim979767@yandex.ru>
2022-08-25 18:16:07 +0300

    task1

--- Урок 5. Практическое задание/task_1/task_1.py
+++ Урок 5. Практическое задание/task_1/task_1.py
@@ -3,3 +3,13 @@
 записать в него построчно данные, вводимые пользователем.
 Об окончании ввода данных свидетельствует пустая строка.
 """
+file = open("test.txt", 'w', encoding='utf-8')
+lines = []
+while True:
+    line = input("Введите данные построчно или нажмите Enter для завершения: ")
+    if line != '':
+        lines.append(line)
+    else:
+        break
+for word in lines:
+    file.writelines(word + '\n')
