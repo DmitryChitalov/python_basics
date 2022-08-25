5fa58fe9e7e350b210dea4f0f9d0a79fab94e809
AlexLis27 <krasnekov@gmail.com>
2022-08-25 12:52:16 +0300

    Lesson_6

--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -15,3 +15,25 @@
 
 Проверить работу примера, создав экземпляр и вызвав описанный метод.
 """
+from time import sleep
+
+
+class TrafficLight:
+    color = ['red', 'yellow', 'green']
+
+    def running(self):
+        i = 0
+        while i < 3:
+            print(f'TrafficLight switch \n '
+                  f'{TrafficLight.color[i]}')
+            if i == 0:
+                sleep(7)
+            elif i == 1:
+                sleep(5)
+            elif i == 2:
+                sleep(3)
+            i += 1
+
+
+TrafficLight = TrafficLight()
+TrafficLight.running()
