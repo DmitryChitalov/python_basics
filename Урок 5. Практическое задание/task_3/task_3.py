f009c1d6ac9b5097d727ea83f205c002e1c78059
i.p <2626@unistar.ru>
2022-08-26 10:07:16 +0300

    Урок 5 задание 3 выполнено

--- Урок 5. Практическое задание/task_3/task_3.py
+++ Урок 5. Практическое задание/task_3/task_3.py
@@ -1,10 +1,22 @@
 """
 4)	Создать (не программно) текстовый файл со следующим содержимым:
-One — 1
-Two — 2
-Three — 3
-Four — 4
+
 Необходимо написать программу, открывающую файл на чтение и считывающую построчно данные.
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
+
+try:
+    with open("D:\TEST\\file1.txt") as f1_obj:
+        for line in f1_obj:
+            print(f"string= {line}")
+            str1 = line
+            str1 = str1.replace("One", "Один")
+            str1 = str1.replace("Two", "Два")
+            str1 = str1.replace("Three", "Три")
+            str1 = str1.replace("Four", "Четыре")
+            with open("D:\TEST\\file2.txt", "a") as f2_obj:  # а - режим на дозапись, а не перезапись файла как w
+                print(str1, file=f2_obj)
+
+except IOError:
+    print("Произошла ошибка ввода-вывода!")
