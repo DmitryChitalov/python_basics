9d97b99047b667014f2456a86eea014169fadf13
Orehov Roman <rorekhov@rating.kz>
2022-08-26 23:28:17 +0600

    Орехов Роман ДЗ №4

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -7,3 +7,19 @@
 
 Реализуйте вариант без и с генераторным выражением
 """
+
+
+def find_befor_min(input_list):
+    new_list = []
+    for x in range(len(input_list) - 1):
+        if input_list[x] < input_list[x + 1]:
+            new_list.append(input_list[x + 1])
+    return new_list
+
+
+current_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+generated_list = [round(el*0.23) for el in current_list]
+
+print(find_befor_min(current_list))
+print(find_befor_min(generated_list))
+
