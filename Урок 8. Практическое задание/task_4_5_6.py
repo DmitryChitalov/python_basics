8701ca125bc5153be3090f786cfbac5f0885791c
Bekbolat Janassov <janassov@magnum.kz>
2022-08-27 18:06:31 +0600

    half of work done:

--- Урок 8. Практическое задание/task_4_5_6.py
+++ Урок 8. Практическое задание/task_4_5_6.py
@@ -19,3 +19,54 @@
 Подсказка: постарайтесь по возможности реализовать в проекте
 «Склад оргтехники» максимум возможностей, изученных на уроках по ООП.
 """
+from abc import ABC
+
+class WareHouse:
+    '''Склад'''
+    pass
+
+class OfficeEquipment(ABC):
+    '''оргтехника'''
+    def __init__(self, brand, model,
+    inv_number):
+        self.brand = brand
+        self.model = model
+        self.inv_number = inv_number
+
+class Printer(OfficeEquipment):
+    '''принтер'''
+    def __init__(self, brand, model,
+    inv_number, dev_type, print_type, print_speed):
+        self.dev_type = dev_type
+        self.print_type = print_type
+        self.print_speed = print_speed
+        super().__init__(brand, model, inv_number)
+
+class Scanner(OfficeEquipment):
+    '''сканер'''
+    def __init__(self, brand, model, inv_number,
+    scan_type, max_letter_type, max_dpi):
+        self.scan_type = scan_type
+        self.max_letter_type = max_letter_type
+        self.max_dpi = max_dpi
+        super().__init__(brand, model, inv_number)
+
+class Xerox(Printer, Scanner):
+    '''ксерокс'''
+    def __init__(self, brand, model, inv_number, dev_type, print_type,
+    print_speed,scan_type, max_letter_type, max_dpi):
+        self.brand = brand
+        self.model = model
+        self.inv_number = inv_number
+        self.dev_type = dev_type
+        self.print_type = print_type
+        self.print_speed = print_speed
+        self.scan_type = scan_type
+        self.max_letter_type = max_letter_type
+        self.max_dpi = max_dpi
+
+
+xer_1 = Xerox('brand','model', 12344,'dev_type',
+'print_type', 'print_speed', 'scan_type', 'max_letter_type', 'max_dpi')
+
+print(xer_1.__dict__)
