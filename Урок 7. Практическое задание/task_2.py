c9c71e5d7bbe8f1b54fe5dd7dacade1138875ef5
ddulebenets <dd@redtrack.io>
2022-08-27 22:40:49 +0400

    homework_7

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -25,3 +25,42 @@
 
 Два класса: абстрактный и Clothes
 """
+from abc import ABC, abstractmethod
+
+
+class AbstractClothes(ABC):
+    @abstractmethod
+    def get_cloth_coat(self):
+        pass
+
+    @abstractmethod
+    def get_cloth_suit(self):
+        pass
+
+    def total_cloth(self):
+        pass
+
+
+class Clothes(AbstractClothes):
+    def __init__(self, v, h):
+        self.v = v
+        self.h = h
+        self.coat_consumption = self.v / 6.5 + 0.5
+        self.suit_consumption = self.h * 2 + 0.3
+
+    def get_cloth_coat(self):
+        return f'Расход ткани на пальто = {self.coat_consumption:.2f}'
+
+    def get_cloth_suit(self):
+        return f'Расход ткани на костюм = {self.suit_consumption:.2f}'
+
+    @property
+    def total_cloth(self):
+        return f'Общий расход ткани = {self.coat_consumption + self.suit_consumption:.2f}'
+
+
+clothes = Clothes(38, 180)
+
+print(clothes.get_cloth_coat())
+print(clothes.get_cloth_suit())
+print(clothes.total_cloth)
