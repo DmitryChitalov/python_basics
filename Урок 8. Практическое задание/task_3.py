bd4e279650aa98edae484831ea06ba12b754d32d
Andrew Emelyanenko <and.maced-one@yandex.ru>
2022-08-28 21:06:24 +0300

    Update task_3.py

--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -9,3 +9,34 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+
+"""
+Выполенине! Емельяненко А.А.
+"""
+
+
+class Error:
+    def __init__(self, *args):
+        self.my_list = []
+
+    def my_input(self):
+
+        while True:
+            try:
+                val = int(input('Введите значения и нажимайте Enter - '))
+                self.my_list.append(val)
+                print(f'Текущий список - {self.my_list} \n ')
+            except:
+                print(f"Недопустимое значение - строка и булево")
+                y_or_n = input(f'Попробовать еще раз? Y/N ')
+
+                if y_or_n == 'Y' or y_or_n == 'y':
+                    print(try_except.my_input())
+                elif y_or_n == 'N' or y_or_n == 'n':
+                    return f'Вы вышли'
+                else:
+                    return f'Вы вышли'3
+
+
+try_except = Error(1)
+print(try_except.my_input())
