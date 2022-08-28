778ff5168a47e2bdd1403aa4dda27cbb8a673677
Nike6op3oB <st.dimon.it@gmail.com>
2022-08-28 11:54:50 +0300

    Lesson_7_done

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -33,6 +33,43 @@
 class Matrix:
     def __init__(self, my_lst):
         self.my_lst = my_lst
+
+    def __str__(self):
+        new_view = [str(el).strip('[]').replace(',', '')
+                    for el in self.my_lst]
+        return "\n".join(new_view)
+
+    def __add__(self, other):
+        for n in range(len(self.my_lst)):
+            for m in range(len(self.my_lst)):
+                self.my_lst[n][m] = int(self.my_lst[n][m]) + int(other.my_lst[n][m])
+        return Matrix(self.my_lst)
+
+
+a = Matrix([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+])
+
+b = Matrix([
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+])
+
+print(f"{a.__dict__}\n{b.__dict__}")
+
+print(f"Значения матрицы а:\n{a}")
+print(f"Значения матрицы b:\n{b}")
+
+c = a + b
+print(f"Сумма матриц а и b:\n{c}")
+
+
+class Matrix:
+    def __init__(self, my_lst):
+        self.my_lst = my_lst
     def __str__(self):
         new_view = [str(el).strip('[]').replace(',', '')
                     for el in self.my_lst]
--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -29,6 +29,44 @@
 
 from abc import ABC, abstractmethod
 
+
+class AbstractClass(ABC):
+    @abstractmethod
+    def coat(self):
+        pass
+
+    @abstractmethod
+    def suit(self):
+        pass
+
+
+class Clothes(AbstractClass):
+
+    def __init__(self, v, h):
+        self.v = v
+        self.h = h
+
+    def coat(self):
+        result = round(self.v / 6.5 + 0.5, 2)
+        return f'Расход ткани на пальто = {result}'
+
+    def suit(self):
+        result = round(2 * self.h + 0.3, 2)
+        return f'Расход ткани на костюм = {result}'
+
+    @property
+    def total_consumption(self):
+        return f'Общий расход ткани = {self.v / 6.5 + 0.5 + 2 * self.h + 0.3 :.2f}'
+
+
+a = Clothes(7, 10)
+print(a.coat())
+print(a.suit())
+print(a.total_consumption)
+
+
+from abc import ABC, abstractmethod
+
 class AbstractClass(ABC):
     @abstractmethod
     def coat(self):
