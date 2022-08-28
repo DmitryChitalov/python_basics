801133000665ca2e46990f37ccb61623147a4947
Дмитрий Стовпченко <86959489+Nike6op3oB@users.noreply.github.com>
2022-08-28 12:25:42 +0300

    Lesson_8_done

--- Урок 8. Практическое задание/task_7.py
+++ Урок 8. Практическое задание/task_7.py
@@ -6,3 +6,20 @@
 создав экземпляры класса (комплексные числа) и выполнив сложение и умножение созданных экземпляров.
 Проверьте корректность полученного результата.
 """
+class ComplexNumber:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __add__(self, other):
+        return f'The sum is {self.a + other.a} + {self.b + other.b}i'
+
+    def __mul__(self, other):
+        return f'Multiplication is {self.a * other.a - (self.b * other.b)} ' \
+               f'+ {self.b * other.a}i'
+
+
+first_numbers = ComplexNumber(478, 7)
+second_numbers = ComplexNumber(71, 724)
+print(first_numbers + second_numbers)
+print(first_numbers * second_numbers)
