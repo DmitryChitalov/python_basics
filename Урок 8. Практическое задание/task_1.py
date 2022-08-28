2e4a1a8233c3687a8619dae9158db41a5dd13df9
Artem <gghicat@gmail.com>
2022-08-28 11:53:18 +0600

    lesson-8 kotov

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,36 @@
 Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца
 и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
 """
+
+
+class MyDate:
+    day_month_year = None
+
+    def __init__(self, day_month_year):
+        MyDate.day_month_year = day_month_year
+
+    @classmethod
+    def parse(cls):
+        dmy = MyDate.day_month_year.split("-")
+        day = int(dmy[0])
+        month = int(dmy[1])
+        year = int(dmy[2])
+        return f"День:{day} Месяц:{month} Год:{year}"
+
+    @staticmethod
+    def valid():
+        try:
+            dmy = MyDate.day_month_year.split("-")
+            day = int(dmy[0])
+            month = int(dmy[1])
+            year = int(dmy[2])
+            return "Дата корректна" if day >= 1 | day <= 31 | month >= 1 | month <= 12 | year >= 1990 | year <= 9999 \
+                else "Дата не корректна"
+        except ValueError:
+            return "Дата не корректна"
+
+
+print(MyDate("1-5-2022").parse())
+print(MyDate("25-5-2022").parse())
+print(MyDate("25-5-2022").valid())
+print(MyDate("250-5-2022").valid())
--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -9,3 +9,34 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+
+
+def is_integer(n):
+    try:
+        float(n)
+    except ValueError:
+        return False
+    else:
+        return float(n).is_integer()
+
+
+class MyControlInput(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
+int_list = []
+while True:
+    try:
+        inp_data = input("Введите число для добавления в список, для выхода введите exit: ")
+
+        if inp_data == "exit":
+            break
+
+        if not is_integer(inp_data):
+            raise MyControlInput(inp_data)
+        int_list.append(int(inp_data))
+    except MyControlInput as err:
+        print("Введите целое число")
+
+print(f"Результат: {int_list}")
