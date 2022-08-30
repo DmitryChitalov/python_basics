0bae91899e77fddcb0ada704c2cd7aac51bad21d
Sashka1988 <mr.penych@gmail.com>
2022-08-30 13:48:33 +0300

    lesson_7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -28,3 +28,43 @@
 8 10 12
 14 16 18
 """
+class Matrix:
+    def __init__(self, list_of_lists):
+        self.mat = list_of_lists
+
+    def __str__(self):
+        string = ''
+        for i in self.mat:
+            for j in i:
+                string = string + '%s\t' % (j)
+            string = string[:-1]
+            string = string + '\n'
+        string = string[:-1]
+        return string
+
+    def __add__(self, other):
+        result = []
+        numbers = []
+        for i in range(len(self.mat)):
+            for j in range(len(self.mat[0])):
+                summa = other.mat[i][j] + self.mat[i][j]
+                numbers.append(summa)
+                if len(numbers) == len(self.mat[0]):
+                    result.append(numbers)
+                    numbers = []
+        return Matrix(result)
+
+
+a = [[3, 5, 32], [2, 4, 6], [-1, 64, -8]]
+b = [[2, 3, 3], [-2, 1, -6], [5, -3, 0]]
+m = Matrix(a)
+mm = Matrix(b)
+
+print("\nМатрица 1")
+print(m.__str__(), "\n")
+
+print("Матрица 2")
+print(mm.__str__(), "\n")
+
+print("Сумма матриц 1 и 2")
+print(m + mm)
