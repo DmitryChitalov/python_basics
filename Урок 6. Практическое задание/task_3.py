343d75064e6fc79df7e186360472a55cb7932aa7
Sashka1988 <mr.penych@gmail.com>
2022-08-30 13:36:58 +0300

    lesson_6

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -15,3 +15,25 @@
 П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку __str__
 __str__(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
 """
+class Worker:
+
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": int(wage), "bonus": int(bonus)}
+
+
+class Position(Worker):
+    def __init__(self, name, surname, position, wage, bonus):
+        super().__init__(name, surname, position, wage, bonus)
+
+    def get_full_name(self):
+        return self.name + ' ' + self.surname
+
+    def get_total_income(self):
+        return self._income["wage"] + self._income["bonus"]
+
+
+p = Position('Александр', 'Петров', 'сотрудник', '100000', '9000')
+print(p.get_full_name(), p.get_total_income())
--- Урок 6. Практическое задание/task_4.py
+++ Урок 6. Практическое задание/task_4.py
@@ -20,3 +20,60 @@ speed, color, name, is_police (булево).
 Выполните доступ к атрибутам, выведите результат.
 Выполните вызов методов и также покажите результат.
 """
+class Vehicle:
+    def __init__(self, speed, color, name, is_police):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def police(self):
+        if self.is_police:
+            return 'Полицейская машина'
+        else:
+            return 'Гражданская машина'
+
+    def full_info(self):
+        return " {} {} Максимальная скорость {} км/ч ".format(self.color, self.name, str(self.speed))
+
+    def go(self):
+        return "Машина поехала"
+
+    def stop(self):
+        return"Машина остановилась"
+
+    def turn(self):
+        return"Машина повернула"
+
+
+class TownCar(Vehicle):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+
+class SportCar(Vehicle):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+
+class WorkCar(Vehicle):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+
+class PoliceCar(Vehicle):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+
+t_c = TownCar(160, "Black", "Shkoda", False)
+print(t_c.police() + t_c.full_info() + t_c.turn())
+
+s_c = SportCar(250, "Red", "Audi", False)
+print(s_c.police() + s_c.full_info() + s_c.go())
+
+w_c = WorkCar(50, "Green", "UAZ", False)
+print(w_c.police() + w_c.full_info() + w_c.stop())
+
+p_c = PoliceCar(400, "White", "BMW", True)
+print(p_c.police() + p_c.full_info() + p_c.go())
