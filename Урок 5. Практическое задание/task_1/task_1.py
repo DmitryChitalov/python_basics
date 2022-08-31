8af7b55ff4479195bc450a2f5119be224f640ad4
mgorya4ev <i@mgorya4ev.ru>
2022-08-31 22:11:37 +0300

    lesson_05 by Mikhail Goryachev

--- Урок 5. Практическое задание/task_1/task_1.py
+++ Урок 5. Практическое задание/task_1/task_1.py
@@ -3,3 +3,10 @@
 записать в него построчно данные, вводимые пользователем.
 Об окончании ввода данных свидетельствует пустая строка.
 """
+
+with open(r"my_random_text_file.txt", "w") as f_obj:
+    while True:
+        in_text = input()
+        f_obj.write(in_text + '\n')
+        if in_text == "":
+            break
