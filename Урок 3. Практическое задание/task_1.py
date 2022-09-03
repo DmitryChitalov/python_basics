5bc8b932408fa22f7f1772e2088b3586a061c149
SoninAleksey <9263976930@mail.ru>
2022-09-03 17:52:04 +0200

    lesson-3

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -17,3 +17,12 @@ Process finished with exit code 0
 
 Process finished with exit code 0
 """
+def posit_argum(a, b):
+    try:
+        return a / b
+    except ZeroDivisionError:
+        print(f"Вы что? Пытаетесь делить на 0!")
+
+a = int(input("Введите первое число: "))
+b = int(input("Введите второе число: "))
+print(f"Результат: {posit_argum(a, b)}")
--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -8,3 +8,10 @@
 Иван Иванов 1846 года рождения, проживает в городе Москва,
 email: jackie@gmail.com, телефон: 01005321456
 """
+def user_data(name, lastname, born_year, city, email, cell_phone):
+    return print(f"{name} {lastname} {born_year} года рождения,"
+                 f" проживает в городе {city}, e-mail: {email}, телефон: {cell_phone}")
+
+
+user_data(name=input("Имя: "), lastname=input("Фамилия: "), born_year=input("Год Рождения: "),
+          city=input("Город проживания: "), email=input("E-Mail: "), cell_phone=input("Телефон: "), )
