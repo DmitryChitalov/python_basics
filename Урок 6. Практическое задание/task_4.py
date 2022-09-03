6508cc79a7089795febb3d390801952706542a06
SoninAleksey <9263976930@mail.ru>
2022-09-03 18:22:07 +0200

    lesson-6

--- Урок 6. Практическое задание/task_4.py
+++ Урок 6. Практическое задание/task_4.py
@@ -20,3 +20,59 @@ speed, color, name, is_police (булево).
 Выполните доступ к атрибутам, выведите результат.
 Выполните вызов методов и также покажите результат.
 """
+class Car:
+
+    def __init__(self, name, speed, color, is_police=False):
+        self.name = name
+        self.speed = speed
+        self.color = color
+        self.is_police = is_police
+
+    def go(self):
+        return f"машина {self.name} поехала"
+
+    def stop(self):
+        return f"\nМашина {self.name} остановилась"
+
+    def turn(self, direction):
+        return f"\nМашина {self.name} повернула {direction}"
+
+    def show_speed(self):
+        return f"\nТекущая скорость автомобиля {self.speed} км\ч"
+
+
+class TownCar(Car):
+    def show_speed(self):
+        if self.speed > 60:
+            return f"\nВаша скорость выше допустимой - {self.speed} км\ч. Пожалуйста сбавьте обороты"
+        else:
+            return f"Скорость автомобиля - {self.name} в норме"
+
+
+class SportCar(Car):
+    pass
+
+
+class WorkCar(Car):
+    def show_speed(self):
+        if self.speed > 40:
+            return f"\nВаша скорость выше допустимой - {self.speed} км\ч. Пожалуйста сбавьте обороты"
+        else:
+            return f"\nСкорость автомобиля - {self.name} в норме"
+
+
+class PoliceCar(Car):
+    pass
+
+
+t = TownCar("Toyota", 85, "Белая", False)
+print("1:\n" + t.color, t.go(), t.show_speed(), t.turn("на лево"), t.turn("на право"), t.stop())
+
+s = SportCar("Mercedes", 200, "Черная", False)
+print("2:\n" + s.color, s.go(), s.show_speed(), s.turn("на лево"), s.stop())
+
+w = WorkCar("Opel", 40, "Красная", False)
+print("3:\n" + w.color, w.go(), w.show_speed(), w.turn("на право"), w.stop())
+
+p = PoliceCar("BMW", 59, "Синяя", False)
+print("4:\n" + p.color, p.go(), p.show_speed(), p.turn("на право"), p.turn("на лево"), p.stop())
--- Урок 6. Практическое задание/task_5.py
+++ Урок 6. Практическое задание/task_5.py
@@ -13,3 +13,37 @@
 Для каждого из классов метод должен выводить уникальное сообщение.
 Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
 """
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print(f"Запуск отрисовки {self.title}")
+
+
+class Pen(Stationery):
+    def draw(self):
+        print(f"Отрисовка ручкой {self.title}")
+
+
+class Pencil(Stationery):
+    def draw(self):
+        print(f"Отрисовка карандашем {self.title}")
+
+
+class Handle(Stationery):
+    def draw(self):
+        print(f"Отрисовка маркером {self.title}")
+
+
+stationery = Stationery("'Канцелярская принадлежность'")
+stationery.draw()
+
+pen = Pen("шариковой")
+pen.draw()
+
+pencil = Pencil("чернографитным")
+pencil.draw()
+
+handle = Handle("перманентным для всех поверхностей")
+handle.draw()
