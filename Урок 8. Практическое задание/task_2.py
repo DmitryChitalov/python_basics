0a1be6ead830c57f639acf14357a0522ad947b51
SoninAleksey <9263976930@mail.ru>
2022-09-03 18:26:44 +0200

    lesson-8

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,24 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+class Dichotomy(Exception):
+    def __init__(self, data):
+        self.data = data
+
+
+def div_num():
+    try:
+        num_a = int(input("Введите первое число: "))
+        num_b = int(input("Введите второе число: "))
+        if num_b == 0:
+            raise Dichotomy("Нельзя делить на ноль")
+        else:
+            num_c = num_a / num_b
+            return num_c
+    except ValueError:
+        return "Данные введены некорректно"
+    except Dichotomy as fatal_error:
+        return fatal_error
+
+
+print(div_num())
--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -9,3 +9,32 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+class ClassExcept(Exception):
+    def __init__(self, data):
+        self.data = data
+
+
+def Check_content_list(str):
+    if str.isdigit():
+        return str
+    else:
+        try:
+            float(str)
+            return str
+        except ValueError:
+            raise ClassExcept(f"Ошибка: {str}")
+
+
+numm = ""
+list_num = []
+print("Чтобы выйти нажмите 'q'")
+print("Ввод числа по одному:")
+while numm != "q":
+    try:
+        numm = input("- ")
+        list_num.append(Check_content_list(numm))
+    except ClassExcept as i:
+        if numm != "q":
+            print(i.data)
+
+print(f"\nСписок: {list_num}")
