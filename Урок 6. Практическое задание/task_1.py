9189bb640637b41d6a74f9d6649f339a2bd470ba
Lordofwinterfield <tim979767@yandex.ru>
2022-09-04 13:16:34 +0300

    task1

--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -15,3 +15,18 @@
 
 Проверить работу примера, создав экземпляр и вызвав описанный метод.
 """
+import time
+
+
+class TrafficLight:
+    __color = {'Красный': 7, 'Желтый': 2, 'Зеленый': 5}
+
+    @staticmethod
+    def running():
+        for key, value in TrafficLight.__color.items():
+            print(f'Светофор переключился в {key} цвет')
+            time.sleep(value)
+
+
+TL = TrafficLight()
+TL.running()
