35aae02e0ffc1f77fc4b86fb91f5e760b9626ef9
Lordofwinterfield <tim979767@yandex.ru>
2022-09-07 21:34:28 +0300

    task1

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -28,3 +28,23 @@
 8 10 12
 14 16 18
 """
+class Matrix:
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def __add__(self, other):
+        for first_matrix in range(len(self.matrix)):
+            for second_matrix in range(len(self.matrix)):
+                self.matrix[first_matrix][second_matrix] = int(self.matrix[first_matrix][second_matrix]) + int(
+                    other.matrix[first_matrix][second_matrix])
+
+        return Matrix(self.matrix)
+
+    def __str__(self):
+        new_str = [str(first_matrix).strip('[]').replace(',', '') for first_matrix in self.matrix]
+        return "\n".join(new_str)
+
+
+matrix1 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+matrix2 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(matrix1 + matrix2)
