058fb61a57e5b370a577c427eb9d59252d022073
mgorya4ev <mgorya4ev@yandex.ru>
2022-09-11 13:06:27 +0300

    lesson-08 by Mikhail Goryachev

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,23 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+
+
+class MyException(Exception):
+
+    @staticmethod
+    def division_func(a, b):
+        try:
+            res = round(a / b, 2)
+        except ZeroDivisionError:
+            print(f"{a} / {b} -> на ноль делить нельзя!\n")
+        else:
+            print(f"{a} / {b} = {res} \n")
+
+
+e_obj = MyException()
+
+e_obj.division_func(1, 2)
+e_obj.division_func(1, 0)
+e_obj.division_func(-1, 5)
+e_obj.division_func(0, 1)
