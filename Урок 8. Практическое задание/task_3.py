9688a3aa4ad8a7bfb567075d5663dacdb6016a96
Orehov Roman <rorekhov@rating.kz>
2022-09-13 22:41:55 +0600

    Орехов Роман ДЗ №8

--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -9,3 +9,24 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+
+
+class IsNotInteger(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+input_integer = 0
+array_integers = []
+while input_integer != 999:
+    print("Для завершения программы введите \"999\"")
+    try:
+        input_integer = int(input("Введите число: "))
+        if type(input_integer) != int:
+            raise IsNotInteger("Что ты творишь?!")
+        else:
+            if input_integer != 999:
+                array_integers.append(input_integer)
+    except ValueError:
+        print("Эти каракули - не число!")
+print(array_integers)
--- Урок 8. Практическое задание/task_7.py
+++ Урок 8. Практическое задание/task_7.py
@@ -6,3 +6,23 @@
 создав экземпляры класса (комплексные числа) и выполнив сложение и умножение созданных экземпляров.
 Проверьте корректность полученного результата.
 """
+
+
+class ComplexNumber:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+        self.z = 'a + b * i'
+
+    def __add__(self, other):
+        return f'Результат сложения комлексных чисел: {self.a + other.a} + {self.b + other.b}i'
+
+    def __mul__(self, other):
+        return f'Результат умножения комлексных чисел: {self.a * other.a - (self.b * other.b) + (self.b * other.a)}i' \
+               f'+{self.b * other.a}i'
+
+
+first_numbers = ComplexNumber(233, 4)
+second_numbers = ComplexNumber(12, 132)
+print(first_numbers * second_numbers)
+print(first_numbers + second_numbers)
