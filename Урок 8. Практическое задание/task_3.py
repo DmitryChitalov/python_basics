654daafba9bfea90bab9be28a5c8047da5a000cc
Alexander Salamatin <alexsw@gmail.com>
2022-09-14 11:58:55 +0300

    Задания урока 8

--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -9,3 +9,34 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+
+
+class MyListError(Exception):
+    def __init__(self):
+        print("Exception: неверный элемент списка!")
+
+    @staticmethod
+    def test_element(arg):
+        """
+        Проверяет аргумент-строку на содержание в ней только чисел
+        :param arg: строка для проверки
+        :return: True - если в строке только числа, False - в остальных случаях
+        """
+        return arg.isdigit()
+
+
+lst = []
+
+while True:
+    s = input("Введите число для списка (пустая строка для завершения:")
+    if s == '':
+        break
+    try:
+        if not MyListError.test_element(s):
+            raise MyListError
+    except MyListError:
+        print("Сработало исключение. Введите число!")
+    else:
+        lst.append(int(s))
+
+print(f"Введены элементы в список: {lst}")
