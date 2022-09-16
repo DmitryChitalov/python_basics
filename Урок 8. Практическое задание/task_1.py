409257ead50f10076e45b399b7f30048ff7e0894
IvanSusanin <send-to-master@yandex.ru>
2022-09-16 14:46:02 +0500

    Урок 8 Задание 1 и 2

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,37 @@
 Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца
 и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
 """
+
+# Выводим на экран задание
+print('\nУрок 8 Задание 1\n')
+
+# Создаем класс
+class DateCheck:
+    def __init__(self, day_month_year):
+        self.day_month_year = str(day_month_year)
+
+    @classmethod
+    def extr_date(cls, day_month_year):
+        date_list = list(day_month_year.split('.'))
+        DateCheck.day = int(date_list[0])
+        DateCheck.month = int(date_list[1])
+        DateCheck.year = int(date_list[2])
+
+    @staticmethod
+    def date_check():
+        if DateCheck.year > 0 and DateCheck.year <= 2022:
+            if DateCheck.month > 0 and DateCheck.month < 13:
+                if DateCheck.day > 0 and DateCheck.day < 31:
+                    return f'Сегодня {DateCheck.day}.{DateCheck.month}.{DateCheck.year} г.'
+                else:
+                    print('Такой даты не существует!')
+            else:
+                print('Такого месяца не существует!')
+        else:
+            print('Такой год еще не настал!')
+
+
+# Объявняем переменные и выводим на экран
+input_date = input('Введите дату(дд.мм.гггг): ')
+DateCheck.extr_date(input_date)
+print(DateCheck.date_check())
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,26 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+
+# Выводим на экран задание
+print('\nУрок 8 Задание 2\n')
+
+# Объявляем класс
+class DenominatorNotZero(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+# Определяем переменные
+in_numerator = int(input('Введите числитель: '))
+in_denominator = int(input('Введите знаминатель: '))
+
+# Проверяем знаминатель и выводим результат
+try:
+    if in_denominator == 0:
+        raise DenominatorNotZero('Знаминатель не может быть равен 0!')
+except ValueError:
+    print('Вы ввели не число')
+except DenominatorNotZero as err:
+    print (err)
+else:
+    print(f'{in_numerator} / {in_denominator} = {"{:.2f}".format(in_numerator / in_denominator)}')
