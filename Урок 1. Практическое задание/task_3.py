ccd31b57d496be510a7007fa158567d177427c6b
IvanSubbotin1 <103884993+IvanSubbotin1@users.noreply.github.com>
2022-10-08 13:26:39 +0300

    Update task_3.py

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,9 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+n = int(input('Введите число>>>'))
+numb = str(n)
+n1 = numb + numb
+n2 = numb + numb + numb
+result = n + int(n1) + int(n2)
+print("Результат:", result )
