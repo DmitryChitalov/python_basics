c36b566123d5a63db7ef87020d4f8b2e29e102d2
acronoo <dima.ourlove@gmail.com>
2022-10-09 21:07:12 +0300

    task 2 complete

--- Урок 2. Практическое задание/task_2.py
+++ Урок 2. Практическое задание/task_2.py
@@ -11,3 +11,11 @@
 Введите целые числа через пробел: 1 2 3
 Результат: 2 1 3
 """
+
+new_elements = input('Введите целые числа через пробел: ')
+new_list = new_elements.split()
+for ind, el in enumerate(new_list):
+    if ind % 2 == 0:
+        new_list.pop(ind)
+        new_list.insert(ind + 1, el)
+print(new_list)
