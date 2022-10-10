8f0fcfb3fa4729a92487e35e8ee823142a15e5e5
studpoly <38002847+studpoly@users.noreply.github.com>
2022-10-10 17:49:40 +0300

    Update task_3.py

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,11 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+count = input("Введите число n: ")
+count_1 = int(count)
+str_2 = count + count
+count_2 = int(str_2)
+str_3 = count + count + count
+count_3 = int(str_3)
+res = count_1 + count_2 + count_3
+print(f"n + nn + nnn = {res}")
