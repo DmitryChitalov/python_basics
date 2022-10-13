9abb26be4d063e8505cf46b19fd3451982a59d7c
IvanSubbotin1 <103884993+IvanSubbotin1@users.noreply.github.com>
2022-10-13 11:32:39 +0300

    Update task_2.py

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -8,3 +8,21 @@
 Иван Иванов 1846 года рождения, проживает в городе Москва,
 email: jackie@gmail.com, телефон: 01005321456
 """
+name2 = str(input("Input name: "))
+surname2 = str(input("Input surname: "))
+year2 = str(input("Input year: "))
+city2 = str(input("Input city: "))
+email2 = str(input("Input email: "))
+phone2 = str(input("Input phone: "))
+
+def personal_info(name,surname,year,city,email,phone):
+    return ' '.join([
+        "name: ", name,
+        ". surname: ", surname,
+        ". year: ", year,
+        ". city: ", city,
+        ". email: ", email,
+        ". phone: ", phone, "."
+        ])
+
+print(personal_info(name=name2, surname=surname2, year=year2, city=city2, email=email2, phone=phone2))
