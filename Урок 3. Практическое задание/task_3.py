3ca56078809887d7f9909a62aa56bd4fc57fef5c
IvanSubbotin1 <103884993+IvanSubbotin1@users.noreply.github.com>
2022-10-13 11:33:04 +0300

    Update task_3.py

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -5,3 +5,19 @@
 1) используя функцию sort()
 2) без функции sort()
 """
+def my_func(*args):
+    args1 = int(input("args1: "))
+    args2 = int(input("args2: "))
+    args3 = int(input("args3: "))
+
+    if args1 >= args2 > args3:
+        return args1 + args2
+    elif args3 >= args2 > args1:
+        return args3 + args2
+    elif args1 >= args3 > args1:
+        return args1 + args3
+    else:
+        exit(-1)
+
+print(my_func())
+
