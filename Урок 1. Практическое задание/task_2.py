40c7c846604b2667b0e455053f804bfb0628c08e
xeon <aleksandr.zhiltsov@tortugasocial.com>
2022-10-13 19:08:25 +0300

    solve 2 task

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -9,3 +9,16 @@
 Введите время в секундах: 3600
 Время в формате ч:м:с - 1.0 : 60.0 : 3600
 """
+
+seconds = int(input("Введите время в секундах: "))
+hours = seconds / 3600.0
+minutes = seconds / 60.0
+
+print("Время в пеерсчете ч:м:с - %.1f : %.1f : %d" % (hours, minutes, seconds))
+
+pass_hours = seconds // 3600
+pass_minutes = seconds // 60 - pass_hours * 60
+pass_seconds = seconds - pass_minutes * 60 - pass_hours * 3600
+
+print(f"Время в формате ч:м:с - {pass_hours:02} : {pass_minutes:02} : {pass_seconds:02}")
+
