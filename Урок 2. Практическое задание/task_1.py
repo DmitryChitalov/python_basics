40d978477c691f4e476b459dc14f6994a89da86e
xeon <aleksandr.zhiltsov@tortugasocial.com>
2022-10-16 11:41:26 +0300

    task 1 complete

--- Урок 2. Практическое задание/task_1.py
+++ Урок 2. Практическое задание/task_1.py
@@ -15,3 +15,19 @@
 <class 'bool'>
 <class 'NoneType'>
 """
+
+any_types = ["abc", 34, True, 0.12367, None, [1, 2, 3], ('a', 'b', 'c'), {11, 3, 8, 4},
+             {"key1": "val1", "key2": "val2"}, b'bytes']
+
+for el in any_types:
+    print(type(el))
+
+print("---------------------------------")
+
+for el in any_types:
+    print(f"element '{el}' of type {type(el)}")
+
+print("---------------------------------")
+
+list_of_string = list('simple string')
+print(list_of_string)
