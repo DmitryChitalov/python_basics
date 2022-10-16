289af1628ce649902c5b5b0c138e00f037cfcb73
acronoo <dima.ourlove@gmail.com>
2022-10-16 16:58:00 +0300

    all tasks done

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -5,3 +5,21 @@
 1) используя функцию sort()
 2) без функции sort()
 """
+
+
+# Используя sort
+def my_func(var_1, var_2, var_3):
+    arg_list = [var_1, var_2, var_3]
+    arg_list.sort(reverse=True)
+    return arg_list[0] + arg_list[1]
+
+
+# Не используя sort
+def my_func2(var_1, var_2, var_3):
+    x = [var_1, var_2, var_3]
+    args_list = [x.pop(x.index(max(x))), max(x)]
+    return args_list[0] + args_list[1]
+
+
+print(my_func(100, 300, 600))
+print(my_func2(200, 50, 80))
