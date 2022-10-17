203a3b72fdc752f78698c5b4cd51b1634750f5e5
studpoly <38002847+studpoly@users.noreply.github.com>
2022-10-17 18:15:20 +0300

    Update task_5.py

--- Урок 3. Практическое задание/task_5.py
+++ Урок 3. Практическое задание/task_5.py
@@ -6,3 +6,22 @@
     символ введен после нескольких чисел, то вначале нужно добавить сумму этих чисел к полученной
     ранее сумме и после этого завершить программу.
 """
+def my_sum ():
+    sum_res = 0
+    ex = False
+    while ex == False:
+        number = input('Введите числа или нажмите X для выхода: ').split()
+
+        res = 0
+        for el in range(len(number)):
+            if number[el] == 'x' or number[el] == 'X':
+                ex = True
+                break
+            else:
+                res = res + int(number[el])
+        sum_res = sum_res + res
+        print(f'Текущая сумма чисел: {sum_res}')
+    print(f'Итоговая сумма чисел: {sum_res}')
+
+
+my_sum()
