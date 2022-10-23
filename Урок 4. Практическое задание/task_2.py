f7f44df78987e5eb40eaa75d2546ee255491bb05
xeon <aleksandr.zhiltsov@tortugasocial.com>
2022-10-23 16:38:03 +0300

    task 2 complete

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -7,3 +7,22 @@
 
 Реализуйте вариант без и с генераторным выражением
 """
+
+from itertools import filterfalse
+
+MIN_THRESHOLD = 50
+
+list_num = [130, 25, 13, 54, 88, 11, 43, 76, 99, 51, 64, 86, 111, 36]
+print(f"Исходный список: {list_num}")
+
+list_for = []
+for el in list_num:
+    if el > MIN_THRESHOLD:
+        list_for.append(el)
+print(f"С помощью for-in: {list_for}")
+
+list_gen = [el for el in list_num if el > MIN_THRESHOLD]
+print(f"С помощью генератора: {list_gen}")
+
+list_filter_false = list(filterfalse(lambda i: i <= MIN_THRESHOLD, list_num))
+print(f"С помощью filterfalse: {list_filter_false}")
