64e548c0c21401600bb63e300b48d41178415c62
detskiikrem <hellsangel@ya.ru>
2022-10-29 14:36:32 +0300

    lesson_3 task_3 done

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -5,3 +5,17 @@
 1) используя функцию sort()
 2) без функции sort()
 """
+
+
+def my_func_sort(var_1, var_2, var_3):
+    my_tuple = (var_1, var_2, var_3)
+    sorted_tuple = tuple(sorted(my_tuple))
+    return sorted_tuple[-2] + sorted_tuple[-1]
+
+
+def my_func(var_1, var_2, var_3):
+    return var_1 + var_2 + var_3 - min(var_1, var_2, var_3)
+
+
+print(my_func_sort(11, 3, 49))
+print(my_func(11, 3, 49))
