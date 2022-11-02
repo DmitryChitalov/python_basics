baa80ad77ef9b7113bd9921c18255a07c98acb8b
detskiikrem <hellsangel@ya.ru>
2022-11-02 15:51:08 +0300

    lesson_4 task_2 done

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -7,3 +7,18 @@
 
 Реализуйте вариант без и с генераторным выражением
 """
+
+
+my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+nogen_list = []
+for el in range(1, len(my_list)):
+    if my_list[el] > my_list[el - 1]:
+        nogen_list.append(my_list[el])
+        el += 1
+    else:
+        el += 1
+print(nogen_list)
+
+
+gen_list = [my_list[el] for el in range(1, len(my_list)) if my_list[el] > my_list[el - 1]]
+print(gen_list)
