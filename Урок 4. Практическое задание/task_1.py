b3856fc78a3f87de6fb531b8967420a307271aa9
studpoly <38002847+studpoly@users.noreply.github.com>
2022-11-05 18:07:38 +0300

    Update task_1.py

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -6,3 +6,17 @@
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
 """
+from sys import argv
+
+script_name, production_h, bet_h, prize = argv
+
+
+def salary_func(production, bet, bonus):
+    try:
+        print(f"Заработная плата сотрудника: {float(production_h) * float(bet_h) + float(prize)}")
+    except ValueError:
+        print("Убедитесь, что в параметрах переданы числа!")
+        exit()
+
+
+salary_func(production_h, bet_h, prize)
