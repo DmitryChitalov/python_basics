af9518cec4e605ebc7386f909da44d9a0dc5ab0c
studpoly <38002847+studpoly@users.noreply.github.com>
2022-11-05 18:14:36 +0300

    Update task_7.py

--- Урок 5. Практическое задание/task_7/task_7.py
+++ Урок 5. Практическое задание/task_7/task_7.py
@@ -9,3 +9,40 @@
 [{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit": 2000}]
 Подсказка: использовать менеджер контекста.
 """
+import json
+
+result_dict = [{}, {"average_profit": 0.0}, {}]
+
+try:
+    with open("firms.data", encoding="utf-8") as f_obj:
+        for f_line in f_obj:
+            l_parts = f_line.split("  ")
+
+            firm_name = l_parts[0]
+            income = float(l_parts[2])
+            expense = float(l_parts[3])
+            revenue = income - expense
+
+            if revenue > 0:
+                result_dict[0][firm_name] = revenue
+            elif revenue < 0:
+                result_dict[2][firm_name] = revenue
+
+except IOError:
+    print("Произошла ошибка ввода вывода!")
+
+
+profit_count = len(result_dict[0])
+if profit_count > 0:
+    result_dict[1]["average_profit"] = sum(result_dict[0].values()) / profit_count
+
+print(f"Результируюший список:\n{result_dict}")
+
+with open("firms.json", "w+", encoding="utf-8") as write_f:
+    json.dump(result_dict, write_f)
+    write_f.seek(0)
+    print(f"Сохраненный файл:\n {write_f.read()}")
+
+with open("firms.json", encoding="utf-8") as read_f:
+    data = json.load(read_f)
+    print(f"Десереализованный json из файла:\n {data}")
