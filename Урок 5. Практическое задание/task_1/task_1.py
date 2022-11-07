4c66ceedf727ec6529d28c32ce89b886fbf72638
MegaGnome <111235046+MegaGnome@users.noreply.github.com>
2022-11-07 17:15:50 +0300

    Update task_1.py

--- Урок 5. Практическое задание/task_1/task_1.py
+++ Урок 5. Практическое задание/task_1/task_1.py
@@ -3,3 +3,17 @@
 записать в него построчно данные, вводимые пользователем.
 Об окончании ввода данных свидетельствует пустая строка.
 """
+print("Ввод:")
+try:
+    with open("user.txt", "w+", encoding="utf-8") as f_obj:
+        while True:
+            line = input(">")
+            if line == "":
+                break
+            f_obj.write(f"{line}\n")
+
+        print("Текущий файл:")
+        f_obj.seek(0)
+        print(f_obj.read())
+except IOError:
+    print("Ошибка!")
