2c85d02c62e0506dc521f970f0eb2cba21430188
testovic585 <testovic585@gmail.com>
2022-11-09 19:47:06 +0300

    homework8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,38 @@
 Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца
 и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
 """
+
+class Date:
+
+    day = 0
+    month = 0
+    year = 0
+    def __init__(self, day_month_year):
+        self.day_month_year = day_month_year
+
+    @classmethod
+    def method_1(cls, day_month_year):
+        date = []
+        for i in day_month_year.split('.'):
+            date.append(i)
+        day = int(date[0])
+        month = int(date[1])
+        year = int(date[2])
+        print (day,month, year)
+        cls.method_2(day, month, year)
+
+    @staticmethod
+    def method_2(day, month, year):
+        if 1 <= day <= 31:
+            if 1 <= month <= 12:
+                if 0 <= year:
+                    result_date = f'{day} - {month} - {year}'
+                    print(f'Дата корректна - выбрана дата {result_date}')
+                else: print(f'Некорректный год')
+            else: print(f'Некорректный месяц')
+        else: print(f'Некорректная дата')
+
+
+Date.method_1('133.11.2033')
+c1 = Date
+print(c1.method_1('10.11.2033'))
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,23 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+
+
+class MyError(Exception):
+    def __init__(self, data):
+        self.data = data
+
+number_1 = input('введите первое число:')
+number_2 = input('введите второе число:')
+
+try:
+    number_1 = int(number_1)
+    number_2 = int(number_2)
+    if number_2 == 0:
+        raise MyError("На ноль делить нельзя")
+    result_n = number_1/number_2
+except MyError as err:
+    print(err)
+else:
+    print(f'Результат операции :{result_n}')
+
