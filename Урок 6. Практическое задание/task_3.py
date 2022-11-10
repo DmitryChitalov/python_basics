419135a3bba48a52502a2d29cbf7be1b74f7e881
detskiikrem <hellsangel@ya.ru>
2022-11-10 17:44:39 +0300

    lesson_6 task_3 done

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -15,3 +15,27 @@
 П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку __str__
 __str__(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
 """
+
+class Worker:
+    def __init__(self, name, surname, position, wage, bonus):
+            self.name = name
+            self.surname = surname
+            self.position = position
+            self._income = {'wage': wage, 'bonus': bonus}
+
+class Position(Worker):
+    def get_full_name(self):
+        return f'{self.surname} {self.name}'
+    def get_total_income(self):
+        return int(self._income['wage']) + int(self._income['bonus'])
+
+    def __str__(self):
+        return f'__str__:\n{self.surname} {self.name} {self.position} {self.get_total_income()}\n'
+
+
+with open('task3.txt', 'r', encoding='utf-8') as in_file:
+    for line in in_file:
+        param = line.split()
+        fullinfo = Position(name=param[0], surname=param[1], position=param[2], wage=param[3], bonus=param[4])
+        print(f'{fullinfo.get_full_name()} - {fullinfo.position}: {fullinfo.get_total_income()}')
+        print(fullinfo)
