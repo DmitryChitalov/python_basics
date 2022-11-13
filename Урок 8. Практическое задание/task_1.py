9f4615119756982adcafc05008ee971167c3adfa
studpoly <38002847+studpoly@users.noreply.github.com>
2022-11-13 23:40:47 +0300

    Update task_1.py

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,32 @@
 Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца
 и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
 """
+class MyDivZero(Exception):
+    def __init__(self):
+        print("Мой обработчик деления на ноль")
+        return
+
+    @staticmethod
+    def test_div_zero(divider):
+        """
+        Тестирует делитель (число) на равенство нулю.
+        :param divider: делитель
+        :return: True - если делитель равен нулю, False - если делитель не равен нулю
+        """
+        return not divider
+
+
+try:
+    a = int(input("Введите делимое:"))
+    b = int(input("Введите делитель:"))
+    if MyDivZero.test_div_zero(b):
+        raise MyDivZero
+except MyDivZero:
+    print("На ноль делить нельзя!")
+except ValueError:
+    print("Некорректный ввод!")
+else:
+    z = a / b
+    print(f"Результат деления: {a} / {b} = {z}")
+finally:
+    print("Работа программы завершена.")
