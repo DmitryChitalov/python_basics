a0ad8ec0c7423759549a9803eace1e57d355460f
detskiikrem <hellsangel@ya.ru>
2022-11-23 11:25:12 +0300

    lesson_8 task_2 done

--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -9,3 +9,26 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+
+class NotNumber(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
+def only_num():
+    num_list = []
+    while True:
+        user_list = input(f'Введите число, или q для выхода: ')
+        if user_list == 'q':
+            break
+        try:
+            if not user_list.isnumeric():
+                raise NotNumber('Ошибка! Введено не число!')
+            num_list.append(user_list)
+        except NotNumber as err:
+            print(err)
+    print(f'------------------------------------------------------ \nВы ввели следующие числа:\n')
+
+    print(*num_list)
+
+only_num()
