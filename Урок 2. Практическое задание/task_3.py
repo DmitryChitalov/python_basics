113caa7ca79bbc809cab855ccf3d059c4cbbd7fd
asstnkv <asstnkv@gmail.com>
2022-11-27 16:11:56 +0300

    Task 3 finished.

--- Урок 2. Практическое задание/task_3.py
+++ Урок 2. Практическое задание/task_3.py
@@ -8,3 +8,31 @@
 Результат через список: Осень
 Результат через словарь: Осень
 """
+month = int(input("Введите номер месяца: "))
+
+seasons = {"зима": (1, 2, 12),
+           "весна": (3, 4, 5),
+           "лето": (6, 7, 8),
+           "осень": (9, 10, 11)}
+result = "Error"
+for key, values in seasons.items():
+    if month in values:
+        result = key
+        break
+print(f"Результат через словарь: {result}")
+
+my_list = ["зима", "весна", "лето", "осень"]
+if 1 <= month <= 12:
+    if month == 1 or month == 2 or month == 12:
+        print(f"Результат через список: {my_list[0]}")
+    elif month == 3 or month == 4 or month == 5:
+        print(f"Результат через список: {my_list[1]}")
+    elif month == 6 or month == 7 or month == 8:
+        print(f"Результат через список: {my_list[2]}")
+    elif month == 9 or month == 10 or month == 11:
+        print(f"Результат через список: {my_list[3]}")
+    else:
+        print("Результат через список: Error")
+else:
+    print("Результат через список: Error")
+
