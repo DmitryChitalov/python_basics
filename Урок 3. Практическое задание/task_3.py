9f1b7fd3ec7efddfd675b0088917aaaa3c4ea15f
asstnkv <asstnkv@gmail.com>
2022-11-29 16:14:45 +0300

    Task 3 finished.

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -5,3 +5,27 @@
 1) используя функцию sort()
 2) без функции sort()
 """
+
+
+def my_func(a, b, c):
+    first_num = max(a, b, c)
+    if first_num == b:
+        second_num = max(a, c)
+    elif first_num == a:
+        second_num = max(b, c)
+    elif first_num == c:
+        second_num = max(a, b)
+    print(f"Сумма двух наибольших чисел без sort(): {first_num + second_num}")
+
+
+def sort_func(sa, sb, sc):
+    res = sorted([sa, sb, sc])
+    print(f"Сумма двух наибольших чисел с sort: {sum(res[1:3])}")
+
+
+val1 = int(input("Первое число: "))
+val2 = int(input("Второе число: "))
+val3 = int(input("Третье число: "))
+
+my_func(val1, val2, val3)
+sort_func(val1, val2, val3)
