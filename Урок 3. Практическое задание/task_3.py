4810c06b9feccf1fe6c1d7a6ac9a901727bc37c2
denflex <denflex@mail.ru>
2022-12-03 12:32:21 -0500

    renew

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -11,15 +11,16 @@ def my_func(arg_1, arg_2, arg_3):
     sum_2 = arg_2 + arg_3
     sum_3 = arg_1 + arg_3
     if sum_1 > sum_2 and sum_1 > sum_3:
-        return sum_1
+        return (f"Функция без sort: {sum_1}")
     elif sum_2 > sum_3:
-        return sum_2
-    return sum_3
-print(f"Функция без sort: {my_func(1, 1, 1)}")
+        return (f"Функция без sort: {sum_2}")
+    return (f"Функция без sort: {sum_3}")
+print(my_func(4, 3, -2))
 
 
 def my_func_1(*nums):
     s_num = sorted(nums)
     s_sum = s_num[-1] + s_num[-2]
-    return s_sum
-print(f"Функция с sort: {my_func_1(3, 0, -1)}")
+    return (f"Функция с sort: {s_sum}")
+
+print(my_func_1(4, 3, -2))
