88fe66808c09fe18535a0045978efc45b5388e6f
VictorPiv <91976881+VictorPiv@users.noreply.github.com>
2022-12-04 20:40:37 +0300

    Update task_1.py

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -17,3 +17,14 @@ Process finished with exit code 0
 
 Process finished with exit code 0
 """
+
+def division():
+    try:
+        return number_1 / number_2
+    except ZeroDivisionError:
+        return "Делить на 0 - нельзя"
+
+
+number_1 = int(input("Введите 1-е число: "))
+number_2 = int(input("Введите 2-е число: "))
+print(division())
