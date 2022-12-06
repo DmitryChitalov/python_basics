cafb2dd028069f5f5ef4dd389135deffa3a9e497
Netdaem0n <bakalets@gmail.com>
2022-12-06 17:42:19 +0500

    -

--- Урок 4. Практическое задание/task_5.py
+++ Урок 4. Практическое задание/task_5.py
@@ -5,3 +5,8 @@
 Необходимо получить результат вычисления произведения всех элементов списка.
 Подсказка: использовать лямбда-функцию и функцию reduce().
 """
+from functools import reduce
+
+
+my_array = [x for x in range(100, 1001) if x % 2 == 0]
+print(reduce(lambda x, y: x * y, my_array))
--- Урок 4. Практическое задание/task_6.py
+++ Урок 4. Практическое задание/task_6.py
@@ -12,3 +12,28 @@
 Во втором также необходимо предусмотреть условие,
 при котором повторение элементов списка будет прекращено.
 """
+import itertools
+
+
+def iter_one(n=3, end=10):
+    array = []
+    data = itertools.count(start=n, step=1)
+    for i in data:
+        if i >= end:
+            break
+        array.append(i)
+    return array
+
+
+def iter_two(elements=10):
+    array = [1, 23, 45, 5, 67, 78, 4]
+    res_array = []
+    for i in itertools.cycle(array):
+        if len(res_array) > elements:
+            break
+        res_array.append(i)
+    return res_array
+
+
+print(iter_one())
+print(iter_two())
