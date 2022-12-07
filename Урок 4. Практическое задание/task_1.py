88b1637cb4200b72a963fd5f0dba7e42610fc86c
asstnkv <asstnkv@gmail.com>
2022-12-07 17:10:23 +0300

    Task 1 finished.

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -6,3 +6,27 @@
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
 """
+
+
+from sys import argv
+
+
+def my_salary(*args):
+    salary = (hours * rate) + bonus
+    return salary
+
+
+if len(argv) > 1:
+    script_name, hours, rate, bonus = argv
+    hours = int(hours)
+    rate = int(rate)
+    bonus = int(bonus)
+    print(my_salary(argv))
+else:
+    hours = int(input("Inpute time work: "))
+    rate = int(input("Inpute ratet: "))
+    bonus = int(input("Inpute prize: "))
+    print(my_salary(hours, rate, bonus))
+
+
+
