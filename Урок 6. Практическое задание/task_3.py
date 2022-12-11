b93080c91ad79963feefca2aa2e76d2d4afc505d
Vladimir V <roadcat@yandex.ru>
2022-12-11 21:47:27 +0500

    Update task_3.py

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -15,3 +15,51 @@
 П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку __str__
 __str__(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
 """
+
+
+class Worker:
+    """Базовый класс"""
+    name: str
+    surname: str
+    position: str
+    _income: dict
+
+    def total_income(self):
+        """Базовый расчет дохода с учетом сорока часовой недели"""
+
+        total = self._income.get('wage') * 40
+
+        if self._income.get('bonus'):
+            total += self._income.get('bonus')
+
+        return total
+
+    def __str__(self):
+        return f'Способ вывода через "__str__" - {self.name} {self.surname}'
+
+
+class Position(Worker):
+    """Класс Position"""
+
+    def income(self, salery: dict):
+        """Зарплата сотрудника"""
+        self._income = salery
+
+    def get_full_name(self):
+        """Полное имя сотрудника"""
+        return f'Сотрудника зовут - {self.name} {self.surname}'
+
+    def get_total_income(self):
+        """Дохос, с учетом премии"""
+        return f"Доход в неделю составил - {self.total_income()} р."
+
+
+person = Position()
+person.name = "Майк"
+person.surname = "Сваровский"
+person.position = "Программист"
+person.income({'wage': 377, 'bonus': 1110})
+
+print(person.get_full_name())
+print(person.__str__())
+print(person.get_total_income())
