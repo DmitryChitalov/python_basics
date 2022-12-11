0c0483258f70e7d83db9c90f929d046d1cd52b8d
denflex <denflex@mail.ru>
2022-12-11 17:22:34 -0500

    Task_1-5

--- Урок 5. Практическое задание/task_4/task_4.py
+++ Урок 5. Практическое задание/task_4/task_4.py
@@ -7,3 +7,19 @@
 Иванов 23543.12
 Петров 13749.32
 """
+
+def avg(income):
+    return round(sum(income)/len(income), 2)
+
+read_file = open("staff.txt", "r", encoding='utf-8')
+salaries = []
+
+print("Сотрудники с доходом менее 20000:")
+for read_line in read_file.readlines():
+    if read_line != "\n":
+        array_words = read_line.split(" ")
+        salaries.append(float(array_words[1]))
+        if float(array_words[1]) < 20000:
+            print(array_words[0])
+print(f"Средний доход всех сотрудников: ", avg(salaries))
+read_file.close()
--- Урок 5. Практическое задание/task_5/task_5.py
+++ Урок 5. Практическое задание/task_5/task_5.py
@@ -2,3 +2,15 @@
 5)	Создать (программно) текстовый файл, записать в него программно набор чисел,
  разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 """
+
+my_file = open("calc_sum.txt", "w")
+my_file.write("3 25 532 123 342 45")
+my_file.close()
+
+res_file = open("calc_sum.txt", "r")
+read_file = res_file.read()
+total = 0
+for el in read_file.split(" "):
+    total += int(el)
+res_file.close()
+print("Сумма всех чисел равна: ", total)
