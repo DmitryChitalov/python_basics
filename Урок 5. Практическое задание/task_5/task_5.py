f103f5510b4239ceb524491ace3bc5f6bb30c16b
VictorPiv <91976881+VictorPiv@users.noreply.github.com>
2022-12-15 22:15:07 +0300

    Update task_5.py

--- Урок 5. Практическое задание/task_5/task_5.py
+++ Урок 5. Практическое задание/task_5/task_5.py
@@ -2,3 +2,10 @@
 5)	Создать (программно) текстовый файл, записать в него программно набор чисел,
  разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 """
+numbers = "10 20 30 40 50 100 1 1000"
+with open("5.txt", "w", encoding="UTF-8") as numb:
+    numb.write(numbers)
+with open("5.txt", "r", encoding="UTF-8") as numb:
+    data = numb.readline().split()
+    data = sum([float(x) for x in data])
+print(data)
