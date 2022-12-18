7f1113bf1b1087d4584d46726a0c5007b88af7da
1NKsan <120363829+1NKsan@users.noreply.github.com>
2022-12-18 21:32:11 +0300

    Update task_1.py

--- Урок 1. Практическое задание/task_1.py
+++ Урок 1. Практическое задание/task_1.py
@@ -11,3 +11,8 @@
 Введите ваш возраст: 45
 Ваши данные для входа в аккаунт: имя - Василий, пароль - vas, возраст - 45
 """
+a = input("Enter your name: ")
+b = int(input("Enter your age: "))
+c = float(input("Enter pass: "))
+
+print(a, b, c)
