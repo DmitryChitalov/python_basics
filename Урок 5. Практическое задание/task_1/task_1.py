1c8147028e67373a18bd4b5c07907a32c70a664b
asstnkv <asstnkv@gmail.com>
2022-12-19 21:14:17 +0300

    Task 1 finished.

--- Урок 5. Практическое задание/task_1/task_1.py
+++ Урок 5. Практическое задание/task_1/task_1.py
@@ -3,3 +3,12 @@
 записать в него построчно данные, вводимые пользователем.
 Об окончании ввода данных свидетельствует пустая строка.
 """
+f_obj = open("file.txt", 'w', encoding='utf-8')
+lines = []
+while True:
+    line = input("Enter smth: ")
+    if line != "":
+        lines.append(line + "\n")
+    else:
+        break
+f_obj.writelines(lines)
