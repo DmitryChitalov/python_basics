4c41e35c109ba83c31ba7ac8a84f9f7a415d3d2a
Vasiliy94O <105280237+Vasiliy94O@users.noreply.github.com>
2022-12-19 21:21:21 +0700

    Update task_4.py

--- Урок 3. Практическое задание/task_4.py
+++ Урок 3. Практическое задание/task_4.py
@@ -5,3 +5,20 @@
 ВНИМАНИЕ: использование встроенной функции = задание не принято
 Постараться придумать свой алгоритм без **
 """
+#Task_4
+def exp_func(x, y):
+    try:
+        if y < 0:
+            exp_x = 1
+            for i in range(y, 0):
+                exp_x *= x
+                i += 1
+            return 1 / exp_x
+        else:
+            return 'Необходимо вводить целое отрицательное число!'
+    except TypeError:
+        return 'Вы ввели не число!'
+    except ZeroDivisionError:
+        return 'Вы пытаетесь делить на 0.'
+print(exp_func(int(input('Введите действительное положительное число x: ')),
+               int(input('Введите целое отрицательное число y: '))))
