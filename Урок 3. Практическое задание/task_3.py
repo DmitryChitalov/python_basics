835d9c6a09413544f73253aa7ba96e38eb84299c
Vasiliy94O <105280237+Vasiliy94O@users.noreply.github.com>
2022-12-19 20:58:59 +0700

    Update task_3.py

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -5,3 +5,8 @@
 1) используя функцию sort()
 2) без функции sort()
 """
+#Task_3
+def func_sort(var_1, var_2, var_3):
+    my_tuple = (var_1, var_2, var_3)
+    sorted_tuple = tuple(sorted(my_tuple))
+    return sorted_tuple[-2] + sorted_tuple[-1]
