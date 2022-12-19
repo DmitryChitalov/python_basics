4fe80f4baa58b42d9bc54450b4f494a40194ad1a
Vasiliy94O <105280237+Vasiliy94O@users.noreply.github.com>
2022-12-19 20:57:36 +0700

    Update task_1.py

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -17,3 +17,17 @@ Process finished with exit code 0
 
 Process finished with exit code 0
 """
+
+def division(num_1, num_2):
+    try:
+        return num_1 / num_2
+    except ZeroDivisionError:
+        return "куда ты делишь?"
+num_1 = int(input("Введите первое число: "))
+num_2 = int(input("Введите второе число: "))
+print(division(num_1, num_2))
+
+#Task_2
+def personality(name, sname, b_date, city, email, ph_numb):
+    print(f'{name} {sname}, {b_date} года рождения, проживающий в городе {city}. Электронная почта: {email}, телефон: {ph_numb}.')
+personality(name = 'Глеб', sname = 'Жиглов', b_date = '1953', city = 'Москва', email = 'g_zhi@ya.ru', ph_numb = '88005553535')
