b2eda774558334cc0be9a79fdfa8e93a2f97e843
Vasiliy94O <105280237+Vasiliy94O@users.noreply.github.com>
2022-12-19 20:59:29 +0700

    Update task_3.py

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -10,3 +10,8 @@ def func_sort(var_1, var_2, var_3):
     my_tuple = (var_1, var_2, var_3)
     sorted_tuple = tuple(sorted(my_tuple))
     return sorted_tuple[-2] + sorted_tuple[-1]
+
+def func(var_1, var_2, var_3):
+    return var_1 + var_2 + var_3 - min(var_1, var_2, var_3)
+print(func_sort(412, 12, 11))
+print(func(412, 12, 11))
