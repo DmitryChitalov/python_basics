999fd0d51c9f7632d4f8b5e8f8a1ff3ffd0d12cd
slin4444 <94636783+slin4444@users.noreply.github.com>
2022-12-20 14:54:46 +0300

    Update task_5.py

--- Урок 3. Практическое задание/task_5.py
+++ Урок 3. Практическое задание/task_5.py
@@ -6,3 +6,20 @@
     символ введен после нескольких чисел, то вначале нужно добавить сумму этих чисел к полученной
     ранее сумме и после этого завершить программу.
 """
+def my_func():
+    result = 0
+    while True:
+        numbers = input('Enter numbers through space. '
+                        'Enter qqq for exit ').split()
+        for i in numbers:
+            try:
+                if i == 'qqq':
+                    print(f'The sum is {result}. Job is done')
+                    return
+                else:
+                    result += int(i)
+            except ValueError:
+                print('Enter numbers or qqq for exit')
+        print(f'The sum is {result}')
+
+my_func()
