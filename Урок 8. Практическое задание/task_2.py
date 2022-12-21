24f599c5ed2e3758c25d504aec22781abf7e5c59
NikolayTroshin <netpoyandex@yandex.ru>
2022-12-21 23:28:51 +0300

    Урок_8

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,22 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+
+
+class MyException(Exception):
+
+    def __str__(self):
+        return f"Недопустимое значение 0"
+
+
+control = True
+while control:
+    try:
+        num = int(input('Введите делитель '))
+        if num == 0:
+            raise MyException
+        print(100 / num)
+        print('Выходим из программы')
+        control = False
+    except MyException as e:
+        print(e)
