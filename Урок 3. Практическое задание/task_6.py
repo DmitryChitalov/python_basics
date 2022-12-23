592dcfccff1e6c28af091727dc9edb980c03d6d3
PavelFPP <98024976+PavelFPP@users.noreply.github.com>
2022-12-23 19:21:18 +0300

    Update task_6.py

--- Урок 3. Практическое задание/task_6.py
+++ Урок 3. Практическое задание/task_6.py
@@ -8,3 +8,20 @@
 Сделать вывод исходной строки, но каждое слово должно начинаться с заглавной буквы.
 Необходимо использовать написанную ранее функцию int_func().
 """
+def int_func(word: str):
+    first_char = word[:1]
+    big_first_char = first_char.upper()
+    tail = word[1:]
+    return big_first_char + tail
+
+
+def int_func_ext(row: str):
+    result = []
+    words = row.split(' ')
+    for item in words:
+        result.append(int_func(item))
+    return ' '.join(result)
+
+
+s = input("Введите строку для преобразования:\n")
+print(int_func_ext(s))
