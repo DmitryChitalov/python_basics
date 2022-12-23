0297d6d333a7726153566db1fec05f1236075fe5
PavelFPP <98024976+PavelFPP@users.noreply.github.com>
2022-12-23 19:16:48 +0300

    Update task_2.py

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -8,3 +8,16 @@
 Иван Иванов 1846 года рождения, проживает в городе Москва,
 email: jackie@gmail.com, телефон: 01005321456
 """
+name = input('имя')
+surname = input('фамилия')
+year = int(input('год рождения'))
+city = input('город проживания')
+email = input('email')
+telephone = int(input('телефон'))
+
+
+def card(name, surname, year, city, email, telephone):
+    return (f'{name} {surname} {year} {city} {email} {telephone}')
+
+
+print(card(name, surname, year, city, email, telephone))
