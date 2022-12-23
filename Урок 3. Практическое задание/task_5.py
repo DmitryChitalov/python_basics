36f2bc4f2e3df49fc2a224e987514a186f1cd5a9
PavelFPP <98024976+PavelFPP@users.noreply.github.com>
2022-12-23 19:20:25 +0300

    Update task_5.py

--- Урок 3. Практическое задание/task_5.py
+++ Урок 3. Практическое задание/task_5.py
@@ -6,3 +6,28 @@
     символ введен после нескольких чисел, то вначале нужно добавить сумму этих чисел к полученной
     ранее сумме и после этого завершить программу.
 """
+def my_sum(my_list):
+    s_sum = 0
+    for item in my_list:
+        try:
+            s_sum += float(item)
+        except ValueError:
+            continue
+    return s_sum
+
+
+def sum_my_string(s):
+    s = s.replace('#', '')
+    s = s.replace(',', '.')
+    numbers = s.split(' ')
+    return my_sum(numbers)
+
+
+numbers_sum = 0
+while True:
+    numbers_sting = input("Введите строку чисел, разделенных пробелом. Для завершения введите символ '#'\n")
+    numbers_sum += sum_my_string(numbers_sting)
+    if numbers_sum != 0:
+        print(f"Сумма значений элементов {numbers_sum}")
+    if numbers_sting.count('#') > 0:
+        break
