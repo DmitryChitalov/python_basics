3c6c841ba3c0e807b883891b3659a5fb9683b7b7
1NKsan <120363829+1NKsan@users.noreply.github.com>
2022-12-24 20:43:10 +0300

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,27 @@
+"""
+Задание 1. Создать список и заполнить его элементами различных типов данных.
+Реализовать проверку типа данных каждого элемента.
+Использовать функцию type() для проверки типа.
+Элементы списка можно не запрашивать у пользователя,
+а указать явно, в программе.
+
+Пример:
+для списка [5, "string", 0.15, True, None]
+результат
+
+<class 'int'>
+<class 'str'>
+<class 'float'>
+<class 'bool'>
+<class 'NoneType'>
+"""
+my_int = 5
+my_float = 1.2
+my_str = "Test type"
+my_list = ['a', '2']
+my_tuple = ('b', '3')
+my_dict = {'test': 'GB', 'pyton': 'github'}
+
+super_list = [my_int, my_float, my_str, my_list, my_tuple, my_dict]
+for i in super_list:
+    print(f'{i} is {type(i)}')
---
+++ task_3.py
@@ -0,0 +1,28 @@
+"""
+3. Пользователь вводит месяц в виде целого числа от 1 до 12.
+Сообщить к какому времени года относится месяц (зима, весна, лето, осень).
+Напишите два варианта решения: через list и через dict.
+
+Пример:
+Введите номер месяца: 10
+Результат через список: Осень
+Результат через словарь: Осень
+"""
+seasons_list = ['Зима', 'Весна', 'Лето', 'Осень']
+seasons_dict = {1 : 'Зима', 2 : 'Весна', 3 : 'Лето', 4 : 'Осень'}
+month = int(input("Введите номер месяца "))
+if month ==1 or month == 12 or month == 2:
+        print(seasons_dict.get(1))
+        print(seasons_list[0])
+elif month == 3 or month == 4 or month ==5:
+    print(seasons_dict.get(2))
+    print(seasons_list[1])
+elif month == 6 or month == 7 or month == 8:
+    print(seasons_dict.get(3))
+    print(seasons_list[2])
+
+elif month == 9 or month == 10 or month == 11:
+    print(seasons_dict.get(4))
+    print(seasons_list[3])
+else:
+        print("Такого месяца не существует")
