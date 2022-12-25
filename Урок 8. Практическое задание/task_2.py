b9865e43a82fbe8837d06bfbf30c53d3da1526f1
Lordofwinterfield <tim979767@yandex.ru>
2022-12-26 00:33:11 +0300

    task2_Tamerlan

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,20 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+class ZeroDivision:
+    def __init__(self, divider, denominator):
+        self.divider = divider
+        self.denominator = denominator
+
+    @staticmethod
+    def divide_by_null(divider, denominator):
+        try:
+            return (divider / denominator)
+        except:
+            return (f"Деление на ноль недопустимо")
+
+
+div = ZeroDivision(10, 100)
+print(ZeroDivision.divide_by_null(10, 0))
+print(ZeroDivision.divide_by_null(10, 0.1))
+print(div.divide_by_null(100, 0))
