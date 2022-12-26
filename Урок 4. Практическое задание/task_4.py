a53a0578ec87245545a6cd7e993a4351aa17a672
Yaroslav-Pavlenko-1993 <yaroslav.pavlenko.1993@mail.ru>
2022-12-26 23:54:03 +0300

    Выполнено Задание 4

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -6,3 +6,8 @@
 Пример исходного списка: [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11].
 Результат: [23, 1, 3, 10, 4, 11]
 """
+
+
+source_list = [22, 122, 442, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+result_list = [source_list[el] for el in range(len(source_list)) if source_list.count(source_list[el]) == 1]
+print(result_list)
