b14a6a12a5b5a7290bc69835dd2dced97d3f3464
PavelFPP <98024976+PavelFPP@users.noreply.github.com>
2022-12-27 19:38:54 +0300

    Update task_1.py

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -6,3 +6,15 @@
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
 """
+from sys import argv
+
+script_name, hours_worked, rate_per_hour, prize = argv
+
+print("Имя скрипта: ", script_name)
+print("\n'Программа рассчета заработной платы сотрудника'")
+print("Выработка в часах: ", hours_worked)
+print("Ставка в час: ", rate_per_hour)
+print("Премия: ", prize)
+print("Зарплата сотрудника: ", (float(hours_worked) * float(rate_per_hour)) + float(prize))
+
+# PS C:\Users\son2001\PycharmProjects\pythonProject> python 1.py 8 2 200
