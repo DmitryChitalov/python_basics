1c45f0a010966f98cc776ec70abbd6be6adf4256
PavelFPP <98024976+PavelFPP@users.noreply.github.com>
2022-12-30 19:44:21 +0300

    Update task_3.py

--- Урок 5. Практическое задание/task_3/task_3.py
+++ Урок 5. Практическое задание/task_3/task_3.py
@@ -8,20 +8,14 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
-out_f = open("dz_3.txt", "w", encoding='utf-8')
-str_list = ['Фещенко 23600.12\n', 'Иванов 22456.01\n', 'Сидоров 5678.45\n', 'Петров 34567.98\n', 'Головин 13543.34\n',
-            'Ким 13333.44\n', 'Лим 4566.11\n', 'Ткаченко 34522.11\n', 'Степанов 11111.33\n', 'Гай 233333.23\n']
+out_f = open("dz_4.txt", "w", encoding='utf-8')
+str_list = ['One — 1\n', 'Two — 2\n', 'Three — 3\n', 'Four — 4\n']
 out_f.writelines(str_list)
 out_f.close()
 
-
-out_f = open("dz_3.txt", "r", encoding='utf-8')
-i = 0
-print("Cотрудники, имееющие оклад менее 20 тысяч:")
-for line in out_f:
-    line_i = line.split()
-    i = i + float(line_i[1])
-    if float(line_i[1]) < 20000:
-        print(line_i[0])
-print(f'Средняя величина дохода сотрудников - {i/float(len(str_list))}')
-out_f.close()
+num = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре'}
+with open('dz_4.txt', 'r', encoding="utf-8") as file:
+    with open('new_dz_4.txt', 'w+', encoding="utf-8") as new_file:
+        for line in file:
+            line_i = line.split()
+            new_file.writelines(f'{num.setdefault(line_i[0])} {line_i[1]} {line_i[2]}\n')
