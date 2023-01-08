b7d41f584a3a4eef33c62fcf6899b18dd46f8b39
Vasiliy94O <105280237+Vasiliy94O@users.noreply.github.com>
2023-01-08 21:37:49 +0700

    Update task_1.py

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,42 @@
 Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца
 и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
 """
+#Task_1
+class Data:
+    def __init__(self, day_month_year):
+        self.day_month_year = str(day_month_year)
+
+    @classmethod
+    def extract(cls, day_month_year):
+        my_date = []
+
+        for el in day_month_year.split():
+            if el != '-': my_date.append(el)
+
+        return int(my_date[0]), int(my_date[1]), int(my_date[2])
+
+    @staticmethod
+    def valid(day, month, year):
+
+        if 1 <= day <= 31:
+            if 1 <= month <= 12:
+                if 2024 >= year >= 0:
+                    return f'Все верно'
+                else:
+                    return f'Неправильный год'
+            else:
+                return f'Неправильный месяц'
+        else:
+            return f'Неправильный день'
+
+    def __str__(self):
+        return f'Текущая дата {Data.extract(self.day_month_year)}'
+
+
+today = Data('08 - 1 - 2023')
+print(today)
+print(Data.valid(22, 12, 2025))
+print(today.valid(12, 12, 2011))
+print(Data.extract('10 - 12 - 2015'))
+print(today.extract('16 - 08 - 2004'))
+print(Data.valid(1, 11, 2000))
