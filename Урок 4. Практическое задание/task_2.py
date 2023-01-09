f1e7f7285c2fe2130c191cd8d3f4ae391691469f
1NKsan <kir.sama@mail.ru>
2023-01-09 20:06:45 +0300

    HW4 task2

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -7,3 +7,17 @@
 
 Реализуйте вариант без и с генераторным выражением
 """
+a = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+c= []
+for i in range(len(a) - 1):
+    n = a[i]
+    i += 1
+    m = a[i]
+    if m > n:
+        c.append(m)
+print(c)
+
+my_list = [2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+my_new_list = [el for num, el in enumerate(my_list) if my_list[num - 1] < my_list[num]]
+print(f'Исходный список {my_list}')
+print(f'Новый список {my_new_list}')
