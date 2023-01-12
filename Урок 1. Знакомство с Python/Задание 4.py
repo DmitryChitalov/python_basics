e4708b54d5ea7209273a0444217be81d477d3a40
sss777999 <spt999@protonmail.com>
2023-01-12 20:33:17 +0300

    Lesson_1 remastered

--- Урок 1. Знакомство с Python/Задание 4.py
+++ Урок 1. Знакомство с Python/Задание 4.py
@@ -2,24 +2,14 @@
 # Найдите самую большую цифру в числе.
 # Для решения используйте цикл while и арифметические операции.
 
-a = int(input('Введите целое положительное число: '))
+inp = input('Введите целое положительное число: ')
 i = 0
-max = 0
-g = str(a)
-while i < len(g):
-    if int(g[i]) > max:
-        max = int(g[i])
-    i+=1
-print(max)
+max_value = 0
 
+while i < len(inp):
+    for _ in inp:
+        if int(_) > max_value:
+            max_value = int(_)
+        i += 1
 
-
-
-
-"""
-inp = int(input())
-g = []
-for i in str(inp):
-    g.append(i)
-print(max(g))
-"""
+print(max_value)
