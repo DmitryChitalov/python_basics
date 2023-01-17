fb7fb1509abb521ccce5f715080cca8f9adb504c
Netdaem0n <bakalets@gmail.com>
2023-01-17 17:59:07 +0500

    -

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,25 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+
+
+class OwnError(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
+def div():
+    try:
+        user_num_1 = int(input('Введите делимое: '))
+        user_num_2 = int(input('Введите делитель: '))
+        if user_num_2 == 0:
+            raise OwnError("Делить на ноль нельзя!")
+        else:
+            res = user_num_1 / user_num_2
+            return res
+    except ValueError:
+        return "Вы ввели не число"
+    except OwnError as err:
+        return err
+
+print(div())
