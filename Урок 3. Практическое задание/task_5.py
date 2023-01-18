9b5f3fe952e355ab5cbad00d4d171747ac271d66
Ilya_Shatalov <mcstriker@list.ru>
2023-01-18 20:41:09 +0300

    hw_lesson_3

--- Урок 3. Практическое задание/task_5.py
+++ Урок 3. Практическое задание/task_5.py
@@ -6,3 +6,23 @@
     символ введен после нескольких чисел, то вначале нужно добавить сумму этих чисел к полученной
     ранее сумме и после этого завершить программу.
 """
+
+import sys
+
+result = 0
+while True:
+    line = input("Enter number or special token q fo exite: ")
+    tokens = line.split(" ")
+    for token in tokens:
+        try:
+            number = float(token)
+            result += number
+        except:
+            if token == 'q':
+                print(f"You sum is {result}. Program is terminated")
+                exit(0)
+            else:
+                print(f"You sum is {result}. Input error", file=sys.stderr)
+                exit(1)
+#print(result)
+#exit()
