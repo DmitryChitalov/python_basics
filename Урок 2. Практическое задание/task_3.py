db69f4844ccdda12194c93496bfab198d482f60d
Ilya_Shatalov <mcstriker@list.ru>
2023-01-18 20:11:52 +0300

    hw_lesson_2

--- Урок 2. Практическое задание/task_3.py
+++ Урок 2. Практическое задание/task_3.py
@@ -8,3 +8,25 @@
 Результат через список: Осень
 Результат через словарь: Осень
 """
+number = int(input("Enter month number: "))
+if number <= 12 and number >= 1:
+    month_dict = {1: 'January',
+                  2: 'February',
+                  3: 'March',
+                  4: 'April',
+                  5: 'May',
+                  6: 'June',
+                  7: 'Jule',
+                  8: 'August',
+                  9: 'September',
+                  10: 'October',
+                  11: 'November',
+                  12: 'December'}
+    month_list = list(month_dict.values())
+    for i, el in enumerate(month_list):
+        if i == number-1:
+            print(f"Month from list is {month_list[i]}")
+            break
+    print(f"Month from dict is {month_dict[number]}")
+else:
+    print("You made a mistake")
