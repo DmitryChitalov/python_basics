aee945a861e172628d667145ded3dd9d17b320d6
1NKsan <kir.sama@mail.ru>
2023-01-21 13:02:08 +0300

    HW7 task2

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -25,3 +25,40 @@
 
 Два класса: абстрактный и Clothes
 """
+from abc import ABC, abstractmethod
+
+
+class Clothes(ABC):
+
+    def __init__(self, param):
+        self.param = param
+
+    @abstractmethod
+    def expense(self):
+        pass
+
+    def __str__(self):
+        return str(self.param)
+
+
+class Coat(Clothes):
+
+    @property
+    def expense(self):
+        return self.param / 6.5 + 0.5
+
+
+class Suit(Clothes):
+
+    @property
+    def expense(self):
+        return self.param * 2 + 0.3
+
+
+a = Coat(52)
+b = Suit(1.80)
+print(a)
+print(a.expense)
+print(b.expense)
+
+
