ecb2823f83a8cd04cf5a9cdea5ab5f453707d309
1NKsan <kir.sama@mail.ru>
2023-01-21 12:59:37 +0300

    HW7 task1

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -28,3 +28,33 @@
 8 10 12
 14 16 18
 """
+import copy
+
+class Matrix:
+    def __init__(self, matrix):
+        self.matrix = matrix
+
+    def __str__(self):
+        s = ''
+        for i in range(len(self.matrix)):
+            s = s + '\t'.join(map(str,self.matrix[i])) + '\n'
+        return s
+
+    def __add__(self, other):
+        if len(self.matrix) != len(other.matrix):
+            return None
+        res = copy.deepcopy(self.matrix)
+        for i in range(len(self.matrix)):
+            for k in range(len(self.matrix[i])):
+                res[i][k] = self.matrix[i][k] + other.matrix[i][k]
+        return Matrix(res)
+
+l1 = [[1,2,4],[3,4,5],[5,6,6]]
+l2 = [[11,21,41],[31,41,51],[51,61,61]]
+m = Matrix(l1)
+s = Matrix(l2)
+print(m)
+z = m + s
+print('z ')
+print(z)
+print(type(z))
