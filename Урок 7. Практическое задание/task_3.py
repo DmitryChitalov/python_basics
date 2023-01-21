8b24f6011c88f96fa90fc2e7f65fbfd4d7eb34e4
1NKsan <kir.sama@mail.ru>
2023-01-21 13:03:18 +0300

    HW7 task3

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -100,3 +100,49 @@ print(cell2.make_order(10))
 *****\n *****\n *****\n *****\n *****\n *****\n
 **********\n **********\n *****
 """
+
+
+
+class Cell:
+    def __init__(self, cell):
+        self.cell = cell
+        self.simbol = '*'
+
+    def __str__(self):
+        return str(f'Количество ячеек - {self.cell}')
+
+    def __sub__(self, other):
+        return Cell(abs(self.cell - other.cell))
+
+    def __mul__(self, other):
+        return Cell(self.cell * other.cell)
+
+    def __truediv__(self, other):
+        return Cell(self.cell // other.cell)
+
+    def __add__(self, other):
+        return Cell(abs(self.cell + other.cell))
+
+    def make_order(self, count):
+        x = self.cell
+        while x > 0:
+            for k in range(1, count + 1):
+                print(self.simbol, end='')
+                x -= 1
+                if x <= 0:
+                    break
+            print('\n', end='')
+
+
+a = Cell(5)
+b = Cell(10)
+c = Cell(3)
+d = Cell(2)
+
+print(a + b)
+print(a - b)
+print(a * b)
+print(c / d)
+
+a.make_order(3)
+b.make_order(3)
