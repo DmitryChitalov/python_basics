ad549cee5d3655e239ab9aad898ca35ab491c71e
Anastasia2812 <122151115+Anastasia2812@users.noreply.github.com>
2023-01-22 23:55:59 +0300

    Задание 1 к уроку 5.
    Решение задания 1 к уроку 5.

--- Урок 5. Практическое задание/task_1/task_1.py
+++ Урок 5. Практическое задание/task_1/task_1.py
@@ -3,3 +3,15 @@
 записать в него построчно данные, вводимые пользователем.
 Об окончании ввода данных свидетельствует пустая строка.
 """
+my_list = []
+while True:
+    line = input("Enter anything: ")
+    if line == '':
+        print(my_list)
+        exit()
+    else:
+        newline = line + '\n'
+        my_list.append(newline)
+
+    with open("test_1.txt", "w") as file_obj:
+        file_obj.writelines(my_list)
