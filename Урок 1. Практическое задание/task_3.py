c662c6b91ad225cb1fbdb49c2f963a6501e737cb
Анна Саломатова <123293341+AnnaSalomatova@users.noreply.github.com>
2023-01-22 17:53:05 +0300

    Update task_3.py

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,13 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+num = int(input("Укажите число от 0 до 9: "))
+if num == 0:
+    result = 0
+elif num > 9:
+    result = "Вы указали число больше 9"
+else:
+    num_num = num * 10 + num
+    num_num_num = num_num * 10 + num
+    result = num + num_num + num_num_num
+print(result)
