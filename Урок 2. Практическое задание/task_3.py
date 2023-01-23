4b6fae051ecc051cea781930d0e0c068484b328e
@rash <111140352+IgoRash2201@users.noreply.github.com>
2023-01-23 19:45:48 +0300

    Update task_3.py

--- Урок 2. Практическое задание/task_3.py
+++ Урок 2. Практическое задание/task_3.py
@@ -8,3 +8,43 @@
 Результат через список: Осень
 Результат через словарь: Осень
 """
+month = int(input(""Введите месяц по номеру id от 1 до 12 : "))
+mlist = ["зима", "весна", "лето", "осень"]
+while True:
+    if month > 12 or month <= 0 :
+        print(f"\tINCORRECT ID!!! \n\tPlease use number range from 1 to 12 only!")
+        month = int(input("Please enter month id from 1 to 12 : "))
+        continue
+    mlist = ["зима", "весна", "лето", "осень"]
+    if month == 12 or (month >= 1 and month < 3):
+        print(f"\tSeason related to your Month id#{month}  is '{mlist[0]}'")
+        break
+    elif month >= 3 and month < 6:
+        print(f"\tSeason related to your Month id# {month} is '{mlist[1]}'")
+        break
+    elif month >= 6 and month < 9:
+        print(f"\tSeason related to your Month id# {month} is '{mlist[2]}'")
+        break
+    elif month >= 9 and month < 12:
+        print(f"\tSeason related to your Month id# {month} is '{mlist[3]}'")
+    else:
+          print("Такого месяца не существует")    
+        
+seasons_list = ['winter', 'spring', 'summer', 'autumn']
+seasons_dict = {1 : 'winter', 2 : 'spring', 3 : 'summer', 4 : 'autumn'}
+month = int(input("Введите месяц по номеру "))
+if month ==12 or month == 1 or month == 2:
+        print(seasons_dict.get(1))
+        print(seasons_list[0])
+elif month == 3 or month == 4 or month ==5:
+    print(seasons_dict.get(2))
+    print(seasons_list[1])
+elif month == 6 or month == 7 or month == 8:
+    print(seasons_dict.get(3))
+    print(seasons_list[2])
+
+elif month == 9 or month == 10 or month == 11:
+    print(seasons_dict.get(4))
+    print(seasons_list[3])
+else:
+        print("Такого месяца не существует")
