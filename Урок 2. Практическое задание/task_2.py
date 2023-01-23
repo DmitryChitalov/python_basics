d4f4e67530be05611d95db1a241f2c9d16812c3a
@rash <111140352+IgoRash2201@users.noreply.github.com>
2023-01-23 20:10:04 +0300

    Update task_2.py

--- Урок 2. Практическое задание/task_2.py
+++ Урок 2. Практическое задание/task_2.py
@@ -17,7 +17,7 @@ my_list = []
 i = 0
 element = 0
 while i < element_count:
-    my_list.append(input("Введите значение списка: "))
+    my_list.append(input("Результат: "))
     i += 1
 for element in range(int(len(my_list)/2)):
         my_list[element], my_list[element + 1] = my_list [element + 1], my_list[element]
