20fa002bff131b99f276de23bd49f0fe9f943071
PavelFPP <98024976+PavelFPP@users.noreply.github.com>
2023-01-23 17:48:39 +0300

    Update task_3.py

--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -9,3 +9,23 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+class Error(Exception):
+    def __init__(self, *args):
+        self.my_list = []
+
+    def my_input(self):
+        while True:
+            try:
+                val = input('Введите значения и нажимайте Enter - ')
+                if val == 'stop':
+                    print(f'Список сформирован {self.my_list}')
+                    break
+                val = int(val)
+                self.my_list.append(val)
+                print(f'Текущий список - {self.my_list} \n ')
+            except ValueError:  # Error(Exception) не отрабатывает по задумке
+                print('Это не цифра')
+
+
+try_except = Error(1)
+print(try_except.my_input())
