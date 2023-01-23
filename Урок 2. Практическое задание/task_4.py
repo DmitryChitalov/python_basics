9f2cf25d242147d7415f9718ce9b415634a2e44d
@rash <111140352+IgoRash2201@users.noreply.github.com>
2023-01-23 20:02:23 +0300

    Update task_4.py

--- Урок 2. Практическое задание/task_4.py
+++ Урок 2. Практическое задание/task_4.py
@@ -14,3 +14,16 @@
 1. раз
 2. перерефриж
 """
+
+""" Решение к заданию №4 """
+my_string = input("Введите слова через пробел: ")
+my_word = []
+number = 1
+for element in range(my_string.count(' ') + 1):
+    my_word = my_string.split()
+    if len(str(my_word)) <= 10:
+        print(f" {number} {my_word [element]}")
+        number += 1
+    else:
+        print(f" {number} {my_word [element] [0:10]}")
+        number += 1
