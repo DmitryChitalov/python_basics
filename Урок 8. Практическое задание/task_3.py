17f0eb80a974966ce896e40112f09fb5fcf147ad
1NKsan <kir.sama@mail.ru>
2023-01-23 16:33:44 +0300

    HW8 task3

--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -9,3 +9,30 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+class NotNumberError(Exception):
+    def __init__(self, text):
+        self.text = text
+
+    def __str__(self):
+        return self.text
+
+
+if __name__ == '__main__':
+    my_list = []
+
+    while True:
+        user_input = input("Введите число для списка, или 'stop' для выхода: ")
+
+        if user_input == "stop":
+            break
+
+        try:
+            if not user_input.isdigit():
+                raise NotNumberError(f"'{user_input}' has not numerical format")
+
+            my_list.append(int(user_input))
+        except NotNumberError as e:
+            print(e)
+
+    print(my_list)
+
