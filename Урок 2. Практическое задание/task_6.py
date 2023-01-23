1ac6bb67abbb606bca58ba9b7f693af77e36230a
@rash <111140352+IgoRash2201@users.noreply.github.com>
2023-01-23 20:58:52 +0300

    Update task_6.py

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -21,3 +21,30 @@
 “ед”: [“шт.”]
 }
 """
+
+""" Решение задания №6 """
+list_1 = []
+while True:
+    list_1.append((input("Номер товара: "),
+                   {"Названия": input("Название: "),
+                    "Цены": input("Цена: "),
+                    "Количества": input("Количество: "),
+                    "ед.": input("Ед. учёта: ")}))
+    e = input("Закончить ввод данных? Y / N: ")
+    if e == "Y":
+        break
+list_1 = [(1, {"название": "компьютер", "цена": 20000, "количество": 5, "ед": "шт."}),
+          (2, {"название": "принтер", "цена": 6000, "количество": 2, "ед": "шт."}),
+          (3,{"название": "сканер", "цена": 2000, "количество": 7, "ед": "шт."})]
+name_list = []
+price_list = []
+count_list = []
+unit_list = []
+result_dict = {}
+for i in range(len(list_1)):
+    name_list.append(list_1[i][1]['названия'])
+    price_list.append(list_1[i][1]['цены'])
+    count_list.append(list_1[i][1]['количества'])
+    unit_list.append(list_1[i][1]['ед.'])
+result_dict.update({'названия': name_list, 'цены': price_list, 'количества': count_list, 'ед.': unit_list})
+print(result_dict)
