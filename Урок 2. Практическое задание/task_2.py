584c8907bca7f0603e5ac5e1677648ed0e648edb
@rash <111140352+IgoRash2201@users.noreply.github.com>
2023-01-23 18:15:34 +0300

    Update task_2.py

--- Урок 2. Практическое задание/task_2.py
+++ Урок 2. Практическое задание/task_2.py
@@ -11,3 +11,13 @@
 Введите целые числа через пробел: 1 2 3
 Результат: 2 1 3
 """
+""" Решение к заданию №2 """ 
+g = int(input("How many items in list do you want to add?\n\t Enter items quantity: "))
+my_lst = []
+for i in range(g):
+    my_lst.append(input(f"Item # {i + 1} : "))
+print(f"Your item list view:\n{my_lst}")
+for x in range(0, (len(my_lst) - 1),2):
+    my_lst[x], my_lst[x+1] = my_lst[x+1], my_lst[x]
+print(f"Your CHANGED item list view:\n{my_lst}")
+
