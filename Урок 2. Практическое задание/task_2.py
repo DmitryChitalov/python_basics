7be2997128a52cecc559031b542e74c9b3bb7f3a
Peter_2021_Python <panchenko.pg@yandex.ru>
2023-01-28 10:59:16 +0300

    python_basic_lesson_2

--- Урок 2. Практическое задание/task_2.py
+++ Урок 2. Практическое задание/task_2.py
@@ -1,13 +1,23 @@
 """
-Задание 2. Для списка реализовать обмен значений соседних элементов,
-т.е. значениями обмениваются элементы с индексами 0 и 1, 2 и 3 и т.д.
-При нечетном количестве элементов последний сохранить на своем месте.
-Для заполнения списка элементов необходимо использовать функцию input().
-
-Пример:
-Введите целые числа через пробел: 1 2 3 4
-Результат: 2 1 4 3
-
-Введите целые числа через пробел: 1 2 3
-Результат: 2 1 3
+2. Для списка реализовать обмен значений соседних элементов.
+Значениями обмениваются элементы с индексами 0 и 1, 2 и 3 и т. д.
+При нечётном количестве элементов последний сохранить на своём месте.
+Для заполнения списка элементов нужно использовать функцию input().
 """
+
+list_length = int(input("введите длинну списка :"))
+list_different_types_data = []
+for index in range(list_length):
+    list_different_types_data.append(int(input("введите элемент списка: ")))
+print(list_different_types_data)
+if list_length % 2 == 0:
+    for index in range(0, list_length, 2):
+        buffer_var = list_different_types_data[index]
+        list_different_types_data[index] = list_different_types_data[index + 1]
+        list_different_types_data[index + 1] = buffer_var
+else:
+    for index in range(0, list_length - 1, 2):
+        buffer_var = list_different_types_data[index]
+        list_different_types_data[index] = list_different_types_data[index + 1]
+        list_different_types_data[index + 1] = buffer_var
+print(list_different_types_data)
--- Урок 2. Практическое задание/task_3.py
+++ Урок 2. Практическое задание/task_3.py
@@ -1,10 +1,23 @@
 """
 3. Пользователь вводит месяц в виде целого числа от 1 до 12.
-Сообщить к какому времени года относится месяц (зима, весна, лето, осень).
-Напишите два варианта решения: через list и через dict.
-
-Пример:
-Введите номер месяца: 10
-Результат через список: Осень
-Результат через словарь: Осень
+Сообщить, к какому времени года относится месяц (зима, весна, лето, осень).
+Напишите решения через list и dict.
 """
+
+list_of_numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+list_of_seasons = ["зима", "весна", "лето", "осень"]
+month_number = int(input("Введите номер месяца: "))
+if month_number == list_of_numbers[0] or month_number == list_of_numbers[1] or month_number == list_of_numbers[11]:
+    print(f"Месяц относится к времени года: {list_of_seasons[0]}")
+elif month_number == list_of_numbers[2] or month_number == list_of_numbers[3] or month_number == list_of_numbers[4]:
+    print(f"Месяц относится к времени года: {list_of_seasons[1]}")
+elif month_number == list_of_numbers[5] or month_number == list_of_numbers[6] or month_number == list_of_numbers[7]:
+    print(f"Месяц относится к времени года: {list_of_seasons[2]}")
+else:
+    print(f"Месяц относится к времени года: {list_of_seasons[3]}")
+dict_numbers_seasons = {(1, 2, 12): "зима", (3, 4, 5): "весна", (6, 7, 8): "лето", (9, 10, 11): "осень"}
+month_number_new = int(input("Введите номер месяца ещё раз: "))
+for element in dict_numbers_seasons:
+    for member in element:
+        if member == month_number_new:
+            print(f"Месяц относится к времени года: {dict_numbers_seasons[element]}")
