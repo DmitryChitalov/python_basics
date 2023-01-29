3c35c42af1e4b8980b31bdb2b11f1c7f0603ecbf
Emarkina3001 <123263731+Emarkina3001@users.noreply.github.com>
2023-01-29 18:33:01 +0300

    Update task_2.py

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -10,9 +10,9 @@
 Время в формате ч:м:с - 1.0 : 60.0 : 3600
 """
 #Вариант1
-secs=int(input("Введите время в секундах:"))
-hours=secs/3600
-minuts=secs/60
+secs = int(input("Введите время в секундах:"))
+hours = secs/3600
+minuts = secs/60
 
 print(f"Время в формате ч:м:с-{hours}:{minuts}:{secs}" )
 
