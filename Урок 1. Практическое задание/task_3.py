9ca38c38fd60666a0e68acfa7b4a3089a3e1ef13
Emarkina3001 <123263731+Emarkina3001@users.noreply.github.com>
2023-01-29 17:24:36 +0300

    Update task_3.py

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,13 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+#Вариант1
+n = int(input("Введите число n:"))
+res = n + n*11 + n*111
+print(f"n+nn+nnn={res}")
+
+
+#Вариант2
+n = input("Введите целое число n:")
+res = int(n)+int(n*2)+int(n*3)
+print = (f'{n}+{n*2}+{n*3}={res}')
