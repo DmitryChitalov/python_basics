0ffe330f90eaca6c74451d83d6f36d7c03f06dbb
Pav-kuz <123264297+Pav-kuz@users.noreply.github.com>
2023-01-30 23:16:42 +0300

    Update task_3.py

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -5,3 +5,17 @@
 1) используя функцию sort()
 2) без функции sort()
 """
+def my_func(a, b, c):
+    """ Возврщает сумму наибольших двух аргументов из трёх """
+    list_of_args = [a, b, c]
+    sum = max(list_of_args)
+    list_of_args.remove(sum)  # удаляем наибольший элемент
+    sum += max(list_of_args)  # добавляем следующий наибольший элемент
+    return sum
+
+
+# вызов функции
+first = int(input('Введите первый аргумент: '))
+second = int(input('Введите второй аргумент: '))
+third = int(input('Введите третий аргумент: '))
+print(my_func(first, second, third))
