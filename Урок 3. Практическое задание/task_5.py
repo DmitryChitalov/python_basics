10869b8660b72e5c68ccff30d6359bcb09044253
@rash <111140352+IgoRash2201@users.noreply.github.com>
2023-01-30 19:46:26 +0300

    Update task_5.py

--- Урок 3. Практическое задание/task_5.py
+++ Урок 3. Практическое задание/task_5.py
@@ -6,3 +6,24 @@
     символ введен после нескольких чисел, то вначале нужно добавить сумму этих чисел к полученной
     ранее сумме и после этого завершить программу.
 """
+
+""" Задание №5 """
+
+def my_sum ():
+    sum_res = 0
+    ex = False
+    while ex == False:
+        my_str = input('Введите строку чисел через пробел (для суммирования). Либо нажмите Z для выхода:  ').split()
+
+        res = 0
+        for el in range(len(my_str)):
+            if my_str[el] == 'z' or my_str[el] == 'Z':
+                ex = True
+                break
+            else:
+                res = res + int(my_str[el])
+        sum_res = sum_res + res
+        print(f'Текущая сумма: {sum_res}')
+    print(f'Ваша окончательная сумма: {sum_res}')
+    exit('Программа завершена')
+my_sum()
