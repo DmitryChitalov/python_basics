d2d10df53241e775ccfc72c3afc0ba20618a5a14
Анна Саломатова <123293341+AnnaSalomatova@users.noreply.github.com>
2023-01-30 22:49:07 +0300

    Update task_2.py

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -8,3 +8,15 @@
 Иван Иванов 1846 года рождения, проживает в городе Москва,
 email: jackie@gmail.com, телефон: 01005321456
 """
+def user_info(name=None, surname=None, year=None, city=None, email=None, phone=None):
+    """ Выводит данные о пользователе """
+    print(f'Имя: {name}, Фамилия: {surname}, год рождения: {year}, '
+          f'город проживания: {city}, email: {email}, телефон: {phone}')
+# Вызов функции
+user_name = input("Укажите имя: ")
+user_surname = input("Укажите фамилию: ")
+user_year = int(input("Укажите год рождения: "))
+user_city = input("Укажите город проживания: ")
+user_email = input("Укажите электронный адрес: ")
+user_phone = input("Укажите телефон: ")
+user_info(name=user_name, year=user_year, surname=user_surname, city=user_city, email=user_email, phone=user_phone)
