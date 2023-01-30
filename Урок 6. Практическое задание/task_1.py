4d9fb96135d2c56758f167ec24908d30a437d3ba
Alexander <121261035+1goldenhaw@users.noreply.github.com>
2023-01-30 18:34:56 +0300

    Alexander_Belov_HW6

--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -15,3 +15,27 @@
 
 Проверить работу примера, создав экземпляр и вызвав описанный метод.
 """
+
+
+from time import sleep
+
+
+class TrafficLight:
+    __color = ['Красный', 'Желтый', 'Зеленый']
+
+    def running(self):
+        i = 0
+        while i != 3:
+            print(TrafficLight.__color[i])
+            if i == 0:
+                sleep(7)
+            elif i == 1:
+                sleep(2)
+            elif i == 2:
+                sleep(7)
+            i += 1
+
+
+my_trafficlight = TrafficLight()
+my_trafficlight.running()
+
--- Урок 6. Практическое задание/task_5.py
+++ Урок 6. Практическое задание/task_5.py
@@ -13,3 +13,37 @@
 Для каждого из классов метод должен выводить уникальное сообщение.
 Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
 """
+
+
+class Stationery:
+    title: str
+
+    def __init__(self, my_title):
+        self.title = my_title
+
+    def draw(self):
+        print("Отрисовка")
+
+
+class Pen(Stationery):
+    def draw(self):
+        print(f"Запуск отрисовки. {self.title} (Pen)")
+
+
+class Pencil(Stationery):
+    def draw(self):
+        print(f"Запуск отрисовки. {self.title} (Pencil)")
+
+
+class Handle(Stationery):
+    def draw(self):
+        print(f"Запуск отрисовки. {self.title} (Handle)")
+
+
+my_pen = Pen("Карандаш")
+my_pencil = Pencil("Ручка")
+my_handle = Handle("Маркер")
+
+my_pen.draw()
+my_pencil.draw()
+my_handle.draw()
