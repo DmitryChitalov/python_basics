f63f8e575ccb31e05c7bd9b10e44b5b643171bfc
Ilya_Shatalov <122379435+Chilanzar176@users.noreply.github.com>
2023-01-31 20:48:40 +0300

    Урок_6

--- Урок 5. Практическое задание/task_5/task_5.py
+++ Урок 5. Практическое задание/task_5/task_5.py
@@ -2,3 +2,16 @@
 5)	Создать (программно) текстовый файл, записать в него программно набор чисел,
  разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 """
+def summary():
+    try:
+        with open('file_5.txt', 'w+') as file_obj:
+            line = input('Введите цифры через пробел \n')
+            file_obj.writelines(line)
+            my_numb = line.split()
+
+            print(sum(map(int, my_numb)))
+    except IOError:
+        print('Ошибка в файле')
+    except ValueError:
+        print('Неправильно набран номер. Ошибка ввода-вывода')
+summary()
--- Урок 6. Практическое задание/task_4.py
+++ Урок 6. Практическое задание/task_4.py
@@ -20,3 +20,81 @@ speed, color, name, is_police (булево).
 Выполните доступ к атрибутам, выведите результат.
 Выполните вызов методов и также покажите результат.
 """
+class Car:
+    # atributes
+    def __init__(self, speed, color, name, is_police):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    # methods
+    def go(self):
+        return f'{self.name} is started'
+
+    def stop(self):
+        return f'{self.name} is stopped'
+
+    def turn_right(self):
+        return f'{self.name} is turned right'
+
+    def turn_left(self):
+        return f'{self.name} is turned left'
+
+    def show_speed(self):
+        return f'Current speed {self.name} is {self.speed}'
+
+
+class TownCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def show_speed(self):
+        print(f'Current speed of town car {self.name} is {self.speed}')
+
+        if self.speed > 40:
+            return f'Speed of {self.name} is higher than allow for town car'
+        else:
+            return f'Speed of {self.name} is normal for town car'
+
+class SportCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+
+class WorkCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def show_speed(self):
+        print(f'Current speed of work car {self.name} is {self.speed}')
+
+        if self.speed > 60:
+            return f'Speed of {self.name} is higher than allow for work car'
+
+
+class PoliceCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def police(self):
+        if self.is_police:
+            return f'{self.name} is from police department'
+        else:
+            return f'{self.name} is not from police department'
+
+
+audi = SportCar(100, 'Red', 'Audi', False)
+oka = TownCar(30, 'White', 'Oka', False)
+lada = WorkCar(70, 'Rose', 'Lada', True)
+ford = PoliceCar(110, 'Blue',  'Ford', True)
+print(lada.turn_left())
+print(f'When {oka.turn_right()}, then {audi.stop()}')
+print(f'{lada.go()} with speed exactly {lada.show_speed()}')
+print(f'{lada.name} is {lada.color}')
+print(f'Is {audi.name} a police car? {audi.is_police}')
+print(f'Is {lada.name}  a police car? {lada.is_police}')
+print(audi.show_speed())
+print(oka.show_speed())
+print(ford.police())
+print(ford.show_speed())
