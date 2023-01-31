8a2821611e9f5b5371977207d3caa6954286439f
tr <ni4555425@gmail.com>
2023-01-31 04:09:55 +0300

    Homeworck 3

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -8,3 +8,21 @@
 Иван Иванов 1846 года рождения, проживает в городе Москва,
 email: jackie@gmail.com, телефон: 01005321456
 """
+
+
+def id_card(user_name, user_surname, date_birth, user_town, email, user_tel):
+    return F'{user_name} {user_surname} {date_birth} года рождения, проживает в городе {user_town} email: {email} телефон: {user_tel}'
+
+
+user_town = input('Введите город : ')
+user_name = input('Введите имя :')
+user_surname = input('Введите фамилию :')
+date_birth = input('Введите дату рождения :')
+email = input('Введите email :')
+user_tel = input('Введите телефон :')
+
+print(id_card(user_town=user_town, user_name=user_name, user_surname=user_surname, date_birth=date_birth, email=email,
+              user_tel=user_tel))
+
+print(id_card(user_town='Москва', user_name='Иван', user_surname='Иванов', date_birth=1988, email='qaz@ws.ru',
+              user_tel='+79991112233'))
