d6338c483f8c10282ef8c2e7021418c7657ea2a0
asket65 <117043937+asket65@users.noreply.github.com>
2023-02-02 21:03:48 +1100

    Update task_5.py

--- Урок 4. Практическое задание/task_5.py
+++ Урок 4. Практическое задание/task_5.py
@@ -5,3 +5,6 @@
 Необходимо получить результат вычисления произведения всех элементов списка.
 Подсказка: использовать лямбда-функцию и функцию reduce().
 """
+from functools import reduce
+initial_list = [i for i in range(100, 1001, 2)]
+print("Сумма элементов списка:\n", reduce(lambda a, b: a * b, initial_list))
