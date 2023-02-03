6c8eb6651cecc310c91d2ea705fe0ca98e5666e3
NataliaHimera <94389596+NataliaHimera@users.noreply.github.com>
2023-02-03 10:38:56 +0300

    Update task_2.py

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -7,3 +7,6 @@
 
 Реализуйте вариант без и с генераторным выражением
 """
+list1 = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+list2 = [el for el in list1 if list1.index(el) != 0 and el > list1[list1.index(el)-1]]
+print(list2)
