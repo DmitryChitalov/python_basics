5d0c03ed7485fd038a1e4756b77676426eaaae44
NataliaHimera <94389596+NataliaHimera@users.noreply.github.com>
2023-02-03 10:38:06 +0300

    Update task_1.py

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -6,3 +6,14 @@
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
 """
+from sys import argv
+prog, employee, hours, rate, bonus = argv
+
+def salary_sum(hours, rate, bonus):
+    try:
+        res = (int(hours) * int(rate)) + int(bonus)
+        return res
+    except TypeError:
+        print("Не правильно введены данные")
+
+print(f"Зарплата сотрудника {employee} составила {salary_sum(hours, rate, bonus)} руб.")
