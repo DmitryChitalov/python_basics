3c232e5a327ce78d29793eb3992abd2375ed53fe
Анна Саломатова <123293341+AnnaSalomatova@users.noreply.github.com>
2023-02-04 14:05:41 +0300

    Update task_4.py

--- Урок 5. Практическое задание/task_4/task_4.py
+++ Урок 5. Практическое задание/task_4/task_4.py
@@ -7,10 +7,10 @@
 Иванов 23543.12
 Петров 13749.32
 """
-with open('les5_task3_text.txt', 'r', encoding='utf-8') as txt:
+with open('les5_task4_text.txt', 'r', encoding='utf-8') as txt:
     salaries = {line.split()[0].strip(): int(line.split()[1]) for line in txt.readlines()}
 sum_of_salaries = 0  
-for surname, salary in salaries.items():  # перебор словаря
+for surname, salary in salaries.items(): 
     sum_of_salaries += salary
     if salary < 20000:
         print(surname)
