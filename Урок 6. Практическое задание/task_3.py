859c86d3ecef671681ca5c857bbeb1dd771a3512
Дмитрий Соков <sokov.de@gmail.com>
2023-02-05 16:10:47 +0300

    Lesson-6 SDE

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -1,17 +1,33 @@
-"""
-Задание 3.
+'''
+3. Реализовать базовый класс Worker (работник).
+определить атрибуты: name, surname, position (должность), income (доход);
+последний атрибут должен быть защищённым и ссылаться на словарь, содержащий элементы: оклад и премия, например, {"wage": wage, "bonus": bonus};
+создать класс Position (должность) на базе класса Worker;
+в классе Position реализовать методы получения полного имени сотрудника (get_full_name) и дохода с учётом премии (get_total_income);
+проверить работу примера на реальных данных: создать экземпляры класса Position, передать данные, проверить значения атрибутов, вызвать методы экземпляров.
+'''
+class Worker:
 
-Реализовать базовый класс Worker (работник),
-в котором определить публичные атрибуты name, surname, position (должность),
-и защищенный атрибут income (доход). Последний атрибут должен ссылаться
-на словарь, содержащий элементы: оклад и премия, например, {"wage": wage, "bonus": bonus}.
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": int(wage), "bonus": int(bonus)}
 
-Создать класс Position (должность) на базе класса Worker. В классе Position реализовать публичные методы
-получения полного имени сотрудника (get_full_name) и дохода с учетом премии (get_total_income).
 
-Проверить работу примера на реальных данных (создать экземпляры класса Position, передать данные,
-проверить значения атрибутов, вызвать методы экземпляров).
+class Position(Worker):
+    def __init__(self, name, surname, position, wage, bonus):
+        super().__init__(name, surname, position, wage, bonus)
 
-П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку __str__
-__str__(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
-"""
+    def get_full_name(self):
+        return self.name + ' ' + self.surname
+
+    def get_total_income(self):
+        return self._income["wage"] + self._income["bonus"]
+
+    def get_postition(self):
+        return self.position
+
+
+p = Position('Иван', 'Петров', 'Системный аналитик', '100000', '25000')
+print(p.get_full_name(),p.get_postition(),  p.get_total_income())
