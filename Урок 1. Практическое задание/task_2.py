bd3ec5623b212f7add4a644568c3699a722f30ff
Teisdarilassa <9055279222@mail.ru>
2023-02-05 19:11:18 +0300

    Auto stash before rebase of "refs/heads/lesson1_homework"
    homework1

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -9,3 +9,29 @@
 Введите время в секундах: 3600
 Время в формате ч:м:с - 1.0 : 60.0 : 3600
 """
+time_seconds = int(input("enter seconds: "))
+hours = time_seconds // 3600
+minutes = time_seconds // 60 - hours * 60
+seconds = time_seconds - hours * 3600 - minutes * 60
+if hours >= 10:
+    if minutes >= 10:
+        if seconds >= 10:
+            print(f'Время: {hours}:{minutes}:{seconds}')
+        else:
+            print(f'Время: {hours}:{minutes}:{0}{seconds}')
+    if minutes < 10:
+        if seconds >= 10:
+            print(f'Время: {hours}:{0}{minutes}:{seconds}')
+        else:
+            print(f'Время: {hours}:{0}{minutes}:{0}{seconds}')
+if hours < 10:
+    if minutes >= 10:
+        if seconds >= 10:
+            print(f'Время: {0}{hours}:{minutes}:{seconds}')
+        else:
+            print(f'Время: {0}{hours}:{minutes}:{0}{seconds}')
+    if minutes < 10:
+        if seconds >= 10:
+            print(f'Время: {0}{hours}:{0}{minutes}:{seconds}')
+        else:
+            print(f'Время: {0}{hours}:{0}{minutes}:{0}{seconds}')
--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,8 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+n = int(input("Вводим число: "))
+n_str = str(n)
+nn = n_str + n_str
+nnn = n_str + n_str + n_str
+print(n + int(nn) + int(nnn))
