a8642ed283741dad265166403b6f37780d8fe2e9
Alexander <121261035+1goldenhaw@users.noreply.github.com>
2023-02-08 17:52:57 +0300

    Belov_Alexander_HW8

--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -9,3 +9,25 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+
+
+class OnlyDigit:
+    def __init__(self, *args):
+        self.my_list = []
+
+    def input_digit(self):
+        while True:
+            val = input('Вводите числа с новой строки.\n(press Q to exit)\n>  ')
+            try:
+                val = int(val)
+                self.my_list.append(val)
+                print(f'Обновлённый список: {self.my_list} \n')
+            except ValueError:
+                if val == 'Q' or val == 'q':
+                    return f'Exit... Goodbye!'
+                else:
+                    print(f"Введено значение неправильного типа!\n")
+
+
+try_except = OnlyDigit(1)
+print(try_except.input_digit())
