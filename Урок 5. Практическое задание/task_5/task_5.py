429317c066bc447389d79f332c972a1babce7617
NataliaHimera <94389596+NataliaHimera@users.noreply.github.com>
2023-02-09 23:17:15 +0300

    Update task_5.py

--- Урок 5. Практическое задание/task_5/task_5.py
+++ Урок 5. Практическое задание/task_5/task_5.py
@@ -2,3 +2,12 @@
 5)	Создать (программно) текстовый файл, записать в него программно набор чисел,
  разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 """
+with open("file5.txt", "w", encoding='utf-8') as file_5:
+    try:
+        my_list = input("Введите числа через пробел: ")
+        file_5.writelines(my_list)
+        my_number = my_list.split(' ')
+        print(f"Сумма чисел: {sum(map(int, my_number))}")
+    except ValueError:
+        print("Вводите только цифры")
+      
