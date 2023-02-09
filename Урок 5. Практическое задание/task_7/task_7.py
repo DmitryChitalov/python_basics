9483ee83ea424cbd00d2845300d8badf9cf6b27f
NataliaHimera <94389596+NataliaHimera@users.noreply.github.com>
2023-02-09 23:20:54 +0300

    Update task_7.py

--- Урок 5. Практическое задание/task_7/task_7.py
+++ Урок 5. Практическое задание/task_7/task_7.py
@@ -9,3 +9,20 @@
 [{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit": 2000}]
 Подсказка: использовать менеджер контекста.
 """
+import json
+total_revenue = 0
+count_revenue = 0
+company_balance = {}
+with open("file7.txt", "r", encoding='utf-8') as file_7:
+    for line in file_7:
+        name, type, revenue, loss = line.split()
+        profit_company = int(revenue) - int(loss)
+        company_balance.update({name: profit_company})
+        if profit_company > 0:
+            count_revenue += 1
+            total_revenue += profit_company
+res = [company_balance, {"average_profit": total_revenue/count_revenue}]
+print(res)
+with open("file7.json", "w", encoding='utf-8') as file7_json:
+    json.dump(res, file7_json)
+    
