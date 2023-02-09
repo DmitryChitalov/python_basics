98f457b40206d09bfdd1ec6993fd7a296cc7fada
LuckyThi13en <www.k.vavilov@gmail.com>
2023-02-09 15:15:33 +0300

    Python_basics_less05_homework_Vavilov

--- Урок 5. Практическое задание/task_3/task_3.py
+++ Урок 5. Практическое задание/task_3/task_3.py
@@ -8,3 +8,12 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
+lib_locale = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре'}
+new_txt_file = []
+with open('numbers.txt', 'r', encoding='utf-8') as txt_file:
+    for i in txt_file:
+        i = i.split(' ', 1)
+        new_txt_file.append(lib_locale[i[0]] + ' ' + i[1])
+
+with open('numbers2.txt', 'w', encoding='utf-8') as txt_file2:
+    txt_file2.writelines(new_txt_file)
