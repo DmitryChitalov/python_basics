b161e4c59d5dc80793bdaaf1962adf1f66086d5e
Valya921 <valya-komardina921@yandex.ru>
2023-02-10 18:03:33 +0300

    Домашнее задание 2

--- Урок 2. Практическое задание/task_1.py
+++ Урок 2. Практическое задание/task_1.py
@@ -15,3 +15,12 @@
 <class 'bool'>
 <class 'NoneType'>
 """
+
+lst_1 = [5, "string", 0.15, True, None]
+for el in lst_1:
+    print (type(el))
+
+
+lst_1 = [5, "string", 0.15, True, None]
+for i in range(len(lst_1)):
+    print(type(lst_1[i]))
