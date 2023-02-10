d755c0301c7274023e8f03fd56d948af9ce0305d
Valya921 <valya-komardina921@yandex.ru>
2023-02-10 19:04:28 +0300

    Задание 5

--- Урок 2. Практическое задание/task_5.py
+++ Урок 2. Практическое задание/task_5.py
@@ -15,3 +15,32 @@
 Набор натуральных чисел можно задать непосредственно в коде,
 например, my_list = [7, 5, 3, 3, 2].
 """
+      
+my_list = [7, 5, 3, 3, 1]
+
+a = False
+while not a:
+    try:
+        new_score = int(input('New score is: '))
+        if new_score < 0:
+            print('Должно быть положительное число')
+            continue
+        if new_score > max(my_list):
+            my_list.insert(0, new_score)
+            break
+        if new_score < min(my_list):
+            my_list.append(new_score)
+            break
+        for i, el in enumerate(my_list):
+            if my_list[-1] == new_score:
+                my_list.append(new_score)
+                a = True
+                break
+            if el == new_score and el != my_list[i + 1]:
+                my_list.insert(i + 1, new_score)
+                a = True
+                break
+    except ValueError:
+        print('Должно быть положительное число')
+
+print(f'Измененный список {my_list}')
