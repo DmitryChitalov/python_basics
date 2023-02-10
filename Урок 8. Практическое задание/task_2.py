b637bd67a3d7d06a1ad805f451a25d8ee75ca893
El_Ildaryao <theprother@gmail.com>
2023-02-10 10:43:23 +0300

    lesson_8

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,18 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+
+
+class ChekDevision(Exception):
+
+    @staticmethod
+    def DevisionByZero():
+        inp_a = int(input("Введите число а: "))
+        inp_b = int(input("Введите число b: "))
+        try:
+            if inp_b == 0:
+                raise ChekDevision
+            print(f"{inp_a} / {inp_b} = {inp_a / inp_b}")
+        except ChekDevision as err:
+            print("На ноль делить нельзя!!")
+ChekDevision.DevisionByZero()
