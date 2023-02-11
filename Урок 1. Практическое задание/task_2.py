e3ce733d240dfd8d1cd74c0ffa68316f45abdf85
V1p3rL0rd <123589830+V1p3rL0rd@users.noreply.github.com>
2023-02-11 20:09:18 +0300

    Update task_2.py

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -9,3 +9,15 @@
 Введите время в секундах: 3600
 Время в формате ч:м:с - 1.0 : 60.0 : 3600
 """
+
+timer = int(input('Введите время в секундах: '))
+hours = timer//3600
+hours_res = (hours) if hours > 10 else ('0' + str(hours))
+minutes = (timer % 3600)//60
+minutes_res = (minutes) if minutes > 10 else ('0' + str(minutes))
+seconds = (timer % 3600) % 60
+seconds_res = (seconds) if seconds > 10 else ('0' + str(seconds))
+if hours > 24:
+    print('ОШИБКА! Количество полученных часов превышает количество часов в сутках, скоректируйте ввод.')
+else:
+    print(f'Время в формате ч:м:с - {hours_res}:{minutes_res}:{seconds_res}')
