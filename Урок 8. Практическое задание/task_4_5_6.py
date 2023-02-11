fb93a434f60e0263d5f632f984902c7d140c7595
Harats15061 <muryzhnikova-natalya@yandex.ru>
2023-02-12 02:02:21 +0500

    выполнение ДЗ

--- Урок 8. Практическое задание/task_4_5_6.py
+++ Урок 8. Практическое задание/task_4_5_6.py
@@ -19,3 +19,91 @@
 Подсказка: постарайтесь по возможности реализовать в проекте
 «Склад оргтехники» максимум возможностей, изученных на уроках по ООП.
 """
+class OfficeEquipmentWarehouse:
+    """Это Класс=склад оргтехники"""
+    print("\nСклад оргтехники")
+
+
+class OfficeEquipment:
+    """Это= Базовый класс оргтехники"""
+    def __init__(self, producer, color):
+        self.producer = producer
+        self.color = color
+
+
+class Printer(OfficeEquipment):
+    """Это=Класс принтер"""
+    amount_pr = 0
+
+    def __init__(self, producer, color, pr_type):
+        super().__init__(producer, color)
+        self.pr_type = pr_type
+        Printer.amount_pr += 1
+
+    @staticmethod
+    def name():
+        return "<<Принтер>>"
+
+    def type_printer(self):
+        return self.pr_type
+
+    def __str__(self):
+        return "\tпроизводитель: {} \tцвет: {}  \tцена принтера: {}".format(self.producer, self.color, self.pr_type)
+
+
+class Scanner(OfficeEquipment):
+    """Это = Класс сканер"""
+    amount_sc = 0
+
+    def __init__(self, producer, color, sc_sensor):
+        super().__init__(producer, color)
+        self.sc_sensor = sc_sensor
+        Scanner.amount_sc += 1
+
+    @staticmethod
+    def name():
+        return"<<Сканер>>"
+
+    def type_sensor(self):
+        return self.sc_sensor
+
+    def __str__(self):
+        return "\tпроизводитель: {} \tцвет: {} \tцена сенсора: {}".format(self.producer, self.color, self.sc_sensor)
+
+
+class Xerox(OfficeEquipment):
+    """Это= Класс ксерокс"""
+    amount_x = 0
+
+    def __init__(self, producer, color, xer_wi_fi):
+        super().__init__(producer, color)
+        self.xer_wi_fi = xer_wi_fi
+        Xerox.amount_x += 1
+
+    @staticmethod
+    def name():
+        return "<<Ксерокс>>"
+
+    def wi_fi_module(self):
+        return self.xer_wi_fi
+
+    def __str__(self):
+        return "\tпроизводитель: {} \tцвет: {}   \tWi-Fi цена: {}".format(self.producer, self.color, self.xer_wi_fi)
+
+p = Printer('Canon', 'red', '5000')
+p2 = Printer('LG', 'yellow', '6500')
+print(p.name(), p.amount_pr, "шт")
+print(p.__str__())
+print(p2.__str__())
+
+s = Scanner('Samsung', 'black', '8700')
+print(s.name(), s.amount_sc, "шт")
+print(s.__str__())
+
+x = Xerox('HP', 'white', 'есть')
+x2 = Xerox('Samsung', 'black', 'есть')
+x3 = Xerox('Kodak', 'red', 'есть')
+print(x.name(), x.amount_x, "шт")
+print(x.__str__())
+print(x2.__str__())
+print(x3.__str__())
