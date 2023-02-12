bdfe079980f16638e1058d8d23030a47e2521481
Arthur Shchebetaw <archisoft6@gmail.com>
2023-02-12 17:38:13 +0400

    task_3 done

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,9 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+usr_numb = input('Введите целое положительное число i \n')
+term_1 = int(usr_numb)
+term_2 = int(usr_numb + usr_numb)
+term_3 = int(usr_numb + usr_numb + usr_numb)
+total = term_1 + term_2 + term_3
+print('Сумма i + ii + iii составляет:', total)
