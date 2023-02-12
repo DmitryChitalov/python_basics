bfa90976f30a9ec63975d54af064b4b1c96e2a9e
treeth <kolisto.07@gmail.com>
2023-02-13 00:36:45 +0300

    Homeworck5 без 2

--- Урок 5. Практическое задание/task_1/task_1.py
+++ Урок 5. Практическое задание/task_1/task_1.py
@@ -3,4 +3,23 @@
 записать в него построчно данные, вводимые пользователем.
 Об окончании ввода данных свидетельствует пустая строка.
 """
-print('Hello')
+# Flag = 1
+# out_f = open('tast_1.txt', 'w', encoding='utf-8')
+
+# while Flag == 1:
+#     a = input('Введите текст: ')
+#     if a == '':
+#         Flag = 0
+#     else:
+#         out_f.writelines(f'{a} \n')
+
+# out_f.close()
+
+flag = 1
+with open('DZ_tast_1.txt', 'w', encoding='utf-8') as out_f:
+    while flag == 1:
+        user_str = input('Введите текст: ')
+        if user_str == '':
+            flag = 0
+        else:
+            out_f.writelines(f'{user_str}\n')
--- Урок 5. Практическое задание/task_2/task_2.py
+++ Урок 5. Практическое задание/task_2/task_2.py
@@ -2,3 +2,11 @@
 2)	Создать текстовый файл (не программно), сохранить в нем несколько строк,
 выполнить подсчет количества строк, количества слов в каждой строке.
 """
+with open('my_file.txt', 'r', encoding='utf-8') as my_file:
+    f_string = 0
+    for line in my_file:
+        f_string += 1
+        words = len(line.split(' '))
+        print(f'В строке = {words} слов')
+
+print(f'Строк в файле = {f_string}')
