193d5c854cbe365b388479b6e59cf5344c1a73de
LuckyThi13en <www.k.vavilov@gmail.com>
2023-02-13 11:44:29 +0300

    Python_basics_less06_homework_Vavilov

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -15,3 +15,26 @@
 П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку __str__
 __str__(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
 """
+
+class Worker:
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+class Position(Worker):
+
+    def get_full_name(self):
+        return f'{self.name} {self.surname}'
+
+    def get_total_income(self):
+        return f'Доход сотрудника {self.name} {self.surname} за месяц: {self._income.setdefault("wage") + self._income.setdefault("bonus")}'
+
+worker_1 = Position('Максим', 'Морозов', 'Системный администратор', 100000, 50000)
+worker_2 = Position('Роман', 'Иванов', 'Директор', 300000, 150000)
+worker_3 = Position('Ирина', 'Климова', 'Бухгалтер', 80000, 40000)
+
+print(worker_1.get_total_income())
+print(worker_2.get_total_income())
+print(worker_3.get_total_income())
--- Урок 6. Практическое задание/task_5.py
+++ Урок 6. Практическое задание/task_5.py
@@ -13,3 +13,36 @@
 Для каждого из классов метод должен выводить уникальное сообщение.
 Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
 """
+
+
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        return 'Запуск отрисовки.'
+
+
+class Pen(Stationery):
+    def draw(self):
+        return f"Отрисовка изменена на {self.title}"
+
+
+class Pencil(Stationery):
+    def draw(self):
+        return f"Отрисовка изменена на {self.title}"
+
+
+class Handle(Stationery):
+    def draw(self):
+        return f"Отрисовка изменена на {self.title}"
+
+
+new_pen = Pen('ручка')
+print(new_pen.draw())
+
+new_pencil = Pencil('карандаш')
+print(new_pencil.draw())
+
+new_handle = Handle('маркер')
+print(new_handle.draw())
