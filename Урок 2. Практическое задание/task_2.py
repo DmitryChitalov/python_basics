04cc0b31e09b046f88fee8647b730f57c4ac773b
Evgeniy Medvedev <e.medvedev@belvg.com>
2023-02-14 08:05:26 +0300

    task2 for

--- Урок 2. Практическое задание/task_2.py
+++ Урок 2. Практическое задание/task_2.py
@@ -11,3 +11,14 @@
 Введите целые числа через пробел: 1 2 3
 Результат: 2 1 3
 """
+ls = '1 8 3 4 6 8 9'
+ls = ls.split()
+
+for v in range(0, len(ls), 2):
+
+    if v + 1 < len(ls):
+        print(ls[v], ls[v+1])
+        var1, var2 = ls[v], ls[v+1]
+        ls[v], ls[v+1] = var2, var1
+
+print(ls)
