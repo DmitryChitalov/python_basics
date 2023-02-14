eeed57e50f823dce84a5637aa857c09b74c6998c
leshevi <79736739+leshevi@users.noreply.github.com>
2023-02-14 20:18:50 +0700

    leshevi_commit

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -18,3 +18,13 @@
 Введите численность сотрудников фирмы: 10
 Прибыль фирмы в расчете на одного сотрудника = 50.0
 """
+revenue = int(input("Введите выручку:"))
+costs = int(input("Введите издержки:"))
+employees = int(input("Введите количество сотрудников:"))
+income = revenue - costs
+if income > 0:
+    print(f"Прибыль {income}, рентабельность {income / revenue * 100:.2f} %, прибыль на человека {income / employees:.2f}")
+elif income == 0:
+    print(f"Работаем без рентабельности")
+else:
+    print(f"Убыток {income * -1}")
