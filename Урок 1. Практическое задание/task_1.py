0a871efdc5e03e56957662e9e0a52edcda5737d0
Public relations & Virtual technology <prvtdata@pm.me>
2023-02-14 11:14:40 +0300

    lesson_1

--- Урок 1. Практическое задание/task_1.py
+++ Урок 1. Практическое задание/task_1.py
@@ -11,3 +11,33 @@
 Введите ваш возраст: 45
 Ваши данные для входа в аккаунт: имя - Василий, пароль - vas, возраст - 45
 """
+
+var_int = 5
+var_float = 5.3
+var_bool = False
+var_str = "Pip pop"
+dict_list = ["var_int", "var_float", "var_bool", "var_str", "dict_list"]
+
+
+def type_var(el, var_target):
+    print(el, type(var_target), var_target)
+
+
+for el in dict_list:
+    if el == "var_int":
+        type_var(el, var_target := var_int)
+    elif el == "var_float":
+        type_var(el, var_target := var_float)
+    elif el == "var_bool":
+        type_var(el, var_target := var_bool)
+    elif el == "var_str":
+        type_var(el, var_target := var_str)
+    elif el == "dict_list":
+        type_var(el, var_target := dict_list)
+    else:
+        print("Pupa")
+
+
+user_name = input("Имя: ")
+age = int(input("Возрост: "))
+print(f"Привет {user_name}, вам {age} лет")
--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,7 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+
+n = int(input("Введите число n: "))
+sum_n = n + int(str(n) + str(n)) + int(str(n) + str(n) + str(n))
+print("n + nn + nnn =", sum_n)
