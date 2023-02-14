55cf7fb294681db7424d5352ba61127a2560b930
Valya921 <valya-komardina921@yandex.ru>
2023-02-14 19:47:55 +0300

    Уро3 задание 5

--- Урок 3. Практическое задание/task_5.py
+++ Урок 3. Практическое задание/task_5.py
@@ -6,3 +6,24 @@
     символ введен после нескольких чисел, то вначале нужно добавить сумму этих чисел к полученной
     ранее сумме и после этого завершить программу.
 """
+def calculate_sum(*nums):
+    sum = 0
+    flag = False
+    for num in nums:
+        try:
+            if num:
+                sum += float(num)
+        except ValueError:
+            flag = True
+    return sum, flag
+
+general_sum = 0
+
+while True:
+    numbers_string = input('Введите числа через пробел: ').split(' ')
+    sum, stop_flag = calculate_sum(*numbers_string)
+    general_sum += sum
+    print(f'сумма {general_sum}')
+
+    if stop_flag:
+        stop
