92a449c5c799fad2be6ad33684218a3716fb9e71
Valya921 <valya-komardina921@yandex.ru>
2023-02-14 19:30:09 +0300

    Урок 3, задание 2

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -8,3 +8,10 @@
 Иван Иванов 1846 года рождения, проживает в городе Москва,
 email: jackie@gmail.com, телефон: 01005321456
 """
+
+def personal_data( *, name, lastname, year_of_birth, city, email, phone):
+    return print(f" {name} {lastname} {year_of_birth} года рождения, проживает в городе {city}, Email: {email} Телефон: {phone}")
+
+
+personal_data(name=input('Имя:'),lastname=input('Фамилия: '), year_of_birth=input('Год Рождения: '),
+    city=input('Город проживания: '),email=input('email: '),phone=input('phone: '),)
