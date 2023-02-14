2fd3a53c48e85233646e88ed93f15add54becc63
leshevi <79736739+leshevi@users.noreply.github.com>
2023-02-14 20:15:37 +0700

    leshevi_commit

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -9,3 +9,8 @@
 Введите время в секундах: 3600
 Время в формате ч:м:с - 1.0 : 60.0 : 3600
 """
+time = int(input("Введите время в секундах:"))
+hour = time // 3600
+minute = (time - hour * 3600) // 60
+second = time - ((hour * 3600) + (minute * 60))
+print(f"Время в формате ч:м:с = {hour}:{minute}:{second}")
