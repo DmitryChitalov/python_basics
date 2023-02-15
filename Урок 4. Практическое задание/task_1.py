89defef4692faadb306f86f53f87778a49503968
Emarkina3001 <123263731+Emarkina3001@users.noreply.github.com>
2023-02-16 00:18:27 +0300

    Update task_1.py

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -6,3 +6,15 @@
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
 """
+import sys
+
+f_obj, name_v, rate_v,hours_v = sys.argv
+print(f_obj)
+
+def calculate_salary(rate,hours):
+    try:
+        print(f'Сотрудник {name_v}) заработал {int(rate) * int(hours)* 1.25}')
+    except TypeError:
+        print("Операция применена к объекту несоответствующего типа")
+        exit()
+calculate_salary(rate_v,hours_v)
