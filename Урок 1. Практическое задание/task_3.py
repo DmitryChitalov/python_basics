a0e17e19451f1135c4c695251052deadc9fcf733
Mikhail <mikhail_8086@mail.ru>
2023-02-16 21:56:09 +0500

    Урок-1

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,9 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+n = 0
+n = input("Введите число n: ")
+n_2 = n+n
+n_3 = n+n+n
+n = int(n) + int(n_2) + int(n_3)
+print(n)
--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -18,3 +18,28 @@
 Введите численность сотрудников фирмы: 10
 Прибыль фирмы в расчете на одного сотрудника = 50.0
 """
+revenue = 0
+costs = 0
+profit = 0
+profitability = 0
+employees = 0
+profit_employees = 0
+
+revenue = int(input("Введите значение выручки: "))
+costs = int(input("Введите значение издержек: "))
+profit = revenue - costs
+
+if profit < 0:
+    print("Финансовый результат фирмы:", profit, "р.", "- Убыток!")
+if profit == 0:
+    print("Финансовый результат фирмы:", profit, "р.", "Сработали в ноль!")
+if profit > 0:
+    print("Финансовый результат фирмы:", profit,"р.","- Прибыль!")
+    profitability = (profit / revenue) * 100
+    profitability = float('{:.2f}'.format(profitability))
+    print ("Рентабельность фирмы:", profitability,"%")
+    employees = int(input("Введите кол-во сотрудников: "))
+    profit_employees = profit / employees
+    profit_employees = float('{:.2f}'.format(profit_employees))
+    print("Прибыль в расчете на одного сотрудника:", profit_employees,"р.")
+
