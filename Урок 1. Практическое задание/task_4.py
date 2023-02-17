f562d50af154daa6b3467b50858399e83fd534c0
Azim9292 <isaev-azim@mail.ru>
2023-02-17 21:53:55 +0600

    initial commit

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -10,3 +10,12 @@
 Ведите целое положительное число: 123456789
 Самая большая цифра в числе: 9
 """
+
+number = int(input("Введите целое положительное число: "))
+max_num = 0
+while number != 0:
+    cur_n = number % 10
+    if max_num < cur_n:
+        max_num = cur_n
+        number = number // 10
+print(f"Самая большая цифра в числе: {max_num}")
