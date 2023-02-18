bbba1096dfc051a6d287b9c05253cafab9e7a980
Emarkina3001 <123263731+Emarkina3001@users.noreply.github.com>
2023-02-18 15:11:26 +0300

    Update task_4.py

--- Урок 5. Практическое задание/task_4/task_4.py
+++ Урок 5. Практическое задание/task_4/task_4.py
@@ -7,3 +7,13 @@
 Иванов 23543.12
 Петров 13749.32
 """
+import json
+
+with open("task_4.json", encoding='utf-8') as read_f:
+    data_dict = json.load(read_f)
+    print(type(data_dict))
+    print("Сотрудники, зарабатывающие менее  20000$ в год:")
+    for keys, values in data_dict.items():
+        if values < 20000:
+            print(keys)
+    print(f"Средняя заработная плата = {sum(data_dict.values())/len(data_dict)} $ в год")
