61e645377966128e4c59c2015ba0b94ae61ab628
Emarkina3001 <123263731+Emarkina3001@users.noreply.github.com>
2023-02-18 16:23:13 +0300

    Update task_1.py

--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -15,3 +15,19 @@
 
 Проверить работу примера, создав экземпляр и вызвав описанный метод.
 """
+
+from time import sleep
+
+
+class TrafficLight:
+    __color = {'Красный': 7, 'Желтый': 2, 'Зеленый': 5}
+
+    @staticmethod
+    def running():
+        for key, value in TrafficLight.__color.items():
+            print(f'Светофор переключился в режим {key}')
+            sleep(value)
+
+
+TL = TrafficLight()
+TL.running()
