0ca9e00669e5a98bc345e5df94b24d16f6a4c45f
Evgeniy Medvedev <e.medvedev@belvg.com>
2023-02-18 15:29:22 +0300

    Task5

--- Урок 3. Практическое задание/task_5.py
+++ Урок 3. Практическое задание/task_5.py
@@ -6,3 +6,23 @@
     символ введен после нескольких чисел, то вначале нужно добавить сумму этих чисел к полученной
     ранее сумме и после этого завершить программу.
 """
+
+
+def sum_str(_sum, _inp_str):
+    arr_str = str(_inp_str).split()
+    arr_int = []
+    for v in arr_str:
+        if v == '!':
+            break
+        arr_int.append(int(v))
+    return sum(arr_int) + _sum
+
+
+sum_numbers = 0
+while True:
+    inp_str = input('Введите числа через пробел!')
+
+    sum_numbers = sum_str(sum_numbers, inp_str)
+    print(sum_numbers)
+    if inp_str.count('!'):
+        break
