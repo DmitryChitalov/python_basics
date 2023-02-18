c4b043e796abe3f3a501738a051e6bbc46866f4b
Emarkina3001 <123263731+Emarkina3001@users.noreply.github.com>
2023-02-18 16:29:20 +0300

    Update task_4.py

--- Урок 6. Практическое задание/task_4.py
+++ Урок 6. Практическое задание/task_4.py
@@ -20,3 +20,90 @@ speed, color, name, is_police (булево).
 Выполните доступ к атрибутам, выведите результат.
 Выполните вызов методов и также покажите результат.
 """
+class Car:
+
+    def __init__(self, speed, color, name, is_police):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def go(self):
+        return f'{self.name} поехала'
+
+    def stop(self):
+        return f'{self.name} остановилась'
+
+    def turn(self, direction):
+        return f'{self.name} повернула в направлении {direction}'
+
+    def show_speed(self):
+        return f'Текущая скорость автомобиля {self.name} составляет {self.speed}'
+
+
+class TownCar(Car):
+
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def show_speed(self):
+        print(f'Текущая скорость городского автомобиля {self.name} составляет {self.speed}')
+
+        if self.speed > 60:
+            return f'Скорость автомобиля {self.name} выше, чем это позволено для городского автомобиля'
+        else:
+            return f'Скорость автомобиля {self.name} нормальная для городского автомобиля'
+
+
+class WorkCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def show_speed(self):
+        print(f'Текущая скорость рабочего автомобиля {self.name} составляет {self.speed}')
+
+        if self.speed > 40:
+            return f'Скорость автомобиля {self.name} выше, чем это позволено для рабочего автомобиля'
+        else:
+            return f'Скорость автомобиля {self.name} нормальная для рабочего автомобиля'
+
+
+class SportCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def sport(self):
+        if self.is_police:
+            return f'Автомобиль {self.name} является спортивным'
+        else:
+            return f'Автомобиль {self.name} не является спортивным'
+
+
+class PoliceCar(Car):
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+    def police(self):
+        if self.is_police:
+            return f'Автомобиль {self.name} является полицейским'
+        else:
+            return f'Автомобиль {self.name} не является полицейским'
+
+
+OKA = TownCar(30, 'Белая', 'Ока', False)
+VAZ = WorkCar(70, 'Вишневая', 'Лада', True)
+AUDI = SportCar(100, 'Красная', 'Ауди', False)
+FORD = PoliceCar(110, 'Белый',  'Форд', True)
+
+print(VAZ.turn("Направо"))
+print(f'Когда {OKA.turn("Налево")}, то {AUDI.stop()}')
+print(f'{VAZ.go()} со скоростью {VAZ.show_speed()}')
+print(f'{VAZ.name} цвета {VAZ.color}')
+
+print(f'{AUDI.name} полицейская машина? {AUDI.is_police}')
+print(f'{VAZ.name} полицейская машина? {VAZ.is_police}')
+
+print(AUDI.show_speed())
+print(OKA.show_speed())
+print(FORD.police())
+print(FORD.show_speed())
