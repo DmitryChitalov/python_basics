4621bd4ef586cb5a753b4583b2c5e9aa7d6bf23b
Evgeniy Medvedev <e.medvedev@belvg.com>
2023-02-18 12:32:12 +0300

    Task2

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -8,3 +8,16 @@
 Иван Иванов 1846 года рождения, проживает в городе Москва,
 email: jackie@gmail.com, телефон: 01005321456
 """
+
+def user_info(_name, _second_name, _years, _city, _email, _phone):
+    print(f'{_name} {_second_name} {_years} года рождения, проживает в городе {_city}, email: {_email}, телефон: {_phone}')
+
+
+name = input('Введите имя: ')
+second_name = input('Введите фамилию: ')
+years = input('Введите год рождения: ')
+city = input('Введите город проживания: ')
+email = input('Введите email: ')
+phone = input('Введите телефон: ')
+
+user_info(name, second_name, years, city, email, phone)
