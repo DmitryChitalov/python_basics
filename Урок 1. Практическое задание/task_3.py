a58af7eed8496d65ec301bf8879b3411990b9d86
Mikhail Nikulin <mikhail.nikulin@itl.team>
2023-02-18 13:42:52 +0300

    add code

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,11 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+
+n = input("Введите целое положительное число:")
+nn= int(n + n)
+nnn= int(n + n + n)
+n=int(n)
+
+print (f"n + nn + nnn =", n+nn+nnn)
+
