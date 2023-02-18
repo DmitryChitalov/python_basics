8a265230014cd40684c87441558fb53fb60e1525
Deman112 <deman112.spas@gmail.com>
2023-02-18 14:29:33 +0300

    Доработанное задание

--- Урок 2. Практическое задание/DZ2_2.py
+++ Урок 2. Практическое задание/DZ2_2.py
@@ -12,8 +12,8 @@ print(my_list)
 
 i = 0
 for el in range(int(len(my_list)/2)):
-        my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
-        i += 2
+    my_list[i], my_list[i + 1] = my_list[i + 1], my_list[i]
+    i += 2
 print(my_list)
 
 
