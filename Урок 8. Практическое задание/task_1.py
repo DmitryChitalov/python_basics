69c6af1fbbfa3e2af9d5501f96fea3b3de8025f8
NataliaHimera <94389596+NataliaHimera@users.noreply.github.com>
2023-02-19 14:48:00 +0300

    Update task_1.py

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,26 @@
 Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца
 и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
 """
+class Date:
+
+    @classmethod
+    def my_method(self, dmy):
+        d = dmy.split("-")
+        self.day, self.month, self.year = dmy.split("-")
+
+    @staticmethod
+    def valid():
+        if 1 <= int(Date.day) <= 31:
+            if 1 <= int(Date.month) <= 12:
+                if 2023 >= int(Date.year) >= 0:
+                    return f'Правильная дата'
+                else:
+                    return f'Неправильный год'
+            else:
+                return f'Неправильный месяц'
+        else:
+            return f'Неправильный день'
+
+d = Date()
+d.my_method('31-14-2023')
+print(d.valid())
