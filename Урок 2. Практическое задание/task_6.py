329a8f2332bc674f079ee01f2dd676a0abb34611
riddik1391 <97393134+riddik1391@users.noreply.github.com>
2023-02-19 08:26:24 +0300

    Update task_6.py

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -25,3 +25,42 @@
 “ед”: [“шт.”]
 }
 """
+
+goods = []
+features = {'name': '', 'price': '', 'quantity': '', 'unit': ''}
+analytics = {'name': [], 'price': [], 'quantity': [], 'unit': []}
+num = 0
+feature_ = None
+control = None
+while True:
+    control = input("For quit press 'Q', for continue press 'Enter', for analytics press 'A'").upper()
+    if control == 'Q':
+        break
+    num += 1
+    if control == 'A':
+        print(f'\n Current analytics \n {"-" * 30}')
+        for key, value in analytics.items():
+            print(f'{key[:25]:>30}: {value}')
+            print("-" * 30)
+    for f in features.keys():
+        feature_ = input(f'Input feature "{f}"')
+        features[f] = int(feature_) if (f == 'price' or f == 'quantity') else feature_
+        analytics[f].append(features[f])
+    goods.append((num, features))
+
+'''
+goods = int(input("Введите количество товара "))
+n = 1
+my_dict = []
+my_list = []
+my_analys = {}
+while n <= goods:
+    my_dict = dict({'название': input("введите название "), 'цена': input("Введите цену "),
+                    'количество': input('Введите количество '), 'eд': input("Введите единицу измерения ")})
+    my_list.append((n, my_dict))
+    n += 1
+    my_analys = dict(
+        {'название': my_dict.get('название'), 'цена': my_dict.get('цена'), 'количество': my_dict.get('количество'),
+         'ед': my_dict.get('ед')})
+print(my_list)
+print(my_analys)
