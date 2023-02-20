592c7e1723a8d5420def72cf5e6affae3dfb4c00
Anastacia <nastia.peresypkina2000@gmail.com>
2023-02-20 20:29:58 +0400

    Homework 4.1

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -1,5 +1,26 @@
 """
-3. Для чисел в пределах от 20 до 240 найти числа, кратные 20 или 21.
-Необходимо решить задание в одну строку.
-Подсказка: использовать функцию range() и генераторное выражение.
+3. Реализовать функцию my_func(), которая принимает три позиционных аргумента,
+и возвращает сумму наибольших двух аргументов.
+Попробуйте решить задачу двумя способами:
+1) используя функцию sort()
+2) без функции sort()
 """
+
+def my_func(*args):
+    my_lst = list(args)
+    my_lst.sort()
+    result = my_lst[1]+my_lst[2]
+    return result
+
+print(my_func(88,9,100))
+
+
+def my_func(*args):
+    args_list = list(args)
+    max_num1 = max(args_list)
+    args_list.remove(max_num1)
+    max_num2 = max(args_list)
+    result = max_num1 + max_num2
+    return result
+
+print(my_func(12,1,12))
