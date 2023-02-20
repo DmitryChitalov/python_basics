e90ce3cfb85411be95d6edb1decb1148da4f5584
V1p3rL0rd <123589830+V1p3rL0rd@users.noreply.github.com>
2023-02-20 15:36:51 +0300

    Update task_6.py

--- Урок 3. Практическое задание/task_6.py
+++ Урок 3. Практическое задание/task_6.py
@@ -8,3 +8,22 @@
 Сделать вывод исходной строки, но каждое слово должно начинаться с заглавной буквы.
 Необходимо использовать написанную ранее функцию int_func().
 """
+def my_sum ():
+    sum_res = 0
+    ex = False
+    while ex == False:
+        number = input('Input numbers or Q for quit - ').split()
+
+        res = 0
+        for el in range(len(number)):
+            if number[el] == 'q' or number[el] == 'Q':
+                ex = True
+                break
+            else:
+                res = res + int(number[el])
+        sum_res = sum_res + res
+        print(f'Current sum is {sum_res}')
+    print(f'Your final sum is {sum_res}')
+
+
+my_sum()
