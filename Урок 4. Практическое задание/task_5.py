f39f1a3bf2ee7bbf63f2a5d909ddd70de2d6ba06
Anastacia <nastia.peresypkina2000@gmail.com>
2023-02-20 23:16:25 +0400

    Added correct tas5

--- Урок 4. Практическое задание/task_5.py
+++ Урок 4. Практическое задание/task_5.py
@@ -3,28 +3,12 @@
 с помощью модуля timeit, опишите результат
 """
 
+
 from timeit import timeit
 
-def my_func(x, y):
-    """
-    :param x: число, возводимое в степень
-    :param y: степень
-    :return: результат возведения x в y
-    """
-    result = x**y
-    return result
+print(timeit("my_func(4, -1)", setup="from task4 import my_func"))
 
+print(timeit("my_func2(4, -1)", setup="from task4 import my_func2"))
 
-print(timeit("my_func(4, -1)", globals=globals()))
-
-def my_func2(x, y):
-    while y < 0:
-        den = x*x
-        y += 1
-        res = 1/den
-    return res
-
-print(timeit("my_func2(4, -1)", globals=globals()))
-
-#Затраты времени на вычисление возведения числа в отрицательную степень меньше с использованием
-#цикла, нежели встроенной арифметической функции. Результаты вычислений так же разные для обеих функций
+# Затраты времени на вычисление возведения числа в отрицательную степень меньше с использованием
+# цикла, нежели встроенной арифметической функции. Результаты вычислений так же разные для обеих функций
