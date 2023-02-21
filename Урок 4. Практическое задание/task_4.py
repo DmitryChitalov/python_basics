cf16d63bb533d34d0431801b45905ff0e5e84baa
Alexander Avdeenko <dvaxela@yandex.ru>
2023-02-21 22:22:44 +0300

    Сделана задача 4

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -6,3 +6,10 @@
 Пример исходного списка: [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11].
 Результат: [23, 1, 3, 10, 4, 11]
 """
+
+
+def repeater(repeat):
+    return [i for i in repeat if repeat.count(i) == 1]
+
+
+print(repeater(input('Введите последовательность чисел: ').split()))
