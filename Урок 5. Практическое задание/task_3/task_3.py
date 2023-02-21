209f7cd3ddbf5f341da0c84850cbaf4d37a6e13d
@rash <111140352+IgoRash2201@users.noreply.github.com>
2023-02-21 19:08:00 +0300

    Update task_3.py

--- Урок 5. Практическое задание/task_3/task_3.py
+++ Урок 5. Практическое задание/task_3/task_3.py
@@ -8,3 +8,13 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
+
+rep_inp = {"One": "Один", "Two": "Два", "Three": "Три", "Four": "Четыре"}
+
+with open("less5_t3_numb.txt", encoding='utf-8') as f_obj:
+    for line in f_obj:
+        for key in rep_inp.keys():
+            line = line.replace(key, rep_inp[key])
+        print(line)
+        with open("less5_t3_rusn.txt", 'a', encoding='utf-8') as t_rus:
+            t_rus.write(f"\n {line}")
