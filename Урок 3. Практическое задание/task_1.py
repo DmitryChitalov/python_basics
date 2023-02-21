552222ad7545da802d44f9256fa0aa6819bdc69c
maxim.pavlov <1@1>
2023-02-21 17:35:43 +0300

    done

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -17,3 +17,22 @@ Process finished with exit code 0
 
 Process finished with exit code 0
 """
+#3.1.Реализовать функцию, принимающую два числа (позиционные аргументы)
+# и выполняющую их деление. Числа запрашивать у пользователя,
+# предусмотреть обработку ситуации деления на ноль.
+def div(*args):
+    try:
+        arg1 = int(input("Введите Делитель "))
+        arg2 = int(input("Введите Делитель "))
+        res = arg1 / arg2
+    except ValueError:
+        return 'Ошибка значения'
+    except ZeroDivisionError:
+        return "На ноль делить нельзя!"
+    if arg2 != 0:
+        return arg1 / arg2
+    else:
+        print("Значение не может быть равно нулю")
+    return res
+
+print(f'result  {div()}')
