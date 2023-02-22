405990fc881559c3e57f58d7c04c8e99abd9900a
Evgeniy Medvedev <e.medvedev@belvg.com>
2023-02-22 15:15:50 +0300

    task6

--- Урок 4. Практическое задание/task_6.py
+++ Урок 4. Практическое задание/task_6.py
@@ -12,3 +12,17 @@
 Во втором также необходимо предусмотреть условие,
 при котором повторение элементов списка будет прекращено.
 """
+from itertools import count, cycle
+
+for el in count(3):
+    if el > 10:
+        break
+    else:
+        print(el)
+
+с = 0
+for el in cycle("ABC"):
+    if с > 10:
+        break
+    print(el)
+    с += 1
