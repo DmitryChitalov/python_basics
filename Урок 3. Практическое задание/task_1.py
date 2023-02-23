9fbf8d1010d475e7c6d09fcc5146b82a1da013fa
Azim9292 <isaev-azim@mail.ru>
2023-02-23 19:02:06 +0600

    initial commit

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -17,3 +17,14 @@ Process finished with exit code 0
 
 Process finished with exit code 0
 """
+def division(first_obj, second_obj):
+    try:
+        return first_obj/second_obj
+    except ZeroDivisionError:
+        return "Вы что? Пытаетесь делить на 0!"
+try:
+    first_number = int(input("Ведите первое число: "))
+    second_number = int(input("Введите второе число: "))
+    print(division(first_number, second_number))
+except ValueError:
+    print("Можно вводить только числа")
--- Урок 3. Практическое задание/task_6.py
+++ Урок 3. Практическое задание/task_6.py
@@ -8,3 +8,16 @@
 Сделать вывод исходной строки, но каждое слово должно начинаться с заглавной буквы.
 Необходимо использовать написанную ранее функцию int_func().
 """
+#1
+def int_func(string):
+    return string.title()
+print(int_func("function"))
+
+#2
+def title_func(string):
+    list_title = []
+    lst = string.split()
+    for el in lst:
+        list_title.append(int_func(el))
+    print(*list_title)
+title_func("все слова с заглавной буквы")
