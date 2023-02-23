241bad0f3a194a8fd6aa8dfa56e528cfba85b90b
V1p3rL0rd <123589830+V1p3rL0rd@users.noreply.github.com>
2023-02-23 15:05:40 +0300

    Update task_6.py

--- Урок 4. Практическое задание/task_6.py
+++ Урок 4. Практическое задание/task_6.py
@@ -12,3 +12,13 @@
 Во втором также необходимо предусмотреть условие,
 при котором повторение элементов списка будет прекращено.
 """
+from itertools import count
+
+for el in count(int(input('Введите стартовое число '))):
+    print(el) # внимание - бесконечный цикл!
+
+from itertools import cycle
+
+my_list = [True, 'ABC', 123, None]
+for el in cycle(my_list):
+    print(el) # внимание - бесконечный цикл!
