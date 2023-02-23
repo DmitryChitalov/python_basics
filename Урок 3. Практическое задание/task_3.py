a2f63b91cc0a79cf5e4056e3aac533dd1d41271b
leshevi <79736739+leshevi@users.noreply.github.com>
2023-02-24 02:23:43 +0700

    Update task_3.py

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -5,3 +5,23 @@
 1) используя функцию sort()
 2) без функции sort()
 """
+def my_func_sort(a, b, c):
+    num_lst = [a, b, c]
+    num_lst.sort()
+    return f'{num_lst[1]+num_lst[2]}'
+
+
+def my_func_without_sort(a, b, c):
+    if a <= b and a <= c:
+        return f'{b+c}'
+    elif b <= a and b <= c:
+        return f'{a+c}'
+    return f'{a+b}'
+
+
+num1 = int(input('Введите целое число: '))
+num2 = int(input('Введите целое число: '))
+num3 = int(input('Введите целое число: '))
+
+print(my_func_sort(num1, num2, num3))
+print(my_func_without_sort(num1, num2, num3))
