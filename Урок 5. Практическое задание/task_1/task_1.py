3f44970aeef0369b6ced4cf6d666a0a224f5f242
Alexander Avdeenko <dvaxela@yandex.ru>
2023-02-24 22:59:00 +0300

    Создал ветку 5 урока

--- Урок 5. Практическое задание/task_1/task_1.py
+++ Урок 5. Практическое задание/task_1/task_1.py
@@ -3,3 +3,9 @@
 записать в него построчно данные, вводимые пользователем.
 Об окончании ввода данных свидетельствует пустая строка.
 """
+
+
+out_f = open("out_file.txt", "w")
+out_f.write("String string string")
+out_f.close()
+print(out_f)
