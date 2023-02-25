a9c05dc47566dd73b629ca3d1694627d54759cf8
Arthur Shchebetaw <archisoft6@gmail.com>
2023-02-25 18:55:42 +0400

    modified:   task_1.py 	modified:   task_2.py 	modified:   task_3.py 	modified:   task_4.py 	modified:   task_5.py 	modified:   task_6.py 	modified:   task_7.py

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -7,3 +7,19 @@
 
 Реализуйте вариант без и с генераторным выражением
 """
+
+# решение с генераторным выражением:
+my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+new_list = ([my_list[i]
+         for i in range(1, len(my_list)) if my_list[i] > my_list[i - 1]])
+print(new_list)
+
+# решение без генераторного выражения
+new_list = []
+n = 0
+for el in my_list:
+        if my_list[n] > my_list[n - 1] and (n - 1) != -1:
+                new_list.append(el)
+        n += 1
+
+print(new_list)
