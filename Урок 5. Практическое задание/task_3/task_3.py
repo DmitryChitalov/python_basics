18de970aca70928647ca36d7719555ad370f8570
V1p3rL0rd <123589830+V1p3rL0rd@users.noreply.github.com>
2023-02-26 10:49:05 +0300

    Update task_3.py

--- Урок 5. Практическое задание/task_3/task_3.py
+++ Урок 5. Практическое задание/task_3/task_3.py
@@ -8,3 +8,13 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
+with open('sal.txt', 'r') as my_file:
+    sal = []
+    poor = []
+    my_list = my_file.read().split('\n')
+    for i in my_list:
+        i = i.split()
+        if int(i[1]) < 20000:
+           poor.append(i[0])
+        sal.append(i[1])
+print(f'Оклад меньше 20.000 {poor}, средний оклад {sum(map(int, sal)) / len(sal)}')
