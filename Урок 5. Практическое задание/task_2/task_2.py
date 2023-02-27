ef04d29d1ed7eba21c1135a78782a12a428127a5
Yaroslav Chervochkov <borisborisbarboris@outlook.com>
2023-02-27 07:39:05 +0300

    Pyatoe001

--- Урок 5. Практическое задание/task_2/task_2.py
+++ Урок 5. Практическое задание/task_2/task_2.py
@@ -2,3 +2,11 @@
 2)	Создать текстовый файл (не программно), сохранить в нем несколько строк,
 выполнить подсчет количества строк, количества слов в каждой строке.
 """
+num_words = 0
+num_lines = 1
+with open("file.txt", 'r') as f:
+    for line in f:
+        num_lines += 1
+        num_words += len(line.split())
+print(f"Строк в файле: {num_lines}")
+print(f"Слов в файле: {num_words}")
