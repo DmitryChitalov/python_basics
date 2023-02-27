3fdb8f6d00f9295b0a5359315bb4c254cc7989ff
Kastilit <kostik.karikov@gmail.com>
2023-02-27 19:42:50 +0300

    lesson3

--- Урок 3. Кариков/task_1.py
+++ Урок 3. Кариков/task_1.py
@@ -17,3 +17,17 @@ Process finished with exit code 0
 
 Process finished with exit code 0
 """
+
+from typing import Union
+
+
+def division(a: int, b: int) -> Union[float, str]:
+    try:
+        return a / b
+    except ZeroDivisionError:
+        return 'На ноль делить нельзя!'
+
+
+first_num = int(input('Введите первое число: '))
+second_num = int(input('Введите второе число: '))
+print(division(first_num, second_num))
