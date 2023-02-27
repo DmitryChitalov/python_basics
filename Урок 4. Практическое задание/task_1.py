d806a8181bfcfa0bf6813841bf868c6aaa47e452
Public relations & Virtual technology <prvtdata@pm.me>
2023-02-27 13:16:05 +0300

    lesson_4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -6,3 +6,20 @@
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
 """
+
+import argparse
+
+def calculate_salary(hours_worked, hourly_rate, bonus):
+    """Рассчитываем заработную плату сотрудника на основе отработанных часов, почасовой ставки и бонуса."""
+    salary = (hours_worked * hourly_rate) + bonus
+    return salary
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Рассчитываем заработную плату сотрудника.')
+    parser.add_argument('hours_worked', type=float, help='Часы, отработанные сотрудником.')
+    parser.add_argument('hourly_rate', type=float, help='Почасовая ставка сотрудника.')
+    parser.add_argument('bonus', type=float, help='Сумма бонуса для сотрудника.')
+    args = parser.parse_args()
+
+    salary = calculate_salary(args.hours_worked, args.hourly_rate, args.bonus)
+    print(f"Заработная плата работника составляет{salary}.")
--- Урок 4. Практическое задание/task_7.py
+++ Урок 4. Практическое задание/task_7.py
@@ -10,3 +10,13 @@
 Подсказка: факториал числа n — произведение чисел от 1 до n.
 Например, факториал четырёх 4! = 1 * 2 * 3 * 4 = 24.
 """
+
+def fact(n):
+    factorial = 1
+    for i in range(1, n+1):
+        factorial *= i
+        yield factorial
+
+n = 5
+for f in fact(n):
+    print(f)
