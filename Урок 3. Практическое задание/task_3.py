a7c2b7df3128e5dccd0118eee5706d40da94fac9
IgorNak <immnakwork@gmail.com>
2023-02-27 15:42:28 +0300

    lesson_03

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -5,3 +5,19 @@
 1) используя функцию sort()
 2) без функции sort()
 """
+a = int(input('Введите число 1: '))
+b = int(input('Введите число 2: '))
+c = int(input('Введите число 3: '))
+
+def my_func_sort(a1, a2, a3):
+    summ = list([a1, a2, a3])
+    summ.sort(reverse=True)
+    return int(summ[0]) + int(summ[1])
+
+
+def my_func_wsort(a1, a2, a3):
+    summ = a1 + a2 + a3 - (min([a1, a2, a3]))
+    return summ
+
+print("С фнукцией sort: ",my_func_sort(a, b, c))
+print("Без функции sort: ",my_func_wsort(a, b, c))
