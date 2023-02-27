e18072bb34846527d9700e3fb6207fa177f55dfd
maxim.pavlov <1@1>
2023-02-27 17:43:26 +0300

    HW_5 done all

--- Урок 5. Практическое задание/task_3/task_3.py
+++ Урок 5. Практическое задание/task_3/task_3.py
@@ -8,3 +8,14 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
+dict = {'One' : 'Один', 'Two' : 'Два', 'Three' : 'Три', 'Four' : 'Четыре'}
+new_file = []
+with open("task-03-eng.txt", "r", encoding='utf-8') as f:
+    for i in f:
+        i = i.split(' ', 1)
+        new_file.append(dict[i[0]] + '  ' + i[1])
+with open('task-03-ru.txt', 'w+', encoding='utf-8') as f2:
+    f2.writelines(new_file)
+    f2.seek(0)
+    cont2 = f2.read()
+    print(cont2)
--- Урок 5. Практическое задание/task_7/task_7.py
+++ Урок 5. Практическое задание/task_7/task_7.py
@@ -9,3 +9,31 @@
 [{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit": 2000}]
 Подсказка: использовать менеджер контекста.
 """
+
+import json
+profit = {}
+pr = {}
+prof = 0
+prof_aver = 0
+i = 0
+with open('task-07.txt.', 'r', encoding='utf-8') as file:
+    for line in file:
+        name, form_ownership, earning, damage = line.split()
+        profit[name] = int(earning) - int(damage)
+        if profit.setdefault(name) >= 0:
+            prof = prof + profit.setdefault(name)
+            i += 1
+    if i != 0:
+        prof_aver = prof / i
+        print(f'Прибыль средняя - {prof_aver:.2f}')
+    else:
+        print(f'Компании работают в убыток')
+    pr = {'average profit': round(prof_aver)}
+    profit.update(pr)
+    print(f'Прибыль компаний - {profit}')
+
+with open('task-07.json', 'w') as write_js:
+    json.dump(profit, write_js)
+    js_str = json.dumps(profit)
+    print(f'Создан файл с расширением json с содержимым: \n'
+          f'{js_str}')
