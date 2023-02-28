3efe70cb31d46d6e4bcc5cc61b607e03809a3665
Valya921 <valya-komardina921@yandex.ru>
2023-02-28 07:55:10 +0300

    Урок 7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -28,3 +28,45 @@
 8 10 12
 14 16 18
 """
+
+
+from typing import List
+
+
+class Matrix:
+    def __init__(self, matrix_data: List[List]):
+        self.__matrix = matrix_data
+
+        m_rows = len(self.__matrix)
+        self.__matrix_size = frozenset([(m_rows, len(row)) for row in self.__matrix])
+
+        if len(self.__matrix_size) != 1:
+            raise ValueError("Invalid matrix size")
+
+    def __add__(self, other: "Matrix") -> "Matrix":
+        if not isinstance(other, Matrix):
+            raise TypeError(f"'{other.__class__.__name__}' "
+                            f"incompatible object type")
+        if self.__matrix_size != other.__matrix_size:
+            raise ValueError(f"Matrix sizes difference")
+
+        result = []
+
+        for item in zip(self.__matrix, other.__matrix):
+            result.append([sum([j, k]) for j, k in zip(*item)])
+
+        return Matrix(result)
+
+    def __str__(self) -> str:
+        return '\n'.join(['\t'.join(map(str, row)) for row in self.__matrix])
+
+
+if __name__ == '__main__':
+    matrix1 = Matrix([[1, 2], [3, 4]])
+    print(matrix1, '\n')
+
+    matrix2 = Matrix([[10, 20], [30, 40]])
+    print(matrix2, '\n')
+
+    print(matrix1 + matrix2)
+
--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -25,3 +25,42 @@
 
 Два класса: абстрактный и Clothes
 """
+
+from abc import ABC, abstractmethod
+
+
+class Clothes(ABC):
+
+    def __init__(self, param):
+        self.param = param
+
+    @property
+    def consumption(self):
+        return f'Сумма ткани равна: {self.param / 6.5 + 0.5 + 2 * self.param + 0.3 :.2f}'
+
+    @abstractmethod
+    def abstract(self):
+        return 'Smth vary abstract'
+
+
+class Coat(Clothes):
+    def consumption(self):
+        return f'Для пальто нужно: {self.param / 6.5 + 0.5 :.2f} ткани'
+
+    def abstract(self):
+        return 'Smth vary abstract second'
+
+
+class Costume(Clothes):
+    def consumption(self):
+        return f'Общий расход ткани: {2 * self.param + 0.3 :.2f} на костюм'
+
+    def abstract(self):
+        pass
+
+
+coat = Coat(400)
+costume = Costume(55)
+print(coat.consumption)
+print(costume.consumption())
+print(coat.abstract())
