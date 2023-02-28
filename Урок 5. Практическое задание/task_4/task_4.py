5abbb60967a782c67cb3831baefe77164f649e98
Yaroslav Chervochkov <borisborisbarboris@outlook.com>
2023-02-28 18:25:43 +0300

    Pyatoe002

--- Урок 5. Практическое задание/task_4/task_4.py
+++ Урок 5. Практическое задание/task_4/task_4.py
@@ -7,3 +7,15 @@
 Иванов 23543.12
 Петров 13749.32
 """
+f_obj = open("file.txt")
+# with open("file.txt") as f_obj:
+
+list_of_list = [line.split()[0] for line in f_obj if float(line.split()[1]) < 20000]
+print(list_of_list)
+
+f_obj.seek(0)
+
+# with open("file.txt") as f_obj:
+list_of_salary = [float(line.split()[1]) for line in f_obj]
+
+print("Средняя зарплата сотрудников: {:.2f}".format(sum(list_of_salary)/len(list_of_salary)))
