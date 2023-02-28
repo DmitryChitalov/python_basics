2f7fe220c8acedb8e8ea410303b43a9b67b5ff87
DvaXela <dvaxela@yandex.ru>
2023-02-28 13:12:33 +0300

    Сделано 4 задания

--- Урок 5. Практическое задание/task_2/task_2.py
+++ Урок 5. Практическое задание/task_2/task_2.py
@@ -12,3 +12,4 @@ for line in test_text:
     word_counter += word_count
     print(f'Слов в строке №{counter} - {word_count}.')
 print(f'Всего в тексте {counter} строк и {word_counter} слов.')
+test_text.close()
