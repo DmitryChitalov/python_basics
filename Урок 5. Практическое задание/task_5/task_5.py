2d57409b40fd27c2f26a506a89cc9879c0729023
Yaroslav Chervochkov <borisborisbarboris@outlook.com>
2023-03-01 07:29:29 +0300

    Pyatoe003

--- Урок 5. Практическое задание/task_5/task_5.py
+++ Урок 5. Практическое задание/task_5/task_5.py
@@ -2,3 +2,18 @@
 5)	Создать (программно) текстовый файл, записать в него программно набор чисел,
  разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 """
+import random
+
+afile = open("file.txt", "w")
+
+try:
+    for i in range(int(input('Сколько чисел записать в файл?: '))):
+        line = str(random.randint(1, 100)) + " "
+        afile.write(line)
+        print(line)
+except ValueError:
+    print("Это не число")
+
+print(f"Сумма чисел: {afile}")
+afile.close()
+
