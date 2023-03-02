a39e729b498e06b8c47297209ccbf54e47ddfcfd
DvaXela <dvaxela@yandex.ru>
2023-03-02 13:08:39 +0300

    Сделана задача 4

--- Урок 6. Практическое задание/task_4.py
+++ Урок 6. Практическое задание/task_4.py
@@ -20,6 +20,8 @@ speed, color, name, is_police (булево).
 Выполните доступ к атрибутам, выведите результат.
 Выполните вызов методов и также покажите результат.
 """
+
+
 class Car():
 
     def __init__(self, speed, color, name, is_police):
@@ -27,36 +29,76 @@ class Car():
         self.color = color
         self.name = name
         self.is_police = is_police
+        if is_police:
+            print(f'Создан полицейский автомобиль'
+                  f' {self.name}, цвет {self.color}')
+        else:
+            print(f'Создан гражданский автомобиль'
+                  f' {self.name}, цвет {self.color}')
 
-    def go():
-        pass
+    def go(self, speed):
+        return print(f'Машина {self.name} едет вперед со скоростью {self.speed}')
 
-    def stop():
-        pass
+    def stop(self):
+        self.speed = 0
+        return print(f'Машина {self.name} остановилась. Скорость {self.speed}')
 
-    def turn():
-        pass
+    def turn(self, dir):
+        return print(f'Машина повернула {dir}')
 
-    def show_speed():
-        pass
+    def show_speed(self):
+        return print(f'Машина едет со скоростью {self.speed}')
 
 
 class SportCar(Car):
-    pass
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
 
 
 class TownCar(Car):
-    pass
+    max_speed = 60
+
+    def show_speed(self):
+        if self.speed >= self.max_speed:
+            return print(f'Превышение скорости на '
+                         f'{self.speed - self.max_speed}')
+        else:
+            return print(f'Машина едет со скоростью {self.speed}')
 
 
 class WorkCar(Car):
-    pass
+    max_speed = 40
+
+    def show_speed(self):
+        if self.speed >= self.max_speed:
+            return print(f'Превышение скорости на '
+                         f'{self.speed - self.max_speed}')
+        else:
+            return print(f'Машина едет со скоростью {self.speed}')
 
 
 class PoliceCar(Car):
-    pass
+    def __init__(self, speed, color, name, is_police):
+        super().__init__(speed, color, name, is_police)
+
+
+def drive(car):
+    car.go(40)
+    car.show_speed()
+    car.turn('налево')
+    car.stop()
+    car.show_speed()
+    car.go(70)
+    car.turn('направо')
+    print('\n')
+
 
 sport_car = SportCar(180, 'Red', 'Viper', False)
 town_car = TownCar(80, 'Grey', 'Ford Focus', False)
 work_car = WorkCar(50, 'Green', 'Transporter', False)
 police_car = PoliceCar(130, 'White and Blue Lines', 'S300', True)
+
+drive(sport_car)
+drive(town_car)
+drive(work_car)
+drive(police_car)
