1601e60f20704b4c8c0e9745f0fc79e400d1ed29
Aleksey Baturin <125389764+ovoshhh@users.noreply.github.com>
2023-03-02 17:51:39 +0300

    исправлено

--- Урок 2. Практическое задание/task_3.py
+++ Урок 2. Практическое задание/task_3.py
@@ -13,8 +13,8 @@ seasons_list = ['winter', 'spring', 'summer', 'autumn']
 seasons_dict = {1 : 'winter', 2 : 'spring', 3 : 'summer', 4 : 'autumn'}
 month = int(input("Введите месяц по номеру "))
 if month ==1 or month == 12 or month == 2:
-        print(seasons_dict.get(1))
-        print(seasons_list[0])
+    print(seasons_dict.get(1))
+    print(seasons_list[0])
 elif month == 3 or month == 4 or month ==5:
     print(seasons_dict.get(2))
     print(seasons_list[1])
