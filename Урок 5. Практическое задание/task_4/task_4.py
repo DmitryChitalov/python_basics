4fbf960a30b8c26f3e026730f273bace248a5187
Azim9292 <isaev-azim@mail.ru>
2023-03-02 18:21:44 +0600

    initial commit

--- Урок 5. Практическое задание/task_4/task_4.py
+++ Урок 5. Практическое задание/task_4/task_4.py
@@ -7,3 +7,15 @@
 Иванов 23543.12
 Петров 13749.32
 """
+
+with open("file_1.txt", "r", encoding='utf-8') as file:
+    peoples = []
+    lines = file.readlines()
+    total = 0
+    for line in lines:
+        name, salary = line.split()
+        salary_num = float(salary)
+        total = total + salary_num
+        if salary_num < 20000:
+            peoples.append(name)
+print(f'Оклад меньше 20.000: {", ".join(peoples)}. Средний оклад: {round(total / len(lines))}')
