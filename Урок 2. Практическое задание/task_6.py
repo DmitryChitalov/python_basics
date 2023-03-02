48899def6732a9e611472b6b52ff80aa7545fb0e
Ivanov85 <ivanov9615871229@gmail.com>
2023-03-02 13:59:17 +0300

    lesson2.1

--- Урок 2. Практическое задание/task_6.py
+++ Урок 2. Практическое задание/task_6.py
@@ -25,3 +25,33 @@
 “ед”: [“шт.”]
 }
 """
+
+my_lst = []
+while True:
+    my_lst.append((input("Номер товара: "),
+                   {"Название": input("Название: "),
+                    "Цена": input("Цена: "),
+                    "Количество": input("Количество: "),
+                    "ед.": input("Единицы учёта: ")}))
+    q = input("Закончить ввод позиций? Да, Нет: ")
+    if q == "Да":
+        break
+
+my_lst = [(1, {"название": "компьютер", "цена": 20000, "количество": 5, "ед": "шт."}),
+          (2, {"название": "принтер", "цена": 6000, "количество": 2, "ед": "шт."}),
+          (3, {"название": "сканер", "цена": 2000, "количество": 7, "ед": "шт."})]
+
+names_lst = []
+prices_lst = []
+counts_lst = []
+units_lst = []
+res_dict = {}
+for i in range(len(my_lst)):
+    names_lst.append(my_lst[i] [1] ['название'])
+    prices_lst.append(my_lst[i] [1] ['цена'])
+    counts_lst.append(my_lst[i] [1] ['количество'])
+    units_lst.append(my_lst[i] [1] ['ед'])
+
+res_dict.update({'названия': names_lst, 'цены': prices_lst, 'количества': counts_lst, 'единицы': units_lst})
+print(res_dict)
+
