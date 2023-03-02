ed2b5357d849383926278afd39e452097817529e
aquamakc <aquamakc@yandex.ru>
2023-03-02 08:55:33 +0300

    Решение задач 5 урок

--- Урок 5. Практическое задание/task_1/task_1.py
+++ Урок 5. Практическое задание/task_1/task_1.py
@@ -3,3 +3,12 @@
 записать в него построчно данные, вводимые пользователем.
 Об окончании ввода данных свидетельствует пустая строка.
 """
+with open("m_file.txt", "w", encoding='utf-8') as f_obj:
+    lin_dan = []
+    while True:
+        str_line = input("Новые данные или ENTER: ")
+        if str_line != '':
+            lin_dan.append(str_line + "\n")
+        else:
+            break
+    f_obj.writelines(lin_dan)
