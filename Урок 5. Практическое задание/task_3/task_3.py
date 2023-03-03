5373571b9032a1b84e084ccd3b4a3ff6eb2a8f2c
IgorNak <immnakwork@gmail.com>
2023-03-03 15:22:58 +0300

    Lesson5

--- Урок 5. Практическое задание/task_3/task_3.py
+++ Урок 5. Практическое задание/task_3/task_3.py
@@ -8,3 +8,17 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
+
+num = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре'}
+f_n = []
+try:
+    with open('f.txt', 'r', encoding="utf-8") as file:
+        with open('nf.txt', 'w', encoding="utf-8") as nf:
+            lines = file.readlines()
+            for line in lines:
+                for k in num.keys():
+                    if k in line:
+                        f_n.append(line.replace(k, num[k]))
+            nf.writelines(f_n)
+except FileNotFoundError:
+    print('Файл не найден.')
--- Урок 5. Практическое задание/task_4/task_4.py
+++ Урок 5. Практическое задание/task_4/task_4.py
@@ -7,3 +7,14 @@
 Иванов 23543.12
 Петров 13749.32
 """
+with open("okl.txt", "r", encoding='utf-8') as okl:
+    sotr = []
+    lines = okl.readlines()
+    total = 0
+    for line in lines:
+        name, salary = line.split()
+        salary_num = float(salary)
+        total = total + salary_num
+        if salary_num < 20000:
+            sotr.append(name)
+print(f'Оклад меньше 20.000: {", ".join(sotr)}. Средний оклад: {round(total / len(lines))}')
