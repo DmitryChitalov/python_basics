b673a72e947a19890cd3f634102df09ffb48918d
@rash <111140352+IgoRash2201@users.noreply.github.com>
2023-03-03 18:44:26 +0300

    Update task_4_5_6.py

--- Урок 8. Практическое задание/task_4_5_6.py
+++ Урок 8. Практическое задание/task_4_5_6.py
@@ -19,3 +19,64 @@
 Подсказка: постарайтесь по возможности реализовать в проекте
 «Склад оргтехники» максимум возможностей, изученных на уроках по ООП.
 """
+
+class Warehouse:
+
+    def __init__(self, name, price, quantity, number_of_lists, *args):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.numb = number_of_lists
+        self.my_warehouse_full = []
+        self.my_warehouse = []
+        self.my_unit = {'Модель устройства': self.name, 'Цена за ед. товара': self.price, 'Количество': self.quantity}
+
+    def __str__(self):
+        return f'{self.name} цена {self.price} количество {self.quantity}'
+
+    def reception(self):
+        try:
+            unit = input(f'Ввод модели устройства: ')
+            price = int(input(f'Ввод цены за единицу в рублях: '))
+            qty = int(input(f'Ввод количества товара: '))
+            unique = {'Модель устройства': unit, 'Цена за ед.товара': price, 'Количество': qty}
+            self.my_unit.update(unique)
+            self.my_warehouse.append(self.my_unit)
+            print(f'Текущий список: \n {self.my_warehouse}')
+        except:
+            return f'Ошибка ввода данных'
+
+        print(f'Для выхода ввести n, для продолжения нажать Enter (ввод)')
+        q = input()
+        if q == 'n':
+            self.my_warehouse_full.append(self.my_warehouse)
+            print(f'Весь склад: \n {self.my_warehouse_full}')
+            return f'Выход '
+        else:
+            return Warehouse.reception(self)
+
+
+class Printer(Warehouse):
+    def to_print(self):
+        return f'напечатать стр.{self.numb} раз'
+
+
+class Scanner(Warehouse):
+    def to_scan(self):
+        return f'отсканировать лист {self.numb} раз'
+
+
+class Copier(Warehouse):
+    def to_copier(self):
+        return f'копировать лист {self.numb} раз'
+
+
+unit_1 = Printer('Samsung', 8500, 10, 30)
+unit_2 = Scanner('HP', 6550, 7, 25)
+unit_3 = Copier('Xerox', 4600, 3, 33)
+print(unit_1.reception())
+print(unit_2.reception())
+print(unit_3.reception())
+print(unit_1.to_print())
+print(unit_2.to_scan())
+print(unit_3.to_copier())
