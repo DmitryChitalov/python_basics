f4b31c9b995045261911c11c45477839445f2f2d
@rash <111140352+IgoRash2201@users.noreply.github.com>
2023-03-03 17:24:40 +0300

    Update task_3.py

--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -9,3 +9,22 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+
+class MyException(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+self_res = []
+while True:
+    try:
+        data = input("Ввеcти число или s для завершения: ")
+        if data == 's':
+            break
+        elif data.isdigit():
+            self_res.append(int(data))
+        else:
+            raise MyException("Ввели не число")
+    except MyException as er:
+        print(er.txt)
+
+print(self_res)
