61170d1554042f23d1c7e50841b544f23a3d82e6
@rash <111140352+IgoRash2201@users.noreply.github.com>
2023-03-03 19:15:36 +0300

    Update task_3.py

--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -10,21 +10,32 @@
 Класс-исключение должен контролировать типы данных элементов списка.
 """
 
+def is_integer(v):
+    try:
+        float(v)
+    except ValueError:
+        return False
+    else:
+        return float(v).is_integer()
+
+
 class MyException(Exception):
     def __init__(self, txt):
         self.txt = txt
 
-self_res = []
+
+int_lst = []
 while True:
     try:
-        data = input("Ввеcти число или s для завершения: ")
-        if data == 's':
-            break
-        elif data.isdigit():
-            self_res.append(int(data))
-        else:
-            raise MyException("Ввели не число")
-    except MyException as er:
-        print(er.txt)
+        inp_data = input("Ввести число для списка, для выхода - !: ")
 
-print(self_res)
+        if inp_data == "!":
+            break
+
+        if not is_integer(inp_data):
+            raise MyException(inp_data)
+        int_lst.append(int(inp_data))
+    except MyException as err:
+        print("Введено не число, повторить попытку")
+
+print(f"В результате: {int_lst}")
