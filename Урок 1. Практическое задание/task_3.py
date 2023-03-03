3e6c4f73a500576baf91efda1e0b6143d2ea4947
SeverTam <92327687+SeverTam@users.noreply.github.com>
2023-03-03 18:18:53 +0600

    Update task_3.py

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -1,10 +1,9 @@
-"""
-Задание 3.
+# собираем целое число из отдельных цифр
+n = int(input("Введите число N: "))
+nn = [n, n]
+nnn = [n, n, n]
 
-Узнайте у пользователя целое положительное число n.
-Найдите сумму чисел n + nn + nnn.
+number_from_nn = int(''.join((str(i) for i in nn)))
+number_from_nnn = int(''.join((str(i) for i in nnn)))
 
-Пример:
-Введите число n: 3
-n + nn + nnn = 369
-"""
+print(f'Сумма n + nn + nnn: {n + number_from_nn + number_from_nnn}')
