0f70ef504a50b995ca459e1f028345e3bcdb8c27
DvaXela <dvaxela@yandex.ru>
2023-03-04 12:58:21 +0300

    Решена первая задача

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -29,5 +29,26 @@
 14 16 18
 """
 
+
 class Matrix:
-    pass
+    def __init__(self, in_matrix):
+        self.in_matrix = in_matrix
+
+    def __str__(self):
+        return '\n'.join(' '.join([str(i) for i in el]) for el in self.in_matrix)
+
+    def __add__(self, other):
+        if len(self.in_matrix) != len(other.in_matrix):
+            return 'Сложение невозможно, матрицы не равны'
+        for el_1, el_2 in zip(self.in_matrix, other.in_matrix):
+            if len(el_1) != len(el_2):
+                return 'Сложение невозможно, матрицы не равны'
+        m3 = [[x + y for x, y in zip(el_1, el_2)] for el_1, el_2 in zip(self.in_matrix, other.in_matrix)]
+        return Matrix(m3)
+
+
+matrix_1 = Matrix([[1, 2, 3], [4, 5, 6]])
+matrix_2 = Matrix([[-1, -2, -3], [-4, -5, -6]])
+print(matrix_1, end='\n\n')
+print(matrix_2, end='\n\n')
+print(matrix_1 + matrix_2)
