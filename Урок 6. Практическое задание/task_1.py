495035668081429208c9d3c706326e8033446f68
ilumtrum <ilumtrum@gmail.com>
2023-03-05 09:25:09 +0500

    Practical work number 6 for the third lesson

--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -15,3 +15,28 @@
 
 Проверить работу примера, создав экземпляр и вызвав описанный метод.
 """
+
+from time import sleep
+from datetime import datetime as dt
+
+class TrafficLight:
+    _states = {'red': 7, 'yellow': 2, 'green': 9}
+    _color = ''
+
+    def running(self):
+        for color, sw_time in self._states.items():
+            self._color = color
+            start_state_time = dt.now()
+
+            print(f"TrafficLight switched to state '{self._color}' "
+                  f"on {sw_time} seconds")
+
+            sleep(sw_time)
+
+            print(f"TrafficLight leave state '{self._color}' after" 
+                  f"{(dt.now() - start_state_time).seconds} seconds")
+
+
+if __name__ == '__main__':
+    tl = TrafficLight()
+    tl.running()
--- Урок 6. Практическое задание/task_5.py
+++ Урок 6. Практическое задание/task_5.py
@@ -13,3 +13,44 @@
 Для каждого из классов метод должен выводить уникальное сообщение.
 Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
 """
+
+class Stationery:
+    title: str
+
+    def draw(self):
+        print("Запуск отрисовки.")
+
+
+class Pen(Stationery):
+    title = "ручка"
+
+    def draw(self):
+        print(f"{self.title} пишет")
+
+
+class Pencil(Stationery):
+    title = "карандаш"
+
+    def draw(self):
+        print(f"{self.title} чертит")
+
+
+class Handle(Stationery):
+    title = "маркер"
+
+    def draw(self):
+        print(f"{self.title} рисует")
+
+
+if __name__ == '__main__':
+    stationery = Stationery()
+    stationery.draw()
+
+    pen = Pen()
+    pen.draw()
+
+    pencil = Pencil()
+    pencil.draw()
+
+    handle = Handle()
+    handle.draw()
