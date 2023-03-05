a13eaeb31715e69db62b714334fad7f829835734
Александр <110670452+Alexander0204@users.noreply.github.com>
2023-03-05 11:06:51 +0400

    Add files via upload

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -4,14 +4,46 @@
 Реализовать базовый класс Worker (работник),
 в котором определить публичные атрибуты name, surname, position (должность),
 и защищенный атрибут income (доход). Последний атрибут должен ссылаться
-на словарь, содержащий элементы: оклад и премия, например, {"wage": wage, "bonus": bonus}.
+на словарь, содержащий элементы: оклад и премия, например,
+{"wage": wage, "bonus": bonus}.
 
-Создать класс Position (должность) на базе класса Worker. В классе Position реализовать публичные методы
-получения полного имени сотрудника (get_full_name) и дохода с учетом премии (get_total_income).
+Создать класс Position (должность) на базе класса Worker. В классе Position
+реализовать публичные методыполучения полного имени сотрудника (get_full_name)
+и дохода с учетом премии (get_total_income).
 
-Проверить работу примера на реальных данных (создать экземпляры класса Position, передать данные,
+Проверить работу примера на реальных данных
+(создать экземпляры класса Position, передать данные,
 проверить значения атрибутов, вызвать методы экземпляров).
 
-П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку __str__
-__str__(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
+П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку
+__str__
+__str__(self) - вызывается функциями str, print и format.
+Возвращает строковое представление объекта.
 """
+
+class Worker:
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+
+class Position(Worker):
+    def __init__(self, name, surname, position, wage, bonus):
+        super().__init__(name, surname, position, wage, bonus)
+
+    def get_full_name(self):
+        return f'{self.name} {self.surname}'
+
+    def get_total_income(self):
+        return self._income['wage'] + self._income['bonus']
+
+    def __str__(self):
+        return f'Сотрудник: {self.name} {self.surname}, ' \
+               f'доход: {self.get_total_income()} '
+
+
+people = Position('Иван', 'Филатов', 'Сварщик', 25000, 53000)
+print(people.get_full_name(), people.get_total_income())
+print(people)
