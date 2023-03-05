9f45935c6ee8eae1df6a03b4d83b65eb34246c7e
Александр <110670452+Alexander0204@users.noreply.github.com>
2023-03-05 11:09:51 +0400

    Add files via upload

--- Урок 6. Практическое задание/task_5.py
+++ Урок 6. Практическое задание/task_5.py
@@ -10,6 +10,39 @@
 
 В каждом из классов реализовать переопределение метода draw.
 
-Для каждого из классов метод должен выводить уникальное сообщение.
-Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
-"""
+Для каждого из классов метод должен выводить уникальное сообщение. Создать
+экземпляры классов и проверить, что выведет описанный метод для каждого
+экземпляра. """
+
+
+class Stationery:
+    def __init__(self, title):
+        self.title = title
+
+    def draw(self):
+        print(f"Запуск отрисовки {self.title}")
+
+
+class Pen(Stationery):
+    def draw(self):
+        print(f"Запуск отрисовки {self.title} ручкой ")
+
+
+class Pencil(Stationery):
+    def draw(self):
+        print(f"Запуск отрисовки {self.title} карандашем")
+
+
+class Handle(Stationery):
+    def draw(self):
+        print(f"Запуск отрисовки {self.title} маркером")
+
+
+s = Stationery('')
+s.draw()
+pen = Pen('шариковой')
+pen.draw()
+pencil = Pencil('черным')
+pencil.draw()
+handle = Handle('компьютерным')
+handle.draw()
