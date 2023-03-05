308661f0d1e4ab1725c66258c213ce4c1c4e10e1
Arthur Shchebetaw <archisoft6@gmail.com>
2023-03-05 23:05:11 +0400

    modified:   task_1.py 	modified:   task_2.py 	modified:   task_3.py 	modified:   task_4.py 	modified:   task_5.py

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -15,3 +15,46 @@
 П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку __str__
 __str__(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
 """
+
+class Worker:
+    name = ''
+    surname = ''
+    position = None
+    _income = {'wage': 0, 'bonus': 0}
+
+    def __init__(self, income):
+        self._income = income
+
+    def __str__(self):
+        total_income = 0
+        for key in self._income:
+            total_income += self._income.get(key)        
+        return f'Worker: {self.position} {self.name} {self.surname}.\n Income = {total_income}'
+        
+
+class Position(Worker):
+
+    def get_full_name(self):       
+        print(f'Worker full name: {self.name} {self.surname}')
+
+    def get_total_income(self):
+        total_income = 0
+        for key in self._income:
+            total_income += self._income.get(key)
+        print(f' Income = {total_income}')
+        return total_income
+    
+        
+income_dict = {'wage': 100000, 'bonus': 500}
+new_worker = Worker(income_dict)
+new_worker.position = 'manager'
+new_worker.name = 'Sasha'
+new_worker.surname = 'Reddy'
+print(new_worker)
+new_pos = Position(income_dict)
+new_pos.position = 'developer'
+new_pos.name = 'Ali'
+new_pos.surname = 'Kobano'
+print(new_pos)
+new_pos.get_full_name()
+new_pos.get_total_income()
--- Урок 6. Практическое задание/task_5.py
+++ Урок 6. Практическое задание/task_5.py
@@ -13,3 +13,35 @@
 Для каждого из классов метод должен выводить уникальное сообщение.
 Создать экземпляры классов и проверить, что выведет описанный метод для каждого экземпляра.
 """
+
+
+class Stationery:
+    title = 'called'
+
+    def draw(self):
+        print('Запуск отрисовки')
+
+class Pencil(Stationery):
+
+    def draw(self):
+        print(f'Сейчас будет рисовать {self.title}')
+
+class Pen(Stationery):
+
+    def draw(self):
+        print('Давай что-нибудь напишем!')
+
+class Handle(Stationery):
+
+    def draw(self):
+        print('Этим можно подчеркнуть.')
+
+my_pencil = Pencil()
+my_pencil.title = 'карандаш'
+my_pencil.draw()
+
+my_pen = Pen()
+my_pen.draw()
+
+my_handle = Handle()
+my_handle.draw()
