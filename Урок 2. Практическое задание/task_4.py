57e8d0411c6c903c3156f2997a32b9d971b6c410
Khodarin86 <124904757+Khodarin86@users.noreply.github.com>
2023-03-04 23:49:16 -0800

    Выполнел кроме 6 задания.

--- Урок 2. Практическое задание/task_4.py
+++ Урок 2. Практическое задание/task_4.py
@@ -14,3 +14,13 @@
 1. раз
 2. перерефриж
 """
+lst_4 = input("Введите слова через пробел: ").split()
+a = 1
+for elem in lst_4:
+    if len(elem) > 10:
+        print(f"{a}. {elem[:10]}")
+    else:
+        print(f"{a}. {elem}")
+        a += 1
+
+print()
