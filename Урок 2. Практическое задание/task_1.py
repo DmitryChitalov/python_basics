50cf550215f8c6cb65a4054614b31118c21d16a6
Tamira Khamnaeva <khamnaevatm@icloud.com>
2023-03-06 21:40:12 +0800

    Lesson 2.

--- Урок 2. Практическое задание/task_1.py
+++ Урок 2. Практическое задание/task_1.py
@@ -15,3 +15,7 @@
 <class 'bool'>
 <class 'NoneType'>
 """
+
+my_list = [5, "string", 0.15, True, None]
+for el in my_list:
+    print(type(el))
--- Урок 2. Практическое задание/task_2.py
+++ Урок 2. Практическое задание/task_2.py
@@ -11,3 +11,11 @@
 Введите целые числа через пробел: 1 2 3
 Результат: 2 1 3
 """
+my_list = input("Введите количество элементов списка через пробел: ").split(' ')
+i, j = 0, 1
+while len(my_list) > j:
+    my_list[i], my_list[j] = my_list[j], my_list[i]
+    i += 2
+    j += 2
+
+print(my_list)
