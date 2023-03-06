1110f5f848b89160135d6d7ce829abfa67737060
Aleksey Baturin <125389764+ovoshhh@users.noreply.github.com>
2023-03-06 18:09:35 +0300

    Lesson 4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -6,3 +6,17 @@
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
 """
+
+from sys import argv
+
+
+def get_arg(args, index):
+    try:
+        return int(args[index])
+    except:
+        return 0
+
+
+salary, time, bonus = get_arg(argv, 1), get_arg(argv, 2), get_arg(argv, 3)
+
+print(f'Salary = {time * salary + bonus}')
