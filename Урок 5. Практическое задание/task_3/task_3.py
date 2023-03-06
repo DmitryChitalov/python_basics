e510f4e999127d4a0f1a6295522f4ce054b5f008
Хайыржан Чуканов <k.kair@list.ru>
2023-03-06 17:05:51 +0600

    Все задания по уроку 5 выполнены!

--- Урок 5. Практическое задание/task_3/task_3.py
+++ Урок 5. Практическое задание/task_3/task_3.py
@@ -8,3 +8,20 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
+with open('numbers.txt', 'r', encoding='utf-8') as usr_file:
+    str_list = usr_file.read().split()
+
+rus_numb = ['Один ', 'Два ', 'Три ', 'Четыре ']
+
+del str_list[::3]
+
+n = 0
+for i in range(0, len(str_list) + 3, 3):
+    str_list.insert(i, rus_numb[n])
+    n += 1
+
+for i in range(3, len(str_list), 4):
+    str_list.insert(i, '\n')
+
+with open('rus_numbers.txt', 'w', encoding='utf-8') as out_file:
+    out_file.write(' '.join(str_list))
--- Урок 5. Практическое задание/task_7/task_7.py
+++ Урок 5. Практическое задание/task_7/task_7.py
@@ -9,3 +9,35 @@
 [{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit": 2000}]
 Подсказка: использовать менеджер контекста.
 """
+def rent_in_line(line):
+    for i in range(2, 3):
+        rent = int(line[i]) - int(line[i + 1])
+    return rent
+
+good_firms = []
+rent_list = []
+
+with open('companies.txt', 'r', encoding='utf-8') as file:
+    for line in file:
+        rent = (rent_in_line(line.split()))
+        if rent > 0:
+            good_firms.append(line.split())
+            rent_list.append(rent)
+
+total = 0
+for el in rent_list:
+    total += el
+av_rent = total / len(rent_list)
+profit_dict = {'average_profit': av_rent}
+
+firmas_list = []
+for el in good_firms:
+    firmas_list.append(el[0])
+
+profit_firms = dict(zip(firmas_list, rent_list))
+profit_list = [profit_firms, profit_dict]
+
+import json
+
+with open('profit_companines.json', 'w') as out_file:
+    json.dump(profit_list, out_file)
