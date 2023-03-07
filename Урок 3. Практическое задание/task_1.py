aa9215de0e808cbba2dcdd5266d69c963684000c
TheGreenMan86 <102001694+TheGreenMan86@users.noreply.github.com>
2023-03-07 16:56:32 +0300

    Update task_1.py

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -2,18 +2,28 @@
 1. Реализовать функцию, принимающую два числа (позиционные аргументы)
 и выполняющую их деление. Числа запрашивать у пользователя,
 предусмотреть обработку ситуации деления на ноль (try except).
-
 Пример:
 Введите первое число: 10
 Введите второе число: 0
 Вы что? Пытаетесь делить на 0!
-
 Process finished with exit code 0
-
 Пример:
 Введите первое число: 10
 Введите второе число: 10
 1.0
-
 Process finished with exit code 0
 """
+def func(*number):
+    try:
+        number1 = int(input("Введите первое число: "))
+        number2 = int(input("Введите второе число: "))
+        res = number1 / number2
+    except ZeroDivisionError:
+        return "Вы что? Пытаетесь делить на 0!"
+    return(res)
+
+    if number2 != 0:
+        return number1 / number2
+    else:
+        print("Вы что? Пытаетесь делить на 0!")
+print(f'result  {func()}')
