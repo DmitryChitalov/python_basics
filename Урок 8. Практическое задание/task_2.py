47ae679220188724188697cd4df9c66015a95d51
Evgeniy Medvedev <e.medvedev@belvg.com>
2023-03-07 13:17:18 +0300

    task2

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,18 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+
+
+class MyZeroDivisionError(ZeroDivisionError):
+    def __init__(self, txt):
+        self.txt = txt
+
+
+number = int(input("Введите число: "))
+divider = int(input("Введите делитель: "))
+try:
+    if divider == 0:
+        raise MyZeroDivisionError('На 0 делить нельзя!')
+    print(number/divider)
+except MyZeroDivisionError as err:
+    print(err)
