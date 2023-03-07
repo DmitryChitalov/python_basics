17df43e7930e484308e3181a231617a903f8b243
aquamakc <aquamakc@yandex.ru>
2023-03-07 21:16:58 +0300

    Решение задач 7 урок

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -28,3 +28,34 @@
 8 10 12
 14 16 18
 """
+
+
+class Matrix:
+    def __init__(self, *args):
+        self.n_list = list(args)
+
+    def __str__(self):
+        res = '\n'.join(map(str, self.n_list))
+        result = res.replace(',', '').replace('[', '').replace(']', '')
+        return result
+
+    def __add__(self, other):
+        n_sum = []
+        l_sum = []
+        for x in range(len(self.n_list)):
+            for y in range(len(self.n_list[x])):
+                l_sum.append(self.n_list[x][y] + other.n_list[x][y])
+            n_sum.append(l_sum)
+            l_sum = []
+        return Matrix('\n'.join(map(str, n_sum)))
+
+M_Obj_1 = Matrix([1, 2, 3], [4, 5, 6], [7, 8, 9])
+print(M_Obj_1)
+print()
+
+M_Obj_2 = Matrix([1, 2, 3], [4, 5, 6], [7, 8, 9])
+print(M_Obj_2)
+print()
+
+print(f'{M_Obj_1 + M_Obj_2}')
+print()
--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -25,3 +25,59 @@
 
 Два класса: абстрактный и Clothes
 """
+
+class Clothes:
+    def __init__(self, v, h):
+        self.v = v
+        self.h = h
+
+    @property
+    def calc_coat(self):
+        return self.v / 6.5 + 0.5
+
+    @property
+    def calc_costume(self):
+        return 2 * self.h + 0.3
+
+    @property
+    def calc_all(self):
+        return self.calc_coat + self.calc_costume
+
+f = Clothes(5, 1)
+print(f'Расход ткани на пальто = {f.calc_coat :.2f}')
+print(f'Расход ткани на костюм = {f.calc_costume :.2f}')
+print(f'Общий расход ткани = {f.calc_all :.2f}')
+
+
+# Два класса: абстрактный и Clothes
+from abc import ABC, abstractmethod
+
+class Clothe(ABC):
+
+    def __init__(self, param):
+        self.param = param
+
+
+    @property
+    def consumption(self):
+        pass
+
+
+class Coat(Clothe):
+    @property
+    def consumption(self):
+        return f'Расход ткани на пальто: {self.param / 6.5 + 0.5 :.2f} м ткани'
+
+
+
+class Costume(Clothe):
+    @property
+    def consumption(self):
+        return f'Расход ткани на костюм: {2 * self.param + 0.3 :.2f} м ткани'
+
+
+
+coat = Coat(5)
+costume = Costume(1)
+print(coat.consumption)
+print(costume.consumption)
