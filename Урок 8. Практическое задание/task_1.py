1be9014442c0f603a60c53ac2dc88377640f4024
Олег Тебряев <o.tebryaev@corp.mail.ru>
2023-03-08 20:10:57 +0300

    done

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,42 @@
 Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца
 и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
 """
+
+
+class Date:
+    day_month_year = None
+
+    @classmethod
+    def set_date(cls, date_string):
+        day, month, year = map(int, date_string.split('.'))
+        cls.day_month_year = (day, month, year)
+
+    @staticmethod
+    def is_valid(day, month, year):  # простая валидация значений
+        if not (1 <= month <= 12):
+            return False
+        if month in [4, 6, 9, 11] and day > 30:
+            return False
+        if month == 2:
+            if year % 4 == 0 and (year % 100 != 0 or year % 400 == 0):
+                if day > 29:
+                    return False
+            else:
+                if day > 28:
+                    return False
+        return True
+
+
+# > Проверить работу полученной структуры на реальных данных
+
+# Создание объекта класса "Date" и установка значения атрибута "day_month_year"
+Date.set_date('08.03.2023')
+
+# Вывод значения атрибута "day_month_year"
+print(Date.day_month_year)  # (8, 3, 2023)
+
+# Проверка валидности даты
+print(Date.is_valid(29, 2, 2020))  # True
+print(Date.is_valid(29, 2, 2021))  # False
+print(Date.is_valid(31, 4, 2022))  # False
+print(Date.is_valid(31, 5, 2023))  # True
--- Урок 8. Практическое задание/task_7.py
+++ Урок 8. Практическое задание/task_7.py
@@ -6,3 +6,27 @@
 создав экземпляры класса (комплексные числа) и выполнив сложение и умножение созданных экземпляров.
 Проверьте корректность полученного результата.
 """
+
+
+class ComplexNumber:
+    def __init__(self, real, imag=0):
+        self.real = real
+        self.imag = int(imag)
+
+    def __add__(self, other):
+        return ComplexNumber(self.real + other.real, self.imag + other.imag)
+
+    def __mul__(self, other):
+        return ComplexNumber(self.real * other.real - self.imag * other.imag,
+                             self.real * other.imag + self.imag * other.real)
+
+    def __str__(self):
+        return f"({self.real}+{self.imag}j)"
+
+
+# Пример использования и проверка результата
+c1 = ComplexNumber(1, 2)
+c2 = ComplexNumber(3, 4)
+
+print(c1 + c2, complex(1, 2) + complex(3, 4))  # (4+6j) (4+6j)
+print(c1 * c2, complex(1, 2) * complex(3, 4))  # (-5+10j) (-5+10j)
