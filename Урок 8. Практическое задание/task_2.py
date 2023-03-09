8c0140c7ad384f44d62112eee5cc8d10dc9b9f34
Azim9292 <isaev-azim@mail.ru>
2023-03-09 19:07:25 +0600

    initial commit

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,22 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+
+
+class DivisionByZeroError(Exception):
+    pass
+
+def divide(a, b):
+    if b == 0:
+        raise DivisionByZeroError("Division by zero is not allowed")
+    return a / b
+
+try:
+    a = float(input("Enter the dividend: "))
+    b = float(input("Enter the divisor: "))
+    result = divide(a, b)
+    print(f"{a} divided by {b} is {result}")
+except ValueError:
+    print("Invalid input")
+except DivisionByZeroError as e:
+    print(e)
--- Урок 8. Практическое задание/task_4_5_6.py
+++ Урок 8. Практическое задание/task_4_5_6.py
@@ -19,3 +19,72 @@
 Подсказка: постарайтесь по возможности реализовать в проекте
 «Склад оргтехники» максимум возможностей, изученных на уроках по ООП.
 """
+
+class OfficeEquipment:
+    def __init__(self, model, price, year):
+        self.model = model
+        self.price = price
+        self.year = year
+
+    def __str__(self):
+        return f"{self.__class__.__name__} {self.model}, price: {self.price}, year: {self.year}"
+
+class Printer(OfficeEquipment):
+    def __init__(self, model, price, year, max_print_speed):
+        super().__init__(model, price, year)
+        self.max_print_speed = max_print_speed
+
+    def __str__(self):
+        return super().__str__() + f", max print speed: {self.max_print_speed} pages per minute"
+
+class Scanner(OfficeEquipment):
+    def __init__(self, model, price, year, max_scan_resolution):
+        super().__init__(model, price, year)
+        self.max_scan_resolution = max_scan_resolution
+
+    def __str__(self):
+        return super().__str__() + f", max scan resolution: {self.max_scan_resolution} dpi"
+
+class Xerox(OfficeEquipment):
+    def __init__(self, model, price, year, max_copy_speed):
+        super().__init__(model, price, year)
+        self.max_copy_speed = max_copy_speed
+
+    def __str__(self):
+        return super().__str__() + f", max copy speed: {self.max_copy_speed} pages per minute"
+
+class Warehouse:
+    def __init__(self):
+        self.equipment = {}
+
+    def add_equipment(self, equipment, quantity):
+        if not isinstance(quantity, int) or quantity < 1:
+            raise ValueError("Invalid quantity")
+        if equipment not in self.equipment:
+            self.equipment[equipment] = quantity
+        else:
+            self.equipment[equipment] += quantity
+
+    def remove_equipment(self, equipment, quantity):
+        if not isinstance(quantity, int) or quantity < 1:
+            raise ValueError("Invalid quantity")
+        if equipment not in self.equipment or self.equipment[equipment] < quantity:
+            raise ValueError("Not enough equipment")
+        self.equipment[equipment] -= quantity
+
+    def move_equipment(self, equipment, quantity, department):
+        self.remove_equipment(equipment, quantity)
+        department.receive_equipment(equipment, quantity)
+
+class Department:
+    def __init__(self, name):
+        self.name = name
+        self.equipment = {}
+
+    def receive_equipment(self, equipment, quantity):
+        if not isinstance(quantity, int) or quantity < 1:
+            raise ValueError("Invalid quantity")
+        if equipment not in self.equipment:
+            self.equipment[equipment] = quantity
+        else:
+            self.equipment[equipment] += quantity
