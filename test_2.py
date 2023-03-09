ec12e5bd077ba507e535c0700ceca5c8c88862bb
Дмитрий Спицын <127041877+Nikopool@users.noreply.github.com>
2023-03-09 18:25:37 +0300

    Практическое задание по первому уроку

---
+++ test_2.py
@@ -0,0 +1,5 @@
+secs = int(input("Введите время в секундах: "))
+hours = secs / 3600
+minutes = secs / 60
+print(f"Время в формате ч:м:с - {secs / 3600:.2f} : {secs / 60:.2f} : {secs}")
+
---
+++ test_3.py
@@ -0,0 +1,3 @@
+n = input("Введите число n: ")
+res = int(n) + int(n * 2) + int(n * 3)
+print(f'{n} + {n * 2} + {n * 3} = {res}')
