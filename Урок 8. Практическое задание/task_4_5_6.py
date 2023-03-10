ede16af05f008b8bd4e1835785fdfd3310a5d329
DvaXela <dvaxela@yandex.ru>
2023-03-10 14:40:43 +0300

    90

--- Урок 8. Практическое задание/task_4_5_6.py
+++ Урок 8. Практическое задание/task_4_5_6.py
@@ -19,3 +19,66 @@
 Подсказка: постарайтесь по возможности реализовать в проекте
 «Склад оргтехники» максимум возможностей, изученных на уроках по ООП.
 """
+
+
+class OfficeEquipment:
+
+    def __init__(self, name, price, quantity, number_of_lists, *args):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.numb = number_of_lists
+        self.my_store_full = []
+        self.my_store = []
+        self.my_unit = {'Модель устройства': self.name, 'Цена за ед': self.price, 'Количество': self.quantity}
+
+    def __str__(self):
+        return f'{self.name} цена {self.price} количество {self.quantity}'
+
+    def reception(self):
+        try:
+            unit_name = input('Введите наименование ')
+            unit_ptice = int(input('Введите стоимость '))
+            unit_quantity = int(input('Введите количество '))
+            unique = {'Название': unit_name,
+                      'Цена': unit_ptice,
+                      'Количество': quantity}
+            self.my_unit.update(unique)
+            self.my_store.append(self.my_unit)
+            print(f'Текущий список -\n {self.my_store}')
+        except:
+            return 'Ошибка ввода данных'
+
+        print('Для выхода - exit, продолжение - Enter')
+        my_input = input()
+        if my_input == 'exit' or my_input == 'q':
+            self.my_store_full.append(self.my_store)
+            print(f'На складе - \n {self.my_store_full}')
+            return 'Выход'
+        else:
+            return OfficeEquipment.reception(self)
+
+
+class Printer(OfficeEquipment):
+    def to_print(self):
+        return f'to print smth {self.numb} times'
+
+
+class Scanner(OfficeEquipment):
+    def to_scan(self):
+        return f'to scan smth {self.numb} times'
+
+
+class Copier(OfficeEquipment):
+    def to_copier(self):
+        return f'to copier smth  {self.numb} times'
+
+
+unit_1 = Printer('hp', 2000, 5, 10)
+unit_2 = Scanner('Canon', 1200, 5, 10)
+unit_3 = Copier('Xerox', 1500, 1, 15)
+print(unit_1.reception())
+print(unit_2.reception())
+print(unit_3.reception())
+print(unit_1.to_print())
+print(unit_3.to_copier())
--- Урок 8. Практическое задание/task_7.py
+++ Урок 8. Практическое задание/task_7.py
@@ -6,3 +6,28 @@
 создав экземпляры класса (комплексные числа) и выполнив сложение и умножение созданных экземпляров.
 Проверьте корректность полученного результата.
 """
+
+
+class ComplexNumber:
+    def __init__(self, a, b, *args):
+        self.a = a
+        self.b = b
+        self.compl = 'a + b * i'
+
+    def __add__(self, other):
+        print('Сумма n и m равна')
+        return f'compl = {self.a + other.a} + {self.b + other.b} * i'
+
+    def __mul__(self, other):
+        print(f'Произведение n и m равно')
+        return f'compl = {self.a * other.a - (self.b * other.b)} + {self.b * other.a} * i'
+
+    def __str__(self):
+        return f'compl = {self.a} + {self.b} * i'
+
+
+n = ComplexNumber(14, -13)
+m = ComplexNumber(32, 41)
+print(n)
+print(n + m)
+print(n * m)
