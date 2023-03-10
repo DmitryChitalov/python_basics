c0dbdbf8ad84123e4ecc17dd77c1b63552b6c93f
NainaNigmedzyanova <127411544+NainaNigmedzyanova@users.noreply.github.com>
2023-03-10 11:28:20 +0300

    Update task_1.py

--- Урок 1. Практическое задание/task_1.py
+++ Урок 1. Практическое задание/task_1.py
@@ -11,3 +11,9 @@
 Введите ваш возраст: 45
 Ваши данные для входа в аккаунт: имя - Василий, пароль - vas, возраст - 45
 """
+
+first_name = input("Ваше имя: ")
+middle_name = input("Ваше отчество: ")
+last_name = input("Ваша фамилия: ")
+a = last_name + first_name + middle_name
+print (a)
