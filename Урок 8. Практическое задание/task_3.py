6d5fe59e9d1e3f2615470dd592f213d7e590b87f
aquamakc <aquamakc@yandex.ru>
2023-03-10 22:02:00 +0300

    Решение итоговых задач

--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -9,3 +9,28 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+class NotDigitException(Exception):
+    pass
+
+
+num_list = []
+is_cancel = False
+is_digit = True
+while is_cancel == False:
+    try:
+        ch = input('Введите число: ')
+        if (ch == ''):
+            is_cancel = True
+            continue
+        try:
+            digit = int(ch)
+        except ValueError:
+            is_digit = False
+        if is_digit == False:
+            raise NotDigitException(f'{ch} - не число')
+        num_list.append(int(ch))
+    except NotDigitException as ex:
+        print(ex)
+        is_digit = True
+
+print(num_list)
--- Урок 8. Практическое задание/task_4_5_6.py
+++ Урок 8. Практическое задание/task_4_5_6.py
@@ -19,3 +19,88 @@
 Подсказка: постарайтесь по возможности реализовать в проекте
 «Склад оргтехники» максимум возможностей, изученных на уроках по ООП.
 """
+
+class EquipmentNotFoundException(Exception):
+    pass
+
+
+class Store:
+    equipments = {}
+
+    @staticmethod
+    def incoming(new_equipments):
+        for eq in new_equipments:
+            if Store.equipments.keys().__contains__(type(eq)):
+                Store.equipments[type(eq)].append(eq)
+            else:
+                Store.equipments[type(eq)] = [eq]
+
+    @classmethod
+    def get_count(cls, eq_type):
+        try:
+            return len(cls.equipments[eq_type])
+        except KeyError:
+            return 0
+
+    @classmethod
+    def transfer(cls, eq, target):
+        if cls.equipments.keys().__contains__(type(eq)) == False or cls.equipments[type(eq)].__contains__(eq) == False:
+            raise EquipmentNotFoundException('Equipment is out from store')
+        cls.equipments[type(eq)].remove(eq)
+        print(f'Equipment {eq} transfer at {target}')
+
+
+class Equipment:
+    def __init__(self, model):
+        self.model = model
+
+    def __str__(self):
+        return self.model
+
+
+class Printer(Equipment):
+    def __init__(self, model, print_type):
+        super().__init__(model)
+        self.print_type = print_type
+
+
+class Scanner(Equipment):
+    def __init__(self, model, scan_size):
+        super().__init__(model)
+        self.scan_size = scan_size
+
+
+class Copier(Equipment):
+    def __init__(self, model, copy_type):
+        super().__init__(model)
+        self.copy_type = copy_type
+
+def scan_store():
+    print('Equipments in store:')
+    v = Store.equipments.values()
+    for types in v:
+        for eq in types:
+            print(eq)
+    print(f'Printers in store: {Store.get_count(Printer)}')
+    print(f'Scanners in store: {Store.get_count(Scanner)}')
+    print(f'Copiers in store: {Store.get_count(Copier)}')
+
+
+p1 = Printer("Printer 1", "Laser")
+s1 = Scanner("Scanner 1", "A3")
+c1 = Copier("Copier 1", "Manual")
+c2 = Copier("Copier 2", "Automatic")
+p2 = Printer("Printer 2", "Matrix")
+
+Store.incoming([p1, s1, c1])
+Store.incoming([p2])
+
+scan_store()
+
+try:
+    print('Transfer equipment')
+    Store.transfer(s1, "Бухгалтерия")
+except EquipmentNotFoundException as ex:
+    print(f'Equipment not transfer, because:\n{ex}')
+
+scan_store()
