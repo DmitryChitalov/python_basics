db968bf278686d41b54efc185b9d6335045918a4
DvaXela <dvaxela@yandex.ru>
2023-03-10 14:56:41 +0300

    100

--- Урок 8. Практическое задание/task_4_5_6.py
+++ Урок 8. Практическое задание/task_4_5_6.py
@@ -21,7 +21,7 @@
 """
 
 
-class OfficeEquipment:
+class StoreMashines:
 
     def __init__(self, name, price, quantity, number_of_lists, *args):
         self.name = name
@@ -35,41 +35,43 @@ class OfficeEquipment:
     def __str__(self):
         return f'{self.name} цена {self.price} количество {self.quantity}'
 
+    # @classmethod
+    # @staticmethod
     def reception(self):
+        # print(f'Для выхода - Q, продолжение - Enter')
+        # while True:
         try:
-            unit_name = input('Введите наименование ')
-            unit_ptice = int(input('Введите стоимость '))
-            unit_quantity = int(input('Введите количество '))
-            unique = {'Название': unit_name,
-                      'Цена': unit_ptice,
-                      'Количество': quantity}
+            unit = input(f'Введите наименование ')
+            unit_p = int(input(f'Введите цену за ед '))
+            unit_q = int(input(f'Введите количество '))
+            unique = {'Модель устройства': unit, 'Цена за ед': unit_p, 'Количество': unit_q}
             self.my_unit.update(unique)
             self.my_store.append(self.my_unit)
             print(f'Текущий список -\n {self.my_store}')
         except:
-            return 'Ошибка ввода данных'
+            return f'Ошибка ввода данных'
 
-        print('Для выхода - exit, продолжение - Enter')
-        my_input = input()
-        if my_input == 'exit' or my_input == 'q':
+        print(f'Для выхода - Q, продолжение - Enter')
+        q = input(f'---> ')
+        if q == 'Q' or q == 'q':
             self.my_store_full.append(self.my_store)
-            print(f'На складе - \n {self.my_store_full}')
-            return 'Выход'
+            print(f'Весь склад -\n {self.my_store_full}')
+            return f'Выход'
         else:
-            return OfficeEquipment.reception(self)
+            return StoreMashines.reception(self)
 
 
-class Printer(OfficeEquipment):
+class Printer(StoreMashines):
     def to_print(self):
         return f'to print smth {self.numb} times'
 
 
-class Scanner(OfficeEquipment):
+class Scanner(StoreMashines):
     def to_scan(self):
         return f'to scan smth {self.numb} times'
 
 
-class Copier(OfficeEquipment):
+class Copier(StoreMashines):
     def to_copier(self):
         return f'to copier smth  {self.numb} times'
 
