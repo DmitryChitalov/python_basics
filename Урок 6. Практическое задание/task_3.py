cf8ff14443cc92135cafc91be682527f27a79895
riddik1391 <97393134+riddik1391@users.noreply.github.com>
2023-03-10 09:56:37 +0300

    Update task_3.py

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -15,3 +15,24 @@
 П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку __str__
 __str__(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
 """
+class Worker:
+
+    def __init__(self, name='Viktor', surname='Mikhailov', position='developer', wage=100, bonus=20):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {'wage': wage, 'bonus': bonus}
+
+
+class Position(Worker):
+
+    def get_full_name(self):
+        return self.name + ' ' + self.surname
+
+    def get_full_salary(self):
+        return self._income['wage'] + self._income['bonus']
+
+
+alex = Position('Aleskey', 'Gromov', 'sysadmin', 110, 30)
+print(f'New attributes are: {alex.name}, {alex.surname}, {alex.position}, {alex._income}')
+print(f'Total salary of {alex.get_full_name()} is {alex.get_full_salary()}')
