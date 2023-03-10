106e1b119216a39cf0047b06074d8c6c65ec46ac
DvaXela <dvaxela@yandex.ru>
2023-03-10 14:17:59 +0300

    50%

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,41 @@
 Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца
 и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
 """
+
+
+class Data:
+    def __init__(self, day_month_year):
+        self.day_month_year = day_month_year
+
+    @classmethod
+    def extract(cls, day_month_year):
+        day_month_year = day_month_year.split('-')
+        
+        return (int(day_month_year[0]),
+                int(day_month_year[1]),
+                int(day_month_year[2]))
+    
+    def __str__(self):
+        return f'Текущая дата {self.day_month_year}'
+    
+    @staticmethod
+    def valid(day, month, year):
+        if 1 <= day <= 31:
+            if 1 <= month <= 12:
+                if 2023 >= year >= 0:
+                    return 'Дата указана корректно'
+                else:
+                    return 'Неправильный год'
+            else:
+                return 'Неправильный месяц'
+        return 'Неправильный день'
+
+
+now = Data('10-03-2023')
+print(now)
+print(Data.valid(10, 3, 2030))
+print(now.valid(10, 00, 2023))
+print(now.valid(00, 00, 2023))
+print(Data.extract('10-03-2023'))
+print(now.extract('10-03-2023'))
+print(Data.valid(10, 3, 2023))
--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -9,3 +9,30 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+
+class MyError(Exception):
+    
+    def __init__(self, data):
+        self.data = data
+
+    def __str__(self):
+        return self.data
+    
+
+if __name__ == '__main__':
+    my_list = []
+
+    while True:
+        my_input = input('Введите число и нажмите Enter, выход - exit: ')
+        if my_input == 'exit':
+            break
+
+        try: 
+            if not my_input.isdigit():
+                raise MyError('Неверный формат')
+            my_list.append(my_input)
+            print(f'Список выглядит так: {my_list}')
+        except MyError as e:
+            print(e)
+
+    print(my_list)
