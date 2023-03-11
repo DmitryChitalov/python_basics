0a423bea7f75f3afef5c564765d0488c80b8c600
Anastasiia <granast7@gmail.com>
2023-03-11 21:28:26 +0300

    дз к практическому заданию 4

--- Урок 4. Практическое задание/task_5.py
+++ Урок 4. Практическое задание/task_5.py
@@ -5,3 +5,7 @@
 Необходимо получить результат вычисления произведения всех элементов списка.
 Подсказка: использовать лямбда-функцию и функцию reduce().
 """
+from functools import reduce
+list_2 = [x for x in range(100,1001) if x % 2 == 0]
+p = reduce((lambda x, y: x * y), list_2)
+print(p)
