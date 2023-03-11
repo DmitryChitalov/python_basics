803683f5bfdf4847fc141e4ef7ab7c1461a28f4e
vhsdecay <vetvwhs@gmail.com>
2023-03-11 20:24:35 +0300

    Задача 5

--- Урок 1. Практическое задание/task_5.py
+++ Урок 1. Практическое задание/task_5.py
@@ -18,3 +18,18 @@
 Введите численность сотрудников фирмы: 10
 Прибыль фирмы в расчете на одного сотрудника = 50.0
 """
+
+revenue = int(input('Введите выручку фирмы: '))
+costs = int(input('Введите издержки фирмы: '))
+profit = revenue - costs
+
+if revenue > costs:
+    print(f'Прибыль составила - {profit}')
+    print(f'Рентабильность выручки - {profit / revenue}')
+
+else:
+    print(f'Убыток составил - {costs - revenue}')
+
+staff = int(input('Введите количество сотрудников: '))
+print(f'Прибыль на одного сотрудника составила - {profit / staff}')
+
