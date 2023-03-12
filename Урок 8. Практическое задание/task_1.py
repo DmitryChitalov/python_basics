43dca6e5fa52430df89e95a2a766256b836d2a66
Александр <110670452+Alexander0204@users.noreply.github.com>
2023-03-12 16:04:10 +0400

    Add files via upload

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -9,6 +9,44 @@
 Первый, с декоратором @classmethod, должен извлекать число, месяц,
 год, преобразовывать их тип к типу «Число» и делать атрибутами класса.
 
-Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца
-и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
-"""
+Второй, с декоратором @staticmethod, должен проводить валидацию числа,
+месяца и года (например, месяц — от 1 до 12). Проверить работу полученной
+структуры на реальных данных. """
+
+class OwnDate:
+    def __init__(self, day_month_year):
+        self.day_month_year = str(day_month_year)
+
+    @classmethod
+    def extract(cls, day_month_year):
+        my_date = []
+
+        for i in day_month_year.split():
+            if i != '-': my_date.append(i)
+
+        return int(my_date[0]), int(my_date[1]), int(my_date[2])
+
+    @staticmethod
+    def valid(day, month, year):
+
+        if 1 <= day <= 31:
+            if 1 <= month <= 12:
+                if 2199 >= year >= 0:
+                    return f'Данные введены верно'
+                else:
+                    return f'Неверный год'
+            else:
+                return f'Неверный месяц'
+        else:
+            return f'Неверный день'
+
+    def __str__(self):
+        return f'Текущая дата {OwnDate.extract(self.day_month_year)}'
+
+
+today = OwnDate('12 - 03 - 2023')
+print(today)
+print(OwnDate.valid(15, 17, 2022))
+print(today.valid(5, 1, 3023))
+print(OwnDate.extract('31 - 12 - 2049'))
+print(today.extract('12 - 03 - 2023'))
