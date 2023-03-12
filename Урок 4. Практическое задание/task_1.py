bd6bae26f525ee9a746e9cc3f6fd070bcfd95196
Inessa <inessagn@gmail.com>
2023-03-12 12:54:47 +0300

    task 4.1 completed

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -5,4 +5,15 @@
 
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
+python task_1.py "Федя" 16 1000 2000
 """
+
+import sys
+
+def calc_salary(work_hours, salary_per_hour, bonus):
+    return work_hours * salary_per_hour + bonus
+
+f_name, name_v, work_hours_v, salary_per_hour_v, bonus_v = sys.argv
+
+salary = calc_salary(int(work_hours_v), int(salary_per_hour_v), int(bonus_v))
+print(f'Сотрудник {name_v} заработал {salary} рублей')
