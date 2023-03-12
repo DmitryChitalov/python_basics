47a11e64e660b6bc87de36d3148fd46af64f7346
Inessa <inessagn@gmail.com>
2023-03-12 13:23:13 +0300

    task 4.4 completed

--- Урок 4. Практическое задание/task_4.py
+++ Урок 4. Практическое задание/task_4.py
@@ -6,3 +6,9 @@
 Пример исходного списка: [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11].
 Результат: [23, 1, 3, 10, 4, 11]
 """
+
+def get_unique(list):
+    return [item for item in list if list.count(item) == 1]
+
+source_list = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
+print("Результат:", get_unique(source_list))
