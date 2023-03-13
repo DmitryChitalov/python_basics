80f180d29e5f976ea086a7f23d9014e7e8d1716e
Arthur Shchebetaw <archisoft6@gmail.com>
2023-03-13 18:28:50 +0400

    modified:   task_1.py 	modified:   task_2.py 	modified:   task_3.py 	modified:   task_4_5_6.py 	modified:   task_7.py

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,24 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+
+class MyError(Exception):
+    def __init__(self, message):
+        self.message = message
+
+
+def usr_div():
+    try:
+        numb_1 = int(input('Какое число будем делить: '))
+        numb_2 = int(input('На что будем делить: '))
+        if numb_2 == 0:
+            raise MyError("Делить на ноль нельзя!")
+        else:
+            res = numb_1 / numb_2
+            return f'Результат деления: {res}'
+    except ValueError:
+        return "Мы работаем только с числами. Пожалуйста, проверьте ввод!"
+    except MyError as error:
+        return error
+
+print(usr_div())
--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -9,3 +9,39 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+
+class MyError(Exception):
+    def __init__(self):
+        pass
+
+
+class ListOfNumbs:
+    def __init__(self):
+        self.my_list = []
+
+    def input_values(self):
+        global user_val
+        while True:
+            try:
+                try:
+                    user_val = int(input('Введите числа по одному: '))
+                    nonstop = input(
+                        'Для прекращения ввода: "q" Для продолжения: "enter": ')
+                    self.my_list.append(user_val)
+                    if nonstop == 'q' or nonstop == 'Q':
+                        print(self.my_list)
+                        break
+                except ValueError:
+                    raise MyError
+            except MyError:
+                nonstop = input(
+                    f"Вы ввели не число! Повторить попытку? Y/n: ")
+                if nonstop == 'n' or nonstop == 'N':
+                    print(self.my_list)
+                    break
+                else:
+                    self.input_values()
+
+
+numb_list = ListOfNumbs()
+numb_list.input_values()
