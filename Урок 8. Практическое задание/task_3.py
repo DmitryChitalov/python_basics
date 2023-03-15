cd633838f4f0428c145c6035d030e823c5210740
Mikhail Nikulin <mikhail.nikulin@itl.team>
2023-03-15 18:26:34 +0300

    less8

--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -9,3 +9,22 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+
+class NumErr(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
+my_list = []
+while True:
+    try:
+        a = input("Введите значения: ")
+        if a == "p":
+            break
+        if a.isnumeric() == True:
+            my_list.append(a)
+        else:
+            raise NumErr("Введено не число")
+        print(my_list)
+    except NumErr as err:
+        print(err)
