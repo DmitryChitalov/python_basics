05ad2cd41e9f42e7b48066259f7a862283369ad0
Anastasiia <granast7@gmail.com>
2023-03-15 18:41:10 +0300

    дз к практическому заданию 5

--- Урок 5. Практическое задание/task_1/task_1.py
+++ Урок 5. Практическое задание/task_1/task_1.py
@@ -3,3 +3,12 @@
 записать в него построчно данные, вводимые пользователем.
 Об окончании ввода данных свидетельствует пустая строка.
 """
+line = input('Введите текст: ')
+
+with open('test.txt', 'w') as my_file:
+    print(line, file=my_file)
+
+my_f = open('test.txt', 'r', encoding='utf-8')
+content = my_f.read()
+print(content)
+my_f.close()
--- Урок 5. Практическое задание/task_6/task_6.py
+++ Урок 5. Практическое задание/task_6/task_6.py
@@ -5,3 +5,21 @@
                                         Физкультура:   —   30(пр)   —
 Пример словаря: {“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}
 """
+subj_list = {'Информатика': 170, 'Физика': 40, 'Физкультура': 30}
+try:
+    file_obj = open("task_5.txt", 'w')
+    for subj, hours in subj_list.items():
+        file_obj.write(subj + ': ' + str(hours) + "\n")
+except IOError:
+    print("Ошибка")
+finally:
+    file_obj.close()
+with open("task_6.txt", "r") as file_obj:
+    for line in file_obj:
+        print(line, end="")
+
+with open('task_6.txt', 'r') as file_obj:
+    for line in file_obj:
+        subject, lecture, practice, lab = line.split()
+        subj[subject] = int(lecture) + int(practice) + int(lab)
+    print(f'Общее количество часов по предмету - \n {subj}')
