f8cac0416d05ab0369755958575fc83e83e47286
DimShpInt <set_xz@mail.ru>
2023-03-15 21:44:35 +0300

    task_2 is done

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -9,3 +9,13 @@
 Введите время в секундах: 3600
 Время в формате ч:м:с - 1.0 : 60.0 : 3600
 """
+# время как в примере, но без остатка
+seconds = int(input("Введите время в секундах: "))
+hours = seconds // 3600
+minutes = seconds // 60
+print(f"Время в формате ч:м:с - {hours} : {minutes} : {seconds}")
+
+# время в нормализованной форме
+normal_min = minutes - 60 * hours
+normal_sec = seconds - 60 * minutes
+print(f"Время в формате ч:м:с - {hours} : {normal_min} : {normal_sec}")
