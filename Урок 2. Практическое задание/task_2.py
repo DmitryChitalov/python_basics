891f4b0c08459bff6f7389ff761d48525c3b9fdd
sp3kt0r <126805884+sp3kt0r@users.noreply.github.com>
2023-03-15 20:21:08 +0300

    Update task_2.py

--- Урок 2. Практическое задание/task_2.py
+++ Урок 2. Практическое задание/task_2.py
@@ -11,3 +11,16 @@
 Введите целые числа через пробел: 1 2 3
 Результат: 2 1 3
 """
+
+element_count = int(input("Введите количество элементов списка "))
+my_list = []
+i = 0
+element = 0
+while i < element_count:
+    my_list.append(input("Введите значение списка "))
+    i += 1
+
+for elem in range(int(len(my_list)/2)):
+        my_list[element], my_list[element + 1] = my_list [element + 1], my_list[element]
+        element += 2
+print(my_list)
