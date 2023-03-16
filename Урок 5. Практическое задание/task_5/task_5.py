df69fcb94a0259aa0725125f4a696c7fabd3d168
Tamira Khamnaeva <khamnaevatm@icloud.com>
2023-03-16 23:42:02 +0800

    Lesson 3.

--- Урок 5. Практическое задание/task_5/task_5.py
+++ Урок 5. Практическое задание/task_5/task_5.py
@@ -2,3 +2,24 @@
 5)	Создать (программно) текстовый файл, записать в него программно набор чисел,
  разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 """
+
+my_file = "text5.txt"
+digits_str = "21 11.2 35 20.3 5 0 11.5"
+if __name__ == "__main__":
+    summ = 0
+
+    try:
+        with open(my_file, 'w', encoding='utf-8') as fhs:
+            fhs.write(digits_str)
+
+        with open(my_file, encoding='utf-8') as fhd:
+            data = fhd.read()
+
+        for item in data.split():
+            summ += float(item)
+    except IOError as e:
+        print(e)
+    except ValueError:
+        print("Неправильно набран номер. Ошибка ввода-вывода")
+
+    print(summ)
