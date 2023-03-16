3e8c55cdc60ec6a7f1deb1b86c4bf4861177c79f
irgor-oskol <irgor.oskol@gmail.com>
2023-03-16 13:19:49 +0300

    Задания №3

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -17,3 +17,13 @@ Process finished with exit code 0
 
 Process finished with exit code 0
 """
+def div(*args):
+    try:
+        arg_1 = int(input("Введите первое число: "))
+        arg_2 = int(input("Введите второе число: "))
+        res = arg_1 / arg_2
+    except ZeroDivisionError:
+        return "Ошибка! На ноль делить нельзя."
+
+    return res
+print(f'Ответ:  {div()}')
--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -8,3 +8,11 @@
 Иван Иванов 1846 года рождения, проживает в городе Москва,
 email: jackie@gmail.com, телефон: 01005321456
 """
+
+
+def my_func(name, surname, year, city, email, telephone):
+    return ' '.join([name, surname, year, city, email, telephone])
+
+
+print(my_func(surname='Иванов', name='Иван', year='1846 г.р.', city='Москва', email='jackie@gmail.com',
+              telephone='01005321456'))
