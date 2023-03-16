9863e37ecb1d6194c99ac38fe3db7783b6672b1d
leshevi <79736739+leshevi@users.noreply.github.com>
2023-03-16 01:45:11 +0000

    lesson8

--- Урок 8. Практическое задание/task_7.py
+++ Урок 8. Практическое задание/task_7.py
@@ -6,3 +6,25 @@
 создав экземпляры класса (комплексные числа) и выполнив сложение и умножение созданных экземпляров.
 Проверьте корректность полученного результата.
 """
+class ComplexNumber:
+
+    def __init__(self, real, imaginary):
+        self.real = real
+        self.imaginary = imaginary
+
+    def __add__(self, other):
+        return ComplexNumber(self.real + other.real, self.imaginary + other.imaginary)
+
+    def __mul__(self, other):
+        return ComplexNumber((self.real * other.real - self.imaginary * other.imaginary),
+                             (self.real * other.imaginary + other.real * self.imaginary))
+
+    def __str__(self):
+        return f'{self.real}{"+" if self.imaginary > 0 else ""}{self.imaginary}i'
+
+
+cn_1 = ComplexNumber(1, -2)
+cn_2 = ComplexNumber(3, 4)
+
+print(cn_1 + cn_2)
+print(cn_1 * cn_2)
