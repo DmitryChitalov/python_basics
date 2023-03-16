e6e0765d972cc66a1d6819ad1dadf88a7167c807
Yaroslav Chervochkov <borisborisbarboris@outlook.com>
2023-03-16 07:38:42 +0300

    boombocks

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,48 @@
 Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца
 и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
 """
+
+import datetime
+
+
+class MyData:
+    day = None
+    month = None
+    year = None
+
+    day_month_year = '11.03.2023'
+
+    def __init__(self, day, month, year):
+        MyData.day_month_year = day + '.' + month + '.' + year
+
+    # def __str__(self):
+    #     return f"{day_month_year}"
+    @classmethod
+    def dispatch(cls, hash_):
+        cls.day = int(hash_[:2])
+        cls.month = int(hash_[2:4])
+        cls.year = int(hash_[4:])
+        return cls(hash_[:2], hash_[2:4], hash_[4:])
+
+    @staticmethod
+    def check_date(day, month, year):
+        isValidDate = True
+        try:
+            datetime.datetime(int(year), int(month), int(day))
+        except ValueError:
+            isValidDate = False
+
+        if isValidDate:
+            print("Дата верна ...")
+        else:
+            print("Дата не верна ...")
+
+    @classmethod
+    def get_data(cls):
+        return f"{cls.day_month_year}"
+
+
+print(MyData.dispatch("22111980").get_data())
+print(MyData.get_data())
+print(MyData.year)
+MyData.check_date(23, 99, 2012)
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,23 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+
+
+# define Python user-defined exceptions
+class DevideByZero(Exception):
+    "Raised when the input value is less than 18"
+    pass
+
+
+# you need to guess this number
+number = 18
+
+try:
+    input_num = int(input("Enter a number: "))
+    if input_num < number:
+        raise InvalidAgeException
+    else:
+        print("Eligible to Vote")
+
+except InvalidAgeException:
+    print("Exception occurred: Invalid Age")
