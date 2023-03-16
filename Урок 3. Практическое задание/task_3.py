71f18293b9b0586076adf18e6229ceeea6b8871d
MilaDurila <bonny8888@yandex.ru>
2023-03-17 00:09:18 +0300

    ДЗ-3

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -5,3 +5,10 @@
 1) используя функцию sort()
 2) без функции sort()
 """
+def sort(a, b, c):
+    return sum(sorted([a, b, c], reverse=True)[:2])
+def no_sort(a, b, c):
+    return max([a + b, a + c, b + c])
+x, y, z = 7, 8, 15
+print(f"используя sort(): {sort(x, y, z)}, "
+      f"без sort(): {no_sort(x, y, z)}")
