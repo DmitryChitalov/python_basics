459e75af83dddfd94e712bc56e3bab24993f51c8
BansheePrime <cmapuk-xpehob@yandex.ru>
2023-03-16 21:39:43 +0300

    Updating with 2, 3 and 4 solutions

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -9,3 +9,13 @@
 Введите время в секундах: 3600
 Время в формате ч:м:с - 1.0 : 60.0 : 3600
 """
+#!/usr/bin/env python3
+user_input = int(input("Пожалуйста, введите продолжительность в секундах: "))
+
+result_hours = user_input // 3600
+result_minutes = (user_input - result_hours * 3600) // 60
+result_seconds = user_input - result_hours * 3600 - result_minutes * 60
+
+print(f"Мы с Питоном насчитали {user_input // 3600:02d}:{(user_input - result_hours * 3600) // 60:02d}:{user_input - result_hours * 3600 - result_minutes * 60:02d} (чч:мм:сс)")
+print(f"Мы с Питоном насчитали {result_hours:02d}:{result_minutes:02d}:{result_seconds:02d} (чч:мм:сс)")
+print(f"Мы с Питоном посмотрели пример и пересчитали.  {result_hours:02d}:{result_minutes:02d}:{result_seconds:02d} (чч:мм:сс)")
