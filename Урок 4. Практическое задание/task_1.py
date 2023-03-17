f3a8b37678cdeb903991b09bd54485d8a3077e60
archisun1987 <archisun@yandex.ru>
2023-03-17 23:48:15 +0300

    lesson_4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -6,3 +6,15 @@
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
 """
+
+from sys import argv
+
+script, work_hours, pay_per_hour, bonus = argv
+
+
+def calc_salary(work_hours, pay_per_hour, bonus):
+    result = (int(work_hours) * int(pay_per_hour)) + int(bonus)
+    return result
+
+
+print(f"Результат: {calc_salary(work_hours, pay_per_hour, bonus)}")
