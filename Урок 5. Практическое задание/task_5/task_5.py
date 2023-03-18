0f26a6f01c87a3ab8bded2114a09be31e3955a3a
Дмитрий <Serafimdimaster@mail.ru>
2023-03-18 12:46:32 +0300

    lesson5

--- Урок 5. Практическое задание/task_5/task_5.py
+++ Урок 5. Практическое задание/task_5/task_5.py
@@ -1,3 +1,16 @@
+a = open("file+.txt", "w", encoding='utf-8')
+list1 = "1 2 3 4 5 6 7"
+a.writelines(list1)
+a.close()
+a = open("file+.txt", "r", encoding='utf-8')
+b = a.read()
+list1 = b.split()
+a.close()
+c = 0
+for el, el2 in enumerate(list1):
+    c += int(el2)
+print(c)
+
 """
 5)	Создать (программно) текстовый файл, записать в него программно набор чисел,
  разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
---
+++ Урок 5. Практическое задание/task_6/test.py
@@ -0,0 +1,19 @@
+for el, el2 in enumerate(list2):
+    if d == 0:
+        key1 = el2
+        d += 1
+        print(el2)
+    elif d == 1:
+        a = el2
+        d += 1
+        print(el2)
+    elif d == 2:
+        b = el2
+        d += 1
+        print(el2)
+    elif d == 3:
+        c = el2
+        d += 1
+        print(el2)
+    elif d == 4:
+        d = 0
