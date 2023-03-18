ce60d54cd8c3cab85a8bb5649b2bafe56b9e32fc
BansheePrime <cmapuk-xpehob@yandex.ru>
2023-03-18 11:42:26 +0300

    fixing solution for task 3

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -9,6 +9,6 @@
 n + nn + nnn = 369
 """
 #!/usr/bin/env python3
-user_input = int(input("Пожалуйста, введите число n: "))
-print(f"Нам с Питоном нужно подумать ... {user_input} + {str(user_input) * 2} + {str(user_input) * 3} ...")
-print(f"Мы с Питоном посчитали, что n + nn + nnn будет: {user_input + int(str(user_input) * 2) + int(str(user_input) * 3)}")
+user_input = input("Пожалуйста, введите число n: ")
+print(f"Нам с Питоном нужно подумать ... {user_input} + {user_input * 2} + {user_input * 3}")
+print(f"Мы с Питоном посчитали, что n + nn + nnn будет: {int(user_input) + int(user_input * 2) + int(user_input * 3)}")
