494f1f1b1d0e81a16aaca691bf9a351e8cd34ed2
MaryRemenyuk <91543154+MaryRemenyuk@users.noreply.github.com>
2023-03-19 22:17:19 +0300

    lesson_1 homework

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -10,3 +10,15 @@
 Ведите целое положительное число: 123456789
 Самая большая цифра в числе: 9
 """
+
+user_input = abs(int(input("Введите целое положительное число ")))
+max_from_input = user_input % 10
+while user_input >= 1:
+    user_input = user_input // 10
+    if user_input % 10 > max_from_input:
+        max_from_input = user_input % 10
+    if user_input > 9:
+        continue
+    else:
+        print("Максимальное цифра в числе ", max_from_input)
+        break
