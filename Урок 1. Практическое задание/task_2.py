1fa6c396e7f2ed2eec12ce48fa0f8fccd623179d
Baurzhan Sissenkul <bsissenkul@gmail.com>
2023-03-19 14:51:49 +0600

    Lesson 1 task 1 done

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -9,3 +9,12 @@
 Введите время в секундах: 3600
 Время в формате ч:м:с - 1.0 : 60.0 : 3600
 """
+time_sec = int(input("Please enter time in seconds: "))
+seconds = time_sec % 60
+# print("seconds: ", seconds)
+minutes1 = time_sec // 60
+minutes = minutes1 % 60
+# print("minutes: ", minutes)
+hours = time_sec // 3600
+# print("hours: ", hours)
+print("Your time is {:0>2d}:{:0>2d}:{:0>2d}".format(hours, minutes, seconds))
