4a936b05f3a94cd047d6e9c847c705637a5715bf
Basov-1974 <127841307+Basov-1974@users.noreply.github.com>
2023-03-19 11:57:17 +0300

    Update task_3.py

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,9 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+namber = int(input("ведите целое положительное число: "))
+namber1 = str(namber)
+a1 = namber1 + namber1
+a2 = namber1 + namber1 + namber1
+a3 = namber + int(a1) + int(a2)
+print("ответ:", a3)
