f10451cb7b18882b0c984e704214f5e7c5fa3cf8
Baurzhan Sissenkul <bsissenkul@gmail.com>
2023-03-19 15:32:31 +0600

    Lesson 1 task 4 done

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -10,3 +10,16 @@
 Ведите целое положительное число: 123456789
 Самая большая цифра в числе: 9
 """
+n = int(input("Please enter number (n): "))
+max_num = 0
+while n > 0:
+    print("Your number: ", n)
+    n1 = n % 10
+    #    print("первая цифра справа: ", n1)
+    if n1 == 9:
+        max_num = n1
+        break
+    elif n1 > max_num:
+        max_num = n1
+    n = n // 10
+print("maximim digit in your number is: ", max_num)
