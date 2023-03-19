a4034b7202bb4ec2f5d505d47c1bd15894d2db4f
Dmitry Simonov <simonov170685@yandex.ru>
2023-03-19 12:07:49 +0500

    Сделал первый урок

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,6 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+num_plus = input("введите целое положительное число ")
+sum = int(num_plus) + int(num_plus + num_plus) + int(num_plus + num_plus + num_plus)
+print(f"сумма равна {sum}")
