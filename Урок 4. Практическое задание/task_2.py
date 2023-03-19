2873d6b3ba2817ce259d6b88486aca1120a6fccf
IrinaKovaleva <ii.kovaleva@yandex.ru>
2023-03-19 22:41:24 +0300

    HomeWork, Lesson 4 (19/03/23)

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -7,3 +7,6 @@
 
 Реализуйте вариант без и с генераторным выражением
 """
+my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+more_then = [my_list[num] for num in range(1, len(my_list)) if my_list[num] > my_list[num-1]]
+print(more_then)
