03d1c8cba025c052e21cc1d8c2362e6ea4de4a2b
dezsploit <68642034+dezsploit@users.noreply.github.com>
2023-03-20 02:54:27 +0300

    Урок 4. Практическое задание 1

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -6,3 +6,13 @@
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
 """
+def plata():
+    try:
+        time = float(input('Выработка в часах: '))
+        salary = float(input('Ставка в час: '))
+        bonus = float(input('Премия: '))
+        result = time * salary + bonus
+        print(f'Итого  {result}')
+    except ValueError:
+        return print('Неверное значение')
+plata()
