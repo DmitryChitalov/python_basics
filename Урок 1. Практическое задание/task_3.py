614b4a91f2d632fa5562b0cfacf08c82717b9277
Baurzhan Sissenkul <bsissenkul@gmail.com>
2023-03-19 15:28:14 +0600

    Lesson 1 task 3 done

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,11 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+n = input("Please enter number (n): ")
+n1 = int(n)
+n2 = int(n + n)
+n3 = int(n + n + n)
+# print("You first number: ", n1)
+# print("You second number: ", n2)
+# print("You third number: ", n3)
+print("You result n+nn+nnn: ", n1 + n2 + n3)
