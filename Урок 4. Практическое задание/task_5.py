eea611e2c36ed17410ab8b4affe18f7605220014
irgor-oskol <irgor.oskol@gmail.com>
2023-03-20 19:50:11 +0300

    Задания №4

--- Урок 4. Практическое задание/task_5.py
+++ Урок 4. Практическое задание/task_5.py
@@ -5,3 +5,15 @@
 Необходимо получить результат вычисления произведения всех элементов списка.
 Подсказка: использовать лямбда-функцию и функцию reduce().
 """
+
+from functools import reduce
+
+def my_num(num_1, num_2):
+    return num_1 * num_2
+
+my_list = [num_2 for num_2 in range(100, 1001, 2)]
+otv_1 = reduce(my_num, my_list)
+
+print(f'Список из четных чисел: {my_list}')
+print(f'Ответ перемножения всех элементов списка: {otv_1}')
+
