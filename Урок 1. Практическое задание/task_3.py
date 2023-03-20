54d53923b44fccb0ae2709d48b957f909fd2d7a3
Nikita <Smerdix@mail.ru>
2023-03-20 08:27:53 +0400

    Решение через сцепление строк

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -10,4 +10,4 @@ n + nn + nnn = 369
 """
 
 number = int(input("Введите число n "))
-print(f"n + nn + nnn = {number}{number + number}{number + number + number}")
+print("n + nn + nnn = " + str(number) + str(number + number) + str(number + number + number))
