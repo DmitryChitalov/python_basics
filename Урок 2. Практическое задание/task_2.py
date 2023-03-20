9dc4a8c64ed52b8ff823b4e6fbdc5c1e4bea4e70
Kosechok <konslet@yandex.ru>
2023-03-20 12:35:17 +0300

    task 1 and task 2

--- Урок 2. Практическое задание/task_2.py
+++ Урок 2. Практическое задание/task_2.py
@@ -11,3 +11,19 @@
 Введите целые числа через пробел: 1 2 3
 Результат: 2 1 3
 """
+sting_for_list = input("Введите целые числа через пробел: ")
+my_list = sting_for_list.split()
+i = 0
+if len(my_list) % 2 == 0:
+    while i <= len(my_list):
+        temp_value = int(my_list[i])
+        my_list[i] = int(my_list[i+1])
+        my_list[i+1] = temp_value
+        i = i + 2
+else:
+    while i <= len(my_list) - 2:
+        temp_value = int(my_list[i])
+        my_list[i] = int(my_list[i+1])
+        my_list[i+1] = temp_value
+        i = i + 2
+print(my_list)
