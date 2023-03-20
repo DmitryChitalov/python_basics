fd990d2600aca1dfe8fdade994a5380dc03295a1
“Cosmatik” <Biznespro2021@yandex.ru>
2023-03-20 21:48:59 +0300

    Добавили решение к задаче 3

--- Урок 1. Практическое задание/task_3.py
+++ Урок 1. Практическое задание/task_3.py
@@ -8,3 +8,6 @@
 Введите число n: 3
 n + nn + nnn = 369
 """
+number = int(input("Введите первое целое число! "))
+sum: int = (number + int(str(number) + str(number)) + int(str(number) + str(number) + str(number)))
+print("Сумма чисел n + nn + nnn - %d" % sum)
