4c36c7aef92bf4d1fe6181cfd4eb76c76ce720b3
Lyudmila <127603205+bozhko-lyudmila@users.noreply.github.com>
2023-03-20 15:58:05 +0300

    Update task_1.py

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -6,3 +6,9 @@
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
 """
+from sys import argv
+
+file_name, worked_hour, rate, benefit = argv
+
+calculation = (int(worked_hour) * int(rate)) + int(benefit)
+print(f"Your pay is equal {calculation}")
