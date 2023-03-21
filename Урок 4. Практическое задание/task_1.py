ea072430ac1ef03a4431582f725dc9d138450242
Mary <marysok-16@mail.ru>
2023-03-21 16:27:24 +0600

    added task1

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -6,3 +6,18 @@
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
 """
+
+from sys import argv
+
+if len(argv) < 1:
+    time_work, rate, prize = argv
+    time_work = int(time_work)
+    rate = int(rate)
+    prize = int(prize)
+else:
+    time_work = int(input("Введите время работы: "))
+    rate = int(input("Введите ставку: "))
+    prize = int(input("Введите сумму бонусов: "))
+    salary = time_work * rate + prize
+    print(f'Зарплата сотрудника: {salary}')
+
