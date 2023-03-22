be699e8088ea05cb014bdfdf0215f160635fa5cc
Leoton98 <pashe11@mail.ru>
2023-03-22 23:41:21 +0300

    1

--- Урок 1. Практическое задание/task_1.py
+++ Урок 1. Практическое задание/task_1.py
@@ -11,3 +11,27 @@
 Введите ваш возраст: 45
 Ваши данные для входа в аккаунт: имя - Василий, пароль - vas, возраст - 45
 """
+
+name = 'Павел'
+surname = 'Леонтьев'
+age = 24
+
+print('Привет!', name, surname, age, 'лет')
+
+name = input("Введите Ваше имя: ")
+surname = input("Введите Вашу фамилию: ")
+age = int(input("Введите Ваш возраст: "))
+print('Привет!', name, surname, age, 'лет')
+
+
+number_1 = int(input("Введите любое число: "))
+number_2 = int(input("Введите другое любое число: "))
+number_3 = int(input("Введите еще одно другое любое число: "))
+sum = number_1 + number_2 + number_3
+print("Сумма чисел равна: ", sum)
+
+
+text = input("Введите текст")
+another_text = input("Введите другой текст")
+print(text)
+print(another_text)
