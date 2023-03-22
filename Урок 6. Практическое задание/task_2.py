3986e6987e06602eebba199cfb8a7eda4f2c6341
Дмитрий <Serafimdimaster@mail.ru>
2023-03-22 17:40:15 +0300

    lesson6

--- Урок 6. Практическое задание/task_2.py
+++ Урок 6. Практическое задание/task_2.py
@@ -1,3 +1,20 @@
+class Road:
+    weight = 25  # Масса
+    thickness = 0.05  # Толщина
+
+    def __init__(self, length, width):
+        self._length = length
+        self._width = width
+
+    def method(self):
+        b = self.weight * self.thickness * self._length * self._width
+        return b
+
+
+a = Road(20, 5000)
+c = a.method()
+print(f"Асфальта понадобится - {int(c)} кг или {int((c) / 1000)} тонн")
+
 """
 Задание 2.
 
