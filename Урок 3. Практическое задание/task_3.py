b31bb92b432b3d25bf8ade410f431b6ea8747193
gidrevichan <gidrevichan@mosmetod.ru>
2023-03-22 17:43:18 +0300

    adding solutions for tasks 1 and 2. Readying templates for tasks 3, 4, 5 and 6

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -5,3 +5,18 @@
 1) используя функцию sort()
 2) без функции sort()
 """
+#!/usr/bin/env python3
+
+def my_func1(arg_1, arg_2, arg_3):
+    sorted_args = [arg_1, arg_2, arg_3]
+    sorted_args.sort(reverse=True)
+    return print(f"Используя функцию sort(): {sorted_args[0] + sorted_args[1]}. Без функции sort(): "
+
+def my_func2(arg_1, arg_2, arg_3):
+    sorted_args = [arg_1, arg_2, arg_3]
+    max
+    remove
+    return print(f"Без функции sort(): ")
+
+my_func1(12, 16, 24)
+my_func2(12, 16, 24)
