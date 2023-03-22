192d9b64ebab96f93ab3dd6e28974c40054366eb
NASTY008 <127878876+NASTY008@users.noreply.github.com>
2023-03-22 15:38:41 +0300

    Update task_2.py

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -8,3 +8,21 @@
 Иван Иванов 1846 года рождения, проживает в городе Москва,
 email: jackie@gmail.com, телефон: 01005321456
 """
+var_1 = str(input("Имя: "))
+var_2 = str(input("Фамилия: "))
+var_3 = str(input("Год рождения: "))
+var_4 = str(input("Город: "))
+var_5 = str(input("email: "))
+var_6 = str(input("Телефон: "))
+
+def a(name,surname,year,city,email,phone):
+    return ' '.join([
+        " ", name,
+        " крвам", surname,
+        ", год рождения ", year,
+        ", проживает в городе ", city,
+        ", email: ", email,
+        ", phone: ", phone, "."
+        ])
+
+print(a(name=var_1, surname=var_2, year=var_3, city=var_4, email=var_5, phone=var_6))
