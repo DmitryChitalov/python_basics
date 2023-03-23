97e08db4c839fa2219c221cdd8b98d52cd25d9fc
dezsploit <68642034+dezsploit@users.noreply.github.com>
2023-03-23 17:38:48 +0300

    Урок 5. Практическое задание 2

--- Урок 5. Практическое задание/task_2/task_2.py
+++ Урок 5. Практическое задание/task_2/task_2.py
@@ -2,3 +2,12 @@
 2)	Создать текстовый файл (не программно), сохранить в нем несколько строк,
 выполнить подсчет количества строк, количества слов в каждой строке.
 """
+def count_info():
+    try:
+        with open('test.txt', 'r', encoding="utf-8") as file:
+            test = file.readlines()
+            for i in range(len(test)):
+                new_l = test[i].split()
+                print(f'Количество строк в файле {len(test)}. В {i + 1}-ой строке {len(test)} слов(а)')
+    except FileNotFoundError:
+        return 'Файл не найден...'
