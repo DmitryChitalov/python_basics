b3fcd3a9736c9ce9c41608317267ecf21483d080
vpakhalin <v.pakhalin@gmail.com>
2023-03-23 04:34:03 +0300

    ДЗ к 7 уроку

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -28,3 +28,30 @@
 8 10 12
 14 16 18
 """
+
+
+class Matrix:
+    def __init__(self, data):
+        self.data = data
+
+    def __str__(self):
+        mview = ''
+        for row in self.data:
+            for num in row:
+                mview += str(num) + ' '
+            mview += '\n'
+        return mview
+
+    def __add__(self, other):
+        result = [[0, 0, 0], [0, 0, 0], [0, 0, 0]]
+        for i in range(len(m1.data)):
+            for j in range(len(m1.data[0])):
+                result[i][j] = m1.data[i][j] + m2.data[i][j]
+        return Matrix(result)
+
+
+m1 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+m2 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(f'Матрица 1:\n{m1}')
+print(f'Матрица 2:\n{m1}')
+print(f'Сумма матриц:\n{m1+m2}')
