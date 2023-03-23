adfe6a80f8d9a758464c8a66e7ab69972f3124e9
dezsploit <68642034+dezsploit@users.noreply.github.com>
2023-03-23 18:09:40 +0300

    Урок 5. Практическое задание 4

--- Урок 5. Практическое задание/task_4/task_4.py
+++ Урок 5. Практическое задание/task_4/task_4.py
@@ -7,3 +7,13 @@
 Иванов 23543.12
 Петров 13749.32
 """
+with open('sal.txt', 'r') as my_file:
+    sal = []
+    poor = []
+    my_list = my_file.read().split('\n')
+    for i in my_list:
+        i = i.split()
+        if int(i[1]) < 20000:
+           poor.append(i[0])
+        sal.append(i[1])
+print(f'Оклад меньше 20.000 {poor}, средний оклад {sum(map(int, sal)) / len(sal)}')
