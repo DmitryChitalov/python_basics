b6a2afb2d8ec95c0c27ac8538fa66aaaa9ee0686
NikolasMalt <malcev_ns@live.ru>
2023-03-23 20:30:06 +0300

    Update task_1.py

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -17,3 +17,23 @@ Process finished with exit code 0
 
 Process finished with exit code 0
 """
+
+def division_numbers (arg_1,arg_2):
+    """
+    Функция деления
+    :param arg_1: Делимое
+    :param arg_2: Делитель
+    :return: Частное
+    """
+    try:
+        return arg_1 / arg_2
+    except ZeroDivisionError:
+        return 'Вы что? Пытаетесь делить на 0!'
+
+try:
+    arg_1 = float(input("Введите первое число: "))
+    arg_2 = float(input("Введите второе число: "))
+except ValueError:
+    print("Ошибочный ввод!")
+else:
+    print(division_numbers(arg_1,arg_2))
