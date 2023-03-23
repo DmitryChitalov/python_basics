10c41740820f74a499d91a70b3a9baa9341cfcea
NikolasMalt <malcev_ns@live.ru>
2023-03-23 20:47:39 +0300

    Update task_2.py

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -8,3 +8,8 @@
 Иван Иванов 1846 года рождения, проживает в городе Москва,
 email: jackie@gmail.com, телефон: 01005321456
 """
+
+def user_data (name_user, surname_user, year_of_birth_user, city_user, email_user, telephone_user):
+    print(f"{name_user} {surname_user} {year_of_birth_user} года рождения, проживает в городе {city_user},\nemail: {email_user}, телефон: {telephone_user}")
+
+user_data(name_user='Николай', surname_user='Мальцев', year_of_birth_user='1991', city_user='Нижний Новгород', email_user='malcev_ns@live.ru', telephone_user='+79876543210')
