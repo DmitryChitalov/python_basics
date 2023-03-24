70862127b8dcb140024cd4379d2974248e4952cf
Aleksey Baturin <125389764+ovoshhh@users.noreply.github.com>
2023-03-24 12:27:27 +0300

    Lesson8

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,19 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+
+
+class ZeroDivisionError(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+
+try:
+    dividend = int(input("Введите делимое: "))
+    divisor = int(input("Введите делитель: "))
+    if divisor == 0:
+        raise ZeroDivisionError("Делите на ноль нельзя!")
+except ZeroDivisionError as err:
+    print(err)
+else:
+    print(f"Все хорошо. Ваш результат: {round(dividend / divisor, 2)}")
