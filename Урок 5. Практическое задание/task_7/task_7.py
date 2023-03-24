a8d2354224661665a69e2e2d1e73ac811f356c45
Lyudmila <127603205+bozhko-lyudmila@users.noreply.github.com>
2023-03-24 21:24:35 +0300

    Update task_7.py

--- Урок 5. Практическое задание/task_7/task_7.py
+++ Урок 5. Практическое задание/task_7/task_7.py
@@ -9,3 +9,25 @@
 [{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit": 2000}]
 Подсказка: использовать менеджер контекста.
 """
+
+import json as js
+
+company_dict = dict()
+avearge_dict = dict()
+sum = 0
+result_list = []
+#Lines = {"firm_1   ООО   10000   5000"}
+with open('data.txt') as file1:
+    for line in Lines:
+        company_dict[line.split()[0]] = int(line.split()[2]) - \
+                                 int(line.split()[3])
+result_list.append(company_dict)
+for value in company_dict.values():
+    sum += value
+avearge_dict['average_profit'] = sum / len(company_dict)
+result_list.append(avearge_dict)
+
+print(result_list)
+
+with open('result.json', 'w') as file2:
+    js.dump(result_list, file2)
