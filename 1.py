cbe266d5c35c7ddab491849e7965677f154e6588
pavel <p.khlud@gmail.com>
2023-03-24 15:16:46 +0700

    Lesson 8

---
+++ 1.py
@@ -0,0 +1,56 @@
+"""
+Задание 1.
+
+Реализовать класс «Дата», на уровне класса определить атрибут day_month_year,
+присвоить ему значение
+
+В рамках класса реализовать два метода.
+
+Первый, с декоратором @classmethod, должен извлекать число, месяц,
+год, преобразовывать их тип к типу «Число» и делать атрибутами класса.
+
+Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца
+и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
+"""
+
+
+class Data:
+    def __init__(self, day_month_year):
+        self.day_month_year = str(day_month_year)
+
+    @classmethod
+    def extract(cls, day_month_year):
+        my_date = []
+
+        for i in day_month_year.split():
+            if i != '-':
+                my_date.append(i)
+
+        return int(my_date[0]), int(my_date[1]), int(my_date[2])
+
+    @staticmethod
+    def valid(day, month, year):
+
+        if 1 <= day <= 31:
+            if 1 <= month <= 12:
+                if 2023 >= year >= 0:
+                    return f'Все верно!'
+                else:
+                    return f'Неправильный год'
+            else:
+                return f'Неправильный месяц'
+        else:
+            return f'Неправильный день'
+
+    def __str__(self):
+        return f'Текущая дата {Data.extract(self.day_month_year)}'
+
+
+if __name__ == '__main__':
+    today = Data('11 - 1 - 2001')
+    print(today)
+    print(Data.valid(11, 11, 2025))
+    print(today.valid(11, 13, 2011))
+    print(Data.extract('11 - 11 - 2011'))
+    print(today.extract('11 - 11 - 2020'))
+    print(Data.valid(1, 11, 2000))
---
+++ 2.py
@@ -0,0 +1,27 @@
+"""
+Задание 2.
+
+Создайте собственный!!! класс-исключение, обрабатывающий ситуацию деления на нуль.
+Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
+в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
+"""
+
+
+class DivisionByNull:
+    def __init__(self, divider, denominator):
+        self.divider = divider
+        self.denominator = denominator
+
+    @staticmethod
+    def divide_by_null(divider, denominator):
+        try:
+            return divider / denominator
+        except ZeroDivisionError:
+            return f"Деление на ноль недопустимо"
+
+
+if __name__ == '__main__':
+    div = DivisionByNull(10, 100)
+    print(DivisionByNull.divide_by_null(10, 0))
+    print(DivisionByNull.divide_by_null(10, 0.1))
+    print(div.divide_by_null(100, 0))
