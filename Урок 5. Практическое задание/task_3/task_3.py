553c3073ccfaa4b2e8ef72dc1b21a65d40faa555
Lyudmila <127603205+bozhko-lyudmila@users.noreply.github.com>
2023-03-24 20:14:06 +0300

    Update task_3.py

--- Урок 5. Практическое задание/task_3/task_3.py
+++ Урок 5. Практическое задание/task_3/task_3.py
@@ -8,3 +8,20 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
+#Lines = ["One — 1\n","Two — 2\n","Three — 3\n","Four — 4\n"]
+ 
+file1 = open('english.txt', 'r')
+Lines = file1.readlines()
+ 
+dictionary = {"One": "Один", "Two": "Два", "Three": "Три", "Four": "Четыре"}
+count = 0
+for line in Lines:
+    count += 1
+    print("English Line{}: {}".format(count, line))
+    for key, value in dictionary.items():
+        line = line.replace(key, value)
+    print("Russian Line{}: {}".format(count, line))
+
+file1 = open('russian.txt', 'w')
+file1.writelines(Lines)
+file1.close()
