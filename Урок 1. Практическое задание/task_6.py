3eba0a7a390ab9e34a89b16b2a26a4c69d183e44
Roman Ilin <rom_zes@list.ru>
2023-03-26 01:26:38 +0300

    Урок 1 задание 1-5

--- Урок 1. Практическое задание/task_6.py
+++ Урок 1. Практическое задание/task_6.py
@@ -19,3 +19,19 @@
 6-й день: 3,22
 Ответ: на 6-й день спортсмен достиг результата — не менее 3 км.
 """
+revenue = int(input("Введите выручку фирмы "))
+costs = int(input("Введите издержки фирмы "))
+profit = revenue - costs
+if profit > 0:
+    print(f"Фирма отработала с прибылью: {profit}")
+    profit_b = profit / revenue
+    print("Рентабельность", profit_b)
+    manager = int(input(f"Укажите численность сотрудников:"))
+    profit_man = profit / manager
+    print(f"Прибыль на одного сотрудника составила: {profit_man}")
+elif profit < 0:
+    print("Фирма работает в убыток")
+else:
+    print("Фирма ничего не заработала ")
+
+
