ac4d6fd6b7852d3fc87c548aeeb0f874fb3e6628
BansheePrime <cmapuk-xpehob@yandex.ru>
2023-03-25 15:55:32 +0300

    adding solution for task 3

--- Урок 4. Практическое задание/task_3.py
+++ Урок 4. Практическое задание/task_3.py
@@ -6,7 +6,11 @@
 "генераторное выржаение" здесь List comprehension
 """
 #!/usr/bin/env python3
-step_input = int(input("Пожалуйста, выберите желаемую кратность (20 или 21): "))
-search_result = [i for i in range(step_input, 241, step_input)]
-print(search_result)
+magic_numbers = [20, 21]
+step_number = 0
 
+while step_number not in magic_numbers:
+    step_number = int(input("Пожалуйста, выберите желаемую кратность (20 ИЛИ 21): "))
+
+search_result = [i for i in range(step_number, 241, step_number)]
+print(search_result)
