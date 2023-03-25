afec2cbefd7fead3e5f90ed222b450a2a19fa855
BansheePrime <cmapuk-xpehob@yandex.ru>
2023-03-25 10:20:50 +0300

    adding solution for task 1. Templating and contemplating other tasks

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -6,3 +6,13 @@
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
 """
+#!/usr/bin/env python3
+from sys import argv
+
+working_hours = int(argv[1])
+hourly_wages = int(argv[2])
+
+def wages_calculation(working_hours, hourly_wages):
+    return working_hours * hourly_wages
+
+print(f"Заработная плата сотрудника:", wages_calculation(working_hours, hourly_wages))
--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -7,3 +7,14 @@
 
 Реализуйте вариант без и с генераторным выражением
 """
+#!/usr/bin/env python3
+initial_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+result_list = []
+
+for i in range(len(initial_list) - 1):
+    if initial_list[i] < initial_list[i + 1]:
+        result_list.append(initial_list[i + 1])
+
+print(result_list[1:])
+result_with_lc_list = [i for i in range(initial_list) if initial_list[i] < initial_list[i + 1]]
+print(result_with_lc_list)
