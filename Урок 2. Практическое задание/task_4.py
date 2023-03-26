9008c595944259320bd24fc5cff8577ee37cc115
mf009 <misha.frol@gmail.com>
2023-03-26 21:08:49 +0300

    Исправление 4 Урок_2

--- Урок 2. Практическое задание/task_4.py
+++ Урок 2. Практическое задание/task_4.py
@@ -16,8 +16,7 @@
 """
 string_1 = input("Введите текст:")
 list_1 = string_1.split()
-t = 1
 for i in list_1:
-    print(t, ' ' + i[:10])
-    t += 1
+    print(i[:10])
+
 #    print( f" {i} : {list_1[i]}")
