145ee2db3ab4cd62aa931a06739c7fc6fd586f12
NikolasMalt <malcev_ns@live.ru>
2023-03-26 10:34:40 +0300

    Update task_7.py

--- Урок 4. Практическое задание/task_7.py
+++ Урок 4. Практическое задание/task_7.py
@@ -10,3 +10,26 @@
 Подсказка: факториал числа n — произведение чисел от 1 до n.
 Например, факториал четырёх 4! = 1 * 2 * 3 * 4 = 24.
 """
+from math import factorial
+
+
+def my_func(num):
+    """
+    Функция отвечает за получение факториала числа
+    :param Число для вычисления
+    :return: Факториала числа
+    """
+    print(f"Факториал числа {num}! = ", end='')
+    for i in range(1, num + 1):
+        print(f"({factorial(i - 1)}*{i})=", end='')
+        yield factorial(i)
+    print(f"= {factorial(i)}")
+
+
+try:
+    my_number = int(input('Введите число: '))
+except ValueError:
+    print("Ошибочный ввод!")
+else:
+    for el in my_func(my_number):
+        print(el, end=' ')
