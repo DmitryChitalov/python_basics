c542888b3669dd8424cb92e2410e66c3b33845df
Inessa <inessagn@mail.ru>
2023-03-26 23:29:50 +0400

    task 8.2 completed

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,34 @@
 Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца
 и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
 """
+
+
+class Date:
+    day_month_year = ''
+    day = 0
+    month = 0
+    year = 0
+
+    @classmethod
+    def set_day_month_year(day_month_year):
+        Date.day_month_year = day_month_year
+        if len(Date.day_month_year) == 8:
+            Date.day = int(Date.day_month_year[0:1])
+            Date.month = int(Date.day_month_year[2:3])
+            Date.year = int(Date.day_month_year[4:7])
+
+    @staticmethod
+    def validate():
+        if (Date.year < 1900):
+            print('Год не может быть меньше 1900')
+        elif (Date.month < 1 or Date.month > 12):
+            print('Месяц должен быть в диапазоне от 1 до 12')
+        elif (Date.day < 1 or Date.day > 31):
+            print('День должен быть в диапазоне от 1 до 31')
+        else:
+            print(f'{Date.day_month_year} - корректна')
+
+Date.set_day_month_year('04052020')
+print(Date.day_month_year)
+print(Date.day, Date.month, Date.year)
+Date.validate()
