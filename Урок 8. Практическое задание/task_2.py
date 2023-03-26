07d9ad9e5843929e8b8f14e18ca6bb4bba064d2b
Inessa <inessagn@mail.ru>
2023-03-26 23:29:50 +0400

    task 8.2 completed

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,23 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+
+class ZeroDivException(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+def divide(numerator, denominator):
+    if (denominator == 0):
+        raise ZeroDivException("Деление на ноль")
+    
+    return numerator / denominator
+
+try:
+    numerator = float(input('Введите числитель: '))
+    denominator = float(input('Введите знаменатель: '))
+    print('Результат деления:', divide(numerator, denominator))
+
+except ZeroDivException as e:
+    print(e.txt)
+
+print('Программа завершилась успешно')
