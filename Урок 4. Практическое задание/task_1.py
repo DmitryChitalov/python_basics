05431c82e5b3896ab1d6fc93acde241fd2c02dfb
Sergey Zharkov <sergey.zharkov.90@mail.ru>
2023-03-27 00:45:35 +0600

    homework_04_done

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -6,3 +6,14 @@
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
 """
+from sys import argv
+
+name_employee, hours, cost_for_hour, bonus = argv
+
+
+def employee_salary(hours, cost_for_hour, bonus):
+    salary = float(hours) * float(cost_for_hour) + float(bonus)
+    return salary
+
+
+print(f"Зарплата сотрудника: {employee_salary(hours, cost_for_hour, bonus)}")
