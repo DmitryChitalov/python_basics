bdb30f2dafe86897168d2a8cab6cda0358ba6ce4
DimShpInt <set_xz@mail.ru>
2023-03-27 16:57:52 +0300

    lesson 4 done

--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -7,3 +7,27 @@
 
 Реализуйте вариант без и с генераторным выражением
 """
+
+num_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+
+# Вариант с циклом while
+res_list = []
+i = 1
+while i < len(num_list):
+    if num_list[i] > num_list[i - 1]:
+        res_list.append(num_list[i])
+    i += 1
+print(f"Новый список: {res_list}")
+
+# Вариант с циклом for
+res_list = []
+dif = num_list[0]
+for el in num_list:
+    if el > dif:
+        res_list.append(el)
+    dif = el
+print(f"Новый список: {res_list}")
+
+# Вариант с генератором списка (list comprehensions)
+res_list = [num_list[index] for index in range(1, len(num_list)) if num_list[index] > num_list[index - 1]]
+print(f"Новый список: {res_list}")
