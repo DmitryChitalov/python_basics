8ebb608bbd2c9ec6b7ac13064ecd0c8160df498a
dezsploit <68642034+dezsploit@users.noreply.github.com>
2023-03-27 18:50:47 +0300

    Урок 6. Практическое задание 2

--- Урок 6. Практическое задание/task_2.py
+++ Урок 6. Практическое задание/task_2.py
@@ -17,19 +17,17 @@ length (длина в метрах), width (ширина в метрах).
 Например: 20м*5000м*25кг*0.05м = 125000 кг = 125 т
 """
 class Road:
-    def xinit(self, xlength, xwidth):
-        self.xlength = xlength
-        self.xwidth = xwidth
+    def __init__(self, _length, _width):
+        self._length = _length
+        self._width = _width
 
     def mass(self):
-        return self.xlength * self.xwidth
-
+        return self._length * self._width
 
 class MassCount(Road):
-    def xinit(self, xlength, xwidth, volume):
-        super().xinit(xlength, xwidth)
+    def __init__(self, _length, _width, volume):
+        super().__init__(_length, _width)
         self.volume = volume
-
 
 r = MassCount(25, 10000, 125)
 print(r.mass())
