52b6396e7ba55b44c7f8396ca48b693017367458
Inessa <inessagn@mail.ru>
2023-03-27 23:56:13 +0400

    task 8.3 completed

--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -9,3 +9,24 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+
+class MyException(Exception):
+    pass
+
+array = []
+
+print('Для выхода введите stop')
+while True:
+    try:
+        number = input('введите число: ')
+        if number == 'stop':
+            break
+        
+        if not number.isdigit():
+            raise MyException(f'{number} не число, пропускаем')
+        array.append(int(number))
+
+    except MyException as ex:
+        print(ex)
+        
+print(array)
