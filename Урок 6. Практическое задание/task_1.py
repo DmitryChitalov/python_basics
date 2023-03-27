36621417f577437e8e7c4a426d485b0455886c98
dezsploit <68642034+dezsploit@users.noreply.github.com>
2023-03-27 18:37:22 +0300

    Урок 6. Практическое задание 1

--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -15,3 +15,23 @@
 
 Проверить работу примера, создав экземпляр и вызвав описанный метод.
 """
+from time import sleep
+
+class TrafficLight:
+    xcolor = ['Красный', 'Желтый', 'Зеленый']
+
+    def running(self):
+        i = 0
+        while i < 3:
+            print(f'Светофор переключается \n '
+                  f'{TrafficLight.xcolor[i]}')
+            if i == 0:
+                sleep(7)
+            elif i == 1:
+                sleep(5)
+            elif i == 2:
+                sleep(3)
+            i += 1
+
+TrafficLight = TrafficLight()
+TrafficLight.running()
