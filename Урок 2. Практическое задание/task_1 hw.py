8eb00f7205f7411fe8b35121423dac41d75c59e3
am <am@debian-BULLSEYE-live-builder-AMD64>
2023-03-28 01:18:42 +0300

    hw lesson 4 + hw lesson 2 task_1 updated

--- Урок 2. Практическое задание/task_1 hw.py
+++ Урок 2. Практическое задание/task_1 hw.py
@@ -1,15 +1,19 @@
 __author__ = 'AndreiM'
-__version__ = "1.0 24.03.2023"
+__version__ = "1.2 26.03.2023"
 print("\n task_1 \n -------- \n")
 
 my_list = [5, "String", 0.15, True, None]
-def my_type(el):
-    for i in range(len(my_list)):
-        print(type(my_list[i]))
-    return
-my_type(my_list)
+print(my_list[:])
+
+for i in range(len(my_list)):
+    print(type(my_list[i]))
 
 """"
+Задание 1. Создать список и заполнить его элементами различных типов данных.
+Реализовать проверку типа данных каждого элемента.
+Использовать функцию type() для проверки типа.
+Элементы списка можно не запрашивать у пользователя, а указать явно, в программе.
+
 Пример:
 для списка [5, "string", 0.15, True, None]
 результат
