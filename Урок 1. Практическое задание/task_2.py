00c507d56b56f27e9a441ef0ccf38c4b9e64896d
Aleksei Shevtsov <afk@ngs.ru>
2023-03-28 19:19:07 +0700

    1-е задание, тест коммита

--- Урок 1. Практическое задание/task_2.py
+++ Урок 1. Практическое задание/task_2.py
@@ -9,3 +9,7 @@
 Введите время в секундах: 3600
 Время в формате ч:м:с - 1.0 : 60.0 : 3600
 """
+sec_count = int(input("Введите время в секундах: "))
+hrs_count = sec_count//3600
+min_count = (sec_count - hrs_count * 3600) // 60
+print(f"{sec_count} секунд - это {hrs_count}ч:{min_count}м:{sec_count - hrs_count * 3600 - min_count * 60}с")
