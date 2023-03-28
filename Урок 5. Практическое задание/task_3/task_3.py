dd61de8e2af7e94280ef5628d355f9b3b3809bc2
Mary <marysok-16@mail.ru>
2023-03-28 18:53:27 +0600

    added task3 and files text3, text3_1

--- Урок 5. Практическое задание/task_3/task_3.py
+++ Урок 5. Практическое задание/task_3/task_3.py
@@ -8,3 +8,14 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
+
+dictionary = {'One': 'Один', 'Two': 'Два', 'Three': 'Три', 'Four': 'Четыре'}
+with open('text3.txt', 'r', encoding='utf-8') as i:
+    a = i.read()
+    b = a
+for el, el2 in dictionary.items():
+    b = b.replace(el, el2)
+
+with open('text3_1.txt', 'w', encoding='UTF-8') as new:
+    new.write(b)
+print('Готово!')
