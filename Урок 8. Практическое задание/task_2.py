1105e6cc6fc77452d8f248ea371fe6f748016aa8
Lyudmila <127603205+bozhko-lyudmila@users.noreply.github.com>
2023-03-28 09:25:56 +0300

    Update task_2.py

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,20 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+class MyErrDivision(Exception):
+    def __init__(self):
+        self.txt = 'My division by zero'
+
+
+def division():
+    try:
+        a = float(input('Dividend: '))
+        b = float(input('Divisor: '))
+        if b == 0:
+            raise MyErrDivision
+        print(f'a / b = {a / b: .4f}')
+    except MyErrDivision as err:
+        print(err.txt)
+
+
+division()
