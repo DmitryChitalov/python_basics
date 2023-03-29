8b214d72e34e93a314062e366dbeb78d7d14328b
firstkain <gleb.glushenko@inbox.ru>
2023-03-29 22:44:34 +0500

    homework less_5

--- Урок 5. Практическое задание/task_2/task_2.py
+++ Урок 5. Практическое задание/task_2/task_2.py
@@ -2,3 +2,10 @@
 2)	Создать текстовый файл (не программно), сохранить в нем несколько строк,
 выполнить подсчет количества строк, количества слов в каждой строке.
 """
+
+with open('new_2.txt', 'r', encoding='UTF-8') as file:
+    lines = file.readlines()
+    print(f"Всего строк: {len(lines)}")
+    for key, value in enumerate(lines):
+        words = value.split(' ')
+        print(f"Слов в строке {key + 1}: {len(words)}")
--- Урок 5. Практическое задание/task_6/task_6.py
+++ Урок 5. Практическое задание/task_6/task_6.py
@@ -5,3 +5,17 @@
                                         Физкультура:   —   30(пр)   —
 Пример словаря: {“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}
 """
+
+import re
+
+report = {}
+with open('new_6.txt', 'r', encoding='UTF-8') as file:
+    text = file.read()
+    file.seek(0)
+    for row in file:
+        row_items = row.split(': ')
+        hours = re.findall(r"\d+", row_items[1])
+        report.update({row_items[0]: sum([int(i) for i in hours])})
+
+print(f"Исходный файл:\n{text}\n")
+print(f"Словарь:\n{report}")
