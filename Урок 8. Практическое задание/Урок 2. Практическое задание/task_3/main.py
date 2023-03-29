7feb64472b6771c87c801ab443d8ce1d677cd923
Mary <marysok-16@mail.ru>
2023-03-29 22:10:44 +0600

    added task3

--- Урок 8. Практическое задание/Урок 2. Практическое задание/task_3/main.py
+++ Урок 8. Практическое задание/Урок 2. Практическое задание/task_3/main.py
@@ -18,3 +18,30 @@ ASCII(например, €);
 Реализовать считывание данных из созданного файла и проверить,
 совпадают ли они с исходными.
 """
+
+import yaml
+
+
+def yaml_new_data():
+    first = {
+        1: [1, 2, 3, 4],
+        2: 1254621,
+        3: {1: '\u2740', 2: '\u2730', 3: '\u03ab'}
+    }
+
+    with open('file.yaml', 'w', encoding='utf-8') as file1:
+        yaml.dump(first, file1, default_flow_style=False,
+                  allow_unicode=True)
+
+
+def read_file():
+    try:
+        with open('file.yaml', 'r', encoding='utf-8') as file2:
+            content = yaml.Loader(file2)
+            return content
+    except FileNotFoundError:
+        print('Файл не найден')
+
+
+yaml_new_data()
+print(read_file())
