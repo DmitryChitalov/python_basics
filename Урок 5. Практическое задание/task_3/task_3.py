0012ce7c4eb9760bc758e675c6bbd9057b2b479b
Sergey Zharkov <sergey.zharkov.90@mail.ru>
2023-03-30 00:44:27 +0600

    homework_05_done

--- Урок 5. Практическое задание/task_3/task_3.py
+++ Урок 5. Практическое задание/task_3/task_3.py
@@ -8,3 +8,11 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
+my_dict = {"One": "Один", "Two": "Два", "Three": "Три", "Four": "Четыре"}
+with open('one_two_tree_four.txt', 'r', encoding='UTF-8') as file:
+    text_2 = file.read()
+text_1 = text_2
+for key, value in my_dict.items():
+    text_1 = text_1.replace(key, value)
+with open('один_два_три_черыре.txt', 'w', encoding='UTF-8') as file2:
+    file2.write(text_1)
--- Урок 5. Практическое задание/task_5/task_5.py
+++ Урок 5. Практическое задание/task_5/task_5.py
@@ -2,3 +2,8 @@
 5)	Создать (программно) текстовый файл, записать в него программно набор чисел,
  разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 """
+with open('some_numbers.txt', 'w', encoding='UTF-8') as file:
+    numbers = input("Введите несколько чисел через пробел: ")
+    file.write(numbers)
+    final_sum = sum(int(value) for value in numbers.split())
+    print(f'Сумма введенных чисел: {final_sum}')
