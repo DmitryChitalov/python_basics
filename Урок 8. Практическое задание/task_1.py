210625f8261e7a6fd7a022cddfd85334ea90d7ec
irgor-oskol <irgor.oskol@gmail.com>
2023-03-30 19:32:15 +0300

    Задания №8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,33 @@
 Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца
 и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
 """
+class Data:
+    def __init__(self, day_month_year):
+        self.day_month_year = str(day_month_year)
+
+    # Извлечем число, месяц и год
+    @classmethod
+    def number(cls, day_month_year):
+        lst = list(day_month_year.split('.'))
+        Data.day = int(lst[0])
+        Data.month = int(lst[1])
+        Data.year = int(lst[2])
+
+    # Проведем валидацию числа, месяца и года согласно условия, в месяце 31 день, в году 12 мес
+    @staticmethod
+    def valid():
+        if Data.day < 0 or Data.day > 31:
+            return f'Такого дня не существует'
+
+        if Data.month < 0 or Data.month > 12:
+            return f'Такого месяца не существет'
+
+        if Data.year % 4 == 0:
+            return f'Год высокосный\nСегодня {Data.day}.{Data.month}.{Data.year}'
+
+        else:
+            return f'Текущая дата {Data.day}.{Data.month}.{Data.year}'
+
+
+Data.number("25.02.2022")
+print(Data.valid())
--- Урок 8. Практическое задание/task_7.py
+++ Урок 8. Практическое задание/task_7.py
@@ -6,3 +6,28 @@
 создав экземпляры класса (комплексные числа) и выполнив сложение и умножение созданных экземпляров.
 Проверьте корректность полученного результата.
 """
+class ComplexNumber:
+    # создание атрибутов x и y для класса ComplexNumber
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return ComplexNumber(x, y)
+
+    def __mul__(self, other):
+        x = (self.x * other.x) - (self.y * other.y)
+        y = (self.x * other.y) + (self.y * other.x)
+        return ComplexNumber(x, y)
+
+    def __str__(self):
+        return f'{self.x} + {self.y}i'
+
+n = ComplexNumber(5, 7)
+m = ComplexNumber(2, 9)
+print(f'Первое число равно: n = {n}')
+print(f'Второе число равно: m = {m}')
+print(f'Результат сложения чисел: n + m = {n + m}')
+print(f'Результат произведения чисел: n * m = {n * m}')
