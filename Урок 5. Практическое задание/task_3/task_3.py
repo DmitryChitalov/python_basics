f4426a2c3d39610d4b3c78805fa8af7cbbb84112
Kosechok <konslet@yandex.ru>
2023-03-30 14:30:12 +0300

    task 3

--- Урок 5. Практическое задание/task_3/task_3.py
+++ Урок 5. Практическое задание/task_3/task_3.py
@@ -8,3 +8,23 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
+try:
+    with open("test_file.txt") as f_obj:
+        dest_file = open("dest_file.txt", "w")
+        for line in f_obj:
+            data_from_line = line.split()
+            if data_from_line[0] == "One":
+                dest_file.write(line.replace("One","Один"))
+            elif data_from_line[0] == "Two":
+                dest_file.write(line.replace("Two", "Два"))
+            elif data_from_line[0] == "Three":
+                dest_file.write(line.replace("Three", "Три"))
+            elif data_from_line[0] == "Four":
+                dest_file.write(line.replace("Four", "Четыре"))
+            else:
+                print("Nothing to change")
+        #    dest_file.write(line)
+        dest_file.close()
+except IOError:
+    print("проблема с открытием файла. возможно его нет")
+print("done")
