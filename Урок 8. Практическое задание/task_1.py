eff7899c3b6f4b5f5548c53b39a8c80683f82f53
MilaDurila <bonny8888@yandex.ru>
2023-03-31 23:01:52 +0300

    ДЗ-8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,27 @@
 Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца
 и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
 """
+from datetime import date
+class Data:
+    def __init__(self, day_month_year):
+        self.day_month_year = str(day_month_year)
+    @classmethod
+    def number(cls, day_month_year):
+        lst = list(day_month_year.split('.'))
+        Data.day = int(lst[0])
+        Data.month = int(lst[1])
+        Data.year = int(lst[2])
+    @staticmethod
+    def valid():
+        if Data.day < 0 or Data.day >= 31:
+            return f'Нет такого дня'
+        if Data.month < 0 or Data.month >= 12:
+            return f'Нет такого месяца'
+        else:
+            return f'Текущая дата {Data.day}.{Data.month}.{Data.year}'
+
+Data.number("31.03.2023")
+print(Data.valid())
+
+
+
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,18 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+class WorkWithZero(Exception):
+    def __unit__(self, zero):
+        self.zero = zero
+number_one = int(input('Введите число: '))
+number_two = int(input('Ведите второе число: '))
+try:
+    if number_two == 0:
+        raise WorkWithZero()
+    else:
+        print((f"Результат: {number_one / number_two}"))
+except WorkWithZero:
+    print('Нельзя делить на ноль!')
+
+
+
