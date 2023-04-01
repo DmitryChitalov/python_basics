9f95b274bb3f19b772fa1d2e67fee97cf81da1a2
Anton Kosykh <anvkosykh@gmail.com>
2023-04-01 12:47:19 +0300

    исправление по Вашим комментириям

--- Урок 2. Практическое задание/task_1.py
+++ Урок 2. Практическое задание/task_1.py
@@ -17,8 +17,7 @@
 """
 
 my_list = [5, "string", 0.15, True, None]
-print(type(my_list[0]))
-print(type(my_list[1]))
-print(type(my_list[2]))
-print(type(my_list[3]))
-print(type(my_list[4]))
+for el in my_list:
+    print(type(el))
+
+
