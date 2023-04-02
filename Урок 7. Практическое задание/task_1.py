dd322fe79cab917c68951c922db9004bfdb4f941
IrinaKovaleva <ii.kovaleva@yandex.ru>
2023-04-02 23:15:20 +0300

    HomeWork, Lesson 7 (02/04/23)

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -28,3 +28,25 @@
 8 10 12
 14 16 18
 """
+
+class Matrix:
+    def __init__(self, my_list):
+        self.my_list = my_list
+
+    def __str__(self):
+        for row in self.my_list:
+            for i in row:
+                print(f"{i:4}", end="")
+            print()
+        return ''
+
+    def __add__(self, other):
+        for i in range(len(self.my_list)):
+            for i_2 in range(len(other.my_list[i])):
+                self.my_list[i][i_2] = self.my_list[i][i_2] + other.my_list[i][i_2]
+        return Matrix.__str__(self)
+
+
+m = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+new_m = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+print(m.__add__(new_m))
