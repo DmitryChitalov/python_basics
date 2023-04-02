e3c6ecdab9b3cda598868669182acfbc51a145da
BansheePrime <cmapuk-xpehob@yandex.ru>
2023-04-02 13:31:54 +0300

    WIP solution for task 5

--- Урок 5. Практическое задание/task_5/task_5.py
+++ Урок 5. Практическое задание/task_5/task_5.py
@@ -4,5 +4,15 @@
 """
 #!/usr/bin/env python3
 import os
+import random
+
 print("Внимание, текущая папка:", os.getcwd())
 
+output_file = open("./output_file5.txt", "w")
+random_numbers = random.sample(range(100, 1000), 10)
+print(*random_numbers, sep=" ")
+line_to_write = " ".join(str(random_numbers))
+print(type(line_to_write), line_to_write)
+output_file.write(line_to_write)
+output_file.close()
+print(f"Сумма чисел в файле: {sum(random_numbers)}")
