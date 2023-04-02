320d4f56003670cac925d768fda1336db3364107
BansheePrime <cmapuk-xpehob@yandex.ru>
2023-04-02 13:09:01 +0300

    adding solution for task 4

--- Урок 5. Практическое задание/task_4/task_4.py
+++ Урок 5. Практическое задание/task_4/task_4.py
@@ -9,5 +9,21 @@
 """
 #!/usr/bin/env python3
 import os
+
 print("Внимание, текущая папка:", os.getcwd())
 
+data_store = {}
+salary_cap = 20000
+
+with open("input_file4.txt", 'r') as source_file:
+    source_data = source_file.readlines()
+
+    for i in source_data:
+        splitting_lines = i.split()
+        data_dict_key = splitting_lines[0].split()
+        data_dict_value = list(map(float, splitting_lines[1].split()))
+        data_store.update({data_dict_key[0]: data_dict_value[0]})
+
+below_the_line = [key for key, value in data_store.items() if value < salary_cap]
+print(f"Список сотрудников с окладом менее {salary_cap:.2f}: ", ", ".join(below_the_line))
+print(f"Средняя величина дохода сотрудников: {sum(data_store.values()) / len(data_store):.2f}")
