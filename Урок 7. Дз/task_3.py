5bf4f108d741aec4efcc9157921377a4faa6a2a0
Andrew <andrewdya@gmail.com>
2023-04-03 12:27:28 +0600

    Homework7

---
+++ Урок 7. Дз/task_3.py
@@ -0,0 +1,47 @@
+
+"""
+Задание 3.
+
+Реализовать базовый класс Worker (работник),
+в котором определить публичные атрибуты name, surname, position (должность),
+и защищенный атрибут income (доход). Последний атрибут должен ссылаться
+на словарь, содержащий элементы: оклад и премия, например, {"wage": wage, "bonus": bonus}.
+
+Создать класс Position (должность) на базе класса Worker. В классе Position реализовать публичные методы
+получения полного имени сотрудника (get_full_name) и дохода с учетом премии (get_total_income).
+
+Проверить работу примера на реальных данных (создать экземпляры класса Position, передать данные,
+проверить значения атрибутов, вызвать методы экземпляров).
+
+П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку str
+str(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
+"""
+
+
+class Worker:
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+
+class Position(Worker):
+    def get_full_name(self):
+        return f"{self.name} {self.surname}"
+
+    def get_total_income(self):
+        return sum(self._income.values())
+
+    def __str__(self):
+        return f"{self.get_full_name()}, должность: {self.position}, доход: {self.get_total_income()}"
+
+
+john_cena = Position("John", "Cena", "Manager", 50000, 10000)
+
+print(john_cena.name)
+print(john_cena.surname)
+print(john_cena.position)
+print(john_cena._income, '\n')
+
+print(john_cena)
