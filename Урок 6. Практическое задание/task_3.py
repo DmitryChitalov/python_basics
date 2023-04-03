f7cc0ed3154fa57ada82b390a7c307169f776311
firstkain <gleb.glushenko@inbox.ru>
2023-04-03 11:37:05 +0500

    homework less_6

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -15,3 +15,35 @@
 П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку __str__
 __str__(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
 """
+
+
+class Worker:
+    name = "Иван"
+    surname = "Иванов"
+    position = "Продавец"
+    profit = 30000
+    bonus = 3000
+    _income = {"Оклад": profit,
+               "Премия": bonus
+               }
+    total_profit = 0
+
+
+class Position(Worker):
+    def get_full_name(self):
+        return "{} \"{} {}\"".format(self.position, self.name, self.surname)
+
+    def get_full_profit(self):
+        self.total_profit = self.profit + self.bonus
+        return ", доход с учётом премии: {}".format(self.total_profit)
+
+
+w = Worker()
+print(w.name)
+print(w.surname)
+print(w.position)
+print(w.profit)
+
+p = Position()
+print("\n<< Общая информация по сотруднику >>")
+print(p.get_full_name() + str(p.get_full_profit()) + " " + str(w._income))
