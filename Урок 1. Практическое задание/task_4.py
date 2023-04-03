ccfd255eedfd3f01dcaedc6b153cc5f0ec41fb54
Nimarun <r.levinov@gmail.com>
2023-04-03 15:55:31 +0300

    Первая домашка. В лекции Вы просили отписаться в телеге. А куда стучаться-то, можно ссыль?

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -13,13 +13,13 @@
 
 n = int(input('Ведите целое положительное число: '))
 
-max = n % 10
+highest = n % 10
 while True:
     n = n // 10
-    if n % 10 > max:
-        max = n % 10
+    if n % 10 > highest:
+        highest = n % 10
     elif n > 9:
         continue
     else:
-        print(f'Самая большая цифра в числе: {max}')
+        print(f'Самая большая цифра в числе: {highest}')
         break
