4b62d716cae760a91a896111c574e1d14039bbb9
morozit3 <morozovupnall@gmail.com>
2023-04-03 09:49:27 +0300

    lesson-8

---
+++ Урок 8 ООП Полезные дополнения/task_3.py
@@ -0,0 +1,38 @@
+"""
+Задание 3.
+
+Создайте собственный класс-исключение,
+который должен проверять содержимое списка на наличие только чисел.
+
+Проверить работу исключения на реальном примере. Необходимо запрашивать у пользователя данные и заполнять
+список только числами.
+
+Класс-исключение должен контролировать типы данных элементов списка.
+"""
+
+class NotNumberError(Exception):
+    def __init__(self, text):
+        self.text = text
+
+    def __str__(self):
+        return self.text
+
+
+if __name__ == '__main__':
+    my_list = []
+
+    while True:
+        user_input = input("Число для внесения в список (для выхода введите 'stop'): ")
+
+        if user_input == "stop":
+            break
+
+        try:
+            if not user_input.isdigit():
+                raise NotNumberError(f"'{user_input}' Не числовой формат")
+
+            my_list.append(int(user_input))
+        except NotNumberError as e:
+            print(e)
+
+    print(my_list)
---
+++ Урок 8 ООП Полезные дополнения/task_4_5_6.py
@@ -0,0 +1,350 @@
+"""
+4. Начните работу над проектом «Склад оргтехники». Создайте класс, описывающий склад.
+А также класс «Оргтехника», который будет базовым для классов-наследников.
+Эти классы — конкретные типы оргтехники (принтер, сканер, ксерокс).
+В базовом классе определить параметры, общие для приведенных типов.
+В классах-наследниках реализовать параметры, уникальные для каждого типа оргтехники.
+
+5. Продолжить работу над четвертым заданием.
+Разработать методы, отвечающие за приём оргтехники на
+склад и передачу в определенное подразделение компании.
+Для хранения данных о наименовании и
+количестве единиц оргтехники, а также других данных,
+можно использовать любую подходящую структуру, например словарь.
+
+6. Продолжить работу над пятым заданием. Р
+еализуйте механизм валидации вводимых пользователем данных.
+Например, для указания количества принтеров,
+отправленных на склад, нельзя использовать строковый тип данных.
+Подсказка: постарайтесь по возможности реализовать в проекте
+«Склад оргтехники» максимум возможностей, изученных на уроках по ООП.
+"""
+
+# Задание 4
+
+class Storage:
+    pass
+
+
+class OfficeEquipment:
+    vat = 0.13
+    added_value = 2.0
+    retail_rate = 1.3
+
+    def __init__(
+            self,
+            eq_type: str,
+            vendor: str,
+            model: str,
+            color: str,
+            purchase_price: float,
+    ):
+        self.type = eq_type
+        self.vendor = vendor
+        self.model = model
+        self.color = color
+
+        self.purchase_price = purchase_price
+
+        self.printable = True if self.type in ("printer", "xerox") else False
+        self.scannable = True if self.type in ("scanner", "xerox") else False
+
+    @property
+    def retail_price(self):
+        return self.wholesale_price * self.retail_rate
+
+    @property
+    def wholesale_price(self):
+        return self.purchase_price * (1 + self.vat) * (1 + self.added_value)
+
+    def __str__(self):
+        return f"{self.type} {self.vendor} {self.model} ({self.color})"
+
+
+class Printer(OfficeEquipment):
+    printable = True
+    scannable = False
+
+    def __init__(self, *args):
+        super().__init__('Принтер', *args)
+
+
+class Scanner(OfficeEquipment):
+    printable = False
+    scannable = True
+
+    def __init__(self, *args):
+        super().__init__('scanner', *args)
+
+
+class Xerox(OfficeEquipment):
+    printable = True
+    scannable = True
+
+    def __init__(self, *args):
+        super().__init__('xerox', *args)
+
+
+if __name__ == '__main__':
+    p1 = Printer("Pantum", "DW3300", "белый", 4000)
+    print(p1)
+
+# Задание 5
+
+class StorageError(Exception):
+    def __init__(self, text):
+        self.text = text
+
+    def __str__(self):
+        return self.text
+
+
+class AddStorageError(StorageError):
+    def __init__(self, text):
+        self.text = f"Невозможно добавить товар на склад:\n {text}"
+
+
+class TransferStorageError(StorageError):
+    def __init__(self, text):
+        self.text = f"Ошибка прередачи оборудования:\n {text}"
+
+
+class Storage:
+    def __init__(self):
+        self.__storage = []
+
+    def add(self, item: "OfficeEquipment"):
+        if not isinstance(item, OfficeEquipment):
+            raise AddStorageError(f"{item} не оргтехника")
+
+        self.__storage.append(item)
+
+    def transfer(self, idx: int, department: str):
+        if not isinstance(idx, int):
+            raise TransferStorageError(f"Недопустимый тип '{type(item)}'")
+
+        item: OfficeEquipment = self.__storage[idx]
+
+        if item.department:
+            raise TransferStorageError(f"Оборудование {item} уже закреплено за отделом {item.department}")
+
+        item.department = department
+
+    def filter_by(self, **kwargs):
+        for idx, item in enumerate(self):
+            a: OfficeEquipment = item
+            if all([a.__getattribute__(key) == kwargs[key] for key in kwargs]):
+                yield idx, item
+
+    def __getitem__(self, key):
+        if not isinstance(key, int):
+            raise TypeError
+
+        return self.__storage[key]
+
+    def __delitem__(self, key):
+        if not isinstance(key, int):
+            raise TypeError
+
+        del self.__storage[key]
+
+    def __iter__(self):
+        return self.__storage.__iter__()
+
+    def __str__(self):
+        return f"На складе сейчас {len(self.__storage)} устройств"
+
+
+class OfficeEquipment:
+    def __init__(
+            self,
+            eq_type: str,
+            vendor: str,
+            model: str,
+    ):
+        self.type = eq_type
+        self.vendor = vendor
+        self.model = model
+
+        self.department = None
+
+    def __getattribute__(self, name):
+        return object.__getattribute__(self, name)
+
+    def __str__(self):
+        return f"{self.type} {self.vendor} {self.model}"
+
+class Printer(OfficeEquipment):
+    def __init__(self, *args):
+        super().__init__('Принтер', *args)
+
+
+class Scanner(OfficeEquipment):
+    def __init__(self, *args):
+        super().__init__('Сканер', *args)
+
+
+class Xerox(OfficeEquipment):
+    def __init__(self, *args):
+        super().__init__('Ксерокс', *args)
+
+
+if __name__ == '__main__':
+    storage = Storage()
+    storage.add(Printer("Pantum", "DW3300"))
+    storage.add(Scanner("HP", "ScanJet Pro 2600 f1 (20G05A)"))
+    storage.add(Xerox("Xerox", "F123"))
+    print(storage)
+
+    last_idx = None
+    for idx, itm in storage.filter_by(department=None):
+        print(idx, itm)
+        last_idx = idx
+
+    print("Передаем 1 устройство")
+    storage.transfer(last_idx, 'Отдел ЯФ')
+
+    for idx, itm in storage.filter_by(department=None):
+        print(idx, itm)
+
+    print(storage)
+    print("Удаляем 1 устройство")
+    del storage[last_idx]
+    print(storage)
+
+# Задание 6
+
+class AppError(Exception):
+    def __init__(self, text):
+        self.text = text
+
+    def __str__(self):
+        return self.text
+
+
+class AcceptStorageError(AppError):
+    def __init__(self, text):
+        self.text = f"Невозможно добавить товар на склад:\n {text}"
+
+
+class TransferStorageError(AppError):
+    def __init__(self, text):
+        self.text = f"Ошибка прередачи оборудования:\n {text}"
+
+
+AddStorageError = AcceptStorageError
+
+
+class ValidateEquipmentError(AppError):
+    pass
+
+
+class Storage:
+    def __init__(self):
+        self.__storage = []
+
+    def add(self, equipments):
+        if not all([isinstance(equipment, OfficeEquipment) for equipment in equipments]):
+            raise AddStorageError(f"Вы пытаетесь добавить на склад не оргтехнику")
+
+        self.__storage.extend(equipments)
+
+    def transfer(self, idx: int, department: str):
+        if not isinstance(idx, int):
+            raise TransferStorageError(f"Недопустимый тип '{type(item)}'")
+
+        item: OfficeEquipment = self.__storage[idx]
+
+        if item.department:
+            raise TransferStorageError(f"Оборудование {item} уже закреплено за отделом {item.department}")
+
+        item.department = department
+
+    def filter_by(self, **kwargs):
+        for id_, item in enumerate(self):
+            if all([item.__getattribute__(key) == kwargs[key] for key in kwargs]):
+                yield id_, item
+
+    def __getitem__(self, key):
+        if not isinstance(key, int):
+            raise TypeError
+
+        return self.__storage[key]
+
+    def __delitem__(self, key):
+        if not isinstance(key, int):
+            raise TypeError
+
+        del self.__storage[key]
+
+    def __iter__(self):
+        return self.__storage.__iter__()
+
+    def __str__(self):
+        return f"На складе сейчас {len(self.__storage)} устройств"
+
+
+class OfficeEquipment:
+    __required_props = ("eq_type", "vendor", "model")
+
+    def __init__(self, eq_type: str = None, vendor: str = "", model: str = ""):
+        self.type = eq_type
+        self.vendor = vendor
+        self.model = model
+
+        self.department = None
+
+    def __setattr__(self, key, value):
+        if key in self.__required_props and not value:
+            raise AttributeError(f"'{key}' должен иметь значение отличное от false")
+
+        object.__setattr__(self, key, value)
+
+    def __str__(self):
+        return f"{self.type} {self.vendor} {self.model}"
+
+    @staticmethod
+    def validate(cnt):
+        if not isinstance(cnt, int):
+            ValidateEquipmentError(f"'{type(cnt)}'; количество инстансов должен быть типа 'int'")
+
+    @classmethod
+    def create(cls, cnt, **properties):
+        cls.validate(cnt)
+        return [cls(**properties) for _ in range(cnt)]
+
+
+class Printer(OfficeEquipment):
+    def __init__(self, **kwargs):
+        super().__init__(eq_type='Printer', **kwargs)
+
+
+class Scanner(OfficeEquipment):
+    def __init__(self, **kwargs):
+        super().__init__(eq_type='Scanner', **kwargs)
+
+
+class Xerox(OfficeEquipment):
+    def __init__(self, **kwargs):
+        super().__init__(eq_type='Xerox', **kwargs)
+
+
+if __name__ == '__main__':
+    storage = Storage()
+    storage.add(Printer.create(4, vendor="Epson", model="XP-400"))
+    storage.add(Scanner.create(3, vendor="OKI", model="5367-AD"))
+    storage.add(Scanner.create(2, vendor="OKI", model="5368-AD"))
+    storage.add(Xerox.create(6, vendor="Xerox", model="F123"))
+    print(storage)
+
+    for idx, itm in storage.filter_by(department=None, vendor="OKI", model="5367-AD"):
+        print(f"Резервируем {itm} в 'Отдел 11'")
+        storage.transfer(idx, 'Отдел ЯФ')
+
+    for idx, itm in storage.filter_by(department=None):
+        print(idx, f"{itm} не распределены по отделам")
+
+    print(storage)
+    print("Списываем 1 устройство")
+    del storage[0]
+    print(storage)
