2fa8e6a11b6469db0584262e0bd0a0fdd3c7517e
Sveta <sveta_chuprina@inbox.ru>
2023-04-03 13:44:44 +0300

    Added solution task 1, 2 HW 9

--- Урок 9/task_2.py
+++ Урок 9/task_2.py
@@ -1,27 +1,21 @@
-'''
-–еализовать метакласс. позвол€ющий создавать всегда один и тот же объект класса
-'''
+# –еализовать метакласс. позвол€ющий создавать всегда один и
+# тот же объект класса
+
+class DocMeta(type):
+    def __int__(self, *args, **kwargs):
+        self.__instance = []
+
+    def __call__(self, *args, **kwds):
+        if self.__instance is None:
+            self.__instance = super().__call__()
+            return self.__instance
 
 
-class TestMeta(type):
-    example = None
-
-    def __call__(self):
-        if self.example is None:
-            self.example = super().__call__()
-        return self.example
-
-
-class MyClass(metaclass=TestMeta):
+class MyClass(metaclass=DocMeta):
     pass
 
 
 ob_1 = MyClass()
 ob_2 = MyClass()
-ob_3 = MyClass()
-ob_4 = MyClass()
 
-print(ob_1 is ob_4)
-print(ob_2 is ob_3)
-print(ob_3 is ob_4)
-print(ob_2 is ob_3)
+print(ob_1 is ob_2)
