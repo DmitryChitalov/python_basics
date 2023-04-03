34d32960df9870ae6d90a9073bd8ef2c88aad374
Sveta <sveta_chuprina@inbox.ru>
2023-04-03 10:34:13 +0300

    Added solution task 5 HW 10

--- Урок 10/task_5.py
+++ Урок 10/task_5.py
@@ -9,8 +9,13 @@
 """
 
 import subprocess
+import chardet
 
-args = ['ping', 'yandex.ru']
-ping = subprocess.Popen(args, stdout=subprocess.PIPE)
-for i in ping.stdout:
-    print(i.decode('cp866'))
+for sites in ['yandex.ru', 'youtube.com']:
+    args = ['ping', sites]
+    subproc_ping = subprocess.Popen(args, stdout=subprocess.PIPE)
+    for line in subproc_ping.stdout:
+        result = chardet.detect(line)
+        line = line.decode(result['encoding']).encode('utf-8')
+        print(line.decode('utf-8'))
+exit()
