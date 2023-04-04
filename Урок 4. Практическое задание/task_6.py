56f1c501c9fb47525cc4a4ea91ceeb56e27549d1
geekbrains <info@geekbrains.ru>
2023-04-04 19:11:59 +0500

    add_task_les4

--- Урок 4. Практическое задание/task_6.py
+++ Урок 4. Практическое задание/task_6.py
@@ -12,3 +12,34 @@
 Во втором также необходимо предусмотреть условие,
 при котором повторение элементов списка будет прекращено.
 """
+from itertools import count, cycle
+
+
+def func_generator(start, stop):
+    result = []
+    for i in count(start):
+        if i <= stop:
+            result.append(i)
+        else:
+            break
+    print(f"Список чисел: {result}")
+
+first_number = int(input('Введите начальное число: '))
+end_number = int(input('Введите последнее число: '))
+func_generator(first_number, end_number)
+
+
+my_list = ['test ', 'AAA ', 11, ' proverka ']
+
+def func_list(my_list):
+    n = 0
+    result = ""
+    for i in cycle(my_list):
+        if n >= end_list:
+            break
+        result = result + str(i)
+        n += 1
+    print(f"Результат выполнения: {result}")
+
+end_list = int(input(' Введите сколько значений необходимо вывести из списка: '))
+func_list(my_list)
