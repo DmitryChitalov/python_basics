a27c8612a0ade929feaa97df42460a724369c11f
Kosechok <konslet@yandex.ru>
2023-04-04 15:31:44 +0300

    task 4

--- Урок 6. Практическое задание/task_4.py
+++ Урок 6. Практическое задание/task_4.py
@@ -20,3 +20,53 @@ speed, color, name, is_police (булево).
 Выполните доступ к атрибутам, выведите результат.
 Выполните вызов методов и также покажите результат.
 """
+class Car:
+    speed = 0
+    _is_police = False
+    def __init__(self, color, name):
+        self.color = color
+        self.name = name
+
+    def go(self, other_speed = 0):
+        print(other_speed)
+        if other_speed != 0:
+            self.speed = other_speed
+        else:
+            self.speed = 10
+
+    def stop(self):
+        self.stop = 0
+
+    def turn(self, direction):
+        print(f"машина повернула на: {direction}")
+    def show_speed(self):
+        print(f"текущую скорость автомобиля: {self.speed}")
+
+
+class TownCar(Car):
+
+    def show_speed(self):
+        super
+        if self.speed > 60:
+            print("превышении скорости!!!")
+
+class SportCar(Car):
+    pass
+
+class WorkCar(Car):
+
+    def show_speed(self):
+        super
+        if self.speed > 40:
+            print("превышении скорости!!!")
+class PoliceCar(Car):
+
+    def __init__(self):
+        super
+        _is_police = True
+
+
+
+obj = WorkCar("red", "bmw")
+obj.go(70)
+obj.show_speed()
