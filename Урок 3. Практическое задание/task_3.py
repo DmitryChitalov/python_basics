c2d51078aedaab8247b134a1a45f6974143091a6
NainaNigmedzyanova <127411544+NainaNigmedzyanova@users.noreply.github.com>
2023-04-04 16:02:04 +0300

    Update task_3.py

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -5,3 +5,13 @@
 1) используя функцию sort()
 2) без функции sort()
 """
+def my_func(a, b, c):
+    list_args = [a, b, c]
+    sum = max(list_args)
+    list_args.remove(sum)
+    sum += max(list_args)
+    return sum
+first = int(input('Введите первое число: '))
+second = int(input('Введите второе число: '))
+third = int(input('Введите третье число: '))
+print(my_func(first, second, third))
