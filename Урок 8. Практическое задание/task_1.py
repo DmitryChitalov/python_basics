f2a22cbf3468f6671e6d40829ac97291c4815051
Дмитрий <Serafimdimaster@mail.ru>
2023-04-05 19:21:13 +0300

    lesson8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -1,14 +1,32 @@
-"""
-Задание 1.
+class Data:
+    day_month_year = "4 - 4 - 2023"
 
-Реализовать класс «Дата», на уровне класса определить атрибут day_month_year,
-присвоить ему значение
+    @classmethod
+    def extract(cls, day_month_year):
+        my_date = []
 
-В рамках класса реализовать два метода.
+        for i in day_month_year.split():
+            if i != "-": my_date.append(i)
 
-Первый, с декоратором @classmethod, должен извлекать число, месяц,
-год, преобразовывать их тип к типу «Число» и делать атрибутами класса.
+        return int(my_date[0]), int(my_date[1]), int(my_date[2])
 
-Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца
-и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
-"""
+    @staticmethod
+    def valid(day, month, year):
+
+        if 1 <= day <= 31:
+            if 1 <= month <= 12:
+                if 0 <= year <= 9999:
+                    return f"Все введено правильно"
+                else:
+                    return f"Неправильные данные"
+            else:
+                return f"Неправильные данные"
+        else:
+            return f"Неправильные данные"
+
+
+a = Data
+
+print(Data.valid(44, 4, 2023))
+print(Data.valid(4, 4, 2023))
+print(Data.extract("4 - 4 - 2023"))
