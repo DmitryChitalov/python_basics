1f355fbf09feee3c4f6f8c81e5075a0bda47fe00
Anton Kosykh <anvkosykh@gmail.com>
2023-04-06 07:52:43 +0300

    ДЗ к уроку № 5

--- Урок 5. Практическое задание/task_5/task_5.py
+++ Урок 5. Практическое задание/task_5/task_5.py
@@ -2,3 +2,15 @@
 5)	Создать (программно) текстовый файл, записать в него программно набор чисел,
  разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 """
+
+with open("file.txt", "w", encoding='utf=8') as f_obj:
+    for i in range(1, 100):
+        f_obj.write(str(i) + " ")
+
+my_sum = 0
+with open("file.txt", encoding='utf=8') as f_obj:
+    content = f_obj.read()
+    print(content)
+    for el in content.split():
+        my_sum += int(el)
+    print(f"Сумма чисел в файле {my_sum}")
