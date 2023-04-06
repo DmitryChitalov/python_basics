fc895db8fa8b044c4a21b1d0fbd48a6d71bbb7d4
Anastasia <nastya2n2.91@mail.ru>
2023-04-06 19:21:10 +0400

    Homework completed

--- Урок 1. Практическое задание/task_4.py
+++ Урок 1. Практическое задание/task_4.py
@@ -12,9 +12,10 @@
 """
 number = int(input("Введите целое положительное число:  "))
 max_num = 0
-while number ! = 0:
+while number!= 0:
   cur_n = number % 10
+
   if max_num < cur_n:
     max_num = cur_n
     number = number // 10
-    print (f "Самая большая цифра в числе: {max_num}")
+    print(f"Самая большая цифра в числе: {max_num}")
