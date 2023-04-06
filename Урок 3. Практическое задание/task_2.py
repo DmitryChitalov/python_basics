dc405fc6b731acef3832bc3a1d79d8fc1dc189bb
IrinaPykhteeva <127942722+IrinaPykhteeva@users.noreply.github.com>
2023-04-06 14:27:49 +0300

    Update task_2.py

--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -8,3 +8,7 @@
 Иван Иванов 1846 года рождения, проживает в городе Москва,
 email: jackie@gmail.com, телефон: 01005321456
 """
+ef person_inf(name,surname,birthday,city,email,phone):
+    return f"Name-{name};Surname-{surname}; birthday- {birthday}; city- {city};"\
+f"email- {email}; phone -{phone}"
+print(person_inf(name='ANN', surname= 'Dark', birthday= '23.08.2003', city= 'Holluwood', email='rtyh8765', phone='4568999'))
