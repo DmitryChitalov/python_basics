f81fefdb0dfafb75b33933a4113da51af911fdcf
“Cosmatik” <Biznespro2021@yandex.ru>
2023-04-07 01:01:45 +0300

    Добавили решение к задаче 5

--- Урок 4. Практическое задание/task_6.py
+++ Урок 4. Практическое задание/task_6.py
@@ -12,3 +12,36 @@
 Во втором также необходимо предусмотреть условие,
 при котором повторение элементов списка будет прекращено.
 """
+from itertools import count, cycle
+
+# Итератор, генерирующий целые числа, начиная с указанного
+def generate_numbers(start, end):
+    for number in count(start):
+        if number > end:
+            break
+        yield number
+
+from itertools import count, cycle
+
+# Итератор, генерирующий целые числа, начиная с указанного
+def generator(start, stop):
+    for num in count(start):
+        if num > stop:
+            break
+        yield num
+
+
+def repeater(data, max_repeats):
+    for item in cycle(data):
+        if max_repeats == 0:
+            break
+        yield item
+        max_repeats -= 1
+
+
+for num in generator(3, 10):
+    print(num)
+
+my_list = ["a", "b", "c"]
+for item in repeater(my_list, 3):
+    print(item)
