cd015eaa503ab837f38fc1b563ce7cbaf7e56540
ilinroman34 <114098197+ilinroman34@users.noreply.github.com>
2023-04-07 18:06:39 +0300

    Delete Урок 1. Практическое задание directory

--- Урок 1. Практическое задание/task_3.py
+++
@@ -1,3 +0,0 @@
-n = int(input("Введите число n: "))
-sum = n + n * 11 + n * 111
-print(f"n + nn + nnn = {sum}")
--- Урок 1. Практическое задание/task_4.py
+++
@@ -1,9 +0,0 @@
-numb = int(input("Ведите целое положительное число: "))
-max_numb = 0
-while numb != 0:
-    cur_numb = numb % 10
-    if max_numb < cur_numb:
-        max_numb = cur_numb
-    numb = numb // 10
-print(f"Самая большая цифра в числе: {max_numb}")
-
