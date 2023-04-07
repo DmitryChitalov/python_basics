be4c69207ea2c3e0646f32694c167454f176cdf6
NikolasMalt <malcev_ns@live.ru>
2023-04-07 21:26:27 +0300

    Update task_3.py

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -15,3 +15,39 @@
 П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку __str__
 __str__(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
 """
+
+
+class Worker:
+
+    def __init__(self, name, surname, position, wage, bonus):
+        self.name = name
+        self.surname = surname
+        self.position = position
+        self._income = {"wage": wage, "bonus": bonus}
+
+
+class Position(Worker):
+
+    def __init__(self, name, surname, position, wage, bonus):
+        super().__init__(name, surname, position, wage, bonus)
+
+    def get_full_name(self):
+        return f'Полное имя сотрудника: {self.surname} {self.name}'
+
+    def get_total_income(self):
+        return f'Доход с учетом премии: {sum(self._income.values())}'
+
+    def __str__(self):
+        return f'Полное имя сотрудника: {self.surname} {self.name}'
+
+
+if __name__ == '__main__':
+    worker_1 = Position('Николай', 'Иванов', 'Инженер 2 разряда', 93000, 12000)
+    worker_2 = Position('Сергей', 'Петров', 'Инженер 3 разряда', 72000, 7000)
+    worker_3 = Position('Геннадий', 'Сидоров', 'Инженер', 51000, 5000)
+    print(worker_1.get_full_name())
+    print(worker_1.get_total_income())
+    print(worker_2.get_full_name())
+    print(worker_2.get_total_income())
+    print(Position('Геннадий', 'Петров', 'Инженер', 51000, 5000))
+    print(worker_3.get_total_income())
