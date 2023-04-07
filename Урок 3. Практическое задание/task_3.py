95d8a841ae3f75b43e15df3a691b6f879f1c2da7
iamAMSP <portsofi101@gmail.com>
2023-04-07 11:20:31 +0300

    lesson 3

--- Урок 3. Практическое задание/task_3.py
+++ Урок 3. Практическое задание/task_3.py
@@ -5,3 +5,19 @@
 1) используя функцию sort()
 2) без функции sort()
 """
+arg_1 = int(input("Введите первое число: "))
+arg_2 = int(input("Введите второе число: "))
+arg_3 = int(input("Введите третье число: "))
+
+my_func = [arg_1, arg_2, arg_3]
+print(sum(sorted(my_func)[1:]))
+
+def my_func(arg1, arg2, arg3):
+    if arg1 >= arg3 and arg2 >= arg3:
+        return arg1 + arg2
+    elif arg1 > arg2 and arg1 < arg3:
+        return arg1 + arg3
+    else:
+        return arg2 + arg3
+
+print(f'Ответ: - {my_func(int(input("Введите первое число: ")), int(input("Введите второе число: ")), int(input("Введите третье число: ")))}')
