aeffa08c392171d905f8b341b67e110ad169de81
mirki-05 <128673689+mirki-05@users.noreply.github.com>
2023-04-07 09:03:59 +0300

    Урок_3_задания 1,2

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -17,3 +17,22 @@ Process finished with exit code 0
 
 Process finished with exit code 0
 """
+def quot_int():
+
+    try:
+        my_bool_true = True
+        Message = 'Введите второе число: '
+        val_1 = float(input('Введите первое число: '))
+        while my_bool_true:
+            val_2 = float(input(Message))
+            if val_2 == 0:
+                Message = 'второе число должно быть больше нуля'
+                my_bool_true = True
+            else:
+                my_bool_true = False
+                return val_1 / val_2
+
+    except ValueError:
+        return 0
+
+print(quot_int())
--- Урок 3. Практическое задание/task_2.py
+++ Урок 3. Практическое задание/task_2.py
@@ -8,3 +8,13 @@
 Иван Иванов 1846 года рождения, проживает в городе Москва,
 email: jackie@gmail.com, телефон: 01005321456
 """
+def people_info():
+    firstname = input('Введите имя: ')
+    lastname = input('Введите фамилию: ')
+    born_year = int(input('Введите год рождения: '))
+    place = input('Введите город проживания: ')
+    e_mail = input('Введите электронную почту: ')
+    phone_number = int(input('Введите номер телефона: '))
+    return firstname, lastname, born_year, place, e_mail, phone_number
+firstname, lastname, born_year, place, e_mail, phone_number = people_info()
+print(f"{firstname} {lastname} {born_year} года рождения, проживает в городе {place}, email: {e_mail}, телефон: {phone_number} ")
