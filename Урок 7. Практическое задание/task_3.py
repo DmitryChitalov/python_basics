a1a8c1fdbd61ca70547d3016d4ed659f428491a5
DimShpInt <set_xz@mail.ru>
2023-04-08 00:53:30 +0300

    lesson 7 done

--- Урок 7. Практическое задание/task_3.py
+++ Урок 7. Практическое задание/task_3.py
@@ -100,3 +100,70 @@ print(cell2.make_order(10))
 *****\n *****\n *****\n *****\n *****\n *****\n
 **********\n **********\n *****
 """
+
+
+class Cell:
+
+    def __init__(self, quantity):
+        self.quantity = quantity
+
+    def __add__(self, other):
+        return self.quantity + other.quantity
+
+    def __sub__(self, other):
+        if self.quantity < other.quantity:
+            return "Разность отрицательна, поэтому операция не выполняется"
+        else:
+            return self.quantity - other.quantity
+
+    def __mul__(self, other):
+        return self.quantity * other.quantity
+
+    def __truediv__(self, other):
+        return self.quantity // other.quantity
+
+    def make_order(self, num_cel):
+        trig = self.quantity
+        cel_str = ""
+        for el in range(0, self.quantity // num_cel + 1):
+            if trig < num_cel:
+                cel_str = cel_str + "*" * trig
+            else:
+                cel_str = cel_str + "*" * num_cel + "\\n"
+            trig = trig - num_cel
+        return cel_str
+
+
+print("Создаем объекты клеток")
+cell1 = Cell(30)
+cell2 = Cell(25)
+
+cell3 = Cell(10)
+cell4 = Cell(15)
+
+print()
+
+print("Складываем")
+print(cell1 + cell2)
+
+print()
+
+print("Вычитаем")
+print(cell2 - cell1)
+print(cell4 - cell3)
+
+print()
+
+print("Умножаем")
+print(cell2 * cell1)
+
+print()
+
+print("Делим")
+print(cell1 / cell2)
+
+print()
+
+print("Организация ячеек по рядам")
+print(cell1.make_order(5))
+print(cell2.make_order(7))
