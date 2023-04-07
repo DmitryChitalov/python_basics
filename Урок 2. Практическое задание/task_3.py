9d350bcee928e20672b0d0be42c5b649da069029
ROMAN ILIN <rom_zes@list.ru>
2023-04-07 16:04:39 +0300

    урок 2 задание 1-3

--- Урок 2. Практическое задание/task_3.py
+++ Урок 2. Практическое задание/task_3.py
@@ -8,3 +8,14 @@
 Результат через список: Осень
 Результат через словарь: Осень
 """
+mseason = {'Зима': [12, 1, 2],'Весна': [3, 4, 5],'Лето': [6, 7, 8],'Осень': [9, 10, 11]}
+month = int(input('Введите мясяц 1-12: '))
+result = ''
+for season in mseason:
+  if month in mseason[season]:
+      result = season.capitalize()
+      break
+  else:
+      result = 'Не правильно указали месяц'
+print(result)
+
