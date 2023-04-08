033c990510bf35e241fa8d99ea1e04c7a99e958c
NikolasMalt <malcev_ns@live.ru>
2023-04-08 07:22:38 +0300

    Update task_4.py

--- Урок 6. Практическое задание/task_4.py
+++ Урок 6. Практическое задание/task_4.py
@@ -21,4 +21,74 @@ speed, color, name, is_police (булево).
 Выполните вызов методов и также покажите результат.
 """
 
-#Завтра доделаю
+
+class Car:
+    speed = 0
+    is_police = False
+
+    def __init__(self, name, color):
+        self.name = name
+        self.color = color
+
+    def go(self):
+        return 'машина поехала'
+
+    def stop(self):
+        return 'машина остановилась'
+
+    def turn(self, direction):
+        if direction in ('налево', 'направо'):
+            return f"машина повернула {direction}"
+
+    def show_speed(self):
+        return f"скорость {self.speed} км/ч"
+
+
+class TownCar(Car):
+    def show_speed(self):
+        if self.speed > 60:
+            return f"превышение скорости -  {self.speed} км/ч"
+        else:
+            return f"скорость {self.speed} км/ч"
+
+
+class SportCar(Car):
+    pass
+
+
+class WorkCar(Car):
+    def show_speed(self):
+        if self.speed > 40:
+            return f"превышение скорости -  {self.speed} км/ч"
+        else:
+            return f"скорость {self.speed} км/ч"
+
+
+class PoliceCar(Car):
+    def is_police(self):
+        return "полиция"
+
+
+townCar = TownCar(name='Mazda', color='Синий')
+workCar = WorkCar(name='Opel', color='Черный')
+policeCar = PoliceCar(name='Lada', color='Белый')
+
+townCar.speed = 92
+workCar.speed = 30
+policeCar.speed = 82
+
+print(
+    f"{townCar.color} {townCar.name} - {townCar.go()} {townCar.show_speed()} {townCar.turn('налево')} {townCar.turn('направо')}")
+print(
+    f"{workCar.color} {workCar.name} - {workCar.go()} {workCar.show_speed()} {workCar.turn('направо')} {workCar.turn('налево')}")
+print(
+    f"{policeCar.color} {policeCar.name} {policeCar.is_police()} - {policeCar.go()} {policeCar.show_speed()} {policeCar.turn('налево')} {policeCar.turn('направо')}")
+
+townCar.speed = 103
+workCar.speed = 10
+policeCar.speed = 120
+
+print(f"{townCar.color} {townCar.name} -  {townCar.show_speed()}  {townCar.turn('направо')} {townCar.stop()}")
+print(f"{workCar.color} {workCar.name} -  {workCar.show_speed()} {workCar.stop()}")
+print(
+    f"{policeCar.color} {policeCar.name} {policeCar.is_police()} - {policeCar.show_speed()} {policeCar.turn('направо')} {policeCar.stop()}")
