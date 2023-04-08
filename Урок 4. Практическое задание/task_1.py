74b61fc4e11aaa4a77970534e834ebc86b3e887d
IrinaPykhteeva <127942722+IrinaPykhteeva@users.noreply.github.com>
2023-04-08 16:53:07 +0300

    Update task_1.py

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -6,3 +6,26 @@
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
 """
+import sys
+
+
+def salary(hours, price, bonus):
+    return hours * price + bonus
+
+
+h, p, b = (0, 0, 0)
+
+try:
+    h = int(sys.argv[1])
+except ValueError:
+    print("Hours should be integer")
+try:
+    p = int(sys.argv[2])
+except ValueError:
+    print("Price should be integer")
+try:
+    b = int(sys.argv[3])
+except ValueError:
+    print("Bonus should be integer")
+
+print(salary(h, p, b))
