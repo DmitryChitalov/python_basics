828a5ddcaae3dbc8289d9c4991b8a68de69e66b0
firstkain <gleb.glushenko@inbox.ru>
2023-04-09 01:25:06 +0500

    homework less_7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -28,3 +28,36 @@
 8 10 12
 14 16 18
 """
+
+
+class Matrix:
+    def __init__(self, data: list):
+        self.data = data
+
+    def __str__(self):
+        result = []
+        for row in self.data:
+            result.append(' '.join([str(k) for k in row]))
+        return '\n'.join(result)
+
+    def __add__(self, other):
+        if len(self.data) == len(other.data):
+            result = []
+            for i, row in enumerate(self.data):
+                new_list = list(map(lambda x, y: x + y, row, other.data[i]))
+                result.append(new_list)
+            return Matrix(result)
+        else:
+            return
+
+
+list_lists_01 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+list_lists_02 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+matrix01 = Matrix(list_lists_01)
+matrix02 = Matrix(list_lists_02)
+matrix03 = matrix01 + matrix02
+
+print(matrix01, end='\n\n')
+print(matrix02, end='\n\n')
+print(matrix03)
