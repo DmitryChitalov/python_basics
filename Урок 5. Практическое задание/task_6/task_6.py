4df38eecba24af50d734f56245d79439264f0af5
iamAMSP <portsofi101@gmail.com>
2023-04-09 22:20:54 +0300

    lesson 5

--- Урок 5. Практическое задание/task_6/task_6.py
+++ Урок 5. Практическое задание/task_6/task_6.py
@@ -5,3 +5,17 @@
                                         Физкультура:   —   30(пр)   —
 Пример словаря: {“Информатика”: 170, “Физика”: 40, “Физкультура”: 30}
 """
+items = {}
+with open("task_6.txt", "r") as my_f:
+    for line in my_f:
+        name, lesson, practice, lab = line.split()
+        items[name] = int(lesson) + int(practice) + int(lab)
+
+    print(f"Result {items}")
+
+"""
+Информатика: 60 100 40
+Физика: 60 0 40
+Физкультура: 60 10 0
+Result {'Информатика:': 200, 'Физика:': 100, 'Физкультура:': 70}
+"""
