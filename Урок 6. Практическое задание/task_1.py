b0612022219ac03d1df61d346588e7d710a491df
natafro72 <natadomas@yandex.ru>
2023-04-09 19:58:44 +0400

    Выполнено

--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -15,3 +15,20 @@
 
 Проверить работу примера, создав экземпляр и вызвав описанный метод.
 """
+
+from time import sleep
+
+class TrafficLight:
+    def __init__(self, color):
+        self._color = color
+    def running(self):
+        for key, value in self._color.items():
+            sleep(value)
+            print(key)
+
+
+traf = TrafficLight(color={
+    "Красный": 7,
+    "Желтый": 2,
+    "Зеленый": 7})
+traf.running()
