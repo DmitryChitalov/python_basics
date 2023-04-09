a1a11b186641a065a14be497780ed6e43972e6df
natafro72 <natadomas@yandex.ru>
2023-04-09 21:17:30 +0400

    Выполнено

--- Урок 6. Практическое задание/task_4.py
+++ Урок 6. Практическое задание/task_4.py
@@ -20,3 +20,102 @@ speed, color, name, is_police (булево).
 Выполните доступ к атрибутам, выведите результат.
 Выполните вызов методов и также покажите результат.
 """
+class Car:
+    def __init__(self, color: str, name: str, is_police: bool):
+        self.speed = 0
+        self.color = color
+        self.name = name
+        self.is_police = is_police
+
+    def go(self, speed):
+        self.speed = speed
+        print(f'Скорость до {speed} км/ч')
+
+    def stop(self):
+        self.speed = 0
+        print('Остановка')
+
+    def turn(self, direction: str):
+        if self.speed > 0:
+            print(f'Поворот {direction}')
+        else:
+            print('нельзя повернуть')
+
+    def show_speed(self):
+        print(f'Скорость {self.speed} км/ч')
+
+
+class TownCar(Car):
+    def __init__(self, color: str, name: str):
+        self.speed = 0
+        self.color = color
+        self.name = name
+        self.is_police = False
+
+    def show_speed(self):
+        if self.speed > 60:
+            print(f'Превышение скорости! {self.speed} км/ч')
+        else:
+            print(f'Скорость {self.speed} км/ч')
+
+
+class SportCar(Car):
+    def __init__(self, color: str, name: str):
+        self.speed = 0
+        self.color = color
+        self.name = name
+        self.is_police = False
+
+
+class WorkCar(Car):
+    def __init__(self, color: str, name: str):
+        self.speed = 0
+        self.color = color
+        self.name = name
+        self.is_police = False
+
+    def show_speed(self):
+        if self.speed > 40:
+            print(f'Превышение скорости! {self.speed} км/ч')
+        else:
+            print(f'Скорость {self.speed} км/ч')
+
+
+class PoliceCar(Car):
+    def __init__(self, color: str, name: str):
+        self.speed = 0
+        self.color = color
+        self.name = name
+        self.is_police = True
+
+
+def test_drive(vehicle):
+    print(f"Тест-драйв {'полицейского' if vehicle.is_police else 'гражданского'} автомобиля {vehicle.name}, цвет {vehicle.color}")
+    vehicle.go(40)
+    vehicle.show_speed()
+    vehicle.turn('направо')
+    vehicle.stop()
+    vehicle.show_speed()
+    vehicle.turn('налево')
+    vehicle.go(60)
+    vehicle.show_speed()
+    vehicle.go(120)
+    vehicle.show_speed()
+    vehicle.stop()
+    print("Тест-драйв окончен", end="\n\n")
+
+
+granta = Car('белый', 'Lada Granta', False)
+test_drive(granta)
+
+polo = TownCar('коричневый', 'Volkswagen Polo')
+test_drive(polo)
+
+veyron = SportCar('желтый', 'Bugatti Veyron')
+test_drive(veyron)
+
+largus = WorkCar('серый', 'Lada Largus')
+test_drive(largus)
+
+hunter = PoliceCar('белый', 'UAZ Hunter')
+test_drive(hunter)
