37a1d22c6ba3954dadf31b177eece055641f33bf
Ksenia Davydova <ksyksyks@yandex.ru>
2023-04-09 23:20:30 +0400

    ДЗ - урок 6

--- Урок 6. Практическое задание/task_4.py
+++ Урок 6. Практическое задание/task_4.py
@@ -20,3 +20,57 @@ speed, color, name, is_police (булево).
 Выполните доступ к атрибутам, выведите результат.
 Выполните вызов методов и также покажите результат.
 """
+
+class Car:
+    def __init__(self, speed, color, name, is_police):
+        self.speed = speed
+        self.color = color
+        self.name = name
+        self.is_polise = is_police
+    def go(self):
+        print(f"Машина поехала {self.name}")
+    def stop(self):
+        print(f"Машина остановилась {self.name}")
+    def turn(self, direction):
+        print(f"Машина {self.name} повернула {direction}")
+    def show_speed(self):
+        print(f"скорость {self.name} движения {self.speed}")
+
+class TownCar(Car):
+    def show_speed(self):
+        print(f"Скорость свыше 60 км/ч")
+
+class SportCar(Car):
+    def type_car(self):
+        print(f"Спотривная машина {self.name} {self.color}")
+
+class WorkCar(Car):
+    def show_speed(self):
+        print(f"Скорость свыше 40 км/ч")
+
+class PoliceCar(Car):
+    def type_car(self):
+        print(f"Полицейская машина {self.name} {self.is_police}")
+
+town_car = TownCar(120, 'белая', 'Машина для города', False)
+sport_car = SportCar(180, 'Желтая', 'Спортивная машина', False)
+work_car = WorkCar(70, 'Зеленая', 'Рабочая машина', False)
+polise_car = PoliceCar(100, 'Синяя', 'Полицейская машина', False)
+
+print(town_car.name)
+print()
+sport_car.show_speed()
+print()
+work_car.show_speed()
+print(work_car.name, work_car.color)
+print()
+polise_car.show_speed()
+sport_car.turn('налево')
+
+
+
+
+
+
+
+
