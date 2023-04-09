ae9d93927d25396cea51db6353e29a964bb000f0
Vladislav <vladislavsilaev@mail.ru>
2023-04-09 18:44:41 +0400

    dz2

---
+++ 75332.py
@@ -0,0 +1,16 @@
+my_list = [7, 5, 3, 3, 2]
+print(f"Рейтинг - {my_list}")
+digit = int(input("Введите число (111 - выход) "))
+while digit != 111:
+    for el in range(len(my_list)):
+        if my_list[el] == digit:
+            my_list.insert(el + 1, digit)
+            break
+        elif my_list[0] < digit:
+            my_list.insert(0, digit)
+        elif my_list[-1] > digit:
+            my_list.append(digit)
+        elif my_list[el] > digit and my_list[el + 1] < digit:
+            my_list.insert(el + 1, digit)
+    print(f"текущий список - {my_list}")
+    digit = int(input("Введите число "))
