744cca4190ae5cf2a442156e6bf992bfddc0be8e
vhsdecay <vetvwhs@gmail.com>
2023-04-09 23:04:33 +0300

    Урок 9 Задача 2

--- Урок 9/task_2.py
+++ Урок 9/task_2.py
@@ -1,34 +1,39 @@
 """
 Задание 2.
-Реализовать класс Road (дорога), в котором определить защищенные атрибуты:
-length (длина в метрах), width (ширина в метрах).
-Значения данных атрибутов должны передаваться при создании экземпляра класса.
-Реализовать публичный метод расчета массы асфальта, необходимого для покрытия
-всего дорожного полотна.
-Использовать формулу: длина * ширина * масса асфальта для покрытия одного кв
-метра дороги асфальтом, толщиной в 1 см * число м толщины полотна.
-Массу и толщину сделать публичными атрибутами.
-Проверить работу метода.
-Например: 20м*5000м*25кг*0.05м = 125000 кг = 125 т
+
+Реализовать метакласс. позволяющий создавать всегда один и тот же объект класса (см. урок)
 """
 
 
-class Road:
-    _width = 0
-    _length = 0
+class MyMeta(type):
+    obj = None
 
-    def __init__(self, length, width, mass, thickness):
+    def __call__(cls, *args):
+        if cls.obj is None:
+            cls.obj = super().__call__(*args)
+            return cls.obj
+        else:
+            return cls.obj
+
+
+class RoadCalc(metaclass=MyMeta):
+    _mass = 25
+    _thickness = 0
+
+    def __init__(self, length, width):
         self._length = float(length)
         self._width = float(width)
-        self.mass = float(mass)
-        self.thickness = float(thickness)
 
     def mass_calculation(self):
-        result = int(self._length * self._width * self.mass * self.thickness)
+        result = int(self._length * self._width * self._mass * self._thickness)
         return print(f'Требуемая масса асфальта = {result}(кг) '
                      f'= {int(result / 1000)}(т)')
 
 
-road_one = Road(input('Введите длинну(м): '), input('Введите ширину(м): '),
-                input('Введите массу(кг): '), input('Введите толщину(м): '))
-road_one.mass_calculation()
+road1 = RoadCalc(20, 5000)
+road2 = RoadCalc(40, 4000)
+road3 = RoadCalc(20, 5000)
+
+print(road1 is road2)
+print(road1 is road3)
+print(road2 is road3)
