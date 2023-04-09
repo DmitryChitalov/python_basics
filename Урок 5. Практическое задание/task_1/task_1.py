eac15c7d424847eab6edbfd3e5cf9fee1df0bab6
ko55ur <96375951+ko55ur@users.noreply.github.com>
2023-04-09 17:06:51 +0300

    Homework 5

--- Урок 5. Практическое задание/task_1/task_1.py
+++ Урок 5. Практическое задание/task_1/task_1.py
@@ -3,3 +3,13 @@
 записать в него построчно данные, вводимые пользователем.
 Об окончании ввода данных свидетельствует пустая строка.
 """
+
+import fileinput
+
+print("Введите данные:")
+for user_input in fileinput.input():
+    if '' == user_input.rstrip():
+        break
+    with open('user_input.txt', 'a') as file:
+        file.write(user_input)
+print(f"Ваши данные записаны в файл {file.name}")
