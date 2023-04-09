9deab8c2c24dec034acc8754a6f70b098343d80f
NikolasMalt <malcev_ns@live.ru>
2023-04-09 15:36:57 +0300

    Update task_2.py

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -25,3 +25,41 @@
 
 Два класса: абстрактный и Clothes
 """
+from abc import ABC, abstractmethod
+
+
+class AbstractClothes(ABC):
+
+    @abstractmethod
+    def cloth_calc(self, what, size=0, height=0):
+        return
+
+
+class Clothes(AbstractClothes):
+    def __init__(self):
+        self.coat_expense = 0
+        self.costume_expense = 0
+
+    def cloth_calc(self, what, size=0, height=0):
+        self.size = size
+        self.height = height
+        try:
+            if what == 'coat':
+                self.coat_expense = round(size / 6.5 + 0.5, 2)
+                return self.coat_expense
+            elif what == 'costume':
+                self.costume_expense = round(2 * height + 0.3, 2)
+                return self.costume_expense
+            return None
+        except TypeError:
+            return None
+
+    @property
+    def cloth_total(self):
+        return self.coat_expense + self.costume_expense
+
+
+my_clothes = Clothes()
+print(f"Расход ткани на пальто = {my_clothes.cloth_calc('coat', size=5)}")
+print(f"Расход ткани на костюм = {my_clothes.cloth_calc('costume', height=10)}")
+print(f"Общий расход ткани = {my_clothes.cloth_total}")
