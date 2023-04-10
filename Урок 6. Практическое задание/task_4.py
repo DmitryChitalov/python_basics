03a7bab815341af15c3a1f59f22ed05e805533bd
IrinaPykhteeva <127942722+IrinaPykhteeva@users.noreply.github.com>
2023-04-10 14:42:03 +0300

    Update task_4.py

--- Урок 6. Практическое задание/task_4.py
+++ Урок 6. Практическое задание/task_4.py
@@ -20,3 +20,30 @@ speed, color, name, is_police (булево).
 Выполните доступ к атрибутам, выведите результат.
 Выполните вызов методов и также покажите результат.
 """
+
+
+class Worker:
+    name = ''
+    surname = ''
+    position = ''
+    _income = {"wage": 500, "bonus": 300}
+
+
+class Position(Worker):
+    def get_full_name(self):
+        return str(self)
+
+    def get_total_income(self):
+        return self._income['wage'] + self._income['bonus']
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
+
+pos = Position()
+pos.name = 'Jane'
+pos.surname = 'Doe'
+pos.position = 'Developer'
+print(f"Full name via method: {pos.get_full_name()}")
+print(f"Full name via magic: {pos}")
+print(f"Total income: {pos.get_total_income()}")
