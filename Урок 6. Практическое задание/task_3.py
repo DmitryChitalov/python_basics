cbd021537db4450ebc17f5617331e19e8ddeabde
IrinaPykhteeva <127942722+IrinaPykhteeva@users.noreply.github.com>
2023-04-10 14:41:22 +0300

    Update task_3.py

--- Урок 6. Практическое задание/task_3.py
+++ Урок 6. Практическое задание/task_3.py
@@ -15,3 +15,30 @@
 П.С. попытайтесь добить вывода информации о сотруднике также через перегрузку __str__
 __str__(self) - вызывается функциями str, print и format. Возвращает строковое представление объекта.
 """
+
+
+class Worker:
+    name = ''
+    surname = ''
+    position = ''
+    _income = {"wage": 500, "bonus": 300}
+
+
+class Position(Worker):
+    def get_full_name(self):
+        return str(self)
+
+    def get_total_income(self):
+        return self._income['wage'] + self._income['bonus']
+
+    def __str__(self):
+        return f"{self.name} {self.surname}"
+
+
+pos = Position()
+pos.name = 'Jane'
+pos.surname = 'Doe'
+pos.position = 'Developer'
+print(f"Full name via method: {pos.get_full_name()}")
+print(f"Full name via magic: {pos}")
+print(f"Total income: {pos.get_total_income()}")
