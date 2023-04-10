477bfb8c762f793505eacdbea1115574bb3fd138
IrinaPykhteeva <127942722+IrinaPykhteeva@users.noreply.github.com>
2023-04-10 14:12:30 +0300

    Update and rename Урок 5. Практическое задание/task_5/task_5.py to Урок 5. Практическое задание/задача 5/task_5.py

--- Урок 5. Практическое задание/задача 5/task_5.py
+++ Урок 5. Практическое задание/задача 5/task_5.py
@@ -2,3 +2,15 @@
 5)	Создать (программно) текстовый файл, записать в него программно набор чисел,
  разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
 """
+import numpy as np
+
+base_list = np.random.randint(10, 100, 5)
+line = ' '.join(str(x) for x in base_list)
+fw = open("numbers.txt", "w")
+fw.write(line)
+fw.close()
+
+fr = open("numbers.txt", "r")
+line = fr.read()
+fr.close()
+print("Summ: " + str(sum(list(int(x) for x in line.split()))))
