e7ad1232ad27323d4ca1caed4c53fe300ee46884
Nikita <s.nikita.a@yandex.ru>
2023-04-10 19:25:05 +0300

    Урок_3

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -17,3 +17,22 @@ Process finished with exit code 0
 
 Process finished with exit code 0
 """
+def div(*args):
+
+    try:
+        arg1 = int(input("Введите Делитель "))
+        arg2 = int(input("Введите Делитель "))
+        res = arg1 / arg2
+    except ValueError:
+        return 'Ошибка значения'
+    except ZeroDivisionError:
+        return "На ноль делить нельзя!"
+
+    return res
+
+    if arg2 != 0:
+        return arg1 / arg2
+    else:
+        print("Значение не может быть равно нулю")
+
+print(f'result  {div()}')
