d6c0c7467e4fd2cdfe6253804d47dab0d297fdaa
vhsdecay <vetvwhs@gmail.com>
2023-04-10 19:07:50 +0300

    Урок 10 Задача 5

---
+++ Урок 10/task_5.py
@@ -0,0 +1,20 @@
+"""
+Задание 5.
+
+Выполнить пинг веб-ресурсов yandex.ru, youtube.com и преобразовать результаты
+из байтовового в строковый тип на кириллице.
+
+Подсказки:
+--- используйте модуль chardet, иначе задание не засчитается!!!
+"""
+
+import subprocess
+import chardet
+
+yandex = [['ping', 'yandex.ru'], ['ping', 'youtube.com']]
+
+for elem in yandex:
+    yandex_ping = subprocess.Popen(elem, stdout=subprocess.PIPE)
+    for line in yandex_ping.stdout:
+        res = chardet.detect(line)
+        print(line.decode(encoding=res['encoding']))
