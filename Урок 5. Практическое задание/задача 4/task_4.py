9f7c7a0ba1ad6f6735333a96dc8645d73690bbd3
IrinaPykhteeva <127942722+IrinaPykhteeva@users.noreply.github.com>
2023-04-10 14:11:15 +0300

    Update and rename Урок 5. Практическое задание/task_4/task_4.py to Урок 5. Практическое задание/задача 4/task_4.py

--- Урок 5. Практическое задание/задача 4/task_4.py
+++ Урок 5. Практическое задание/задача 4/task_4.py
@@ -7,3 +7,15 @@
 Иванов 23543.12
 Петров 13749.32
 """
+persons = {}
+fr = open("persons.txt", "r")
+for line in fr:
+    person, salary = line.split()
+    persons[person] = float(salary)
+fr.close()
+
+low_salary = {person: salary for (person, salary) in persons.items() if salary < 10000}
+mid = sum(persons.values()) / len(persons)
+
+print(f"Low salary: {low_salary}")
+print(f"Mid salary: {mid}")
