7f8c6f393d91a29b4cc7c07a2202a0e35232314f
PalchikovDmitriy <dmitrij.palchikof@yandex.ru>
2023-04-11 21:20:25 +0300

    Выполнена 8-я домашняя работа

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,32 @@
 Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца
 и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
 """
+
+
+class Date:
+
+    def __init__(self, day_month_year):
+        pass
+
+    @classmethod
+    def first_func(cls, day_month_year):
+        ex_list = day_month_year.split('.')
+        day = int(ex_list[0])
+        month = int(ex_list[1])
+        year = int(ex_list[2])
+        return print(f'День: {day}, Месяц: {month}, Год: {year}')
+
+    @staticmethod
+    def second_func(day_month_year):
+        ex_list = day_month_year.split('.')
+
+        if int(ex_list[0]) > 31 or int(ex_list[1]) > 12 or int(ex_list[2]) > 2023:
+            return print('В дате ошибка! Валидация не пройдена')
+        else:
+            return print('Валидация пройдена!')
+
+
+Date.first_func('10.10.2010')
+Date.second_func('10.13.2010')
+Date.second_func('10.11.2010')
+Date.first_func('23.10.2020')
--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,22 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+
+
+class ExampleError(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
+f_num = int(input('Введите первое число: '))
+s_num = int(input('Введите второе число: '))
+
+try:
+    if s_num == 0:
+        raise ExampleError('Вы ввели число 0')
+    else:
+        result = f_num / s_num
+except ExampleError as err:
+    print(err)
+else:
+    print(f'Ответ = {result}')
