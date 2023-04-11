24bdfd95572388d615605e677230351b2ab13b45
Nimarun <r.levinov@gmail.com>
2023-04-11 07:53:54 +0300

    Вторая домашка (исправление)

--- Урок 2. Практическое задание/task_4.py
+++ Урок 2. Практическое задание/task_4.py
@@ -15,7 +15,14 @@
 2. перерефриж
 """
 
-my_list = input('Введите слова через пробел: ').split()
-
-for i, el in enumerate(my_list, 1):
-    print(i, el[0:10])
+my_str = input("Введите слова через пробел: ")
+my_word = []
+num = 1
+for el in range(my_str.count(' ') + 1):
+    my_word = my_str.split()
+    if len(str(my_word)) <= 10:
+        print(f" {num} {my_word [el]}")
+        num += 1
+    else:
+        print(f" {num} {my_word [el] [0:10]}")
+        num += 1
