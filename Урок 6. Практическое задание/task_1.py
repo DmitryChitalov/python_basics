2a45fc169bb25bbe66a64375c0bbede980e65502
MaryRemenyuk <91543154+MaryRemenyuk@users.noreply.github.com>
2023-04-11 22:49:18 +0300

    Complete task 1

--- Урок 6. Практическое задание/task_1.py
+++ Урок 6. Практическое задание/task_1.py
@@ -15,3 +15,26 @@
 
 Проверить работу примера, создав экземпляр и вызвав описанный метод.
 """
+import time
+
+class TrafficLight():
+    def __init__(self, color):
+        self.__color = color
+    def running(self):
+        for i in range(0,3):
+            if self.__color == 'red':
+                print(self.__color)
+                time.sleep(7)
+                self.__color = 'yellow'
+            if self.__color == 'yellow':
+                print(self.__color)
+                time.sleep(2)
+                self.__color = 'green'
+            if self.__color == 'green':
+                print(self.__color)
+                time.sleep(5)
+                self.__color = 'red'
+
+
+tr = TrafficLight('yellow')
+tr.running()
