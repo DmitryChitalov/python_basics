a539685ddeb60718f577245afd4ec9e77aeacdb9
Nimarun <r.levinov@gmail.com>
2023-04-11 12:00:43 +0300

    Третья домашка

--- Урок 3. Практическое задание/task_1.py
+++ Урок 3. Практическое задание/task_1.py
@@ -17,3 +17,18 @@ Process finished with exit code 0
 
 Process finished with exit code 0
 """
+
+def my_func(*args):
+
+    try:
+        arg1 = int(input('Введите первое число: '))
+        arg2 = int(input('Введите второе число: '))
+        res = arg1 / arg2
+    except ValueError:
+        return 'Неверное значение'
+    except ZeroDivisionError:
+        return 'Вы что? Пытаетесь делить на 0!'
+
+    return res
+
+print(f'result  {my_func()}')
