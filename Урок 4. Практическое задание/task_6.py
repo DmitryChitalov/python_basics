78a1ffa74413d5033fbc6249a957ed70e4c41810
Asandrik86 <asandrik8686@gmail.com>
2023-04-13 10:49:44 +0300

    done

--- Урок 4. Практическое задание/task_6.py
+++ Урок 4. Практическое задание/task_6.py
@@ -12,3 +12,27 @@
 Во втором также необходимо предусмотреть условие,
 при котором повторение элементов списка будет прекращено.
 """
+from typing import Iterable
+from itertools import cycle
+def get_repeated(iterable: Iterable, count: int):
+
+
+    if not isinstance(count, int):
+        raise TypeError(f"count '{count.__class__.__name__}' is illegat type")
+
+    if count < 0:
+        raise ValueError(f"count 'can't be less than 0")
+
+    # убираем брекется и получаем стандартный режим работы sycle
+    iterator = cycle([iterable])
+
+    while count:
+        yield next(iterator)
+        count -= 1
+
+
+if __name__ == '__main__':
+    source_list = [1, 2, 3]
+    repeat = 4
+
+    print(list(get_repeated(source_list, repeat)))
