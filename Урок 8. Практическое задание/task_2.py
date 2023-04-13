6ef1244df91444b42bff07f0f1959ae079b8e3d1
IrinaPykhteeva <127942722+IrinaPykhteeva@users.noreply.github.com>
2023-04-13 13:28:25 +0300

    Update task_2.py

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,22 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+
+class CustomZeroDividingError(Exception):
+    pass
+
+
+def divide(number1, number2):
+    if number2 == 0:
+        raise CustomZeroDividingError()
+    return number1 / number2
+
+
+try:
+    num1 = int(input('Input number #1:'))
+    num2 = int(input('Input number #2:'))
+    print(f"Result: {divide(num1, num2)}")
+except CustomZeroDividingError:
+    print("Can't divide on zero")
+except ValueError:
+    print('Incorrect input')
