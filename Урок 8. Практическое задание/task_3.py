bc6009a9b5059da04b783965b822bb3dc693bbac
IrinaPykhteeva <127942722+IrinaPykhteeva@users.noreply.github.com>
2023-04-13 13:29:04 +0300

    Update task_3.py

--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -9,3 +9,34 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+
+class NumberValidationError(Exception):
+    def __init__(self, value):
+        self.message = f"{value} is not a number"
+        super().__init__(self.message)
+
+
+# data = input("Input numbers, separated by space: ")
+list = []
+
+
+def check(value):
+    try:
+        val = int(value)
+    except ValueError:
+        raise NumberValidationError(value)
+
+    return val
+
+
+while True:
+    data = input("Input value. Leave empty to finish: ")
+    if data == '':
+        break
+
+    try:
+        list.append(check(data))
+    except NumberValidationError:
+        pass
+
+print(f"Result list of numbers: {list}")
