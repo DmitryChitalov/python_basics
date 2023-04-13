53093b36b830dc223e8beed40a678a5018c34907
sp3kt0r <126805884+sp3kt0r@users.noreply.github.com>
2023-04-13 15:36:26 +0300

    Update task_2.py

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -25,41 +25,47 @@
 
 Два класса: абстрактный и Clothes
 """
+from enum import Enum
 from abc import ABC, abstractmethod
 
 
-class Clothes(ABC):
+class ClothType(Enum):
 
-    def __init__(self, param):
-        self.param = param
+    COAT = 0
+    COSTUME = 1
 
-    @property
-    def consumption(self):
-        return f'Сумма затраченной ткани равна: {self.param / 6.5 + 0.5 + 2 * self.param + 0.3 :.2f}'
+
+class ACloth(ABC):
+
+    __name: str
+    __type: 'ClothType'
+
+    def __init__(self, name: str, cloth_type: 'ClothType') -> None:
+        self.__name = name
+        self.__type = cloth_type
 
     @abstractmethod
-    def abstract(self):
-        return 'Smth vary abstract'
+    def calc_cloth(self) -> float:
 
 
-class Coat(Clothes):
-    def consumption(self):
-        return f'Для пошива пальто нужно: {self.param / 6.5 + 0.5 :.2f} ткани'
 
-    def abstract(self):
-        return 'Smth vary abstract second'
+class Coat(ACloth):
+    __size: float
 
+    def __init__(self, name: str, size: float) -> None:
+        super().__init__(name, ClothType.COAT)
+        self.__size = size
 
-class Costume(Clothes):
-    def consumption(self):
-        return f'Для пошива костюма нужно: {2 * self.param + 0.3 :.2f} ткани'
-
-    def abstract(self):
-        pass
+    def calc_cloth(self) -> float:
+        return self.__size / 6.5 + 0.5
 
 
-coat = Coat(400)
-costume = Costume(55)
-print(coat.consumption)
-print(costume.consumption())
-print(coat.abstract())
+class Costume(ACloth):
+    __height: float
+
+    def __init__(self, name: str, height) -> None:
+        super().__init__(name, ClothType.COSTUME)
+        self.__height = height
+
+    def calc_cloth(self) -> float:
+        return 2 * self.__height + 0.3
