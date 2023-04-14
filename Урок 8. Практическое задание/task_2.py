977f62d083f86d7066e290f1c3f567a0d35fdd89
Anton Kosykh <anvkosykh@gmail.com>
2023-04-14 18:36:48 +0300

    ДЗ к уроку №8

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,23 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+
+
+class OwnError(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
+divisible = int(input("Введите делимое: "))
+divider = int(input("Введите делитель: "))
+
+try:
+    divider = int(divider)
+    if divider == 0:
+        raise OwnError("На 0 делить нельзя!")
+except OwnError as err:
+    print(err)
+else:
+    print(f"Результат = {divisible / divider}")
+finally:
+    print("Работа программы завершена")
--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -9,3 +9,62 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+
+
+class OwnError(Exception):
+    def __init__(self, value):
+        try:
+            isinstance(int(value), int)
+            self.value = value
+        except ValueError:
+            print(f"{value} - не является числом")
+            self.value = 0
+
+
+result_list = []
+
+while True:
+    user_value = input("Введите число: ")
+
+    if user_value == 'stop':
+        break
+
+    element = OwnError(user_value)
+    if element.value == user_value:
+        result_list.append(user_value)
+
+print('Результат:', *result_list)
+print('\nExit Program')
+
+
+
+
+
+
+'''     
+        
+        try:
+            int(el)
+            new_list.append(el)
+            raise OwnError("В списке не все данные являются числами!")
+        except OwnError as err:
+
+    print(err)
+
+
+
+    for el in user_list:
+        el = int(el)
+            raise OwnError("В списке не все данные являются  числами!")
+        if not isinstance(el, int):
+            raise OwnError("В списке не все данные являются  числами!")
+            
+            result = [int, (el) for el in user_list]
+            
+             if el == 'stop':
+            print(f"Стоп-фактор. Завершение работы программы.\n"
+                  f"Результат ввода: {new_list}")
+            
+            
+            
+'''
