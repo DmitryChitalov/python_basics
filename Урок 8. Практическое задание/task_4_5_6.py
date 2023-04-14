bbd592de5147410a8e22e0f8e4088fe9bcdc1d4f
gidrevichan <gidrevichan@mosmetod.ru>
2023-04-14 11:04:18 +0300

    adding solution for task 4, 5, 6 With time limits I cant do better for now

--- Урок 8. Практическое задание/task_4_5_6.py
+++ Урок 8. Практическое задание/task_4_5_6.py
@@ -19,128 +19,65 @@
 Подсказка: постарайтесь по возможности реализовать в проекте
 «Склад оргтехники» максимум возможностей, изученных на уроках по ООП.
 """
-class Storage:
-    def __init__(self, name):
+import pprint
+
+class OfficeEquipment:
+    def __init__(self, name, price, quantity, number_of_lists):
         self.name = name
-        self.storage_items = {}
-
-    def add_item(self, some_item, my_quantity):
-        if some_item in self.storage_items:
-            self.storage_items[some_item] += my_quantity
-        else:
-            self.storage_items[some_item] = my_quantity
-
-    def remove_item(self, some_item, my_quantity):
-        if some_item in self.storage_items:
-            if self.storage_items[some_item] >= my_quantity >= 0:
-                self.storage_items[some_item] -= my_quantity
-            else:
-                raise ValueError('Количество не может быть меньше нуля')
-        else:
-            raise ValueError('Такого предмета не существует')
-        for my_item in [printer, scanner, copier]:
-            if str(my_item) == some_item:
-                my_item.quantity -= my_quantity
-
-    def move_item(self, some_item, my_quantity, department):
-        if not isinstance(my_quantity, int):
-            raise ValueError('Количество должно быть целым числом!')
-        if my_quantity <= 0:
-            raise ValueError('Количество должно быть больше нуля!')
-        if some_item in self.storage_items and my_quantity <= self.storage_items[some_item]:
-            self.storage_items[some_item] -= my_quantity
-            department.receive_item(some_item, my_quantity)
-            for my_item in [printer, scanner, copier]:
-                if str(my_item) == some_item:
-                    my_item.quantity -= my_quantity
-                    department.receive_item(str(my_item), my_quantity)
-        else:
-            raise ValueError('На складе нет такого количества')
-        return self.storage_items
-
-    def get_items(self):
-        items = {}
-        for my_item, my_quantity in self.storage_items.items():
-            items[my_item] = my_quantity
-        return items
-
-
-class OfficeItem:
-    def __init__(self, name, model, price, my_quantity):
-        self.name = name
-        self.model = model
         self.price = price
-        self.quantity = my_quantity
+        self.number_of_lists = number_of_lists
 
-    def __str__(self):
-        return f"'{self.name}' {self.model}, цена: {self.price}$, начальное количество: {self.quantity} шт."
+        try:
+            if isinstance(quantity, int):
 
+                self.quantity = quantity
 
-class Printer(OfficeItem):
-    def __init__(self, name, model, price, my_quantity, print_type):
-        super().__init__(name, model, price, my_quantity)
-        self.print_type = print_type
-
-    def __str__(self):
-        return f"'{self.name}' {self.model}, цена: {self.price}$, начальное количество: {self.quantity} шт., " \
-               f"тип печати: {self.print_type}"
-
-
-class Scanner(OfficeItem):
-    def __init__(self, name, model, price, my_quantity, scanner_type):
-        super().__init__(name, model, price, my_quantity)
-        self.scanner_type = scanner_type
-
-    def __str__(self):
-        return f"'{self.name}' {self.model}, цена: {self.price}$, начальное количество: {self.quantity} шт.," \
-               f" тип сканирования: {self.scanner_type}"
-
-
-class Copier(OfficeItem):
-    def __init__(self, name, model, price, my_quantity, copying_speed):
-        super().__init__(name, model, price, my_quantity)
-        self.copying_speed = copying_speed
-
-    def __str__(self):
-        return f"'{self.name}' {self.model}, цена: {self.price}$, начальное количество: {self.quantity} шт.," \
-               f" скорость копирования: {self.copying_speed}"
-
-
-class Department:
-    def __init__(self, name):
-        self.name = name
-        self.department_items = {}
-
-    def receive_item(self, some_item, my_quantity):
-        if my_quantity > 0:
-            if some_item not in self.department_items:
-                self.department_items[some_item] = my_quantity
+                self.warehouse_units = {
+                    "Модель устройства": self.name,
+                    "Цена за ед": self.price,
+                    "Количество": self.quantity}
             else:
-                self.department_items[some_item] += my_quantity
-            if self.department_items[some_item] < 0:
-                raise ValueError('Количество не может быть меньше нуля')
-            return
-        raise ValueError('Количество должно быть целым числом')
+                self.warehouse_units = {}
+                raise CustomExceptionError("На ввод принимаются только числа.")
 
-    def get_items(self):
-        items = {}
-        for my_item, my_quantity in self.department_items.items():
-            items[my_item] = my_quantity
-        return items
+        except CustomExceptionError as custom_error:
+            print(custom_error.txt)
 
 
-storage = Storage("Склад")
-development_department = Department("Отдел разработки")
-accounting_department = Department("Бухгалтерия")
+class CustomExceptionError(Exception):
+    def __init__(self, txt):
+        self.txt = txt
 
-printer = Printer("Принтер", "Epson L3211", 250, 10, "лазерный")
-scanner = Scanner("Сканер", "HP LaserJet Pro", 300, 3, "потоковый")
-copier = Copier("Копир", "Pantum M6700DW", 200, 5, "высокая")
+class Printer(OfficeEquipment):
+    def please_print_me(self):
+        return f"Печатать можно {self.number_of_lists} раз."
 
-storage.add_item(str(printer), 10)
-storage.add_item(str(scanner), 3)
-storage.add_item(str(copier), 5)
 
-print("Техника на складе:")
-for item, quantity in storage.get_items().items():
-    print(f"{item}, Остаток: {quantity} шт.")
+class Scanner(OfficeEquipment):
+    def please_scan_me(self):
+        return f"Сканировать можно {self.number_of_lists} раз."
+
+
+class Copier(OfficeEquipment):
+    def please_copy_me(self):
+        return f"Копировать можно {self.number_of_lists} раз."
+
+class Warehouse:
+    goods = []
+
+    @classmethod
+    def reception(cls, obj):
+        cls.goods.append(obj.warehouse_units)
+
+first_unit = Printer('Hewlett-Packard', 5000, 8, 12)
+Warehouse.reception(first_unit)
+second_unit = Scanner('Canon', 4111, 12, 6)
+Warehouse.reception(second_unit)
+third_unit = Scanner('Ricoh', 2088, 18, 7)
+Warehouse.reception(third_unit)
+
+# whole_sale = Warehouse()
+# whole_sale()
+
+for i in Warehouse.goods:
+    pprint.pprint(i)
