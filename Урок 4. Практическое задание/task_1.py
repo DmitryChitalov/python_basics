3ec964127241d7e9aba7fac38adbc2d843a41d54
Nikita <s.nikita.a@yandex.ru>
2023-04-16 10:47:53 +0300

    Урок_4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -6,3 +6,18 @@
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
 """
+from sys import argv
+
+if len(argv) > 1:
+    name_script, times, rate, premiums = argv
+    times = int(times)
+    rate = float(rate)
+    premiums = float(premiums)
+    print((times * rate) + premiums)
+else:
+    times = int(input("Отработанные часы: "))
+    rate = float(input("Ставка в час: "))
+    premiums = float(input("Премия: "))
+    summ = ((times * rate) + premiums)
+    print(f'Размер заработной платы составил: {summ}')
+
