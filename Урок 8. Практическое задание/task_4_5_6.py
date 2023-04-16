0f19a4afcdc2d1f62d4185c50111edb50b89c269
sp3kt0r <126805884+sp3kt0r@users.noreply.github.com>
2023-04-16 22:12:19 +0300

    Update task_4_5_6.py

--- Урок 8. Практическое задание/task_4_5_6.py
+++ Урок 8. Практическое задание/task_4_5_6.py
@@ -19,3 +19,65 @@
 Подсказка: постарайтесь по возможности реализовать в проекте
 «Склад оргтехники» максимум возможностей, изученных на уроках по ООП.
 """
+
+class Storage:
+    def __init__(self, name):
+        self.name = name
+        self.inventory = {}
+
+    def receive(self, item, quantity):
+        if type(quantity) != int:
+            print("Ошибка: количество должно быть целым числом")
+            return
+        if item.name not in self.inventory:
+            self.inventory[item.name] = quantity
+        else:
+            self.inventory[item.name] += quantity
+
+    def transfer(self, item, quantity, department):
+        if type(quantity) != int:
+            print("Ошибка: количество должно быть целым числом")
+            return
+        if item.name not in self.inventory or self.inventory[item.name] < quantity:
+            print("Ошибка: недостаточное количество на складе")
+            return
+        if department not in item.departments:
+            print("Ошибка: указанный отдел недействителен для данного товара")
+            return
+        if department not in self.inventory:
+            self.inventory[department] = {}
+        if item.name not in self.inventory[department]:
+            self.inventory[department][item.name] = quantity
+        else:
+            self.inventory[department][item.name] += quantity
+        self.inventory[item.name] -= quantity
+
+
+class OfficeEquipment:
+    def __init__(self, name, price, weight):
+        self.name = name
+        self.price = price
+        self.weight = weight
+        self.departments = []
+
+    def add_department(self, department):
+        self.departments.append(department)
+
+
+class Printer(OfficeEquipment):
+    def __init__(self, name, price, weight, color, paper_size):
+        super().__init__(name, price, weight)
+        self.color = color
+        self.paper_size = paper_size
+
+
+class Scanner(OfficeEquipment):
+    def __init__(self, name, price, weight, resolution):
+        super().__init__(name, price, weight)
+        self.resolution = resolution
+
+
+class Xerox(OfficeEquipment):
+    def __init__(self, name, price, weight, speed):
+        super().__init__(name, price, weight)
+        self.speed = speed
