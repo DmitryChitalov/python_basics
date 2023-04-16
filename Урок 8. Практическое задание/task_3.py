951e0e8ed0a5cf23b6c8983ad39e358561e585f9
Ksenia Davydova <ksyksyks@yandex.ru>
2023-04-16 18:53:10 +0400

    lesson-8

--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -9,3 +9,25 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+
+class MyClass(Exception):
+    def __init__(self, text):
+        self.text = text
+        super().__init__(self.text)
+def x_list():
+    y_list = []
+    while True:
+        try:
+            my_val = input("Введите число: ")
+            if my_val == "end":
+                break
+            if not my_val.isdigit():
+                raise MyClass
+            y_list.append(int(my_val))
+        except MyClass as my_error:
+            print(my_error)
+    return y_list
+print(x_list())
+
+
+
