ef4b79e4fe4219ee86aa60d7f6cfe9ad63a13747
natafro72 <natadomas@yandex.ru>
2023-04-16 20:37:27 +0400

    Выполнено

--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -9,3 +9,31 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+
+
+class Check:
+    def __init__(self, *args):
+        self.new_list = []
+
+    def my_input(self):
+
+
+        while True:
+            try:
+                val = int(input('Введите числовые значения,в конце нажмите Enter - '))
+                self.new_list.append(val)
+                print(f'Сформирован список - {self.new_list} \n ')
+            except:
+                print(f"Ошибка! Допустимы только числа.")
+                y_or_n = input(f'Продолжить? Y/N ')
+
+                if y_or_n == 'Y' or y_or_n == 'y':
+                    print(try_except.my_input())
+                elif y_or_n == 'N' or y_or_n == 'n':
+                    return f'Завершено'
+                else:
+                    return f'Завершено'
+
+
+try_except = Check(1)
+print(try_except.my_input())
