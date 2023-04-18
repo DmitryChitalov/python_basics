27ee3904ecf148e00bbef30b0a9440705effc5d2
Asandrik86 <asandrik8686@gmail.com>
2023-04-18 11:42:07 +0300

    done

--- Урок 5. Практическое задание/task_7/task_7.py
+++ Урок 5. Практическое задание/task_7/task_7.py
@@ -9,3 +9,27 @@
 [{"firm_1": 5000, "firm_2": 3000, "firm_3": 1000}, {"average_profit": 2000}]
 Подсказка: использовать менеджер контекста.
 """
+import json
+
+companies_data = []
+
+with open('task_7.txt') as input_data:
+    companies_dict = {}
+    profit_list = []
+
+    for company_row in input_data:
+        name, form, revenue, costs = company_row.split()
+
+        profit = float(revenue) - float(costs)
+        companies_dict[name] = profit
+
+        if profit:
+            profit_list.append(profit)
+
+    companies_data.append(companies_dict)
+    companies_data.append({
+        'average_profit': round(sum(profit_list) / len(profit_list), 2)
+    })
+
+with open('task_7.json', 'w') as output_data:
+    json.dump(companies_data, output_data)
