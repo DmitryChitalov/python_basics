486a7b08051099eebc75c30d147cf7db82663bcb
MaryRemenyuk <91543154+MaryRemenyuk@users.noreply.github.com>
2023-04-18 19:41:53 +0300

    Complete task 1

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -28,3 +28,36 @@
 8 10 12
 14 16 18
 """
+
+
+class Matrix:
+
+    def __init__(self, list_values):
+        self.list_values = list_values
+
+    def __str__(self):
+        print_result = ''
+        for i in range(0, len(self.list_values)):
+            print_result += f'{self.list_values[i]} \n'
+        return print_result
+
+    def __add__(self, other):
+        if (len(self.list_values) == len(other.list_values)) \
+                and (len(self.list_values[0]) == len(other.list_values[0])):
+            result = []
+            for i in range(0, len(self.list_values)):
+                row = []
+                for j in range(0, len(self.list_values[0])):
+                    row.append(self.list_values[i][j] + other.list_values[i][j])
+                result.append(row)
+            return Matrix(result)
+        else:
+            return f'Error! You summarize matrices of different dimensions'
+
+
+m = Matrix([[3], [3]])
+k = Matrix([[2, 3], [3, 4]])
+l = Matrix([[2, 3], [3, 4]])
+print(m)
+print(m + k)
+print(k + l)
