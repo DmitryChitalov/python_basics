85afbfc1c8df4dbcb7fa537de060cd0b6e1f007d
Asandrik86 <asandrik8686@gmail.com>
2023-04-21 13:50:43 +0300

    done

--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -9,3 +9,37 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+import traceback
+class MyException(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+    def __str__(self):
+        return self.txt
+
+class CheckAndAdd:
+
+    def __init__(self):
+        self.__result = []
+        while True:
+            data = input('Число: ')
+            try:
+                self.__result.append(int(data))
+            except ValueError as e:
+                if data == 'stop':
+                    break
+                print('Ошибка с моим обработчиком:\n', traceback.format_exc())
+            except Exception as e:
+                if data == 'stop':
+                    break
+                raise MyException('Какая-то другая ошибка')
+
+    def __str__(self):
+        return ', '.join(map(str, self.__result))
+
+
+if __name__ == '__main__':
+    c = CheckAndAdd()
+    print(c)
+    d = CheckAndAdd()
+    print(d)
--- Урок 8. Практическое задание/task_7.py
+++ Урок 8. Практическое задание/task_7.py
@@ -6,3 +6,29 @@
 создав экземпляры класса (комплексные числа) и выполнив сложение и умножение созданных экземпляров.
 Проверьте корректность полученного результата.
 """
+
+class MyComplex:
+
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __str__(self):
+        return '(%s+%sj)' % (self.a, self.b)
+
+    def __add__(self, other):
+        new_a = self.a + other.a
+        new_b = self.b + other.b
+        return MyComplex(new_a, new_b)
+
+    def __mul__(self, other):
+        new_a = self.a * other.a - self.b * other.b
+        new_b = self.b * other.a + self.a * other.b
+        return MyComplex(new_a, new_b)
+
+
+z1 = MyComplex(1, 2)
+z2 = MyComplex(2, 3)
+
+print(f'{z1} + {z2} = ', z1 + z2)
+print(f'{z1} * {z2} = ', z1 * z2)
