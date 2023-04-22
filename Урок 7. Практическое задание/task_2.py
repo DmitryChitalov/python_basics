7df5b3a13bfe5f8f856e4414503bed59eafce8ea
ko55ur <96375951+ko55ur@users.noreply.github.com>
2023-04-22 23:04:17 +0300

    Homework 7

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -25,3 +25,65 @@
 
 Два класса: абстрактный и Clothes
 """
+
+from abc import ABC, abstractmethod
+
+
+class Clothes(ABC):
+    def __init__(self, params):
+        self.params = params
+
+    @abstractmethod
+    def fabric_consumption(self):
+        pass
+
+    def get_params(self):
+        return self.params
+
+    def __add__(self, other):
+        return self.fabric_consumption() + other.fabric_consumption()
+
+
+class Coat(Clothes):
+    @property
+    def params(self):
+        return self.__params
+
+    @params.setter
+    def params(self, params):
+        if params > 100:
+            self.__params = 100
+        elif params < 13:
+            self.__params = 13
+        else:
+            self.__params = params
+
+    def fabric_consumption(self):
+        return (self.get_params() / 6.5) + 0.5
+
+
+class Suit(Clothes):
+
+    @property
+    def params(self):
+        return self.__params
+
+    @params.setter
+    def params(self, params):
+        if params > 2.2:
+            self.__params = 2.2
+        elif params < 1.3:
+            self.__params = 1.3
+        else:
+            self.__params = params
+
+    def fabric_consumption(self):
+        return 2 * self.get_params() + 0.3
+
+
+coat = Coat(58)
+suit = Suit(1.83)
+
+print(f'Расход ткани на пальто = {round(coat.fabric_consumption(), 2)}')
+print(f'Расход ткани на костюм = {round(suit.fabric_consumption(), 2)}')
+print(f'Общий расход ткани = {round((coat + suit), 2)}')
