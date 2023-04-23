20c9822ca62c2208aecbf31066e76467d9e45ea7
Dina Bagautdinova <da-din_a-se@mail.ru>
2023-04-23 17:27:33 +0500

    Урок_7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -28,3 +28,29 @@
 8 10 12
 14 16 18
 """
+
+class Matrix:
+    def __init__(self, *args):
+        self.my_list = list(args)
+
+    def __str__(self):
+        matrix = ''
+        for i in range(len(self.my_list)):
+            matrix += ' '.join(map(str, self.my_list[i])) + '\n'
+        return matrix
+
+    def __add__(self, other):
+        matrix_1 = self.my_list
+        new_matrix = ''
+        for i in range(len(self.my_list)):
+            for el in range(len(self.my_list[i])):
+                matrix_1[i][el] = self.my_list[i][el] + other.my_list[i][el]
+            new_matrix += ' '.join(map(str, self.my_list[i])) + '\n'
+        return new_matrix
+
+
+m_1 = Matrix([0, 1, 2], [3, 4, 5], [6, 7, 8])
+print(m_1)
+m_2 = Matrix([9, 8, 7], [6, 5, 4], [3, 2, 1])
+print(m_2)
+print(m_1 + m_2)
