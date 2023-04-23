e0e13db4fc1ec9eccb020152e9a16fe65b3bcef9
ko55ur <96375951+ko55ur@users.noreply.github.com>
2023-04-23 19:27:04 +0300

    Homework 8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,49 @@
 Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца
 и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
 """
+
+
+class Date:
+    date_string: str
+
+    def __init__(self, date_string):
+        self.date_string = date_string
+
+    @classmethod
+    def transform(cls, date_string):
+        datepart = date_string.split('-')
+        return [int(datepart[0]), int(datepart[1]), int(datepart[2])]
+
+    @staticmethod
+    def valid(int_string):
+        if int_string[1] > 12 or int_string[1] < 1:
+            return f"Неверно введены данные. Номер месяца не может быть больше 12 и меньше 1."
+        elif int_string[0] < 1:
+            return f"Неверно введены данные. Номер дня не может быть меньше 1."
+        elif int_string[1] in [1, 3, 5, 7, 8, 10, 12]:
+            if int_string[0] > 31:
+                return f"Неверно введены данные. В месяце с номером {int_string[1]} должно быть меньше 31 дня"
+            else:
+                return f"Дата введена верно"
+        elif int_string[1] in [4, 6, 9, 11]:
+            if int_string[0] > 30:
+                return f"Неверно введены данные. В месяце {int_string[1]} должно быть меньше 30 дней"
+            else:
+                return f"Дата введена верно"
+        else:
+            if int_string[2] % 4 == 0:
+                if int_string[0] > 29:
+                    return f"Неверно введены данные. В феврале високосного года не может быть больше 29 дней"
+                else:
+                    return f"Дата введена верно"
+            else:
+                if int_string[0] > 28:
+                    return f"Неверно введены данные. В феврале невисокосного года не может быть больше 28 дней"
+                else:
+                    return f"Дата введена верно"
+
+
+example_string = "29-02-2021"
+
+print(Date.valid(Date.transform(example_string)))
+print(Date.valid(Date.transform('01-01-1970')))
