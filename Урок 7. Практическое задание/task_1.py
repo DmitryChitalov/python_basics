9d285d24dfbb041a59d4823a3f135e56719583f5
mf009 <misha.frol@gmail.com>
2023-04-23 23:10:06 +0300

    Подготовка папки для урока 7.2

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -28,3 +28,35 @@
 8 10 12
 14 16 18
 """
+
+
+class Matrix:
+    def __init__(self, my_matrix):
+        self.my_matrix = my_matrix
+
+    def __str__(self):
+        for i in range(0, len(self.my_matrix)):
+            for j in range(0, len(self.my_matrix[i])):
+                print(self.my_matrix[i][j], end=' ')
+            print()
+        return ' '
+
+    def __add__(self, other):
+        matrix_temp = self.my_matrix
+        for i in range(0, len(matrix_temp)):
+            for j in range(0, len(matrix_temp[i])):
+                matrix_temp[i][j] = self.my_matrix[i][j] + other.my_matrix[i][j]
+        return Matrix(matrix_temp)
+
+
+a = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+b = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+
+a_matrix = Matrix(a)
+b_matrix = Matrix(b)
+print("---------Matrix 1----------")
+print(a_matrix)
+print("---------Matrix 2----------")
+print(b_matrix)
+print("---------Matrix 1 + 2 ----------")
+print(a_matrix + b_matrix)
