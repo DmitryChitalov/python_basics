2f57cd325fc0dc046acbed59adad4e252ee1d04f
Grishaev Yuriy <Donvorrin@gmail.com>
2023-04-25 20:35:28 +0700

    fix task_7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -43,19 +43,15 @@ class Matrix:
 
     def __add__(self, other):
         res_list = deepcopy(other.mass)  # [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-        if isinstance(other, Matrix):
-            for inx, val in enumerate(self.mass):
-                for j_inx, j_val in enumerate(val):
-                    res_list[inx][j_inx] += j_val
-            return res_list
-        else:
-            return 5
+        for inx, val in enumerate(self.mass):
+            for j_inx, j_val in enumerate(val):
+                res_list[inx][j_inx] += self.mass[inx][j_inx]
+        return Matrix(res_list)
 
 
 mx1 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-mx2 = Matrix([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
-mx3 = Matrix(mx1 + mx2)
+mx2 = Matrix([[3, 2, 1], [6, 5, 4], [9, 8, 7]])
+mx3 = mx1 + mx2
 print(f'Матрица 1:\n{mx1}')
 print(f'Матрица 2:\n{mx2}')
 print(f'Сумма матриц:\n{mx3}')
-
