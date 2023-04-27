53691b917622bb902282c7e256351da303308934
Nimarun <r.levinov@gmail.com>
2023-04-27 18:46:06 +0300

    Восьмая домашка. Полюбому где-то накосячил, но больше ничего не придумывается.

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,60 @@
 Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца
 и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
 """
+
+
+import re
+
+
+class Date:
+    max_days_in_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    max_days_in_month_ly = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+    def __init__(self, date_string):
+        match_result = re.match(r'^\d\d-\d\d-\d\d\d\d$', date_string)
+        if match_result is None:
+            raise Exception(f'{date_string} - неверный формат, используйте дд-мм-гггг')
+        self.date_string = date_string
+        self.day, self.month, self.year = map(int, date_string.split('-'))
+
+    @classmethod
+    def extract(cls, date_string):
+        date = cls(date_string)
+        return [date.day, date.month, date.year]
+
+    @staticmethod
+    def validate(date_string):
+        date = Date(date_string)
+        is_not_zero = date.day > 0 and date.month > 0 and date.year > 0
+        if date.year % 4 == 0 and date.year % 100 != 0 or date.year % 400 == 0:
+            is_fit_boundaries = date.month <= 12 and date.day <= date.max_days_in_month_ly[date.month - 1]
+        else:
+            is_fit_boundaries = date.month <= 12 and date.day <= date.max_days_in_month[date.month - 1]
+        return is_not_zero and is_fit_boundaries
+
+
+if __name__ == '__main__':
+    real_date_str = '22-12-1983'
+    not_valid_date_str = '29-02-2019'
+    incorrect_date_str = '9-02/*019'
+
+    real_date = Date(real_date_str)
+    print(f'{real_date.extract(real_date_str)} - извлеченное значение')
+
+    not_valid_date = Date(not_valid_date_str)
+    print(f'{real_date.extract(not_valid_date_str)} - извлеченное значение')
+
+    try:
+        incorrect_date = Date(incorrect_date_str)
+    except Exception as e:
+        print(e)
+
+    if Date.validate(real_date_str):
+        print(f'{real_date_str} - корректное значение')
+    else:
+        print(f'{real_date_str} - некорректное значение')
+
+    if Date.validate(not_valid_date_str):
+        print(f'{not_valid_date_str} - корректное значение')
+    else:
+        print(f'{not_valid_date_str} - некорректное значение')
--- Урок 8. Практическое задание/task_3.py
+++ Урок 8. Практическое задание/task_3.py
@@ -9,3 +9,35 @@
 
 Класс-исключение должен контролировать типы данных элементов списка.
 """
+
+
+class NaNError(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+
+def number_filter(string):
+    if string.isdigit():
+        return string
+    else:
+        try:
+            float(string)
+            return string
+        except ValueError:
+            raise NaNError(f'Ошибка: {string} - не число')
+
+
+input_txt = ''
+counter = 1
+numbers_list = []
+print("Введите числа по одному, для выхода введите 'stop'")
+while input_txt != 'stop':
+    try:
+        input_txt = input(f'{counter}: ')
+        numbers_list.append(number_filter(input_txt))
+        counter += 1
+    except NaNError as e:
+        if input_txt != 'stop':
+            print(e.txt)
+
+print(f'Результат:\n{numbers_list}')
