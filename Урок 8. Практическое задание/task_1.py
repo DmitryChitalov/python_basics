9f50fe61b582daa1df786a84931c987121460ae8
mf009 <misha.frol@gmail.com>
2023-04-28 20:39:16 +0300

    Урок_8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,34 @@
 Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца
 и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
 """
+class Data:
+    def __init__(self, day_month_year):
+        self.day_month_year = day_month_year
+
+    @classmethod
+    def my_method(cls, day_month_year):
+        s = day_month_year.split(sep='.')
+        print(s)
+        day = int(s[0])
+        month = int(s[1])
+        year = int(s[2])
+        print(F"{day} / {month} / {year}")
+
+    @staticmethod
+    def valid_data(day_month_year):
+        a = 0
+        s = day_month_year.split(sep='.')
+        if 0 < int(s[0]) < 13:
+           a += 1
+        if 0 < int(s[1]) < 32:
+           a += 1
+        if int(s[2]) == 2023:
+           a += 1
+        if a == 3:
+           print(F"{s[0]} / {s[1]} / {s[2]}")
+        else:
+            print("Неверный формат")
+
+Data.my_method("12.02.2023")
+
+Data.valid_data("12.33.2023")
