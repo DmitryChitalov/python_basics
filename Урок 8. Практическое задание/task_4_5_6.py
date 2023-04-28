96fc925b9a2281a24efd5181395badf5ff24372d
Grishaev Yuriy <Donvorrin@gmail.com>
2023-04-28 23:50:03 +0700

    Add homework 8

--- Урок 8. Практическое задание/task_4_5_6.py
+++ Урок 8. Практическое задание/task_4_5_6.py
@@ -19,3 +19,54 @@
 Подсказка: постарайтесь по возможности реализовать в проекте
 «Склад оргтехники» максимум возможностей, изученных на уроках по ООП.
 """
+from abc import ABC
+
+
+class ErrorPrice(Exception):
+    pass
+
+
+class Storage:
+    my_storage = None
+
+
+class OfficeEquipment(ABC):
+    responsible_person = 'Bob'
+    name = None
+    max_price = None
+
+    def __init__(self, color='white', model='Toshiba 7x', price=300):
+        self.color = color
+        self.model = model
+        self.price = price
+
+    def __str__(self):
+        try:
+            if self.price > self.max_price:
+                raise ErrorPrice('Превышена максимальная цена товара. ')
+        except ErrorPrice as ep:
+            print(ep)
+        else:
+            print(f'{self.name} {self.model} успешно преобретен за ${self.price}')
+            Storage.my_storage[{self.name}] += 1
+        finally:
+            return 'Bye!'
+
+
+class Printer(OfficeEquipment):
+    max_price = 300
+    name = 'Принтер'
+
+
+class Scanner(OfficeEquipment):
+    max_price = 250
+    name = 'Сканер'
+
+
+class Xerox(OfficeEquipment):
+    max_price = 480
+    name = 'Ксерокс'
+
+
+pr_by = Printer('Red', 'MXX', 13)
+print(pr_by)
--- Урок 8. Практическое задание/task_7.py
+++ Урок 8. Практическое задание/task_7.py
@@ -6,3 +6,30 @@
 создав экземпляры класса (комплексные числа) и выполнив сложение и умножение созданных экземпляров.
 Проверьте корректность полученного результата.
 """
+
+
+class ComplexNum:
+
+    def __init__(self, num_a, num_b):
+        self.num_a = num_a
+        self.num_b = num_b
+
+    def __add__(self, other):
+        a = self.num_a + other.num_a
+        b = self.num_b + other.num_b
+        return ComplexNum(a, b)
+
+    def __mul__(self, other):
+        a = self.num_a * other.num_a - self.num_b * other.num_b
+        b = self.num_b * other.num_a + self.num_a * other.num_b
+        return ComplexNum(a, b)
+
+    def __str__(self):
+        return f'{self.num_a}+{self.num_b}j'
+
+
+complex_1 = ComplexNum(7, 3)
+complex_2 = ComplexNum(6, 2)
+
+print(f'{complex_1} + {complex_2} = {complex_1 + complex_2}')
+print(f'{complex_1} * {complex_2} = {complex_1 * complex_2}')
