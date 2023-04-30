c3078478ce7972c978c4b69aa3ee6128d498017b
Nikita <s.nikita.a@yandex.ru>
2023-04-30 21:24:11 +0300

    Урок_8

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,21 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+class dividebyzero:
+    def __str__(self):
+        return self.text
+class Number(float):
+    def __truediv__(self, other):
+        if other is not None and not other:
+            raise Error
+        return Number(float(self) / float(other))
+
+if __name__ == '__main__':
+    while True:
+        try:
+            separator, divisor = map(Number, input("Введите значения через пробел: ").split())
+            print(separator / divisor)
+        except Error as e:
+            print(e)
+        except ValueError as e:
+            print(e)
