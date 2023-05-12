0631e50fa5fc32e8cefc1dbad18c007656a4f6b4
Leoton98 <pashe11@yandex.ru>
2023-05-13 00:07:33 +0300

    Lesson_7

--- Урок 7. Практическое задание/task_1.py
+++ Урок 7. Практическое задание/task_1.py
@@ -28,3 +28,40 @@
 8 10 12
 14 16 18
 """
+from typing import List
+class Matrix:
+    def __init__(self, matrix_data: List[List]):
+        self.__matrix = matrix_data
+
+        m_rows = len(self.__matrix)
+        self.__matrix_size = frozenset([(m_rows, len(row)) for row in self.__matrix])
+
+        if len(self.__matrix_size) != 1:
+            raise ValueError("Недопустимый размер матрицы")
+
+    def __add__(self, other: "Matrix") -> "Matrix":
+        if not isinstance(other, Matrix):
+            raise TypeError(f"'{other.__class__.__name__}' "
+                            f"несовместимый тип объекта")
+        if self.__matrix_size != other.__matrix_size:
+            raise ValueError(f"Матрицы разных размеров")
+
+        result = []
+
+        for item in zip(self.__matrix, other.__matrix):
+            result.append([sum([j, k]) for j, k in zip(*item)])
+
+        return Matrix(result)
+
+    def __str__(self) -> str:
+        return '\n'.join(['\t'.join(map(str, row)) for row in self.__matrix])
+
+
+if __name__ == '__main__':
+    m1 = Matrix([[77, 77, 77], [33, 33, 33]])
+    print(m1, '\n')
+
+    m2 = Matrix([[33, 33, 33], [77, 77, 77]])
+    print(m2, '\n')
+
+    print(m1 + m2)
