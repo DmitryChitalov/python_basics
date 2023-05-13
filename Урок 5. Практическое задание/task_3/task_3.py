420a550ba64970ec7e1249924312637103144bae
enterflet <volkof23@yandex.ru>
2023-05-13 08:40:18 +0300

    Задания урока 5

--- Урок 5. Практическое задание/task_3/task_3.py
+++ Урок 5. Практическое задание/task_3/task_3.py
@@ -8,3 +8,12 @@ Four — 4
 При этом английские числительные должны заменяться на русские.
 Новый блок строк должен записываться в новый текстовый файл.
 """
+
+with open("file_3.txt", "r", encoding='utf-8') as f1_obj:
+    with open("file_3_rus.txt", "w", encoding='utf-8') as f2_obj:
+        eng_list = ['One', 'Two', 'Three', 'Four']
+        rus_list = ['Один', 'Два', 'Три', 'Четыре']
+        for line in f1_obj:
+            for i in range(0, 4):
+                line = line.replace(eng_list[i], rus_list[i])
+            f2_obj.write(line)
