cb9f913b168d500c5d448f2edf27fe3c63b9c1c5
GorelovNN <gorelov.nn@mail.ru>
2023-05-21 12:39:34 +0500

    HomeWork_7 done

--- Урок 7. Практическое задание/task_2.py
+++ Урок 7. Практическое задание/task_2.py
@@ -8,9 +8,11 @@
 К типам одежды в этом проекте относятся пальто и костюм.
 
 У этих типов одежды существуют параметры:
-размер (для пальто) и рост (для костюма). Это могут быть обычные числа: v и h, соответственно.
+размер (для пальто) и рост (для костюма). Это могут быть обычные числа: v и h,
+ соответственно.
 
-Для определения расхода ткани по каждому типу одежды использовать формулы: для пальто (v/6.5 + 0.5),
+Для определения расхода ткани по каждому типу одежды использовать формулы: для
+пальто (v/6.5 + 0.5),
 для костюма (2*h + 0.3). Проверить работу этих методов на реальных данных.
 
 Реализовать общий подсчет расхода ткани.
@@ -25,3 +27,44 @@
 
 Два класса: абстрактный и Clothes
 """
+
+
+from abc import ABC, abstractmethod
+
+
+class AbstractClothes(ABC):
+    @abstractmethod
+    def get_coat_consumption(self):
+        pass
+
+    @abstractmethod
+    def get_suit_consumption(self):
+        pass
+
+    def get_total_consumption(self):
+        pass
+
+
+class Clothes(AbstractClothes):
+    def __init__(self, v, h):
+        self.v = v
+        self.h = h
+        self.coat_consumption = self.v / 6.5 + 0.5
+        self.suit_consumption = self.h * 2 + 0.3
+
+    def get_coat_consumption(self):
+        return str(f"Расход ткани на пальто = {self.coat_consumption:.2f}")
+
+    def get_suit_consumption(self):
+        return str(f"Расход ткани на костюм = {self.suit_consumption:.2f}")
+
+    @property
+    def get_total_consumption(self):
+        return str(f"Общий расход ткани = "
+                   f"{self.coat_consumption + self.suit_consumption:.2f}")
+
+
+clothes = Clothes(5, 10)
+print(clothes.get_coat_consumption())
+print(clothes.get_suit_consumption())
+print(clothes.get_total_consumption)
