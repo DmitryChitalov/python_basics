eb09401d147e214893d2e0747d71164d422506b1
Tsarenkus <101929003+Tsarenkus@users.noreply.github.com>
2023-06-22 17:37:14 +0300

    Update task_2.py

--- Урок 8. Практическое задание/task_2.py
+++ Урок 8. Практическое задание/task_2.py
@@ -5,3 +5,22 @@
 Проверьте его работу на данных, вводимых пользователем. При вводе пользователем нуля
 в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
 """
+class Own_error(Exception):
+    def __init__(self, txt):
+        self.txt = txt
+
+def div_by_null():
+    try:
+        div = int(input('Ввод делимого: '))
+        denominator = int(input('Ввод делителя: '))
+        if denominator == 0:
+            raise Own_error("На ноль делить нельзя")
+        else:
+            quotient = div / denominator
+            return quotient
+    except ValueError:
+        return "Введено не число"
+    except Own_error as err:
+        return err
+
+print(div_by_null())
