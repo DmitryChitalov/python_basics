90886944fd4ae61a7a858f45aa1ccd850cc30d32
ChintsovAndrey <av.chintsov@yandex.ru>
2023-07-20 14:54:13 +0300

    Урок_2

--- Урок 2. Практическое задание/task_5.py
+++ Урок 2. Практическое задание/task_5.py
@@ -17,3 +17,10 @@
 """
 
 my_list = [7, 5, 3, 3, 2]
+
+user_input = int(input('Введите целое, положительное число: '))
+
+my_list.append(user_input)
+my_list.sort(reverse=True)
+
+print(f'Рейтинг: {my_list}')
