b58cd1551fed0d0776bdf0925e5c71b3e8cf1101
ChintsovAndrey <av.chintsov@yandex.ru>
2023-08-30 08:05:44 +0300

    Урок_8

--- Урок 8. Практическое задание/task_1.py
+++ Урок 8. Практическое задание/task_1.py
@@ -12,3 +12,32 @@
 Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца
 и года (например, месяц — от 1 до 12). Проверить работу полученной структуры на реальных данных.
 """
+
+class Data:
+
+    def __init__(self, date):
+        self.day_month_year = date
+
+    @classmethod
+    def make_attributes(cls, date):
+        temp_list = date.split('-')
+        Data.day = int(temp_list[0])
+        Data.month = int(temp_list[1])
+        Data.year = int(temp_list[2])
+
+    @staticmethod
+    def validate():
+        if Data.day not in range(1, 31):
+            return "imposible day num"
+        if Data.month not in range(1, 12):
+            return "imposible day month"
+        if Data.year <= 0:
+            return "imposible day year"
+        return f"current date is: {Data.day}/{Data.month}/{Data.year}"
+
+#    def __str__(self):
+ #       print(f"Date is {Data.make_attributes}")
+
+
+Data.make_attributes("10-04-2023")
+print(Data.validate())
--- Урок 8. Практическое задание/task_7.py
+++ Урок 8. Практическое задание/task_7.py
@@ -6,3 +6,43 @@
 создав экземпляры класса (комплексные числа) и выполнив сложение и умножение созданных экземпляров.
 Проверьте корректность полученного результата.
 """
+
+import re
+
+
+class Complexus:
+
+    def __init__(self, comlex_number):
+        if comlex_number[0] == '-':
+            comlex_number = comlex_number[1:-1]
+            if comlex_number.find('-') != -1:
+                self.first = int("-" + re.split('[- ]', comlex_number)[0])
+                self.second = int('-' + re.split('[- ]', comlex_number)[1].replace('i', ''))
+            else:
+                self.first = int("-" + re.split('[+ ]', comlex_number)[0])
+                self.second = int('-' + re.split('[+ ]', comlex_number)[1].replace('i', ''))
+        else:
+            if comlex_number.find('-') != -1:
+                self.first = int(re.split('[- ]', comlex_number)[0])
+                self.second = int('-' + re.split('[- ]', comlex_number)[1].replace('i', ''))
+            else:
+                self.first = int(re.split('[+ ]', comlex_number)[0])
+                self.second = int('-' + re.split('[+ ]', comlex_number)[1].replace('i', ''))
+
+    def __str__(self):
+        return f"{self.first}{self.second}i"
+
+    def __add__(self, other):
+        a = self.first + other.first
+        b = self.second + other.second
+        return Complexus(f"{a}{b}i")
+
+    def __mul__(self, other):
+        a = (self.first * other.first) - (self.second * other.second)
+        b = (self.first * other.second) + (self.second * other.first)
+        return Complexus(f"{a}{b}i")
+
+c1 = Complexus('5-7i')
+c2 = Complexus('4-5i')
+print(f"sum of c1 and  c2 will be {c1 + c2}")
+print(f"multiplication of c1 and c2 will be {c1 * c2}")
