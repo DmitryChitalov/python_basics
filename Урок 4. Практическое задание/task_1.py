fc5cc3c95931fc8ff97184d9155e73144c4757a2
Evgeniy Bocharov <bocharovik@yandex.ru>
2023-12-10 13:24:52 +0300

    lesson_4

--- Урок 4. Практическое задание/task_1.py
+++ Урок 4. Практическое задание/task_1.py
@@ -6,3 +6,14 @@
 Для выполнения расчета для конкретных значений
 необходимо запускать скрипт с параметрами.
 """
+
+from sys import argv
+(script_name, work_hours, hourly_rate, perm) = argv
+def staf_pay():
+    print("Выработка в часах: ", work_hours)
+    print("Ставка в час: ", hourly_rate)
+    print("Премия: ", perm)
+    return int(work_hours) * int(hourly_rate) + int(work_hours) * int(hourly_rate) / 100 * int(perm)
+print(staf_pay())
+
+
--- Урок 4. Практическое задание/task_2.py
+++ Урок 4. Практическое задание/task_2.py
@@ -7,3 +7,36 @@
 
 Реализуйте вариант без и с генераторным выражением
 """
+my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
+
+#варианты без генератора
+
+#1-ый
+new_list = []
+i1 = 0
+i2 = 1
+while i2 <  len(my_list):
+    if my_list[i1] < my_list[i2]:
+        new_list.append(my_list[i2])
+    i1, i2 = i1 + 1, i2 + 1
+print(new_list)
+
+#2-ой
+new_list = []
+a = 0
+for el in my_list[1:]:
+    if el > my_list[a]:
+        new_list.append(el)
+    a += 1
+print(new_list)
+
+#3-й
+new_list = []
+for i in range(1, len(my_list)):
+    if my_list[i] > my_list[i - 1]:
+        new_list.append(my_list[i])
+print(new_list)
+
+#Вариант с генератором
+new_list = [my_list[i] for i in range(1, len(my_list)) if my_list[i] > my_list[i - 1]]
+print(new_list)
