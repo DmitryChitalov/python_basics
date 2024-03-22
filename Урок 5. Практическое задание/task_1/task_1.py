e820b52e977adc6689e5c01034ec8c9412507067
Evgeniy Bocharov <bocharovik@yandex.ru>
2024-03-22 09:51:59 +0300

    lesson_5

--- Урок 5. Практическое задание/task_1/task_1.py
+++ Урок 5. Практическое задание/task_1/task_1.py
@@ -3,3 +3,19 @@
 записать в него построчно данные, вводимые пользователем.
 Об окончании ввода данных свидетельствует пустая строка.
 """
+
+
+def string(text_input):
+    str2 = str(text_input)
+    total = ""
+    while str2 != "":
+        total = f'{total}{str2}\n'
+        str2 = input()
+    my_file = open("task1_leeson5.txt", "a")
+    my_file.write(total)
+    my_file.close()
+    return total
+
+print(string(input()))
+
+
--- Урок 5. Практическое задание/task_2/task_2.py
+++ Урок 5. Практическое задание/task_2/task_2.py
@@ -2,3 +2,23 @@
 2)	Создать текстовый файл (не программно), сохранить в нем несколько строк,
 выполнить подсчет количества строк, количества слов в каждой строке.
 """
+my_f = open("task2_lesson5.txt", "r")
+print(my_f.read())
+
+my_f = open("task2_lesson5.txt", "r")
+lines = 0
+words = 0
+numb = 1
+
+for line in my_f:
+    print(f'{numb}-я строка содержит слов - {len(line.split())}')
+    words += len(line.split())
+    lines += 1
+    numb += 1
+
+print(f"Количество строк в файле - {lines}")
+print(f"Общее количество слов в файле - {words}")
+
+my_f.close()
+
+
